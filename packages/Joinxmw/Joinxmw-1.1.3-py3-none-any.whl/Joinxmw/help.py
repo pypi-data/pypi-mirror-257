3b8387993d9e168ebf507fdb43d0c8ea151efa42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:39 +0000

    Add package Joinxmw/1.1.3/Joinxmw-1.1.3-py3-none-any.whl

---
+++ packages/Joinxmw/Joinxmw-1.1.3-py3-none-any.whl/Joinxmw/help.py
@@ -0,0 +1,9 @@
+def help():
+    print('''
+    This is a module connecting to the Chinese programming community Xiaomawang, which can use user IDs to call many content. For example, user fans, works, etc.
+ The current methods available are as follows:
+ help -- you can view help and introduction
+ reviewid -- you can get user information by introducing the parameter ID
+ getworks -- as the name implies, you can get the number of works, total number of views, etc.
+ Note: Except twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+for help, the above methods need to be outputted by print or other methods!''')
