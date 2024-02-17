96d8a7cb9d008c7a35fbace5bb1d30020aaf17b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:44 +0000

    Add package shipyard-bigquery/0.1.2a1/shipyard_bigquery-0.1.2a1.tar.gz

---
+++ packages/shipyard-bigquery/shipyard_bigquery-0.1.2a1.tar.gz/shipyard_bigquery-0.1.2a1/shipyard_bigquery/cli/authtest.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from shipyard_bigquery import BigQueryClient
+from shipyard_templates import ShipyardLogger
+
+
+logger = ShipyardLogger.get_logger()
+def main():
+    google_client = BigQueryClient(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+    try:
+        google_client.connect()
+        logger.info("Successfully established a connection")
+        sys.exit(0)
+    except Exception as e:
+        logger.error("Could not establish a connection")
+        logger.debug(f"Response from Google BigQuery API: {str(e)}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
