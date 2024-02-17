e2deae3b4eeb7a845ddc220ec2dada93887b1d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:29 +0000

    Add package w3tm/1.0.12/w3tm-1.0.12.tar.gz

---
+++ packages/w3tm/w3tm-1.0.12.tar.gz/w3tm-1.0.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='w3tm',
+    version='1.0.12',
+    description='W3TM command line tool',
+    author='alexzshl',
+    author_email='alexzshl@126.com',
+    # packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'w3tm = main:main'
+        ]
+    },
+    install_requires=[
+        # 这里列出你的项目依赖
+        'pillow~=10.2.0',
+        'argparse~=1.4.0'
+    ],
+)
