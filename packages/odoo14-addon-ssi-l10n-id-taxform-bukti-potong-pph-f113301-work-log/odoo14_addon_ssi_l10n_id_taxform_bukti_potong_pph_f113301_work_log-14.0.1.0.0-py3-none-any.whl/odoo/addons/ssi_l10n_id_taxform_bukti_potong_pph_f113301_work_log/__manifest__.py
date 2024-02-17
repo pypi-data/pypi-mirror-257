ab895a26b894f027ffdfcbfa9f830b6dcdf4d8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:23 +0000

    Add package odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113301-work-log/14.0.1.0.0/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113301_work_log-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-l10n-id-taxform-bukti-potong-pph-f113301-work-log/odoo14_addon_ssi_l10n_id_taxform_bukti_potong_pph_f113301_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_l10n_id_taxform_bukti_potong_pph_f113301_work_log/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Bukti Potong PPh 21/26 Tidak Final (f.1.1.33.01) - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_l10n_id_taxform_bukti_potong_pph_f113301",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
