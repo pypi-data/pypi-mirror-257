8813f0abb1ff48cf17c26d2e2910cc868bd68436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:25 +0000

    Add package mb-cli/0.1.0/mb_cli-0.1.0-py3-none-any.whl

---
+++ packages/mb-cli/mb_cli-0.1.0-py3-none-any.whl/mb/__main__.py
@@ -0,0 +1,11 @@
+import typer
+from commands import robots, jobs
+
+
+app = typer.Typer()
+app.add_typer(robots.app, name="robot")
+app.add_typer(jobs.app, name="job")
+
+
+if __name__ == "__main__":
+    app()
