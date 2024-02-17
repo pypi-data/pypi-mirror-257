fc1dbf95aebc92c940832d96d9c5261000b583b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:03 +0000

    Add package budgetguard/0.36.3/budgetguard-0.36.3-py3-none-any.whl

---
+++ packages/budgetguard/budgetguard-0.36.3-py3-none-any.whl/budgetguard/core/pipelines/raw_to_bronze/metadata_pipeline.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from loguru import logger
+from typing import List, Dict
+
+here = os.path.dirname(__file__)
+
+sys.path.append(os.path.join(here, ".."))
+
+from .raw_to_bronze_pipeline import RawToBronzePipeline  # noqa: E402
+
+
+class RawToBronzeMetadataPipeline(RawToBronzePipeline):
+    INPUT_KEY = "metadata"
+    OUTPUT_KEY = "metadata"
+
+    def transform(self, source_data: List[Dict[str, str]]):
+        """
+        Transforms the data.
+
+        :param source_data: The data to transform.
+        :return: The transformed data.
+        """
+        logger.info("Transforming data.")
+        return [self.format_metadata(data) for data in source_data]
+
+    def format_metadata(self, metadata):
+        """
+        Formats the metadata.
+
+        :param metadata: The metadata to format.
+        :return: The formatted metadata.
+        """
+        return metadata
