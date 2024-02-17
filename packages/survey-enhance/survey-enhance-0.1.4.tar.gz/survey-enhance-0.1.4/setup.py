3a8a339df7047dc89850acdd99a5c0006399eb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:43 +0000

    Add package survey-enhance/0.1.4/survey-enhance-0.1.4.tar.gz

---
+++ packages/survey-enhance/survey-enhance-0.1.4.tar.gz/survey-enhance-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="survey-enhance",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "microdf_python",
+        "torch",
+        "policyengine_core",
+        "ipython",
+        "scikit-learn",
+        "tensorboard",
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "jupyter-book",
+            "sphinx>=4.5.0,<5",
+            "sphinx-argparse>=0.3.2,<1",
+            "sphinx-math-dollar>=1.2.1,<2",
+            "furo<2023",
+        ],
+    },
+)
---
+++ packages/survey-enhance/survey-enhance-0.1.4.tar.gz/survey-enhance-0.1.4/survey_enhance/__init__.py
@@ -0,0 +1,3 @@
+from policyengine_core.data import Dataset
+from survey_enhance.impute import Imputation
+from survey_enhance.reweight import CalibratedWeights, LossCategory
