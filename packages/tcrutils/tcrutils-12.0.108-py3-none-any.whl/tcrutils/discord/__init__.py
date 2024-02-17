6dc4440d84819d7af2535f46eeb1f9a1e6cc3320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:54 +0000

    Add package tcrutils/12.0.108/tcrutils-12.0.108-py3-none-any.whl

---
+++ packages/tcrutils/tcrutils-12.0.108-py3-none-any.whl/tcrutils/discord/__init__.py
@@ -0,0 +1,19 @@
+from .tcrd_constants import DISCORD_EPOCH
+from .tcrd_embeds import embed
+from .tcrd_limits import DiscordLimits
+from .tcrd_permissions import PERMISSIONS_DICT, Permission, permissions
+from .tcrd_permissions import Permission as DiscordPermission
+from .tcrd_permissions import permissions as discord_permissions
+from .tcrd_snowflake import Snowflake, is_snowflake
+from .tcrd_string import get_token
+
+__all__ = [
+  'DiscordLimits',
+  'DiscordPermission',
+  'discord_permissions',
+  'tcrd_embeds',
+  'is_snowflake',
+  'Snowflake',
+  'embed',
+  'get_token',
+]
