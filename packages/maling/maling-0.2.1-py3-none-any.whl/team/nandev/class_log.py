0066ae74f7a9b35d9b34d75022989c0e7be5458f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:06 +0000

    Add package maling/0.2.1/maling-0.2.1-py3-none-any.whl

---
+++ packages/maling/maling-0.2.1-py3-none-any.whl/team/nandev/class_log.py
@@ -0,0 +1,33 @@
+from os import execvp
+from sys import executable
+import logging
+
+
+class ConnectionHandler(logging.Handler):
+    def __init__(self):
+        super().__init__()
+
+    def emit(self, record):
+        for X in ["Connection lost", "OSError"]:
+            if X in record.getMessage():
+                execvp(executable, [executable, "-m", "Mix"])
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s", "%d-%b %H:%M")
+stream_handler = logging.StreamHandler()
+
+stream_handler.setFormatter(formatter)
+connection_handler = ConnectionHandler()
+
+logger.addHandler(stream_handler)
+logger.addHandler(connection_handler)
+
+LOGS = logging.getLogger()
+
+
+def LOGG(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+    
+LOGGER = LOGG("Mix-Userbot")
