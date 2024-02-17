448a5b83d9bff999ea957bd2ea7312460f084851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:55 +0000

    Add package keycove/0.1.0/keycove-0.1.0.tar.gz

---
+++ packages/keycove/keycove-0.1.0.tar.gz/keycove-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keycove",
+    version="0.1.0",
+    description="An API key authorization library",
+    packages=find_packages(),
+    install_requires=[],
+)
