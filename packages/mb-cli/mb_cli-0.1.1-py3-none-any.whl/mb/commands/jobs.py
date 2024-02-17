0d547b1ec877149b927748a71d67394cad2ec1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:30 +0000

    Add package mb-cli/0.1.1/mb_cli-0.1.1-py3-none-any.whl

---
+++ packages/mb-cli/mb_cli-0.1.1-py3-none-any.whl/mb/commands/jobs.py
@@ -0,0 +1,28 @@
+import typer
+import json
+import requests
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from mb.settings import API_URL, API_TOKEN, ORGANIZATION_ID
+
+
+app = typer.Typer()
+
+console = Console()
+
+
+@app.command()
+def add(args_path: str, robot_ids: str):
+    robot_ids = robot_ids.split(',')
+    res = requests.post(API_URL + '/robots.addJobs', json={
+        "organizationId": ORGANIZATION_ID,
+        "robotIds": robot_ids,
+        "job_type": "docker-container-launch",  
+        "args": json.load(open(args_path, 'r'))
+
+        }, headers={'authorization': API_TOKEN}).json()
+    print(res)
+
+if __name__ == "__main__":
+    app()
---
+++ packages/mb-cli/mb_cli-0.1.1-py3-none-any.whl/mb/commands/robots.py
@@ -0,0 +1,24 @@
+import typer
+import requests
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from mb.settings import API_URL, API_TOKEN, ORGANIZATION_ID
+
+
+app = typer.Typer()
+
+console = Console()
+
+
+@app.command()
+def list():
+    res = requests.post(API_URL + '/robots.list', json={'organizationId': ORGANIZATION_ID}, headers={'authorization': API_TOKEN}).json()
+    table = Table("Id", "Name", "Description", "Status", 'Api Key')
+    for robot in res['robots']:
+        table.add_row(robot['id'], robot['name'], robot['description'], robot['status'], robot['api_key'])
+    console.print(table)
+
+
+if __name__ == "__main__":
+    app()
