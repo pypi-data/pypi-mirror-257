24248a204a65f36b6fc81a5fda4bd7a5a8891a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:22 +0000

    Add package django-listing/0.7.4/django_listing-0.7.4-py2.py3-none-any.whl

---
+++ packages/django-listing/django_listing-0.7.4-py2.py3-none-any.whl/django_listing/__init__.py
@@ -0,0 +1,29 @@
+""" django_listing root __init__.py file
+
+   isort:skip_file
+"""
+
+__version__ = "0.7.4"
+__author__ = "Eric Lapouyade"
+__copyright__ = "Copyright 2018, The Django listing Project"
+__credits__ = ["Eric Lapouyade"]
+__license__ = "Dual licensing"
+__maintainer__ = "Eric Lapouyade"
+__status__ = "Beta"
+
+EXPORT_FORMATS = ["CSV", "DBF", "HTML", "JSON", "ODS", "TSV", "XLS", "XLSX", "YAML"]
+EXPORT_FORMATS_KEEP_ORIGINAL_TYPE = ["XLSX", "JSON", "XLS", "DBF"]
+EXPORT_FORMATS_USE_COL_NAME = ["JSON"]
+
+from .listing import *
+from .columns import *
+from .exceptions import *
+from .views import *
+from .html_attributes import *
+from .context import *
+from .paginators import *
+from .toolbar import *
+from .filters import *
+from .aggregations import *
+from .attached_form import *
+from .actions_buttons_column import *
