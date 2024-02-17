0c2d0eda2abab2ef4dd42b235ab55fc30295a2c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:36 +0000

    Add package screwmpcpy/0.2.4/screwmpcpy-0.2.4.tar.gz

---
+++ packages/screwmpcpy/screwmpcpy-0.2.4.tar.gz/screwmpcpy-0.2.4/tests/test_pandamg.py
@@ -0,0 +1,123 @@
+"""Test the motion generator for panda robot."""
+from __future__ import annotations
+
+import numpy as np
+import pytest
+from dqrobotics.robots import FrankaEmikaPandaRobot
+
+from screwmpcpy.pandamg import PandaScrewMotionGenerator, PandaScrewMpMotionGenerator
+from screwmpcpy.screwmpc import BOUND
+
+
+def test_panda_base_mg():
+    """Test base panda motion generator."""
+    Np = 50  # prediction horizon, can be tuned;
+    Nc = 10  # control horizon, can be tuned
+    R = 10e-3  # weight matirix
+    Q = 10e9  # weight matrix
+
+    JOINT_STATE = np.array(
+        [
+            0.000165068,
+            -0.785579,
+            0.000134417,
+            -2.35485,
+            0.00100466,
+            1.57225,
+            0.785951,
+            -6.904296578564673e-18,
+            7.806316005606509e-17,
+            -3.694169137454414e-17,
+            1.075687377186247e-16,
+            -2.603546867034339e-17,
+            -2.953811484569149e-17,
+            -5.316185667767154e-17,
+        ]
+    )
+
+    ub_jerk = np.array([8500.0, 8500.0, 8500.0, 4500.0, 4500.0, 4500.0])
+    lb_jerk = -ub_jerk.copy()
+
+    ub_acc = np.array([17.0, 17.0, 17.0, 9.0, 9.0, 9.0])
+    lb_acc = -ub_acc.copy()
+
+    ub_v = np.array([2.5, 2.5, 2.5, 3.0, 3.0, 3.0])
+    lb_v = -ub_v.copy()
+
+    jerk_bound = BOUND(lb_jerk, ub_jerk)
+    acc_bound = BOUND(lb_acc, ub_acc)
+    vel_bound = BOUND(lb_v, ub_v)
+
+    franka_kin = FrankaEmikaPandaRobot.kinematics()
+    goal = franka_kin.fkm(
+        [0.173898, 0.667434, 0.782032, -1.86421, 1.44847, 1.57491, 0.889156]
+    )
+
+    mg = PandaScrewMotionGenerator(Np, Nc, Q, R, vel_bound, acc_bound, jerk_bound)
+    dq = mg.step(JOINT_STATE[: JOINT_STATE.shape[0] // 2], goal)
+
+    np.testing.assert_almost_equal(dq, JOINT_STATE[JOINT_STATE.shape[0] // 2 :])
+
+    error_msg = "Select sclerp between 0 and 1!"
+    with pytest.raises(ValueError, match=error_msg):
+        PandaScrewMotionGenerator(Np, Nc, Q, R, vel_bound, acc_bound, jerk_bound, -1)
+
+
+def test_panda_manipulability_mg():
+    """Test panda manipulability motion generator."""
+    Np = 50  # prediction horizon, can be tuned;
+    Nc = 10  # control horizon, can be tuned
+    R = 10e-3  # weight matirix
+    Q = 10e9  # weight matrix
+
+    JOINT_STATE = np.array(
+        [
+            0.000165068,
+            -0.785579,
+            0.000134417,
+            -2.35485,
+            0.00100466,
+            1.57225,
+            0.785951,
+            -6.904296578564673e-18,
+            7.806316005606509e-17,
+            -3.694169137454414e-17,
+            1.075687377186247e-16,
+            -2.603546867034339e-17,
+            -2.953811484569149e-17,
+            -5.316185667767154e-17,
+        ]
+    )
+
+    ub_jerk = np.array([8500.0, 8500.0, 8500.0, 4500.0, 4500.0, 4500.0])
+    lb_jerk = -ub_jerk.copy()
+
+    ub_acc = np.array([17.0, 17.0, 17.0, 9.0, 9.0, 9.0])
+    lb_acc = -ub_acc.copy()
+
+    ub_v = np.array([2.5, 2.5, 2.5, 3.0, 3.0, 3.0])
+    lb_v = -ub_v.copy()
+
+    jerk_bound = BOUND(lb_jerk, ub_jerk)
+    acc_bound = BOUND(lb_acc, ub_acc)
+    vel_bound = BOUND(lb_v, ub_v)
+
+    franka_kin = FrankaEmikaPandaRobot.kinematics()
+    goal = franka_kin.fkm(
+        [0.173898, 0.667434, 0.782032, -1.86421, 1.44847, 1.57491, 0.889156]
+    )
+
+    mg = PandaScrewMpMotionGenerator(Np, Nc, Q, R, vel_bound, acc_bound, jerk_bound)
+    mg.step(JOINT_STATE[: JOINT_STATE.shape[0] // 2], goal)
+
+    # np.testing.assert_almost_equal(dq, JOINT_STATE[JOINT_STATE.shape[0] // 2 :])
+
+    error_msg = "Select sclerp between 0 and 1!"
+    with pytest.raises(ValueError, match=error_msg):
+        PandaScrewMpMotionGenerator(Np, Nc, Q, R, vel_bound, acc_bound, jerk_bound, -1)
+
+    error_msg = "rho_i must be greater than rho_s!"
+    with pytest.raises(ValueError, match=error_msg):
+        PandaScrewMpMotionGenerator(
+            Np, Nc, Q, R, vel_bound, acc_bound, jerk_bound, rho_i=0.1, rho_s=0.9
+        )
