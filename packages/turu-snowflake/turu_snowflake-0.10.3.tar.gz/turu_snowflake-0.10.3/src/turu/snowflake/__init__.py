16beeb1e798561852e4de8a71b60ead5ac74983c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:58 +0000

    Add package turu-snowflake/0.10.3/turu_snowflake-0.10.3.tar.gz

---
+++ packages/turu-snowflake/turu_snowflake-0.10.3.tar.gz/turu_snowflake-0.10.3/src/turu/snowflake/__init__.py
@@ -0,0 +1,40 @@
+"""
+Turu Snowflake Adapter
+
+References:
+    https://yassun7010.github.io/turu-py/adapters/snowflake/index.html
+"""
+
+import importlib.metadata
+
+from .async_connection import AsyncConnection
+from .async_cursor import AsyncCursor
+from .connection import Connection
+from .cursor import Cursor
+from .mock_async_connection import MockAsyncConnection
+from .mock_async_cursor import MockAsyncCursor
+from .mock_connection import MockConnection
+from .mock_cursor import MockCursor
+
+__version__ = importlib.metadata.version("turu-snowflake")
+
+
+__all__ = [
+    "connect_async_from_env",
+    "connect_from_env",
+    "AsyncConnection",
+    "AsyncCursor",
+    "connect_async",
+    "connect",
+    "Connection",
+    "Cursor",
+    "MockAsyncConnection",
+    "MockAsyncCursor",
+    "MockConnection",
+    "MockCursor",
+]
+
+connect = Connection.connect
+connect_from_env = Connection.connect_from_env
+connect_async = AsyncConnection.connect
+connect_async_from_env = AsyncConnection.connect_from_env
