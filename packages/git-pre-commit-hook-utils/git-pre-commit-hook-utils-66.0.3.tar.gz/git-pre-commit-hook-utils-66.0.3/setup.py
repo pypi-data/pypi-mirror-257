cbf88ab0353573ecda23909dde8ebc1f0489f945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:26 +0000

    Add package git-pre-commit-hook-utils/66.0.3/git-pre-commit-hook-utils-66.0.3.tar.gz

---
+++ packages/git-pre-commit-hook-utils/git-pre-commit-hook-utils-66.0.3.tar.gz/git-pre-commit-hook-utils-66.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class InstallCommand(install):
+    def run(self):
+        raise RuntimeError("You are trying to install a stub package git-pre-commit-hook-utils. Maybe you are using the wrong pypi?")
+
+
+setup(
+    name='git-pre-commit-hook-utils',
+    version='66.0.3',
+    author='Yandex',
+    author_email='security@yandex-team.ru',
+    url='https://ya.ru',
+    readme="README.md",
+    long_description="""This is a security placeholder package.
+If you want to claim this name for legitimate purposes,
+please contact us at security@yandex-team.ru or pypi-security@yandex-team.ru""",
+    long_description_content_type='text/markdown',
+    description='A package to prevent Dependency Confusion attacks against Yandex.',
+    cmdclass={
+        'install': InstallCommand,
+    },
+)
