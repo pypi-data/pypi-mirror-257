e68934343499d995d30bd410199d2113327a142e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:40:57 +0000

    Add package premise/2.0.1/premise-2.0.1-py3-none-any.whl

---
+++ packages/premise/premise-2.0.1-py3-none-any.whl/premise/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ("NewDatabase", "clear_cache", "get_regions_definition")
+__version__ = (2, 0, 1)
+
+
+from .new_database import NewDatabase
+from .utils import clear_cache, get_regions_definition
