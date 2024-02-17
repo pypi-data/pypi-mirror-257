aa41a36962fee679e77e8b58f9dd52dc03c065e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:41 +0000

    Add package box-sdk-gen/0.5.1/box-sdk-gen-0.5.1.tar.gz

---
+++ packages/box-sdk-gen/box-sdk-gen-0.5.1.tar.gz/box-sdk-gen-0.5.1/box_sdk_gen/auth.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from abc import abstractmethod
+
+from box_sdk_gen.schemas import AccessToken
+
+from box_sdk_gen.network import NetworkSession
+
+
+class Authentication:
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def retrieve_token(
+        self, network_session: Optional[NetworkSession] = None
+    ) -> AccessToken:
+        pass
+
+    @abstractmethod
+    def refresh_token(self, network_session: NetworkSession) -> AccessToken:
+        pass
