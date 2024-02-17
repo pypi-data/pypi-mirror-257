e81aff54a3dabb1aa2ba93c375ff25beb6c49fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:04 +0000

    Add package ploomber-cloud/0.1.10/ploomber_cloud-0.1.10-py3-none-any.whl

---
+++ packages/ploomber-cloud/ploomber_cloud-0.1.10-py3-none-any.whl/ploomber_cloud/constants.py
@@ -0,0 +1,11 @@
+VALID_PROJECT_TYPES = {
+    # voila is not supported yet
+    "streamlit",
+    "docker",
+    "panel",
+    "solara",
+}
+
+FORCE_INIT_MESSAGE = (
+    "You may re-initialize the project by running 'ploomber-cloud init --force'."
+)
