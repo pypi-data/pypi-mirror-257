dd66305e0a8effd658439f4ac88bfdfa56df4d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:13 +0000

    Add package segmentation-skeleton-metrics/0.5.0/segmentation_skeleton_metrics-0.5.0.tar.gz

---
+++ packages/segmentation-skeleton-metrics/segmentation_skeleton_metrics-0.5.0.tar.gz/segmentation_skeleton_metrics-0.5.0/src/segmentation_skeleton_metrics/graph_utils.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 15 12:00:00 2023
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+"""
+from scipy.spatial.distance import euclidean as dist
+from random import sample
+
+import math
+import networkx as nx
+
+
+# -- edit graph --
+def remove_edge(graph, i, j):
+    """
+    Remove the edge "(i,j)" from "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to edited.
+    i : int
+        Node part of edge to be removed.
+    j : int
+        Node part of edge to be removed.
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Graph with edge removed.
+
+    """
+    graph.remove_edges_from([(i, j)])
+    return graph
+
+
+def delete_nodes(graph, delete_id, return_cnt=False):
+    """
+    Deletes nodes in "graph" whose pred_id is identical to "delete_id".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be searched and edited.
+    delete_id : int
+        pred_id to be removed from graph.
+    return : bool, optional
+        Indication of whether to return the number of nodes deleted from
+        "graph". The default is False.
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Updated graph.
+
+    """
+    # Find nodes matching delete_marker
+    delete_nodes = []
+    for i in graph.nodes:
+        label = graph.nodes[i]["pred_id"]
+        if label == delete_id:
+            delete_nodes.append(i)
+
+    # Count deleted edges (if applicable)
+    if return_cnt:
+        subgraph = graph.subgraph(delete_nodes)
+        cnt = subgraph.number_of_edges()
+
+    # Update graph
+    graph.remove_nodes_from(delete_nodes)
+    if return_cnt:
+        return graph, cnt
+    else:
+        return graph
+
+
+# -- attribute utils --
+def get_coord(graph, i):
+    """
+    Gets (x,y,z) image coordinates of node "i".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be queried.
+    i : int
+        Node of "graph".
+
+    Returns
+    -------
+    tuple
+        The (x,y,z) image coordinates of node "i".
+
+    """
+    return tuple(graph.nodes[i]["xyz"])
+
+
+def upd_labels(graph, nodes, label):
+    """
+    Updates the label of each node in "nodes" with "label".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be updated.
+    nodes : list
+        List of nodes to be updated.
+    label : int
+        New label of each node in "nodes".
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Updated graph.
+
+    """
+    for i in nodes:
+        graph.nodes[i].update({"pred_id": label})
+    return graph
+
+
+def store_labels(graph):
+    """
+    Iterates over all nodes in "graph" and stores their pred_id in a graph
+    attribute called pred_ids.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be updated
+
+    Returns
+    -------
+    graph : networkx.Graph
+        Updated graph.
+
+    """
+    for i in graph.nodes:
+        label = graph.nodes[i]["pred_id"]
+        graph.graph["pred_ids"].add(label)
+    return graph
+
+
+# -- eval tools --
+def count_splits(graph):
+    """
+    Counts the number of splits in "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be evaluated.
+
+    Returns
+    -------
+    int
+        Number of splits in "graph".
+
+    """
+    return max(len(list(nx.connected_components(graph))) - 1, 0)
+
+
+def compute_run_lengths(graph):
+    run_lengths = []
+    for nodes in nx.connected_components(graph):
+        subgraph = graph.subgraph(nodes)
+        run_lengths.append(compute_path_length(subgraph))
+    return run_lengths
+
+
+def compute_path_length(graph):
+    """
+    Computes path length of graph.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be parsed.
+
+    Returns
+    -------
+    path_length : float
+        Path length of graph.
+
+    """
+    path_length = 0
+    for i, j in nx.dfs_edges(graph):
+        xyz_1 = graph.nodes[i]["xyz"]
+        xyz_2 = graph.nodes[j]["xyz"]
+        path_length += dist(xyz_1, xyz_2)
+    return path_length
+
+# -- miscellaneous --
+def sample_leaf(graph):
+    """
+    Samples leaf node from "graph".
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be sampled from.
+
+    Returns
+    -------
+    int
+        Leaf node of "graph"
+
+    """
+    leafs = [i for i in graph.nodes if graph.degree[i] == 1]
+    return sample(leafs, 1)[0]
