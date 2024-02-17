0045749e3328d8e1cdb1f155ebc227c395590f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:19 +0000

    Add package realswift/3.0.2/realswift-3.0.2-py3-none-any.whl

---
+++ packages/realswift/realswift-3.0.2-py3-none-any.whl/realswift/browser_utils.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+import sys
+import time
+import shutil
+import psutil
+from reporter import generate_html
+from config_module import config
+edge_browser_path = config.edge_browser_path
+chrome_browser_path = config.chrome_browser_path
+browser = config.browser
+screenshots_folder = config.screenshots_folder
+options = config.options.split(";")
+window_chk = any("--window-size" in element for element in options)
+if window_chk:
+    options.append("--window-position=0,0")
+def open_browser(url):
+    global proc
+   # try:
+    if os.path.isdir(screenshots_folder):
+        shutil.rmtree(screenshots_folder)
+    os.mkdir(screenshots_folder)
+    os.mkdir(screenshots_folder+ "/old_" + screenshots_folder)
+# except Exception as e:
+#    print(f"{e}")
+
+
+    if browser == "chrome":
+        proc = subprocess.Popen([chrome_browser_path, url]+options,stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+    close_fds=True)
+    elif browser == "edge":
+        proc = subprocess.Popen([edge_browser_path, url]+options,stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+    close_fds=True)
+    time.sleep(2)
+def tear_down():
+    proc.kill
+    exit(0)
+def _activate_window():
+    if sys.platform == "win32":
+        try:
+            if browser == "chrome":
+                active_window = gw.getWindowsWithTitle("Google Chrome")
+                active_window[0].activate()
+            elif browser == "edge":
+                active_window = gw.getWindowsWithTitle("Edge")
+                active_window[0].activate()
+        except Exception as e:
+            print(f"window activation failed with error {e}")
+
---
+++ packages/realswift/realswift-3.0.2-py3-none-any.whl/realswift/image_utils.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import cv2
+import numpy as np
+import pyautogui
+from config_module import config
+browser = config.browser
+screenshots_path = config.screenshots_path
+img_objects_folder = config.img_objects_folder
+element_ss_path = config.element_ss_path
+
+def _take_screenshot():
+    """
+    Captures a screenshot of the active window and saves it to a file.
+
+    The function identifies the active window, captures a screenshot of that window, and saves it as an image.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    if sys.platform == "win32":
+        import pygetwindow as gw
+        if browser == "chrome":
+            active_window = gw.getWindowsWithTitle("Google Chrome")
+        elif browser == "edge":
+            active_window = gw.getWindowsWithTitle(" Edge")
+
+        if active_window is not None:
+            active_window = active_window[0]
+            try:
+                active_window.activate()
+            except Exception as e:
+                print("unable to activate edge window.. trying again")
+            try:
+                active_window.activate()
+            except Exception as e:
+                print(f"unable to activate edge window..Caught exception {e}")
+                exit(1)
+            window_x, window_y, window_width, window_height = active_window.left, active_window.top, active_window.width, active_window.height
+
+            screenshot = pyautogui.screenshot(region=(window_x, window_y, window_width, window_height))
+
+            screenshot.save(screenshots_path)
+        else:
+            print("no active window found.")
+    elif sys.platform == "linux":
+        from wand.image import Image
+        os.system(f"import -window root {screenshots_path}")
+    else:
+        print(f"Currently {sys.platform} platform is not supported")
+        exit(1)
+def _find_image(object, item=0, midpointRX=-1, midpointRY=-1):
+    """
+    Finds an image in the captured screenshot and returns its midpoint coordinates.
+
+    The function searches for a specified image within the captured screenshot and returns the midpoint coordinates
+    if a match is found.
+
+    Args:
+        object (str): The filename of the image to search for.
+        item (int): Index of the match if there are multiple matches. Defaults to 0.
+        midpointRX (int): Expected X-coordinate of the midpoint. Defaults to -1.
+        midpointRY (int): Expected Y-coordinate of the midpoint. Defaults to -1.
+
+    Returns:
+        tuple: A tuple containing the X and Y coordinates of the midpoint if a match is found, or None if no match is found.
+    """
+    _take_screenshot()
+    larger_image = cv2.imread(screenshots_path)
+    template = cv2.imread(f'{img_objects_folder}/{object}')
+    result = cv2.matchTemplate(larger_image, template, cv2.TM_CCORR_NORMED)
+    # cv2.normalize(result, result, 0, 1, cv2.NORM_MINMAX)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    confidence_threshold = 0.999  # Adjust as needed
+    loc = np.where(result >= confidence_threshold)
+    i = 0
+    # Draw rectangles around the matches
+    for pt in zip(*loc[::-1]):  # pt marks the location of the match
+        h, w = template.shape[:2]
+        top_left = pt
+        bottom_right = (pt[0] + w, pt[1] + h)
+        midpointX = (top_left[0] + bottom_right[0]) // 2
+        midpointY = (top_left[1] + bottom_right[1]) // 2
+        if not midpointRX == -1:
+            if abs(midpointRX - midpointX) < 5:
+                midpoint = midpointX, midpointY
+                break
+        elif not midpointRY == -1:
+            #print(f'midpoint diff of {midpointRY},{midpointY}=={abs(midpointRY - midpointY)}')
+            if abs(midpointRY - midpointY) < 5:
+                midpoint = midpointX, midpointY
+                break
+        elif item == i:
+            midpoint = midpointX, midpointY
+            break
+        i += 1
+    cv2.rectangle(larger_image, top_left, bottom_right, (0, 0, 255), 2)
+    cv2.imwrite(element_ss_path, larger_image)
+    print(f"element '{object}' found at the location {midpointX},{midpointY} on the webpage with confidence: {max_val}")
+    return midpoint
