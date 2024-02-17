3fbd15792b173ed571c1b9543e86bfe2b4c21780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:07 +0000

    Add package fake-bpy-module-latest/20240217/fake_bpy_module_latest-20240217-py3-none-any.whl

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240217-py3-none-any.whl/bl_text_utils/external_editor/__init__.pyi
@@ -0,0 +1,6 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+def open_external_editor(filepath, line, column): ...
---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20240217-py3-none-any.whl/bpy_extras/id_map_utils/__init__.pyi
@@ -0,0 +1,16 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+def get_all_referenced_ids(i, ref_m):
+    """Return a set of IDs directly or indirectly referenced by id."""
+
+    ...
+
+def get_id_reference_map():
+    """Return a dictionary of direct datablock references for every datablock in the blend file."""
+
+    ...
+
+def recursive_get_referenced_ids(ref_map, id, referenced_ids, visited): ...
