3969fcd40e6604a9337f3df12f48847143ae4486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:40:51 +0000

    Add package qshader/1.1.0/qshader-1.1.0.tar.gz

---
+++ packages/qshader/qshader-1.1.0.tar.gz/qshader-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+setup(
+    name='qshader',
+
+    packages=['qshader'],
+
+    version='1.1.0',
+
+    license='MIT',
+
+    description='QShader - Shaders support for 2D PyQt Games.',
+
+    long_description_content_type='text/x-rst',
+    long_description=open('README.rst', 'r').read(),
+
+    author='Ivan Perzhinsky.',
+    author_email='name1not1found.com@gmail.com',
+
+    url='https://github.com/xzripper/qshader',
+    download_url='https://github.com/xzripper/qshader/archive/refs/tags/v1.1.0-alpha.tar.gz',
+
+    keywords=['pyqt', 'qt', 'shaders', 'games', '2d'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
