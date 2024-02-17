c50574afe505165c1914e3614c68c74832f0a74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:27 +0000

    Add package translate-by-country/0.0.7/translate_by_country-0.0.7.tar.gz

---
+++ packages/translate-by-country/translate_by_country-0.0.7.tar.gz/translate_by_country-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+
+setup(
+    name='translate_by_country',  
+    version= '0.0.7', 
+    description='A library to translate text by country information',
+    long_description=readme,
+    author='Sina157',  
+    packages=find_packages(),
+    author_email='sina.shams@yahoo.com', 
+    url='https://github.com/Sina157/translate-by-country',  
+    install_requires=[
+     'googletrans==4.0.0-rc1'
+    ],
+    entry_points={}
+)
