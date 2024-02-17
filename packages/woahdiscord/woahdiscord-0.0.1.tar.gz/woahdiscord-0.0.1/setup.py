8893a851af3cd61a702b4208c1b8ffb74e6b4977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:43 +0000

    Add package woahdiscord/0.0.1/woahdiscord-0.0.1.tar.gz

---
+++ packages/woahdiscord/woahdiscord-0.0.1.tar.gz/woahdiscord-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+with open('requirements.txt', 'r') as file:
+    requirements = file.readlines()
+
+setup(
+    name='woahdiscord',
+    version='0.0.1',
+    description='A Python package to create repo and push code on Github',
+    author='TAWSIF AHMED',
+    author_email='sleeping4cat@outlook.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements
+)
