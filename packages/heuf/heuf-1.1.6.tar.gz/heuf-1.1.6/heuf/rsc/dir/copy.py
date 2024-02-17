2971eb9d0cae47f28e5ed08bd3b903123fb261d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:43 +0000

    Add package heuf/1.1.6/heuf-1.1.6.tar.gz

---
+++ packages/heuf/heuf-1.1.6.tar.gz/heuf-1.1.6/heuf/rsc/dir/copy.py
@@ -0,0 +1,12 @@
+import shutil
+
+
+def copy(src_path, dest_path) -> bool:
+    from .is_creatable import is_creatable
+    from .is_exists import is_exists
+    from .is_readable import is_readable
+    if not is_exists(src_path) or not is_readable(src_path):
+        return False
+    if is_exists(dest_path) or not is_creatable(dest_path):
+        return False
+    return shutil.copytree(src_path, dest_path) is None
