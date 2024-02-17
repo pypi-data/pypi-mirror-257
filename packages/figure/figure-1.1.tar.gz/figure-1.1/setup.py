cdd08e42588d2c44b2b8f1bc979a8ab86f12b223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:06 +0000

    Add package figure/1.1/figure-1.1.tar.gz

---
+++ packages/figure/figure-1.1.tar.gz/figure-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='figure',
+      version='1.1',
+      description='circle, tiangle, rectangle',
+      packages=['figure.circle', 'figure.triangle', 'figure.rectangle'],
+      author_email='alexeykozhakin@gmail.com',
+      zip_safe=False)
