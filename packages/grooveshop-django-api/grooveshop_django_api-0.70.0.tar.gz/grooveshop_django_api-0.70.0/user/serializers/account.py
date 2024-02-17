f3c3376e1a8dfde0c011e66a6af86b6d9400ab89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:26 +0000

    Add package grooveshop-django-api/0.70.0/grooveshop_django_api-0.70.0.tar.gz

---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.70.0.tar.gz/grooveshop_django_api-0.70.0/user/serializers/account.py
@@ -0,0 +1,55 @@
+from django.http import HttpRequest
+from drf_spectacular.utils import extend_schema_field
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
+
+from user.models import UserAccount
+
+
+class UserAccountSerializer(serializers.ModelSerializer):
+    last_login = serializers.SerializerMethodField("get_last_login")
+    phone = PhoneNumberField(required=False)
+
+    @extend_schema_field(serializers.DateTimeField)
+    def get_last_login(self, user_account) -> str or None:
+        request: HttpRequest = self.context.get("request")
+        try:
+            last_login = request.session.get("last_login", None)
+        except AttributeError:
+            last_login = None
+        return last_login
+
+    class Meta:
+        model = UserAccount
+        fields = (
+            "id",
+            "email",
+            "image",
+            "first_name",
+            "last_name",
+            "phone",
+            "city",
+            "zipcode",
+            "address",
+            "place",
+            "country",
+            "region",
+            "is_active",
+            "is_staff",
+            "birth_date",
+            "twitter",
+            "linkedin",
+            "facebook",
+            "instagram",
+            "website",
+            "youtube",
+            "github",
+            "bio",
+            "main_image_filename",
+            "is_superuser",
+            "last_login",
+            "is_active",
+            "created_at",
+            "updated_at",
+            "uuid",
+        )
