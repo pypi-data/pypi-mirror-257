91181916c39a869890406fa9d45d7313994b9ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:38 +0000

    Add package clipman/3.1.8/clipman-3.1.8-py3-none-any.whl

---
+++ packages/clipman/clipman-3.1.8-py3-none-any.whl/clipman/__version__.py
@@ -0,0 +1,7 @@
+"""
+Created for storing version number.
+The version number will be placed here based on the tag in Github using the tags_marker.py script.
+
+Having "3.1.8" in final releases is NOT allowed.
+"""
+__version__="3.1.8"
