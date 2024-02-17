97ca16865c3228beb10caacf24360527f033d3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:48:05 +0000

    Add package cyp2d6-parser/1.0.0/cyp2d6_parser-1.0.0.tar.gz

---
+++ packages/cyp2d6-parser/cyp2d6_parser-1.0.0.tar.gz/cyp2d6_parser-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="cyp2d6_parser",
+    version="1.0.0",
+    license="MIT",
+    description=long_description,
+    author="Andrew Haddad, PharmD",
+    author_email="andrew.haddad@pitt.edu",
+    packages=["cyp2d6_parser"],
+    entry_points={"console_scripts": ["cyp2d6_parser = cyp2d6_parser.__main__:main"]},
+    install_requires=["pandas", "numpy"],
+    package_data={"cyp2d6_parser": ["*.csv"]},
+    include_package_data=True,
+)
