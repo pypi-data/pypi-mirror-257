55812b0a95a9eb94003bf80c68b14a60a453fabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:38 +0000

    Add package hydra-ui/1.0.0/hydra-ui-1.0.0.tar.gz

---
+++ packages/hydra-ui/hydra-ui-1.0.0.tar.gz/hydra-ui-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+	name="hydra-ui",
+	version="1.0.0",
+	description="A framework for developing hybrid CLI/GUI programs.",
+	url="https://github.com/rweathers/hydra",
+	author="Ryan Weathers",
+	author_email="ryanweathers63@gmail.com",
+	license="GPLv3+",
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Application Frameworks",
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+		"Programming Language :: Python :: 3 :: Only"
+	],
+	py_modules=["hydra"]
+)
