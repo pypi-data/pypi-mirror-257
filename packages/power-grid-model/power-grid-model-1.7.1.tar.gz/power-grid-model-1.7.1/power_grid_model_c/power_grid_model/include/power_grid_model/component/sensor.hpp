fd6c3188883e8d29d0265923b43242874e7f5e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:09 +0000

    Add package power-grid-model/1.7.1/power-grid-model-1.7.1.tar.gz

---
+++ packages/power-grid-model/power-grid-model-1.7.1.tar.gz/power-grid-model-1.7.1/power_grid_model_c/power_grid_model/include/power_grid_model/component/sensor.hpp
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+#ifndef POWER_GRID_MODEL_COMPONENT_SENSOR_HPP
+#define POWER_GRID_MODEL_COMPONENT_SENSOR_HPP
+
+#include "base.hpp"
+
+#include "../auxiliary/input.hpp"
+#include "../auxiliary/output.hpp"
+#include "../auxiliary/update.hpp"
+#include "../calculation_parameters.hpp"
+#include "../exception.hpp"
+#include "../power_grid_model.hpp"
+
+namespace power_grid_model {
+
+class Sensor : public Base {
+  public:
+    static constexpr char const* name = "sensor";
+    using InputType = SensorInput;
+    using ShortCircuitOutputType = SensorShortCircuitOutput;
+
+    ID measured_object() const { return measured_object_; };
+
+    // sensor always energized
+    bool energized(bool /* is_connected_to_source */) const final { return true; }
+    ComponentType math_model_type() const final { return ComponentType::sensor; }
+
+  protected:
+    // constructor
+    explicit Sensor(SensorInput const& sensor_input)
+        : Base{sensor_input}, measured_object_{sensor_input.measured_object} {}
+    Sensor(Sensor const&) = default;
+    Sensor(Sensor&&) = default;
+    Sensor& operator=(Sensor const&) = default;
+    Sensor& operator=(Sensor&&) = default;
+
+  public:
+    ~Sensor() override = default;
+
+  private:
+    ID measured_object_;
+};
+
+} // namespace power_grid_model
+
+#endif
