69887d6f320425d106d1febf5acd8e26d5ec1550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:23 +0000

    Add package py-netty/0.0.39/py_netty-0.0.39-py3-none-any.whl

---
+++ packages/py-netty/py_netty-0.0.39-py3-none-any.whl/py_netty/bytebuf.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from concurrent.futures import Future
+
+EMPTY_BUFFER = b''
+
+
+@dataclass
+class Chunk:
+
+    buffer: bytes
+    future: Future = None
+    close: bool = False
+
+    def __post_init__(self):
+        self.future = self.future or Future()
+
+    def __str__(self):
+        return f"Chunk(bytes={len(self.buffer)}, future={self.future}, close={self.close})"
+
+    def __repr__(self):
+        return self.__str__()
