107db542996f89d30acbd9273e355c2a41a0e9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:39:22 +0000

    Add package mlpro-int-scikit-learn/0.1.0/mlpro-int-scikit-learn-0.1.0.tar.gz

---
+++ packages/mlpro-int-scikit-learn/mlpro-int-scikit-learn-0.1.0.tar.gz/mlpro-int-scikit-learn-0.1.0/src/mlpro_int_sklearn/wrappers/basics.py
@@ -0,0 +1,40 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - The integrative middleware framework for standardized machine learning
+## -- Package : mlpro_int_sklearn.wrappers
+## -- Module  : basics.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2024-02-16  1.0.0     DA       First version
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2024-02-16)
+
+This module contains the abstract root class for all scikit-learn wrapper classes.
+
+Learn more:
+https://scikit-learn.org
+
+"""
+
+from mlpro.bf.various import ScientificObject
+from mlpro.wrappers.models import Wrapper
+
+
+
+
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
+class WrapperSklearn (Wrapper):
+    """
+    Root class for all scikit-learn wrapper classes.
+    """
+
+    C_TYPE              = 'Wrapper scikit-learn'
+    C_WRAPPED_PACKAGE   = 'scikit-learn'
+    C_MINIMUM_VERSION   = '1.4.1'
+
+    C_SCIREF_TYPE       = ScientificObject.C_SCIREF_TYPE_ONLINE
+    C_SCIREF_AUTHOR     = 'scikit-learn community'
+    C_SCIREF_URL        = 'https://scikit-learn.org'
