916fefa69a36a32886e57bf8a854c4faa0484c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:51:08 +0000

    Add package bpm-ai/1.1.3/bpm_ai-1.1.3.tar.gz

---
+++ packages/bpm-ai/bpm_ai-1.1.3.tar.gz/bpm_ai-1.1.3/bpm_ai/common/multimodal.py
@@ -0,0 +1,19 @@
+from bpm_ai_core.speech_recognition.asr import ASRModel
+from bpm_ai_core.util.audio import is_supported_audio_file
+from bpm_ai_core.util.image import is_supported_img_file
+
+
+def prepare_images(input_data: dict):
+    return {
+        k: f"[# image {v} #]"
+        if (isinstance(v, str) and is_supported_img_file(v))
+        else v for k, v in input_data.items()
+    }
+
+
+async def prepare_audio(input_data: dict, asr: ASRModel | None):
+    return {
+        k: await asr.transcribe(v)
+        if (asr and isinstance(v, str) and is_supported_audio_file(v))
+        else v for k, v in input_data.items()
+    }
