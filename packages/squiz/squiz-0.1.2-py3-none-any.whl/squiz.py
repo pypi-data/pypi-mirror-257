8f9bf070a64967943a5178718fef5969362e0fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:43 +0000

    Add package squiz/0.1.2/squiz-0.1.2-py3-none-any.whl

---
+++ packages/squiz/squiz-0.1.2-py3-none-any.whl/squiz.py
@@ -0,0 +1,33 @@
+import builtins
+from inspect import getmembers
+
+
+# Terminal print colors
+_NONE = '\33[0m'
+_GREY = '\33[90m'
+_BLUE = '\033[34m'
+
+_BUILTIN_TYPES = tuple(value for name, value in getmembers(builtins) if type(value) is type)
+
+
+def _squiz(obj: object, depth: int):
+    # Don't bother inspecting built-in types
+    if type(obj) not in _BUILTIN_TYPES:
+        for name, value in getmembers(obj):
+            # Ignore hidden members and magic methods
+            if not name.startswith('__'):
+                # Print members details
+                print(f"{depth * '   ' + '   '}{_BLUE}{name} {_NONE}= {_GREY}{{{type(value).__name__}}} {_NONE}{value}")
+
+                # Recursively check nested members
+                _squiz(value, depth + 1)
+
+
+def squiz(obj: object) -> None:
+    """
+    Prints the direct and nested member names, types, and values of the target object.
+    """
+    # Print the target object details
+    print(f'{_GREY}{{{type(obj).__name__}}} {_NONE}{obj}')
+
+    _squiz(obj, 0)
