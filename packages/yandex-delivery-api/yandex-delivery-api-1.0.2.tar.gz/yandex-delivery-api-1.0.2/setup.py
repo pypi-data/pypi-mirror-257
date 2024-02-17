a996462bd7c403a86c2a2192844829e2cb691aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:10 +0000

    Add package yandex-delivery-api/1.0.2/yandex-delivery-api-1.0.2.tar.gz

---
+++ packages/yandex-delivery-api/yandex-delivery-api-1.0.2.tar.gz/yandex-delivery-api-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='yandex-delivery-api',
+      version='1.0.2',
+      description='Python wrapper for Yandex Delivery API',
+      packages=['yandex_delivery'],
+      author_email='edm.rud3nko@yandex.ru',
+      zip_safe=False)
