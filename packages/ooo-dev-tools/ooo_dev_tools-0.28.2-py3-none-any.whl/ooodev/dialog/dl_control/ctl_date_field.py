e42049647201fa432c5613a4082603427169c036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:55:05 +0000

    Add package ooo-dev-tools/0.28.2/ooo_dev_tools-0.28.2-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.28.2-py3-none-any.whl/ooodev/dialog/dl_control/ctl_date_field.py
@@ -0,0 +1,168 @@
+# region imports
+from __future__ import annotations
+from typing import Any, cast, TYPE_CHECKING
+import contextlib
+import datetime
+import uno  # pylint: disable=unused-import
+
+from ooodev.adapter.awt.spin_events import SpinEvents
+from ooodev.adapter.awt.text_events import TextEvents
+from ooodev.events.args.listener_event_args import ListenerEventArgs
+from ooodev.utils.kind.border_kind import BorderKind as BorderKind
+from ooodev.utils.kind.dialog_control_kind import DialogControlKind
+from ooodev.utils.kind.dialog_control_named_kind import DialogControlNamedKind
+
+# pylint: disable=useless-import-alias
+from ooodev.utils.kind.date_format_kind import DateFormatKind as DateFormatKind
+from ooodev.utils.date_time_util import DateUtil
+from .ctl_base import DialogControlBase
+
+if TYPE_CHECKING:
+    from com.sun.star.awt import UnoControlDateField  # service
+    from com.sun.star.awt import UnoControlDateFieldModel  # service
+# endregion imports
+
+
+class CtlDateField(DialogControlBase, SpinEvents, TextEvents):
+    """Class for Date Field Control"""
+
+    # pylint: disable=unused-argument
+
+    # region init
+    def __init__(self, ctl: UnoControlDateField) -> None:
+        """
+        Constructor
+
+        Args:
+            ctl (UnoControlDateField): Date Field Control
+        """
+        # generally speaking EventArgs.event_data will contain the Event object for the UNO event raised.
+        DialogControlBase.__init__(self, ctl)
+        generic_args = self._get_generic_args()
+        # EventArgs.event_data will contain the ActionEvent
+        SpinEvents.__init__(self, trigger_args=generic_args, cb=self._on_spin_events_listener_add_remove)
+        TextEvents.__init__(self, trigger_args=generic_args, cb=self._on_text_events_listener_add_remove)
+
+    # endregion init
+
+    # region Lazy Listeners
+    def _on_spin_events_listener_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.view.addSpinListener(self.events_listener_spin)
+        event.remove_callback = True
+
+    def _on_text_events_listener_add_remove(self, source: Any, event: ListenerEventArgs) -> None:
+        # will only ever fire once
+        self.view.addTextListener(self.events_listener_text)
+        event.remove_callback = True
+
+    # endregion Lazy Listeners
+
+    # region Overrides
+    def get_view_ctl(self) -> UnoControlDateField:
+        return cast("UnoControlDateField", super().get_view_ctl())
+
+    def get_uno_srv_name(self) -> str:
+        """Returns ``com.sun.star.awt.UnoControlDateField``"""
+        return "com.sun.star.awt.UnoControlDateField"
+
+    def get_model(self) -> UnoControlDateFieldModel:
+        """Gets the Model for the control"""
+        return cast("UnoControlDateFieldModel", self.get_view_ctl().getModel())
+
+    def get_control_kind(self) -> DialogControlKind:
+        """Gets the control kind. Returns ``DialogControlKind.DATE_FIELD``"""
+        return DialogControlKind.DATE_FIELD
+
+    def get_control_named_kind(self) -> DialogControlNamedKind:
+        """Gets the control named kind. Returns ``DialogControlNamedKind.DATE_FIELD``"""
+        return DialogControlNamedKind.DATE_FIELD
+
+    # endregion Overrides
+
+    # region Properties
+    @property
+    def border(self) -> BorderKind:
+        """Gets/Sets the border style"""
+        return BorderKind(self.model.Border)
+
+    @border.setter
+    def border(self, value: BorderKind) -> None:
+        self.model.Border = value.value
+
+    @property
+    def date(self) -> datetime.date:
+        """Gets/Sets the date"""
+        return DateUtil.uno_date_to_date(self.get_model().Date)
+
+    @date.setter
+    def date(self, value: datetime.date) -> None:
+        self.get_model().Date = DateUtil.date_to_uno_date(value)
+
+    @property
+    def date_format(self) -> DateFormatKind:
+        """Gets/Sets the format"""
+        return DateFormatKind(self.get_model().DateFormat)
+
+    @date_format.setter
+    def date_format(self, value: DateFormatKind) -> None:
+        self.get_model().DateFormat = value.value
+
+    @property
+    def date_max(self) -> datetime.date:
+        """Gets/Sets the min date"""
+        return DateUtil.uno_date_to_date(self.get_model().DateMax)
+
+    @date_max.setter
+    def date_max(self, value: datetime.date) -> None:
+        self.get_model().DateMax = DateUtil.date_to_uno_date(value)
+
+    @property
+    def date_min(self) -> datetime.date:
+        """Gets/Sets the min date"""
+        return DateUtil.uno_date_to_date(self.get_model().DateMin)
+
+    @date_min.setter
+    def date_min(self, value: datetime.date) -> None:
+        self.get_model().DateMin = DateUtil.date_to_uno_date(value)
+
+    @property
+    def dropdown(self) -> bool:
+        """Gets/Sets the if the control has a dropdown."""
+        return self.get_model().Dropdown
+
+    @dropdown.setter
+    def dropdown(self, value: bool) -> None:
+        self.get_model().Dropdown = value
+
+    @property
+    def model(self) -> UnoControlDateFieldModel:
+        return self.get_model()
+
+    @property
+    def read_only(self) -> bool:
+        """Gets/Sets the read-only property"""
+        with contextlib.suppress(Exception):
+            return self.get_model().ReadOnly
+        return False
+
+    @read_only.setter
+    def read_only(self, value: bool) -> None:
+        """Sets the read-only property"""
+        with contextlib.suppress(Exception):
+            self.get_model().ReadOnly = value
+
+    @property
+    def text(self) -> str:
+        """Gets/Sets the text"""
+        return self.get_model().Text
+
+    @text.setter
+    def text(self, value: str) -> None:
+        self.get_model().Text = value
+
+    @property
+    def view(self) -> UnoControlDateField:
+        return self.get_view_ctl()
+
+    # endregion Properties
---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.28.2-py3-none-any.whl/ooodev/dialog/dl_control/ctl_fixed_text.py
@@ -0,0 +1,91 @@
+# region imports
+from __future__ import annotations
+from typing import cast, TYPE_CHECKING
+import uno  # pylint: disable=unused-import
+
+from ooodev.utils.kind.border_kind import BorderKind as BorderKind
+from ooodev.utils.kind.dialog_control_kind import DialogControlKind
+from ooodev.utils.kind.dialog_control_named_kind import DialogControlNamedKind
+from .ctl_base import DialogControlBase
+
+if TYPE_CHECKING:
+    from com.sun.star.awt import UnoControlFixedText  # service
+    from com.sun.star.awt import UnoControlFixedTextModel  # service
+# endregion imports
+
+
+class CtlFixedText(DialogControlBase):
+    """Class for Fixed Text Control"""
+
+    # region init
+    def __init__(self, ctl: UnoControlFixedText) -> None:
+        """
+        Constructor
+
+        Args:
+            ctl (UnoControlFixedText): Fixed Text Control
+        """
+        # generally speaking EventArgs.event_data will contain the Event object for the UNO event raised.
+        DialogControlBase.__init__(self, ctl)
+
+    # endregion init
+
+    # region Overrides
+    def get_view_ctl(self) -> UnoControlFixedText:
+        return cast("UnoControlFixedText", super().get_view_ctl())
+
+    def get_uno_srv_name(self) -> str:
+        """Returns ``com.sun.star.awt.UnoControlFixedText``"""
+        return "com.sun.star.awt.UnoControlFixedText"
+
+    def get_model(self) -> UnoControlFixedTextModel:
+        """Gets the Model for the control"""
+        return cast("UnoControlFixedTextModel", self.get_view_ctl().getModel())
+
+    def get_control_kind(self) -> DialogControlKind:
+        """Gets the control kind. Returns ``DialogControlKind.FIXED_TEXT``"""
+        return DialogControlKind.FIXED_TEXT
+
+    def get_control_named_kind(self) -> DialogControlNamedKind:
+        """Gets the control named kind. Returns ``DialogControlNamedKind.FIXED_TEXT``"""
+        return DialogControlNamedKind.FIXED_TEXT
+
+    # endregion Overrides
+
+    # region Properties
+    @property
+    def border(self) -> BorderKind:
+        """Gets/Sets the border style"""
+        return BorderKind(self.model.Border)
+
+    @border.setter
+    def border(self, value: BorderKind) -> None:
+        self.model.Border = value.value
+
+    @property
+    def label(self) -> str:
+        """Gets/Sets the label"""
+        return self.model.Label
+
+    @label.setter
+    def label(self, value: str) -> None:
+        self.model.Label = value
+
+    @property
+    def model(self) -> UnoControlFixedTextModel:
+        return self.get_model()
+
+    @property
+    def multi_line(self) -> bool:
+        """Gets/Sets the multi line"""
+        return self.model.MultiLine
+
+    @multi_line.setter
+    def multi_line(self, value: bool) -> None:
+        self.model.MultiLine = value
+
+    @property
+    def view(self) -> UnoControlFixedText:
+        return self.get_view_ctl()
+
+    # endregion Properties
