5a97351ad6485427b52e85f0743d0e88081b0784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:46 +0000

    Add package dclxviclan/0.0.113/dclxviclan-0.0.113-py3-none-any.whl

---
+++ packages/dclxviclan/dclxviclan-0.0.113-py3-none-any.whl/libname/__init__.py
@@ -0,0 +1,1 @@
+from dclxviclan import *
---
+++ packages/dclxviclan/dclxviclan-0.0.113-py3-none-any.whl/libname/dclxviclan.py
@@ -0,0 +1,6 @@
+import webbrowser
+import os
+
+webbrowser.open('https://dclxviclan.itch.io')
+print("hi bro")
+os.system("xdg-open \"\" https://dclxviclan.itch.io")
