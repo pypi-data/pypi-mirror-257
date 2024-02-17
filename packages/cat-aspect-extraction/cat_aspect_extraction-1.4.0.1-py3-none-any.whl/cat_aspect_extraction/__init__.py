ef205a0afb6bcee527343d7dcf60cd7d69c5b294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:30 +0000

    Add package cat-aspect-extraction/1.4.0.1/cat_aspect_extraction-1.4.0.1-py3-none-any.whl

---
+++ packages/cat-aspect-extraction/cat_aspect_extraction-1.4.0.1-py3-none-any.whl/cat_aspect_extraction/__init__.py
@@ -0,0 +1,11 @@
+from .cat import CAt
+from .attention import Attention, RBFAttention, CosineAttention, EuclideanAttention, SoftmaxAttention
+
+__all__ = [
+    'CAt',
+    'Attention',
+    'RBFAttention',
+    'CosineAttention',
+    'EuclideanAttention',
+    'SoftmaxAttention'
+]
