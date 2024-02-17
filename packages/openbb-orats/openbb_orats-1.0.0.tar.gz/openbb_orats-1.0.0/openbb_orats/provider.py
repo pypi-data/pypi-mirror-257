c342b9a0e7c486c90bb55c371ef15037356c44cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:24 +0000

    Add package openbb-orats/1.0.0/openbb_orats-1.0.0.tar.gz

---
+++ packages/openbb-orats/openbb_orats-1.0.0.tar.gz/openbb_orats-1.0.0/openbb_orats/provider.py
@@ -0,0 +1,21 @@
+"""openbb_orats OpenBB Platform Provider."""
+
+from openbb_core.provider.abstract.provider import Provider
+
+from openbb_orats.models.tickers import OratsTickersOptionsChainsFetcher
+
+# mypy: disable-error-code="list-item"
+
+provider = Provider(
+    name="orats",
+    description="Data provider for ORATS.",
+    # Only add 'credentials' if they are needed.
+    # For multiple login details, list them all here.
+    credentials=["api_key"],
+    website="https://orats.com",
+    # Here, we list out the fetchers showing what our provider can get.
+    # The dictionary key is the fetcher's name, used in the `router.py`.
+    fetcher_dict={
+        "Tickers": OratsTickersOptionsChainsFetcher,
+    },
+)
