0f3399522cb6810a4756ea591621b25a05d7f943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:46:25 +0000

    Add package pypoolsync/0.0.2/pypoolsync-0.0.2.tar.gz

---
+++ packages/pypoolsync/pypoolsync-0.0.2.tar.gz/pypoolsync-0.0.2/pypoolsync/const.py
@@ -0,0 +1,11 @@
+"""Constants."""
+from __future__ import annotations
+
+from typing import Final
+
+CLIENT_ID: Final = "NTJscmFyZHI5aDZ1cmtxbzdzamFoamVtZ20="
+IDENTITY_POOL_ID: Final = (
+    "dXMtZWFzdC0xOjJjMzA5MzY2LTFjYTgtNGFjZC1hMzdlLWI0NTJhNmVhNTJkYQ=="
+)
+REGION_NAME: Final = "us-east-1"
+USER_POOL_ID: Final = "dXMtZWFzdC0xX3pBejdFMU81Rw=="
