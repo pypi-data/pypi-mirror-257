9cf36218688c82414448d2badfea0d006a1d8df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:55 +0000

    Add package tippecanoe/2.45.0/tippecanoe-2.45.0-py2.py3-none-macosx_11_0_arm64.whl

---
+++ packages/tippecanoe/tippecanoe-2.45.0-py2.py3-none-macosx_11_0_arm64.whl/tippecanoe/__init__.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+import sys
+
+try:
+    from ._version import __version__, __version_tuple__
+except ModuleNotFoundError:
+    __version__ = ""
+    __version_tuple__ = ()
+
+BIN_DIR = os.path.join(os.path.dirname(__file__), "bin")
+
+
+def _program(name: str, *args):
+    return subprocess.call([os.path.join(BIN_DIR, name)] + list(args))
+
+
+def tippecanoe():
+    raise SystemExit(_program("tippecanoe", *sys.argv[1:]))
+
+
+def tile_join():
+    raise SystemExit(_program("tile-join", *sys.argv[1:]))
+
+
+def tippecanoe_decode():
+    raise SystemExit(_program("tippecanoe-decode", *sys.argv[1:]))
+
+
+def tippecanoe_enumerate():
+    raise SystemExit(_program("tippecanoe-enumerate", *sys.argv[1:]))
+
+
+def tippecanoe_json_tool():
+    raise SystemExit(_program("tippecanoe-json-tool", *sys.argv[1:]))
+
+
+def tippecanoe_overzoom():
+    raise SystemExit(_program("tippecanoe-overzoom", *sys.argv[1:]))
