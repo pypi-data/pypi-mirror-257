420cc93353adff62182d09965a882558a1d6f6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:26 +0000

    Add package sshUploadandDownloadFileFolder/2.0/sshUploadandDownloadFileFolder-2.0.tar.gz

---
+++ packages/sshUploadandDownloadFileFolder/sshUploadandDownloadFileFolder-2.0.tar.gz/sshUploadandDownloadFileFolder-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='sshUploadandDownloadFileFolder',  # 模块的名称
+    version='2.0',           # 版本号
+    packages=['.'],          # 模块所在的包
+    description='MZCL',  # 模块的简要描述
+    author='pythonking',      # 作者名字
+    author_email='woshilaijiajunya@126.com',     # 作者邮箱
+    url='https://github.com/yourusername/yourmodule',  # 模块的URL
+)
