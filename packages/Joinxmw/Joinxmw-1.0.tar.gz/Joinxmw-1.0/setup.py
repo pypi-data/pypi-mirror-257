d655d9632545f6ff1edbece4228563f5ec3dfdd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:28 +0000

    Add package Joinxmw/1.0/Joinxmw-1.0.tar.gz

---
+++ packages/Joinxmw/Joinxmw-1.0.tar.gz/Joinxmw-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="Joinxmw",
+    version="1.0",
+    author="Hagkiest",
+    author_email="hagkiest@outlook.com",
+    description="Joinxmw",
+    long_description="This is a module that can call the programming website small code King, which can obtain the amount of user works, attention, etc., please check the official document for details",
+    long_description_content_type="Joinxmw",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+
+    ],
+)
