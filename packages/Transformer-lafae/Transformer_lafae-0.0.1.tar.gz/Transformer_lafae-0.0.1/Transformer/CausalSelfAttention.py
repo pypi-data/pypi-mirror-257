0447e9f1473af585edde59e2116849eda80d49ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:25 +0000

    Add package Transformer-lafae/0.0.1/Transformer_lafae-0.0.1.tar.gz

---
+++ packages/Transformer-lafae/Transformer_lafae-0.0.1.tar.gz/Transformer_lafae-0.0.1/Transformer/CausalSelfAttention.py
@@ -0,0 +1,28 @@
+from BaseAttention import BaseAttention
+class CausalSelfAttention(BaseAttention):
+    """
+    Call self attention on the input sequence, ensuring that each position in the 
+    output depends only on previous positions (i.e. a causal model).
+
+    Methods:
+        call: Performs the forward pass of the layer.
+
+    Attributes:
+        mha (tf.keras.layers.MultiHeadAttention): The MultiHeadAttention layer.
+        layernorm (tf.keras.layers.LayerNormalization): The LayerNormalization layer.
+        add (tf.keras.layers.Add): The Add layer.
+    """
+    def call(self, x: tf.Tensor) -> tf.Tensor:
+        """
+        The call function that performs the causal self-attention operation.
+        
+        Args:
+            x (tf.Tensor): The input sequence of shape (batch_size, seq_length, d_model).
+
+        Returns:
+            tf.Tensor: The output sequence of shape (batch_size, seq_length, d_model).
+        """
+        attn_output = self.mha(query=x, value=x, key=x, use_causal_mask = True)
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
