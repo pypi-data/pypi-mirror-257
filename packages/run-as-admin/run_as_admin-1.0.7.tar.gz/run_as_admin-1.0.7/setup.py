65c6eb3cef1949af2ff8357a20bab7e174613db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:38 +0000

    Add package run-as-admin/1.0.7/run_as_admin-1.0.7.tar.gz

---
+++ packages/run-as-admin/run_as_admin-1.0.7.tar.gz/run_as_admin-1.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='run_as_admin',
+    version='1.0.7',
+    packages=find_packages(),
+    author='Barno Chakraborty',
+    author_email='barno.baptu@gmail.com',
+    description='This script provides a simple way to execute a file as an administrator on Windows systems.',
+    url='https://github.com/barno1994/runasadmin',
+)
