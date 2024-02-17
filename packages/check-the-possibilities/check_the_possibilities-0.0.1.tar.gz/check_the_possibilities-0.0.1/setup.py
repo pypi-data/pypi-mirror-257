5a7afb5d2ec77277c07419940afd9267af1fafb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:35:34 +0000

    Add package check-the-possibilities/0.0.1/check_the_possibilities-0.0.1.tar.gz

---
+++ packages/check-the-possibilities/check_the_possibilities-0.0.1.tar.gz/check_the_possibilities-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='check_the_possibilities',
+    version='0.0.1',
+    author='alexander_petrenko',
+    author_email='gd@reseco.ru',
+    description='This is the simplest module for print some text',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AlexanderPetrenko83/prototype_python_library.git',
+    packages=find_packages(),
+    # install_requires=['requests>=2.25.1'],
+    # classifiers=[
+    #     'Programming Language :: Python :: 3.11',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Operating System :: OS Independent'
+    # ],
+    # keywords='files speedfiles ',
+    # project_urls={
+    #     'GitHub': 'your_github'
+    # },
+    python_requires='>=3.12'
+)
