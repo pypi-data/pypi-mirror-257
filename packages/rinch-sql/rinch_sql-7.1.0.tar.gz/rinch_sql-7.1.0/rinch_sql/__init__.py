27a030f7817624489a25fafe0d7e26cf001dd282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:29:27 +0000

    Add package rinch-sql/7.1.0/rinch_sql-7.1.0.tar.gz

---
+++ packages/rinch-sql/rinch_sql-7.1.0.tar.gz/rinch_sql-7.1.0/rinch_sql/__init__.py
@@ -0,0 +1,8 @@
+from mysql.connector.errors import IntegrityError
+
+from .__version__ import __version__
+from .db_config import DbConfig
+from .mysql import Mysql
+from .sql import Sql
+from .sql_static import SqlStatic
+from .table import Table
