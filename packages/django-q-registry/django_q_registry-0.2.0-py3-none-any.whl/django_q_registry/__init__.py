efacb766ae5c51e28eeb206036b9e3f8d85975f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:59 +0000

    Add package django-q-registry/0.2.0/django_q_registry-0.2.0-py3-none-any.whl

---
+++ packages/django-q-registry/django_q_registry-0.2.0-py3-none-any.whl/django_q_registry/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from django_q_registry.registry import register_task
+
+__version__ = "0.2.0"
+__template_version__ = "2024.10"
+
+__all__ = [
+    "register_task",
+]
