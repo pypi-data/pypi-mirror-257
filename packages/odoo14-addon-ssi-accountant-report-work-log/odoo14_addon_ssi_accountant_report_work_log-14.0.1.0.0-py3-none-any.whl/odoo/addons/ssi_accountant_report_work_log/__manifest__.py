38c0a51f43fff80ebb759fcb666d4fe7f7349dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:30:14 +0000

    Add package odoo14-addon-ssi-accountant-report-work-log/14.0.1.0.0/odoo14_addon_ssi_accountant_report_work_log-14.0.1.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-accountant-report-work-log/odoo14_addon_ssi_accountant_report_work_log-14.0.1.0.0-py3-none-any.whl/odoo/addons/ssi_accountant_report_work_log/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Accountant Report - Work Log Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_accountant_report",
+        "ssi_work_log_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
