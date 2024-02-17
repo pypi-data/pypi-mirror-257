159cd31e537bc80feaf69775485596d63397aab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:38:59 +0000

    Add package manuf2/2.0.0/manuf2-2.0.0.tar.gz

---
+++ packages/manuf2/manuf2-2.0.0.tar.gz/manuf2-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+README = open("./README.md", "r").read()
+
+setup(
+    name="manuf2",
+    packages=["manuf2"],
+    version="2.0.0",
+    description="Parser library for Wireshark's OUI database",
+    author="Josh Schmelzle, Michael Huang",
+    url="https://github.com/joshschmelzle/manuf2",
+    license="Apache License 2.0 or GPLv3",
+    keywords=["manuf2", "mac address", "networking"],
+    entry_points={
+        "console_scripts": ["manuf2=manuf2.manuf:main"],
+    },
+    package_data={"manuf2": ["manuf"]},
+    long_description=README,
+    long_description_content_type="text/markdown",
+)
+
+# To publish package run:
+# $ rm -rf dist #Delete all previous build that you might not want to upload
+# $ python setup.py build check sdist bdist_wheel #Build
+# $ twine upload --verbose dist/* #Upload
