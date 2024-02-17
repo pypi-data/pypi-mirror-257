0751931edbfa7aa56f472fd7346cc978096f6378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:41:36 +0000

    Add package cdl/0.12.0/cdl-0.12.0-py3-none-any.whl

---
+++ packages/cdl/cdl-0.12.0-py3-none-any.whl/cdl/tests/features/textimport_unit.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the terms of the BSD 3-Clause
+# (see cdl/LICENSE for details)
+
+"""
+Import wizard test
+"""
+
+# guitest: show
+
+from guidata.qthelpers import exec_dialog, qt_app_context
+from qtpy import QtWidgets as QW
+
+from cdl.env import execenv
+from cdl.tests.data import get_test_fnames
+from cdl.widgets.textimport import TextImportWizard
+
+
+def file_to_clipboard(filename: str) -> None:
+    """Copy file content to clipboard"""
+    with open(filename, "r") as file:
+        text = file.read()
+    QW.QApplication.clipboard().setText(text)
+
+
+def test_import_wizard():
+    """Test the import wizard"""
+    with qt_app_context():
+        for destination, fname in (
+            ("image", "fiber.txt"),
+            ("signal", "paracetamol.txt"),
+        ):
+            path = get_test_fnames(fname)[0]
+            file_to_clipboard(path)
+            wizard = TextImportWizard(destination=destination)
+            if exec_dialog(wizard):
+                for obj in wizard.get_objs():
+                    execenv.print(obj)
+
+
+if __name__ == "__main__":
+    test_import_wizard()
