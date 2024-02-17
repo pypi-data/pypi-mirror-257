6ba8f4a6f09325ad75fffe1bb455a13708e9126c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:08 +0000

    Add package lightningrobot-cli/0.1.0/lightningrobot_cli-0.1.0.tar.gz

---
+++ packages/lightningrobot-cli/lightningrobot_cli-0.1.0.tar.gz/lightningrobot_cli-0.1.0/lightningrobot_cli/main.py
@@ -0,0 +1,29 @@
+# 导入功能
+from lightningrobot_cli import runbot, install, create
+
+import sys
+
+logo = """
+      _      _       _     _         _             _____       _           _
+     | |    (_)     | |   | |       (_)           |  __ \     | |         | |
+     | |     _  __ _| |__ | |_ _ __  _ _ __   __ _| |__) |___ | |__   ___ | |_ 
+     | |    | |/ _` | '_ \| __| '_ \| | '_ \ / _` |  _  // _ \| '_ \ / _ \| __|
+     | |____| | (_| | | | | |_| | | | | | | | (_| | | \ \ (_) | |_) | (_) | |_ 
+     |______|_|\__, |_| |_|\__|_| |_|_|_| |_|\__, |_|  \_\___/|_.__/ \___/ \__|
+            __/ |                         __/ |                           
+           |___/                         |___/                             
+"""
+def cli():
+    if sys.argv[1] == "run":
+        print(logo)
+        runbot.main()
+    if sys.argv[1] == "create":
+        print(logo)
+        create.main()
+    if sys.argv[1] == "install":
+        print(logo)
+        name = sys.argv[3]
+        if sys.argv[2] == "adapter":
+            install.main(1,name)
+        if sys.argv[2] == "plugin":
+            install.main(2,name)
