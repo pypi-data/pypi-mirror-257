46a39a9085df04b7d61c9ce3bcd450fa928a5bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:34:42 +0000

    Add package backup-devops/0.1.0/backup_devops-0.1.0-py3-none-any.whl

---
+++ packages/backup-devops/backup_devops-0.1.0-py3-none-any.whl/backup_devops/__main__.py
@@ -0,0 +1,9 @@
+from backup_devops.app_backup import AppBackup
+        
+def main():
+    app_backup = AppBackup()
+    app_backup.backup_organization()
+    #AppBackup.delete_dir(GitOperations.REPOS_DIRECTORY)
+
+if __name__ == "__main__":
+    main()
