621245affd69005cfcd6429f2a6b10f128b937cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:37:10 +0000

    Add package maling/0.0.3/maling-0.0.3.tar.gz

---
+++ packages/maling/maling-0.0.3.tar.gz/maling-0.0.3/team/keyboard/__init__.py
@@ -0,0 +1,4 @@
+from .inline_keyboard import InlineButton, InlineKeyboard
+from .inline_pagination_keyboard import InlineButton, InlinePaginationKeyboard
+from .reply_keyboard import (ForceReply, ReplyButton, ReplyKeyboard,
+                             ReplyKeyboardRemove)
---
+++ packages/maling/maling-0.0.3.tar.gz/maling-0.0.3/team/sulap/__init__.py
@@ -0,0 +1,8 @@
+from .autopilot import *
+from .class_emoji import *
+from .class_handler import *
+from .class_logger import *
+from .class_modules import CMD_HELP, paginate_modules, refresh_modules
+from .class_pytgc import *
+from .class_ubot import *
+from .database import *
