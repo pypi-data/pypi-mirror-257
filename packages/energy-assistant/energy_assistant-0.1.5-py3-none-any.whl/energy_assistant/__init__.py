3b80c22cc1a3e2c2dd0778107d4cefed5bee7ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:31:18 +0000

    Add package energy-assistant/0.1.5/energy_assistant-0.1.5-py3-none-any.whl

---
+++ packages/energy-assistant/energy_assistant-0.1.5-py3-none-any.whl/energy_assistant/__init__.py
@@ -0,0 +1,13 @@
+"""The applicaton."""
+
+from abc import ABC, abstractmethod
+import uuid
+
+
+class Optimizer(ABC):
+    """Base class for optimizers."""
+
+    @abstractmethod
+    def get_optimized_power(self, deviceId: uuid.UUID) -> float:
+        """Get the optimized power budget for a give device."""
+        pass
