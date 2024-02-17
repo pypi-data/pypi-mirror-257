ee04ea16fe3e92474439f3728643b864b92014c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:54:25 +0000

    Add package maling/0.0.7/maling-0.0.7-py3-none-any.whl

---
+++ packages/maling/maling-0.0.7-py3-none-any.whl/team/sulap/class_mix.py
@@ -0,0 +1,13 @@
+from Mix import bot, user
+from config import *
+
+user = user
+bot = bot
+api_id = api_id
+api_hash = api_hash
+session = session
+bot_token = bot_token
+db_name = db_name
+mongo_uri = mongo_uri
+owner_id = owner_id
+log_pic = log_pic
