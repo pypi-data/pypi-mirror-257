13577b2645ad2fae4b6d65d9f54233739c48c7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-17 23:47:35 +0000

    Add package Sum-Multi-Calc/0.1/Sum-Multi-Calc-0.1.tar.gz

---
+++ packages/Sum-Multi-Calc/Sum-Multi-Calc-0.1.tar.gz/Sum-Multi-Calc-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Sum-Multi-Calc',
+    version='0.1',
+    description='a simple python library for calculating the sum and the product of two numeric values.',
+    author='Daniel Huber',
+    packages=find_packages(),
+)
