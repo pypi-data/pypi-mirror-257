66004485bfcbb7d02d3f86d598f88dd34ad1ec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:21 +0000

    Add package psqlpy/0.1.3/psqlpy-0.1.3-cp39-none-win_amd64.whl

---
+++ packages/psqlpy/psqlpy-0.1.3-cp39-none-win_amd64.whl/psqlpy/__init__.py
@@ -0,0 +1,11 @@
+from ._internal import PSQLPool, QueryResult, Transaction, IsolationLevel
+
+__all__ = [
+    "PSQLPool",
+    "QueryResult",
+    "Transaction",
+    "SmallInt",
+    "Integer",
+    "BigInt",
+    "IsolationLevel",
+]
