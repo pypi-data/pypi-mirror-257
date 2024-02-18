67f7f27ddb9a2c39d77e648100cc335a01db6a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:55 +0000

    Add package E13Testing1/0.0.1/E13Testing1-0.0.1.tar.gz

---
+++ packages/E13Testing1/E13Testing1-0.0.1.tar.gz/E13Testing1-0.0.1/README.txt
@@ -0,0 +1,1 @@
+Test for an upcoming project
---
+++ packages/E13Testing1/E13Testing1-0.0.1.tar.gz/E13Testing1-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='E13Testing1',
+  version='0.0.1',
+  description='A very basic calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='E13_',
+  author_email='E13OnTop@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='test', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
