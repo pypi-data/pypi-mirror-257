429b07e2138721dabb518c49af1f17399220c6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:08 +0000

    Add package torchkeras/3.9.6/torchkeras-3.9.6.tar.gz

---
+++ packages/torchkeras/torchkeras-3.9.6.tar.gz/torchkeras-3.9.6/torchkeras/__init__.py
@@ -0,0 +1,16 @@
+__version__="3.9.6"
+
+from torchkeras.vlog import VLog
+
+try:
+    from torchkeras.kerasmodel import KerasModel
+    from torchkeras.summary import summary, flop_summary
+    from torchkeras.utils import seed_everything,printlog,colorful,delete_object
+except Exception as err:
+    print(err)
+
+try:
+    from .hugmodel import HugModel
+except Exception:
+    pass
+
---
+++ packages/torchkeras/torchkeras-3.9.6.tar.gz/torchkeras-3.9.6/torchkeras/chat/__init__.py
@@ -0,0 +1,4 @@
+from .chatgpt import ChatGPT
+from .chatglm import ChatGLM
+from .conversations import get_conv_template, conv_templates
+from .chatllm import ChatLLM
