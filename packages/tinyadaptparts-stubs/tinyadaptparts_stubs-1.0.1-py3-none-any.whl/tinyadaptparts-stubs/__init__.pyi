57456bd8a9ef2e52453fcd7a1edbcd9025c4765f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:42 +0000

    Add package tinyadaptparts-stubs/1.0.1/tinyadaptparts_stubs-1.0.1-py3-none-any.whl

---
+++ packages/tinyadaptparts-stubs/tinyadaptparts_stubs-1.0.1-py3-none-any.whl/tinyadaptparts-stubs/__init__.pyi
@@ -0,0 +1,6 @@
+from tinyadaptparts.led import Led, LedState
+from tinyadaptparts.button import Button, PullOption
+from tinyadaptparts.analog import Analog
+from tinyadaptparts.SH1106oled import SH1106oled, Color
+
+__all__ = ["Led", "LedState", "Button", "PullOption", "Analog", "SH1106oled", "Color"]
