f498a1862a66a917026945b95b96e8b4239bf711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:03:50 +0000

    Add package keliya-lib/0.2.1/keliya_lib-0.2.1.tar.gz

---
+++ packages/keliya-lib/keliya_lib-0.2.1.tar.gz/keliya_lib-0.2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keliya_lib',
+    version='0.2.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'boto3'
+    ],
+    author='Erandra Jayasundara',
+    author_email='erandraj@gmail.com',
+    description='AWS Lambda helpers',
+    license='MIT'
+)
+
+#python3 setup.py sdist bdist_wheel
+#twine upload dist/*
