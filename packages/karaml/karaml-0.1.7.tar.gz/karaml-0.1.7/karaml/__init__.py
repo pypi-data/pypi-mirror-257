eaa8c54d3d509865ab2c1c27a5ad87cb8a6edf21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:19 +0000

    Add package karaml/0.1.7/karaml-0.1.7.tar.gz

---
+++ packages/karaml/karaml-0.1.7.tar.gz/karaml-0.1.7/karaml/__init__.py
@@ -0,0 +1,24 @@
+
+#print("Running __init__.py in karaml package")
+print("Welcome to the KARaML Tools package (version 0.1.7)")
+import os
+HOST_DIR = os.getcwd()
+if not os.path.exists('KARaML_Tools'):
+  os.mkdir('KARaML_Tools')
+
+import subprocess
+
+#Changed from
+#subprocess.run(['pip', 'install', '--upgrade', 'gdown'])
+subprocess.run(['pip', 'install', 'gdown==v4.6.4'])
+import gdown
+print("Using gdown version:", gdown.__version__)
+gdown.download(id="1TjmLUBzDJg32A3YvI85PctAcSi4xsKVa",
+               output='KARaML_Tools/karaml_setup.py',
+               quiet=True)
+os.chdir('KARaML_Tools')
+import karaml_setup
+os.chdir(HOST_DIR)
+
+#print("Import everything from karaml_tools.py")
+#from .karaml_tools import *
