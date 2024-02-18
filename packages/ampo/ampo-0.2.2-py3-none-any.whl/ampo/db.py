30de4ea97c34ffbe50b1c6d27da0a6e2b6be8090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:23 +0000

    Add package ampo/0.2.2/ampo-0.2.2-py3-none-any.whl

---
+++ packages/ampo/ampo-0.2.2-py3-none-any.whl/ampo/db.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from motor import motor_asyncio
+from pymongo.database import Database
+
+from .log import logger
+
+
+class AMPODatabase:
+    """
+    Singleton.
+    Class for work with montodb
+    """
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            logger.debug('Creating the object')
+            cls._instance = super(AMPODatabase, cls).__new__(cls)
+            cls._instance.__init(*args, **kwargs)
+        return cls._instance
+
+    def __init(self, url: str):
+        """
+        Initialization
+
+        Parameters
+        ----------
+            url : str
+                URL for connect to mongodb
+        """
+        self._client: Optional[motor_asyncio.AsyncIOMotorClient] = None
+        self._db: Optional[Database] = None
+
+        # Connect
+        self._client = motor_asyncio.AsyncIOMotorClient(url)
+        self._db = self._client.get_default_database()
+
+    @classmethod
+    def get_db(cls) -> motor_asyncio.AsyncIOMotorDatabase:
+        if cls._instance is None:
+            raise RuntimeError("Database not initialize")
+        return cls._instance._db
+
+    @classmethod
+    def clear(cls):
+        cls._instance = None
