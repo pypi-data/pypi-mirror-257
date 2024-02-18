b83670db026f3183544b804ca0cb2106ac2d3553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:56 +0000

    Add package infinity-emb/0.0.24/infinity_emb-0.0.24.tar.gz

---
+++ packages/infinity-emb/infinity_emb-0.0.24.tar.gz/infinity_emb-0.0.24/infinity_emb/engine.py
@@ -0,0 +1,186 @@
+import os
+from typing import Dict, List, Optional, Set, Tuple, Union
+
+# prometheus
+from infinity_emb.inference import (
+    BatchHandler,
+    Device,
+    select_model,
+)
+from infinity_emb.log_handler import logger
+from infinity_emb.primitives import EmbeddingReturnType, ModelCapabilites
+from infinity_emb.transformer.utils import InferenceEngine
+
+try:
+    # enable hf hub transfer if available
+    import hf_transfer  # type: ignore # noqa
+
+    if "HF_HUB_ENABLE_HF_TRANSFER" not in os.environ:
+        os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+except ImportError:
+    pass
+
+
+class AsyncEmbeddingEngine:
+    def __init__(
+        self,
+        model_name_or_path: str = "BAAI/bge-small-en-v1.5",
+        *,
+        batch_size: int = 64,
+        revision: Optional[str] = None,
+        trust_remote_code: bool = True,
+        engine: Union[InferenceEngine, str] = InferenceEngine.torch,
+        model_warmup: bool = False,
+        vector_disk_cache_path: str = "",
+        device: Union[Device, str] = Device.auto,
+        lengths_via_tokenize: bool = False,
+    ) -> None:
+        """Creating a Async EmbeddingEngine object.
+
+        Args:
+            model_name_or_path, str:  Defaults to "BAAI/bge-small-en-v1.5".
+            batch_size, int: Defaults to 64.
+            revision, str: Defaults to None.
+            trust_remote_code, bool: Defaults to True.
+            engine, InferenceEngine: backend for inference.
+                Defaults to InferenceEngine.torch.
+            model_warmup, bool: decide if warmup with max batch size . Defaults to True.
+            vector_disk_cache_path, str: file path to folder of cache.
+                Defaults to "" - default no caching.
+            device, Device: device to use for inference. Defaults to Device.auto,
+            lengths_via_tokenize, bool: schedule by token usage. Defaults to False
+
+        Example:
+            ```python
+            from infinity_emb import AsyncEmbeddingEngine, transformer
+            sentences = ["Embedded this via Infinity.", "Paris is in France."]
+            engine = AsyncEmbeddingEngine(engine="torch")
+            async with engine: # engine starts with engine.astart()
+                embeddings = np.array(await engine.embed(sentences))
+            # engine stops with engine.astop().
+            # For frequent restarts, handle start/stop yourself.
+            ```
+        """
+        self.batch_size = batch_size
+        self.running = False
+        self._vector_disk_cache_path = vector_disk_cache_path
+        self._model_name_or_path = model_name_or_path
+        self._lengths_via_tokenize = lengths_via_tokenize
+        if isinstance(engine, str):
+            engine = InferenceEngine[engine]
+        if isinstance(device, str):
+            device = Device[device]
+
+        self._model, self._min_inference_t, self._max_inference_t = select_model(
+            model_name_or_path=model_name_or_path,
+            revision=revision,
+            trust_remote_code=trust_remote_code,
+            batch_size=batch_size,
+            engine=engine,
+            model_warmup=model_warmup,
+            device=device,
+        )
+
+    async def astart(self):
+        """startup engine"""
+        if self.running:
+            raise ValueError(
+                "DoubleSpawn: already started `AsyncEmbeddingEngine`. "
+                " recommended use is via AsyncContextManager"
+                " `async with engine: ..`"
+            )
+        self.running = True
+        self._batch_handler = BatchHandler(
+            max_batch_size=self.batch_size,
+            model=self._model,
+            batch_delay=self._min_inference_t / 2,
+            vector_disk_cache_path=self._vector_disk_cache_path,
+            verbose=logger.level <= 10,
+            lengths_via_tokenize=self._lengths_via_tokenize,
+        )
+        await self._batch_handler.spawn()
+
+    async def astop(self):
+        """stop engine"""
+        self._check_running()
+        self.running = False
+        await self._batch_handler.shutdown()
+
+    async def __aenter__(self):
+        await self.astart()
+
+    async def __aexit__(self, *args):
+        await self.astop()
+
+    def overload_status(self):
+        self._check_running()
+        return self._batch_handler.overload_status()
+
+    def is_overloaded(self) -> bool:
+        self._check_running()
+        return self._batch_handler.is_overloaded()
+
+    @property
+    def capabilities(self) -> Set[ModelCapabilites]:
+        return self._model.capabilities
+
+    async def embed(
+        self, sentences: List[str]
+    ) -> Tuple[List[EmbeddingReturnType], int]:
+        """embed multiple sentences
+
+        Args:
+            sentences (List[str]): sentences to be embedded
+
+        Raises:
+            ValueError: raised if engine is not started yet"
+
+        Returns:
+            List[numpy.ndarray]: embeddings
+                2D list-array of shape( len(sentences),embed_dim )
+            Usage:
+        """
+
+        self._check_running()
+        embeddings, usage = await self._batch_handler.embed(sentences)
+        return embeddings, usage
+
+    async def rerank(
+        self, *, query: str, docs: List[str], raw_scores: bool = False
+    ) -> Tuple[List[float], int]:
+        """rerank multiple sentences
+
+        Args:
+            query (str): query to be reranked
+            docs (List[str]): docs to be reranked
+            raw_scores (bool): return raw scores instead of sigmoid
+        """
+        self._check_running()
+        scores, usage = await self._batch_handler.rerank(
+            query=query, docs=docs, raw_scores=raw_scores
+        )
+
+        return scores, usage
+
+    async def classify(
+        self, *, sentences: List[str], raw_scores: bool = False
+    ) -> Tuple[List[Dict[str, float]], int]:
+        """rerank multiple sentences
+
+        Args:
+            query (str): query to be reranked
+            docs (List[str]): docs to be reranked
+            raw_scores (bool): return raw scores instead of sigmoid
+        """
+        self._check_running()
+        scores, usage = await self._batch_handler.classify(sentences=sentences)
+
+        return scores, usage
+
+    def _check_running(self):
+        if not self.running:
+            raise ValueError(
+                "didn't start `AsyncEmbeddingEngine` "
+                " recommended use is via AsyncContextManager"
+                " `async with engine: ..`"
+            )
---
+++ packages/infinity-emb/infinity_emb-0.0.24.tar.gz/infinity_emb-0.0.24/infinity_emb/inference/select_model.py
@@ -0,0 +1,98 @@
+import json
+from pathlib import Path
+from typing import Optional, Tuple, Union
+
+from infinity_emb.log_handler import logger
+from infinity_emb.primitives import (
+    Device,
+)
+from infinity_emb.transformer.abstract import BaseCrossEncoder, BaseEmbedder
+from infinity_emb.transformer.utils import (
+    EmbedderEngine,
+    InferenceEngine,
+    PredictEngine,
+    RerankEngine,
+)
+
+
+def get_engine_type_from_config(
+    model_name_or_path: str, engine: InferenceEngine
+) -> Union[EmbedderEngine, RerankEngine]:
+    if engine in [InferenceEngine.debugengine, InferenceEngine.fastembed]:
+        return EmbedderEngine.from_inference_engine(engine)
+
+    if Path(model_name_or_path).is_dir():
+        logger.debug("model is a directory, opening config.json")
+        config_path = Path(model_name_or_path) / "config.json"
+    else:
+        from huggingface_hub import hf_hub_download  # type: ignore
+
+        config_path = hf_hub_download(
+            model_name_or_path,
+            filename="config.json",
+        )
+
+    with open(config_path, "r") as f:
+        config = json.load(f)
+
+    if any(
+        "SequenceClassification" in arch for arch in config.get("architectures", [])
+    ):
+        id2label = config.get("id2label", {"0": "dummy"})
+        if len(id2label) < 2:
+            return RerankEngine.from_inference_engine(engine)
+        else:
+            return PredictEngine.from_inference_engine(engine)
+    else:
+        return EmbedderEngine.from_inference_engine(engine)
+
+
+def select_model(
+    model_name_or_path: str,
+    batch_size: int,
+    revision: Optional[str] = None,
+    trust_remote_code: bool = True,
+    engine: InferenceEngine = InferenceEngine.torch,
+    model_warmup=True,
+    device: Device = Device.auto,
+) -> Tuple[Union[BaseCrossEncoder, BaseEmbedder], float, float]:
+    logger.info(
+        f"model=`{model_name_or_path}` selected, using engine=`{engine.value}`"
+        f" and device=`{device.value}`"
+    )
+    # TODO: add EncoderEngine
+    unloaded_engine = get_engine_type_from_config(model_name_or_path, engine)
+
+    loaded_engine = unloaded_engine.value(
+        model_name_or_path,
+        revision=revision,
+        device=device.value,
+        trust_remote_code=trust_remote_code,
+    )
+
+    min_inference_t = 4e-3
+    max_inference_t = 4e-3
+    if model_warmup:
+        # size one, warm up warm start timings.
+        loaded_engine.warmup(batch_size=batch_size, n_tokens=1)
+        # size one token
+        min_inference_t = min(
+            loaded_engine.warmup(batch_size=1, n_tokens=1)[1] for _ in range(10)
+        )
+        loaded_engine.warmup(batch_size=batch_size, n_tokens=1)
+        emb_per_sec_short, max_inference_t, log_msg = loaded_engine.warmup(
+            batch_size=batch_size, n_tokens=1
+        )
+        logger.info(log_msg)
+        # now warm up with max_token, max batch size
+        loaded_engine.warmup(batch_size=batch_size, n_tokens=512)
+        emb_per_sec, _, log_msg = loaded_engine.warmup(
+            batch_size=batch_size, n_tokens=512
+        )
+        logger.info(log_msg)
+        logger.info(
+            f"model warmed up, between {emb_per_sec:.2f}-{emb_per_sec_short:.2f}"
+            f" embeddings/sec at batch_size={batch_size}"
+        )
+
+    return loaded_engine, min_inference_t, max_inference_t
