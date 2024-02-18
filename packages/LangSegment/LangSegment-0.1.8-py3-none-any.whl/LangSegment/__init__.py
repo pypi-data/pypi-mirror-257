7427e06e3884fdfb6ab8e0b8e0896e5f82444fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:16 +0000

    Add package LangSegment/0.1.8/LangSegment-0.1.8-py3-none-any.whl

---
+++ packages/LangSegment/LangSegment-0.1.8-py3-none-any.whl/LangSegment/__init__.py
@@ -0,0 +1,8 @@
+from .LangSegment import LangSegment,getTexts,classify,getCounts,printList,setLangfilters,getLangfilters
+
+# release
+__version__ = '0.1.8'
+
+
+# develop
+__develop__ = 'dev-0.0.1'
