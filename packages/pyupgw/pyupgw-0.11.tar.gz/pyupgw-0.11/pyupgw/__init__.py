88332cdc9f08dccb4f8f195d2c3a011922d3044c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:05 +0000

    Add package pyupgw/0.11/pyupgw-0.11.tar.gz

---
+++ packages/pyupgw/pyupgw-0.11.tar.gz/pyupgw-0.11/pyupgw/__init__.py
@@ -0,0 +1,18 @@
+"""Python client for Unisenza Plus"""
+
+__version__ = "0.11"
+
+from .client import Client, create_api, create_client
+from .errors import AuthenticationError, ClientError
+from .models import (
+    Device,
+    DeviceAttributes,
+    DeviceType,
+    Gateway,
+    GatewayAttributes,
+    HvacAttributes,
+    HvacDevice,
+    Occupant,
+    RunningState,
+    SystemMode,
+)
