b8ed8ea71d7b5daeda3b45ec4015908265a003a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:22 +0000

    Add package nidibot/1.1.0/nidibot-1.1.0-py2.py3-none-any.whl

---
+++ packages/nidibot/nidibot-1.1.0-py2.py3-none-any.whl/nidibot/server_provider/server_provider_interface.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+
+
+@dataclass
+class ServerProviderConfiguration:
+    type: str = ""
+    token: str = ""
+
+
+@dataclass
+class ServerStatus:
+    game_name: str = ""
+    game_version: str = ""
+    server_address: str = ""
+    status: str = ""
+    available_until: str = ""
+    update_available: bool = False
+    players_limit: int = 0
+    players_connected: int = 0
+    player_names: list = field(default_factory=list)
+
+
+class ServerProviderInterface(ABC):
+    @abstractmethod
+    def _poll(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_servers(self) -> list:
+        pass
+
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @abstractmethod
+    def status(self, server_id: str = "") -> ServerStatus:
+        pass
+
+    @abstractmethod
+    def start(self, server_id: str = "") -> bool:
+        pass
+
+    @abstractmethod
+    def stop(self, server_id: str = "") -> bool:
+        pass
+
+    @abstractmethod
+    def restart(self, server_id: str = "") -> bool:
+        pass
+
+    @abstractmethod
+    def create_backup(self, server_id: str = "") -> bool:
+        pass
+
+    @abstractmethod
+    def restore_backup(self, server_id: str = "") -> bool:
+        pass
