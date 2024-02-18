278c3bf96a4f2bc86903dfa652c9687b6f49fdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:17 +0000

    Add package fastask/0.3.1/fastask-0.3.1.tar.gz

---
+++ packages/fastask/fastask-0.3.1.tar.gz/fastask-0.3.1/setup.py
@@ -0,0 +1,23 @@
+# file: setup.py
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fastask',
+    packages=['fastask'],
+    version='0.3.1',
+    py_modules=['fastask.ask'],
+    include_package_data=True,
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'ask = fastask.ask:main',
+        ],
+    },
+    description='A fast and easy tool for getting answers in the command line',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
