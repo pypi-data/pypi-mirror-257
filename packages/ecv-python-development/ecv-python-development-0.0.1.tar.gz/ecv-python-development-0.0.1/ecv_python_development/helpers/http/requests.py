04c299c6f30eafe62a6fdee18c37f7d1dc0aed7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:04 +0000

    Add package ecv-python-development/0.0.1/ecv-python-development-0.0.1.tar.gz

---
+++ packages/ecv-python-development/ecv-python-development-0.0.1.tar.gz/ecv-python-development-0.0.1/ecv_python_development/helpers/http/requests.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import requests
+from typing_extensions import Any
+
+
+def send_get_request(
+    url: str, headers: dict[Any, Any], payload: dict[Any, Any] = {}
+) -> dict[str, Any]:
+    response = requests.request(
+        "GET", url, headers=headers, data=payload, params=payload
+    )
+    return {
+        "code": response.status_code,
+        "reason": response.reason,
+        "body": response.json(),
+    }
+
+
+def send_post_request(
+    url: str, headers: dict[Any, Any], payload: dict[Any, Any]
+) -> dict[str, Any]:
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return {
+        "code": response.status_code,
+        "reason": response.reason,
+        "body": response.json(),
+    }
