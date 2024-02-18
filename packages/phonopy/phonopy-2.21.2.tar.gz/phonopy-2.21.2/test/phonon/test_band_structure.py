9a7ef9d387f96e0e4318fa8c6f3aaeaccbad2cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:21 +0000

    Add package phonopy/2.21.2/phonopy-2.21.2.tar.gz

---
+++ packages/phonopy/phonopy-2.21.2.tar.gz/phonopy-2.21.2/test/phonon/test_band_structure.py
@@ -0,0 +1,36 @@
+"""Tests for band structure calculation."""
+
+from phonopy.phonon.band_structure import get_band_qpoints
+
+
+def test_band_structure(ph_nacl):
+    """Test band structure calculation by NaCl."""
+    ph_nacl.run_band_structure(
+        _get_band_qpoints(), with_group_velocities=False, is_band_connection=False
+    )
+    ph_nacl.get_band_structure_dict()
+
+
+def test_band_structure_gv(ph_nacl):
+    """Test band structure calculation with group velocity by NaCl."""
+    ph_nacl.run_band_structure(
+        _get_band_qpoints(), with_group_velocities=True, is_band_connection=False
+    )
+    ph_nacl.get_band_structure_dict()
+
+
+def test_band_structure_bc(ph_nacl):
+    """Test band structure calculation with band connection by NaCl."""
+    ph_nacl.run_band_structure(
+        _get_band_qpoints(), with_group_velocities=False, is_band_connection=True
+    )
+    ph_nacl.get_band_structure_dict()
+
+
+def _get_band_qpoints():
+    band_paths = [
+        [[0, 0, 0], [0.5, 0.5, 0.5]],
+        [[0.5, 0.5, 0], [0, 0, 0], [0.5, 0.25, 0.75]],
+    ]
+    qpoints = get_band_qpoints(band_paths, npoints=11)
+    return qpoints
