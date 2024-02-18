eee2a07c13681029985c78b0f0721758af26b68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:09 +0000

    Add package mkdocs-obsidian-support-plugin/0.9.0/mkdocs-obsidian-support-plugin-0.9.0.tar.gz

---
+++ packages/mkdocs-obsidian-support-plugin/mkdocs-obsidian-support-plugin-0.9.0.tar.gz/mkdocs-obsidian-support-plugin-0.9.0/obsidian_support/plugin.py
@@ -0,0 +1,26 @@
+from mkdocs.plugins import BasePlugin
+
+from obsidian_support.conversion.admonition import AdmonitionConversion
+from obsidian_support.conversion.admonition_backquotes import AdmonitionBackquotesConversion
+from obsidian_support.conversion.excalidraw import ExcalidrawConversion
+from obsidian_support.conversion.image_link import ImageLinkConversion
+from obsidian_support.conversion.tags import TagsConversion
+from obsidian_support.markdown_convert import markdown_convert
+
+"""
+A mkdocs plugin that support conversion from
+'obsidian syntax' to 'mkdocs-material syntax'
+"""
+
+
+class ObsidianSupportPlugin(BasePlugin):
+
+    def on_page_markdown(self, markdown, page, config, files):
+        ## apply conversions
+        markdown = markdown_convert(markdown, page, AdmonitionConversion())
+        markdown = markdown_convert(markdown, page, AdmonitionBackquotesConversion())
+        # markdown = markdown_convert(markdown, page, ExcalidrawConversion())
+        markdown = markdown_convert(markdown, page, ImageLinkConversion())
+        markdown = markdown_convert(markdown, page, TagsConversion())
+
+        return markdown
