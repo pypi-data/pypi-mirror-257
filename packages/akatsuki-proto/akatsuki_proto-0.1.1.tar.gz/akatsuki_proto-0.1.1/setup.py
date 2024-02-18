d5a5b94b3ce0c6cc3219f76903035aff4a41a0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:03 +0000

    Add package akatsuki-proto/0.1.1/akatsuki_proto-0.1.1.tar.gz

---
+++ packages/akatsuki-proto/akatsuki_proto-0.1.1.tar.gz/akatsuki_proto-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+import setuptools
+
+setup(
+    name="akatsuki_proto",
+    version="0.1.1",
+    author="Akatsuki",
+    description="Protobuf files & source generation for Akatsuki",
+    packages=setuptools.find_packages(),
+    package_data={
+        package: ["*.pyi", "**/*.pyi"]
+        for package in setuptools.find_packages()
+    },
+    install_requires=["protobuf"],
+    include_package_data=True,
+    python_requires=">=3.9",
+)
