5966e438be57303ecb5e2cc058d4025efc9f06f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:10 +0000

    Add package crunpyroll/2.3.4/crunpyroll-2.3.4-py3-none-any.whl

---
+++ packages/crunpyroll/crunpyroll-2.3.4-py3-none-any.whl/crunpyroll/types/__init__.py
@@ -0,0 +1,16 @@
+from .series import Series
+from .seasons import SeasonsQuery, Season
+from .episodes import EpisodesQuery, Episode
+from .movies import Movie
+from .search import SearchQuery
+from .images import Image, Images
+from .cms import CMS
+from .index import SessionIndex
+from .profile import Profile
+from .content import Content
+from .streams import MediaStreams
+from .subtitles import SubtitlesStream
+from .hardsub import HardsubStream
+from .old_streams import OldMediaStreams
+from .manifest import Manifest, ManifestVideoStream, ManifestAudioStream
+from .drm import DRM, ContentProtection
