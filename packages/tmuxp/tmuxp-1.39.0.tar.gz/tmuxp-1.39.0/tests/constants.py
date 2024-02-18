a8af64bc5466f6148e65cc52bd0dbfe11b802a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:32 +0000

    Add package tmuxp/1.39.0/tmuxp-1.39.0.tar.gz

---
+++ packages/tmuxp/tmuxp-1.39.0.tar.gz/tmuxp-1.39.0/tests/constants.py
@@ -0,0 +1,6 @@
+"""Constant variables for tmuxp tests."""
+import pathlib
+
+TESTS_PATH = pathlib.Path(__file__).parent
+EXAMPLE_PATH = TESTS_PATH.parent / "examples"
+FIXTURE_PATH = TESTS_PATH / "fixtures"
