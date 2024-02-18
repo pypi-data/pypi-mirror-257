d8832f47776bd7f75c73705ae0a225a1d21c1304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:08 +0000

    Add package scistringparser/0.1.2/scistringparser-0.1.2.tar.gz

---
+++ packages/scistringparser/scistringparser-0.1.2.tar.gz/scistringparser-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scistringparser',
+    version='0.1.2',
+    description='A python module that converts strings in the metric scientific notation to numerical data types.',
+    long_description=open("README.md", 'r').read(),
+    long_description_content_type='text/markdown',
+    author='Rodrigo Moraes',
+    author_email='rodrigo.smoraes98@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
