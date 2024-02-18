0865ccb490f8b0f6514505ad42ff82b36a284fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:16 +0000

    Add package dbterd/1.10.1/dbterd-1.10.1-py3-none-any.whl

---
+++ packages/dbterd/dbterd-1.10.1-py3-none-any.whl/dbterd/adapters/adapter.py
@@ -0,0 +1,21 @@
+from importlib import import_module
+
+
+def load_executor(name: str):
+    """Import the target extension dynamically
+
+    Args:
+        name (str): Target name e.g. dbml, mermaid
+
+    Raises:
+        Exception: Target not found
+
+    Returns:
+        ModuleType: Imported module
+    """
+    try:
+        return import_module(name=f".{name}", package="dbterd.adapters.targets")
+    except ModuleNotFoundError as exc:
+        if exc.name == "dbterd.adapters.targets." + name:
+            raise Exception(f"Could not find adapter target type {name}!")
+        raise  # pragma: no cover
