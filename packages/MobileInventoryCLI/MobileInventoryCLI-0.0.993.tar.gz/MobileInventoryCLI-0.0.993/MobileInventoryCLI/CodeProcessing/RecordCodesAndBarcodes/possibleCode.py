3e1adbefdfef0a2c9baf4168ef37c4190d46eed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:30 +0000

    Add package MobileInventoryCLI/0.0.993/MobileInventoryCLI-0.0.993.tar.gz

---
+++ packages/MobileInventoryCLI/MobileInventoryCLI-0.0.993.tar.gz/MobileInventoryCLI-0.0.993/MobileInventoryCLI/CodeProcessing/RecordCodesAndBarcodes/possibleCode.py
@@ -0,0 +1,58 @@
+import upcean
+from barcode import UPCA
+from colored import Fore,Style,Back
+
+class PossibleCodes:
+    def __init__(self,scanned):
+        try:
+            if len(scanned) > 8:
+                if len(scanned) == 10:
+                    scanned=f"0{scanned}"
+                upca=UPCA(scanned)
+                upca_ean=UPCA(scanned,make_ean=True)
+                upce=upcean.convert.convert_barcode_from_upca_to_upce(str(upca))
+                upca_stripped=str(upca) 
+                if upca_stripped.startswith("0"):
+                    upca_stripped=upca_stripped[1:]
+                upca_stripped=upca_stripped[:-1]
+                print(
+            f"""
+{Fore.cyan}UPCA -> {upca}{Style.reset}
+{Fore.magenta}UPCA Stripped -> {upca_stripped}{Style.reset}
+{Fore.red}UPCA_EAN -> {upca_ean}{Style.reset}
+{Fore.dark_goldenrod}UPCE -> {upce}{Style.reset}
+
+                   """)
+            else:
+                upca=upcean.convert.convert_barcode_from_upce_to_upca(scanned)
+                if upca:
+                    upca_ean=UPCA(upca,make_ean=True)
+                    upca=UPCA(upca)
+                    upca_stripped=str(upca) 
+                    if upca_stripped.startswith("0"):
+                        upca_stripped=upca_stripped[1:]
+                    upca_stripped=upca_stripped[:-1]
+
+                    print(f"""{Fore.red}UPCA_EAN -> {upca_ean}{Style.reset}
+{Fore.green}UPCA-Checked -> {upca}{Style.reset}
+{Fore.dark_goldenrod}UPCA Stripped -> {upca_stripped}{Style.reset}""")
+
+                print(f"""
+{Fore.cyan}UPCA -> {upca}{Style.reset}
+{Fore.yellow}UPCE -> {scanned}{Style.reset}
+
+                """)
+
+        except Exception as e:
+            print(e)
+if __name__ == "__main__":
+    PossibleCodes(scanned=input("code"))
+
+
+def run():
+    while True:
+        code=input("code: ")
+        if code.lower() in ['q','quit']:
+            exit()
+        else:
+            PossibleCodes(scanned=code)
