27814f3bd760c2b39ed1ebad81d8f9ecf8e78cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:50 +0000

    Add package supriya/24.2b0/supriya-24.2b0-cp311-cp311-win_amd64.whl

---
+++ packages/supriya/supriya-24.2b0-cp311-cp311-win_amd64.whl/supriya/typing.py
@@ -0,0 +1,69 @@
+from os import PathLike
+from pathlib import Path
+from typing import (
+    TYPE_CHECKING,
+    Coroutine,
+    Dict,
+    Optional,
+    SupportsFloat,
+    SupportsInt,
+    Tuple,
+    Union,
+    runtime_checkable,
+)
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol  # type: ignore
+
+from .enums import AddAction, CalculationRate, HeaderFormat, SampleFormat
+
+if TYPE_CHECKING:
+    import numpy
+
+    from .osc import OscBundle, OscMessage
+
+
+class Default:
+    pass
+
+
+class Missing:
+    pass
+
+
+@runtime_checkable
+class SupportsOsc(Protocol):
+    def to_osc(self) -> Union["OscBundle", "OscMessage"]:
+        pass
+
+
+@runtime_checkable
+class SupportsPlot(Protocol):
+    def __plot__(self) -> "numpy.ndarray":
+        pass
+
+
+@runtime_checkable
+class SupportsRender(Protocol):
+    def __render__(
+        self,
+        output_file_path: Optional[PathLike] = None,
+        render_directory_path: Optional[PathLike] = None,
+        **kwargs,
+    ) -> Coroutine[None, None, Tuple[Optional[Path], int]]:
+        pass
+
+
+@runtime_checkable
+class SupportsRenderMemo(Protocol):
+    def __render_memo__(self) -> SupportsRender:
+        pass
+
+
+AddActionLike = Optional[Union[AddAction, SupportsInt, str]]
+CalculationRateLike = Optional[Union[CalculationRate, SupportsInt, str]]
+HeaderFormatLike = Optional[Union[HeaderFormat, SupportsInt, str]]
+SampleFormatLike = Optional[Union[SampleFormat, SupportsInt, str]]
+UGenInputMap = Optional[Dict[str, Union[SupportsFloat, str, None]]]
---
+++ packages/supriya/supriya-24.2b0-cp311-cp311-win_amd64.whl/supriya/ugens/safety.py
@@ -0,0 +1,45 @@
+from .bases import UGen, param, ugen
+
+
+@ugen(ar=True, kr=True)
+class CheckBadValues(UGen):
+    """
+    Tests for infinity, not-a-number, and denormals.
+
+    ::
+
+        >>> source = supriya.ugens.SinOsc.ar()
+        >>> ugen_id = 23
+        >>> post_mode = 0
+        >>> check_bad_values = supriya.ugens.CheckBadValues.ar(
+        ...     source=source,
+        ...     ugen_id=ugen_id,
+        ...     post_mode=post_mode,
+        ... )
+        >>> check_bad_values
+        CheckBadValues.ar()
+    """
+
+    source = param(None)
+    ugen_id = param(0)
+    post_mode = param(2)
+
+    def __init__(self, calculation_rate=None, ugen_id=0, post_mode=2, source=None):
+        assert int(post_mode) in (0, 1, 2)
+        UGen.__init__(
+            self,
+            calculation_rate=calculation_rate,
+            ugen_id=ugen_id,
+            post_mode=post_mode,
+            source=source,
+        )
+
+
+@ugen(ar=True, kr=True)
+class Sanitize(UGen):
+    """
+    Remove infinity, NaN, and denormals.
+    """
+
+    source = param(None)
+    replace = param(0.0)
