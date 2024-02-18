4f7cb8a6a7f5ce1dddcd009cf7d0a93aa0cbd5b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:48 +0000

    Add package smart-energy.data-vald3nir/1.0.4/smart_energy_data_vald3nir-1.0.4.tar.gz

---
+++ packages/smart-energy.data-vald3nir/smart_energy_data_vald3nir-1.0.4.tar.gz/smart_energy_data_vald3nir-1.0.4/keys/vald3flix_secrets.py
@@ -0,0 +1,23 @@
+FIREBASE_CERTIFICATE = {
+    "type": "service_account",
+    "project_id": "valdeflix-4f315",
+    "private_key_id": "64b78b52d37035fbe35b3f318d2986f9899b016d",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC+XUgSBPl/AfbY\n1xug8S+K7sSlIeVbpR29q4Qd9jt5JYGFlhuLmsEyRCM+nSG+hA/DhliJWunJDyM1\nA7F0To4NE+THohNigreisLoJLGHwt2+oRQghJOCHMu34zcZpSXyU59arqXBDLKKO\nn14ik3wAAXz4DeLcRgGTYys2XyRJj6FjUAgdRvczPiZVNYkrhJoDGHrFtdVrUe4Q\nSj7q8LMzyh70jUWIdYw2BIV5R/610oB5yQFafS+VHEOkYghUPL4/VT2eGvfGRoOw\nNWplHylMZLL4RUFRqmQlpd7I3SY800fNi1HLYKu4DCY22BmdNT/+EHezGbkCh6oU\nEWyVBFXZAgMBAAECggEAGNwYsdW5eVcZ0+Z/gqyMj2GwB3qeN7If6KmR5tHFoYP+\n3qGkNj5+9WAFlEMz6WvYnSmZvVJEjo+uAtnZHj21+ko7ScDGNEqqzZGzscf8gFGM\nQNbHAYEN20oQ3rmMrCo/HTkLhT5q3TLihaqVQgGto1iOZ9x0YFy6DvPX7FFPbVWd\nP3v2MFM1NCoElBE4h29VK59JbX9XXN4YcfqHmj+aSc3qyVAaAehshPrgWGj93lYa\nek6YmpngzHqX4rE6PgIBbGzYLbYKOWJEo2xHbhZlmCednW3PG872NQ86zGvJMwck\nC8OhkCHhBzEkqmKKAKIcBedEIsEAJyEyDC7hnvPBSQKBgQDnEVgcJQ8xIfHuB6n2\nXGwS4MIUP9fEb5bBpJnTU70I17dWIAwQ6kDhjwFmgQEHdLpUnOxMbR3CO5HSLEg7\n7HURwLEjBZku/QrEGxKeRXub2ySFVMb+4ZTkTru3q+FugKXIR9omW7DcPPUQHcFP\no9R4zS2497nwHdvpdhWVbTXqzQKBgQDS55xfHxOMty6OHgV533JdYO3fTFbLYycC\n10//SWUnRignXAIpHds3tEvP1GNQWgbX0DUNygwn3nUAI8qBGG56mpm8ga2AmKRV\nkcnMmy4COz0asC2WGPqQNzAH7Pjq2EyxpGvPNdqjrEKRPnUp/F2pn25XmHK07FkK\ngLtuaM7vPQKBgQCK9Q87vb2pGnTXZJx9n48Ou0N70fj8FO9gfJBVWiJwUunaTWN0\nOKvT+aAp+KKZzUTQ5Z3lSCmBSTmkXyUBhG3Sw0LDgPZiH6J91JoZvEddrygDVrxl\nQ1u7AO46wo7LWWpZS3yRXMq7Z50ol/xM3IVGMH2yoqebExnRCEZqmka2cQKBgQDC\nZIS3EDe+vaEFkRpPCOuqsgrOhNiDQvLUJY0wqn+3wWyFSsMbDs/d+ireds7LYw9n\noBsmiJLHtL120YZ3kwNRsmn9XDGFlCv0SxoLowS3Pc7GaJi7/m4EuzlkNMICBDg8\njGZqaSMLNV1t7nmjp17VV4Qm7PbdV/OCQMGBtMz3qQKBgQC2Z02PusBhtyoWiDe9\nmIvwk0zcY944D4zLAoiTfDStWPzxZ2ZFMbC+WYwwn9O/BTGtxWpMC/4uL1jZyubH\n2ysVw+m0XXfdtJfiW8Z+pyiTIkaCY8HBFA0nahdC2Il29J7ULXSXopF2vjW3xzvx\nNIQaUSqDUwefUBFwEBo8V/F7lg==\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-hff62@valdeflix-4f315.iam.gserviceaccount.com",
+    "client_id": "105046204906487227839",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hff62%40valdeflix-4f315.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
+FIREBASE_CONFIG = {
+    "apiKey": "AIzaSyADDWdUuDi0LjYwD4IWVMT7zoWVqF5WmLA",
+    "authDomain": "valdeflix-4f315.firebaseapp.com",
+    "databaseURL": "https://valdeflix-4f315-default-rtdb.firebaseio.com",
+    "projectId": "valdeflix-4f315",
+    "storageBucket": "valdeflix-4f315.appspot.com",
+    "messagingSenderId": "71291928299",
+    "appId": "1:71291928299:web:5433cd439c79c4bacc08ae"
+}
