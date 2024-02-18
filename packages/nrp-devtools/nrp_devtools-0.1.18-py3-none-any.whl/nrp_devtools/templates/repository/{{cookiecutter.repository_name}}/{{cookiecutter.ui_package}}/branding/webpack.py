c84520e7b74768dfa38d3dc3fe19ca9bacc46844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 15:04:01 +0000

    Add package nrp-devtools/0.1.18/nrp_devtools-0.1.18-py3-none-any.whl

---
+++ packages/nrp-devtools/nrp_devtools-0.1.18-py3-none-any.whl/nrp_devtools/templates/repository/{{cookiecutter.repository_name}}/{{cookiecutter.ui_package}}/branding/webpack.py
@@ -0,0 +1,22 @@
+from invenio_assets.webpack import WebpackThemeBundle
+
+theme = WebpackThemeBundle(
+    __name__,
+    ".",
+    default="semantic-ui",
+    themes={
+        "semantic-ui": {
+            "entry": {},
+            "dependencies": {
+                "react-searchkit": "^2.0.0",
+            },
+            "devDependencies": {},
+            "aliases": {
+                "../../theme.config$": "less/theme.config",
+                "../../less/site": "less/site",
+                "../../less": "less",
+                "@less": "less",
+            },
+        }
+    },
+)
