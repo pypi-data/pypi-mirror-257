687507130d870435e3c699bd6ccbb8ec7e3f2488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:37 +0000

    Add package gtsystem/0.0.8/gtsystem-0.0.8.tar.gz

---
+++ packages/gtsystem/gtsystem-0.0.8.tar.gz/gtsystem-0.0.8/gtsystem/ollama.py
@@ -0,0 +1,34 @@
+import ollama
+import base64
+import requests
+
+from .instrument import metrics
+
+@metrics.track
+def llama_text(system='', prompt='', temperature=0.0, topP=1.0, model='llama2'):
+    response = ollama.generate(model=model, prompt=prompt, system=system, 
+                               options={
+                                   "temperature": temperature,
+                                   "top_p": topP
+                               })
+    return response['response']
+
+@metrics.track
+def mistral_text(system='', prompt='', temperature=0.0, topP=1.0, model='mistral'):
+    response = ollama.generate(model=model, prompt=prompt, system=system, 
+                               options={
+                                   "temperature": temperature,
+                                   "top_p": topP
+                               })
+    return response['response']
+
+@metrics.track
+def codellama(system='', prompt='', temperature=0.0, topP=1.0, model='codellama'):
+    response = ollama.generate(model=model, prompt=prompt, system=system, 
+                               options={
+                                   "temperature": temperature,
+                                   "top_p": topP
+                               })
+    return response['response']
+
+
