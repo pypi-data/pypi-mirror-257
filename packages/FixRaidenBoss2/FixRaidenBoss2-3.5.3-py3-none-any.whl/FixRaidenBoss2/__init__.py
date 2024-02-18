b88bb447fee1ba408f4498bfba7d17b9a6f1ec7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:08 +0000

    Add package FixRaidenBoss2/3.5.3/FixRaidenBoss2-3.5.3-py3-none-any.whl

---
+++ packages/FixRaidenBoss2/FixRaidenBoss2-3.5.3-py3-none-any.whl/FixRaidenBoss2/__init__.py
@@ -0,0 +1,3 @@
+from .FixRaidenBoss2 import RaidenBossFixService, Mod, IniFile, FileService, Logger, RemapBlendModel, IfTemplate, Error, FileException, MissingFileException, DuplicateFileException, BlendFileNotRecognized, ConflictingOptions
+
+__all__ = ["RaidenBossFixService", "Mod", "IniFile", "FileService", "Logger", "RemapBlendModel", "IfTemplate", "Error", "FileException", "MissingFileException", "DuplicateFileException", "BlendFileNotRecognized", "ConflictingOptions"]
