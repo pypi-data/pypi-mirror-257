e7de9ca7909fb0cb7bfbe68dd164b808a650599f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:05:00 +0000

    Add package avro.py/2024.2.17/avro.py-2024.2.17-py3-none-any.whl

---
+++ packages/avro.py/avro.py-2024.2.17-py3-none-any.whl/avro/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+
+# Import local modules.
+from .main import *
+
+# Set version information.
+__version_info__ = ('2024', '2', '17')  # YYYY / MM / DD
+__version__ = '.'.join(__version_info__)
