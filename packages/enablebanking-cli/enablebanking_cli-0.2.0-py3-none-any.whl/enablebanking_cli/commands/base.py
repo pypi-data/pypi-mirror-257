9414f916d4594ac05646e255c1536ca1851b835e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:49 +0000

    Add package enablebanking-cli/0.2.0/enablebanking_cli-0.2.0-py3-none-any.whl

---
+++ packages/enablebanking-cli/enablebanking_cli-0.2.0-py3-none-any.whl/enablebanking_cli/commands/base.py
@@ -0,0 +1,10 @@
+class BaseCommand:
+    def handle(self, args):
+        command = getattr(args, self.subparsers.dest)
+        if command:
+            if not hasattr(self, command):
+                print(f"Command {command} is not implemented")
+                return 1
+            command_handler = getattr(self, command)
+            return command_handler(args)
+        self.parser.print_help()
