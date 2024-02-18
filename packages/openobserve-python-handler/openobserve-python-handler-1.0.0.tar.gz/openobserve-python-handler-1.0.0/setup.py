de612e78a6abaaf6d98712a527a1ddbcdd488592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:42 +0000

    Add package openobserve-python-handler/1.0.0/openobserve-python-handler-1.0.0.tar.gz

---
+++ packages/openobserve-python-handler/openobserve-python-handler-1.0.0.tar.gz/openobserve-python-handler-1.0.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="openobserve-python-handler",
+    version="1.0.0",
+    description="Logging handler to send logs to your OpenObserver service",
+    keywords="logging handler",
+    author="digithrone",
+    maintainer="digithrone",
+    mail="info@digithrone.com",
+    url="https://github.com/digithrone/openobserve-python-handler",
+    license="Apache License 2",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.27.0",
+        "protobuf>=3.20.2",
+        "opentelemetry-instrumentation-logging==0.39b0",
+    ],
+    extras_require={},
+    test_requires=["future"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
