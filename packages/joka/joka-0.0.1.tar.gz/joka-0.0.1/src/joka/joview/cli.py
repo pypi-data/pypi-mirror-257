bd6c3570294125eed7873d7cbb27612e06ec9e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:35 +0000

    Add package joka/0.0.1/joka-0.0.1.tar.gz

---
+++ packages/joka/joka-0.0.1.tar.gz/joka-0.0.1/src/joka/joview/cli.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+
+"""
+The command line interface for the viewer component.
+Multiple people should be able to view the data concurrently while one person should be able to
+control the data.
+"""
+
+def main():
+    print("Hello, World!")
