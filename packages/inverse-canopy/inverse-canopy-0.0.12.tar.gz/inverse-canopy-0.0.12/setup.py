904e6fe02bbbfb098c13679b066d705de79dbe9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:46 +0000

    Add package inverse-canopy/0.0.12/inverse-canopy-0.0.12.tar.gz

---
+++ packages/inverse-canopy/inverse-canopy-0.0.12.tar.gz/inverse-canopy-0.0.12/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    README = f.read()
+
+setup(
+    name='inverse-canopy',
+    url='https://openpra.org',
+    version='0.0.12',
+    author='Arjun Earthperson',
+    author_email='arjun@openpra.org',
+    license='AGPL-3.0',
+    description='Tensorflow based library for back-fitting event tree conditional/functional event probability '
+                'distributions to match target end-state frequencies.',
+    long_description_content_type='text/markdown',
+    long_description=README,
+    install_requires=[
+        'tensorflow==2.15.0',
+        'tensorflow-probability==0.23.0',
+        'numpy',
+        'scipy',
+        'matplotlib'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3.8',
+    ],
+    packages=['inverse_canopy'],
+    python_requires='>=3.6',
+    entry_points={
+    },
+)
