cf37d5bb0128d370a51455159a1ccceae9636e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:56 +0000

    Add package gitlabform/3.9.0/gitlabform-3.9.0.tar.gz

---
+++ packages/gitlabform/gitlabform-3.9.0.tar.gz/gitlabform-3.9.0/gitlabform/processors/project/hooks_processor.py
@@ -0,0 +1,66 @@
+from logging import debug
+from typing import Dict, Any, List
+
+from gitlab.base import RESTObject, RESTObjectList
+from gitlab.v4.objects import Project
+
+from gitlabform.gitlab import GitLab
+from gitlabform.processors.abstract_processor import AbstractProcessor
+
+
+class HooksProcessor(AbstractProcessor):
+    def __init__(self, gitlab: GitLab):
+        super().__init__("hooks", gitlab)
+
+    def _process_configuration(self, project_and_group: str, configuration: dict):
+        debug("Processing hooks...")
+        project: Project = self.gl.projects.get(project_and_group)
+        hooks_list: RESTObjectList | List[RESTObject] = project.hooks.list()
+        config_hooks: tuple[str, ...] = tuple(
+            x for x in sorted(configuration["hooks"]) if x != "enforce"
+        )
+
+        for hook in config_hooks:
+            gitlab_hook: RESTObject | None = next(
+                (h for h in hooks_list if h.url == hook), None
+            )
+            hook_id = gitlab_hook.id if gitlab_hook else None
+            if configuration.get("hooks|" + hook + "|delete"):
+                if hook_id:
+                    debug("Deleting hook '%s'", hook)
+                    project.hooks.delete(hook_id)
+                else:
+                    debug("Not deleting hook '%s', because it doesn't exist", hook)
+            else:
+                hook_config = {"url": hook}
+                hook_config.update(configuration["hooks"][hook])
+                gl_hook_dict = gitlab_hook.asdict() if gitlab_hook else {}
+                diffs = (
+                    map(
+                        lambda k: hook_config[k] != gl_hook_dict[k],
+                        hook_config.keys(),
+                    )
+                    if gl_hook_dict
+                    else iter(())
+                )
+                if not hook_id:
+                    debug("Creating hook '%s'", hook)
+                    created_hook: RESTObject = project.hooks.create(hook_config)
+                    debug("Created hook '%s'", created_hook)
+                elif hook_id and any(diffs):
+                    debug("Changing existing hook '%s'", hook)
+                    changed_hook: Dict[str, Any] = project.hooks.update(
+                        hook_id, hook_config
+                    )
+                    debug("Changed hook to '%s'", changed_hook)
+                elif hook_id and not any(diffs):
+                    debug(f"Hook {hook} remains unchanged")
+
+        if configuration.get("hooks|enforce"):
+            for gh in hooks_list:
+                if gh.url not in config_hooks:
+                    debug(
+                        "Deleting hook '%s' currently setup in the project but it is not in the configuration and enforce is enabled",
+                        gh.url,
+                    )
+                    project.hooks.delete(gh.id)
---
+++ packages/gitlabform/gitlabform-3.9.0.tar.gz/gitlabform-3.9.0/tests/acceptance/standard/test_hooks.py
@@ -0,0 +1,188 @@
+import logging
+import pytest
+from typing import TYPE_CHECKING
+
+from tests.acceptance import run_gitlabform, get_random_name
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="class")
+def urls():
+    first_name = get_random_name("hook")
+    second_name = get_random_name("hook")
+    first_url = f"http://hooks/{first_name}.org"
+    second_url = f"http://hooks/{second_name}.org"
+    return first_url, second_url
+
+
+class TestHooksProcessor:
+    def get_hook_from_url(self, project, url):
+        return next(h for h in project.hooks.list() if h.url == url)
+
+    def test_hooks_create(self, gl, project, urls):
+        target = project.path_with_namespace
+        first_url, second_url = urls
+
+        test_yaml = f"""
+            projects_and_groups:
+              {target}:
+                hooks:
+                  {first_url}:
+                    push_events: false
+                    merge_requests_events: true
+                  {second_url}:
+                    job_events: true
+                    note_events: true
+            """
+
+        run_gitlabform(test_yaml, target)
+
+        first_created_hook = self.get_hook_from_url(project, first_url)
+        second_created_hook = self.get_hook_from_url(project, second_url)
+        if TYPE_CHECKING:
+            assert isinstance(first_created_hook, ProjectHook)
+            assert isinstance(second_created_hook, ProjectHook)
+        assert len(project.hooks.list()) == 2
+        assert (
+            first_created_hook.push_events,
+            first_created_hook.merge_requests_events,
+        ) == (False, True)
+        assert (second_created_hook.job_events, second_created_hook.note_events) == (
+            True,
+            True,
+        )
+
+    def test_hooks_update(self, caplog, gl, project, urls):
+        first_url, second_url = urls
+        target = project.path_with_namespace
+        first_hook = self.get_hook_from_url(project, first_url)
+        second_hook = self.get_hook_from_url(project, second_url)
+
+        update_yaml = f"""
+            projects_and_groups:
+              {target}:
+                hooks:
+                  {first_url}:
+                    id: {first_hook.id}
+                    merge_requests_events: false
+                    note_events: true
+                  {second_url}:
+                    job_events: true
+                    note_events: true
+            """
+
+        run_gitlabform(update_yaml, target)
+        updated_first_hook = self.get_hook_from_url(project, first_url)
+        updated_second_hook = self.get_hook_from_url(project, second_url)
+
+        with caplog.at_level(logging.DEBUG):
+            assert f"Hook {second_url} remains unchanged" in caplog.text
+            assert f"Changing existing hook '{first_url}'" in caplog.text
+        assert updated_first_hook.asdict() != first_hook.asdict()
+        assert updated_second_hook.asdict() == second_hook.asdict()
+        # push events defaults to True, but it stays False
+        assert updated_first_hook.push_events == False
+        assert updated_first_hook.merge_requests_events == False
+        assert updated_first_hook.note_events == True
+
+    def test_hooks_delete(self, gl, project, urls):
+        target = project.path_with_namespace
+        first_url, second_url = urls
+        orig_second_hook = self.get_hook_from_url(project, second_url)
+
+        delete_yaml = f"""
+        projects_and_groups:
+          {target}:
+            hooks:
+              {first_url}:
+                delete: true
+              {second_url}:
+                job_events: false
+                note_events: false
+        """
+
+        run_gitlabform(delete_yaml, target)
+        hooks = project.hooks.list()
+        second_hook = self.get_hook_from_url(project, second_url)
+
+        assert len(hooks) == 1
+        assert first_url not in (h.url for h in hooks)
+        assert second_hook in hooks
+        assert second_hook == orig_second_hook
+
+    def test_hooks_enforce(self, gl, group, project, urls):
+        target = project.path_with_namespace
+        first_url, second_url = urls
+        hooks_before_test = [h.url for h in project.hooks.list()]
+        assert len(hooks_before_test) == 1
+        assert second_url == hooks_before_test[0]
+
+        enforce_yaml = f"""
+                projects_and_groups:
+                  {target}:
+                    hooks:
+                      enforce: true
+                      {first_url}:
+                        merge_requests_events: false
+                        note_events: true
+                """
+
+        run_gitlabform(enforce_yaml, target)
+        hooks_after_test = [h.url for h in project.hooks.list()]
+        assert len(hooks_after_test) == 1
+        assert first_url in hooks_after_test
+        assert second_url not in hooks_after_test
+
+        not_enforce_yaml = f"""
+                projects_and_groups:
+                  {target}:
+                    hooks:
+                      enforce: false
+                      http://www.newhook.org:
+                        merge_requests_events: false
+                        note_events: true
+                """
+
+        run_gitlabform(not_enforce_yaml, target)
+        hooks_after_test = [h.url for h in project.hooks.list()]
+        assert len(hooks_after_test) == 2
+        assert (
+            first_url in hooks_after_test
+            and "http://www.newhook.org" in hooks_after_test
+        )
+
+        parent_target = f"{group.path}/*"
+        enforce_star_yaml = f"""
+                projects_and_groups:
+                  {parent_target}:
+                    hooks:
+                      enforce: true
+                      {first_url}:
+                        push_events: true
+                  {target}:
+                    hooks:
+                      {second_url}:
+                        job_events: true
+                  """
+
+        run_gitlabform(enforce_star_yaml, target)
+        hooks_after_test = [h.url for h in project.hooks.list()]
+
+        assert len(hooks_after_test) == 2
+        assert first_url in hooks_after_test and second_url in hooks_after_test
+        assert "http://www.newhook.org" not in hooks_after_test
+
+        enforce_delete_yaml = f"""
+                projects_and_groups:
+                  {target}:
+                    hooks:
+                      enforce: true
+                      {first_url}:
+                        delete: true
+                """
+
+        run_gitlabform(enforce_delete_yaml, target)
+        hooks_after_test = [h.url for h in project.hooks.list()]
+        assert len(hooks_after_test) == 0
