81e3fbec936627b2691fa20de4b467f086da4366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:10 +0000

    Add package gpnam/0.0.16/gpnam-0.0.16.tar.gz

---
+++ packages/gpnam/gpnam-0.0.16.tar.gz/gpnam-0.0.16/gpnam/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.16"
+__author__ = 'Wei Zhang'
+
+
+from . import utils
+from . import data
+from . import trainer
+from . import model
+from . import download_datasets
+from . import sklearn
