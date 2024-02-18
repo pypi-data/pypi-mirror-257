446b9620164b277dcea4915eb58a9a378396a2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:56 +0000

    Add package open-interpreter/0.2.1a0/open_interpreter-0.2.1a0.tar.gz

---
+++ packages/open-interpreter/open_interpreter-0.2.1a0.tar.gz/open_interpreter-0.2.1a0/interpreter/core/computer/skills/skills.py
@@ -0,0 +1,24 @@
+import glob
+import os
+
+from ...utils.lazy_import import lazy_import
+
+# Lazy import of aifs, imported when needed to speed up start time
+aifs = lazy_import('aifs')
+
+
+class Skills:
+    def __init__(self, computer):
+        self.computer = computer
+        self.skills_dir = None
+
+    def search(self, query):
+        result = aifs.search(self.skills_dir, query, python_docstrings_only=True)
+        return result
+
+    def import_skills(self):
+        self.computer.save_skills = False
+        for file in glob.glob(os.path.join(self.skills_dir, "*.py")):
+            with open(file, "r") as f:
+                self.computer.run("python", f.read())
+        self.computer.save_skills = True
