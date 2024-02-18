731d16b5d219957ac924fd1d73c4819451dd3ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:43 +0000

    Add package django-query-limiter/0.0.1/django_query_limiter-0.0.1-py3-none-any.whl

---
+++ packages/django-query-limiter/django_query_limiter-0.0.1-py3-none-any.whl/query_limiter/limiter.py
@@ -0,0 +1,44 @@
+from contextlib import ExitStack, contextmanager
+from dataclasses import dataclass
+from django.db import connections
+
+from .exceptions import QueryLimitExceeded
+
+DISABLED = False
+
+
+@dataclass
+class QueryLimiter:
+    limit: int
+    queries: list[str] = None
+
+    def __call__(self, execute, sql, params, many, context):
+        if not self.queries:
+            self.queries = []
+        self.queries.append(sql)
+
+        if len(self.queries) > self.limit:
+            raise QueryLimitExceeded(self.limit, self.queries)
+        return execute(sql, params, many, context)
+
+
+@contextmanager
+def limit_queries(limit: int, db_connections: list[str] | None = None):
+    if DISABLED:
+        yield
+        return
+
+    db_connections = [connections[connection_name] for connection_name in (db_connections or list(connections))]
+    limiter = QueryLimiter(limit=limit)
+
+    managers = [connection.execute_wrapper(limiter) for connection in db_connections]
+
+    with ExitStack() as stack:
+        for manager in managers:
+            stack.enter_context(manager)
+        yield
+
+
+def disable_query_limiter():
+    global DISABLED
+    DISABLED = True
