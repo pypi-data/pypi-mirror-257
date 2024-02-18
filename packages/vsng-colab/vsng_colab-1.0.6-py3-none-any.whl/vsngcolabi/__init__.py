9d2f50df53906c5190304114cefbf33249213342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:59 +0000

    Add package vsng-colab/1.0.6/vsng_colab-1.0.6-py3-none-any.whl

---
+++ packages/vsng-colab/vsng_colab-1.0.6-py3-none-any.whl/vsngcolabi/__init__.py
@@ -0,0 +1,26 @@
+from bashi import bash , printerror , printok
+from pyngrok import ngrok
+from urllib.parse import urlencode, urljoin
+from google.colab import userdata
+import threading , collections
+
+
+def configure(install = True, port  = 5000 ,folder = "/content" ,kill = False ) : 
+    install_vscode = bash("""
+    curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
+    tar -xf vscode_cli.tar.gz
+    rm vscode_cli.tar.gz
+    mv code /usr/bin/
+    """  )
+    if not install_vscode.ok :
+        printerror(install_vscode.stderr)
+    if kill : ngrok.kill()
+    ngrok.set_auth_token(userdata.get('NGROK_TOKEN'))
+    http_tunnel = ngrok.connect(port, "http")
+    url = urljoin(http_tunnel.public_url, '?' + urlencode(dict(folder = folder)) )
+    server = threading.Thread(target = bash ,args = (f"""
+    code  serve-web --without-connection-token --accept-server-license-terms --port {port}
+    """,))
+    server.start()
+    printok( url)
+    return collections.namedtuple('vsngcolabi', ['url', 'server'])(url = url ,server = server )
