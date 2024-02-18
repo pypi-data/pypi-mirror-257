021fd24e43517559b5fa7f22416eb6b2e744847e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:41 +0000

    Add package keras-nlp-nightly/0.8.0.dev2024021803/keras-nlp-nightly-0.8.0.dev2024021803.tar.gz

---
+++ packages/keras-nlp-nightly/keras-nlp-nightly-0.8.0.dev2024021803.tar.gz/keras-nlp-nightly-0.8.0.dev2024021803/keras_nlp/src/samplers/greedy_sampler.py
@@ -0,0 +1,64 @@
+# Copyright 2023 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from keras_nlp.src.api_export import keras_nlp_export
+from keras_nlp.src.backend import ops
+from keras_nlp.src.samplers.sampler import Sampler
+from keras_nlp.src.samplers.sampler import call_args_docstring
+from keras_nlp.src.utils.python_utils import format_docstring
+
+
+@format_docstring(call_args=call_args_docstring)
+@keras_nlp_export("keras_nlp.samplers.GreedySampler")
+class GreedySampler(Sampler):
+    """Greedy sampler class.
+
+    This sampler is implemented on greedy search, i.e., always picking up the
+    token of the largest probability as the next token.
+
+    Call arguments:
+        {{call_args}}
+
+    Examples:
+    ```python
+    # Use a simple alphabet of lowercase characters with ids in range [0, 25].
+    int_lookup = {i: chr(i + ord('a')) for i in range(26)}
+    char_lookup = {v: k for k, v in int_lookup.items()}
+    batch_size, length, vocab_size = 1, 12, len(int_lookup)
+
+    def next(prompt, cache, index):
+        hidden_states = np.ones((batch_size, 10))
+        # A uniform distribution over our alphabet.
+        logits = np.ones((batch_size, vocab_size))
+        return logits, hidden_states, cache
+
+    output = keras_nlp.samplers.GreedySampler()(
+        next=next,
+        prompt=np.full((batch_size, length,), char_lookup['z'], dtype="int32"),
+        index=5,
+    )
+    print(["".join([int_lookup[i] for i in s]) for s in output.numpy()])
+    # >>> ['zzzzzaaaaaaa']
+    ```
+    """
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+    def get_next_token(self, probabilities):
+        return ops.argmax(probabilities, axis=-1)
+
