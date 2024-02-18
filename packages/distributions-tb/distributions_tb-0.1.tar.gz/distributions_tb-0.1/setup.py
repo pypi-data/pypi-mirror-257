98050c5e415e4b5850d2ae7b4cd9fcfef8c605f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:06 +0000

    Add package distributions-tb/0.1/distributions_tb-0.1.tar.gz

---
+++ packages/distributions-tb/distributions_tb-0.1.tar.gz/distributions_tb-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_tb',
+      version='0.1',
+      description='Different distributions',
+      packages=['distributions_tb'],
+      author='Thomas Breuer',
+      zip_safe=False)
