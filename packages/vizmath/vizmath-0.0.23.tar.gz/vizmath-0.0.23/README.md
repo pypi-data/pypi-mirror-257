5b959d5141825c1e8d36d8521e12be13a00a498b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:04:52 +0000

    Add package vizmath/0.0.23/vizmath-0.0.23.tar.gz

---
+++ packages/vizmath/vizmath-0.0.23.tar.gz/vizmath-0.0.23/README.md
@@ -0,0 +1,7 @@
+# Welcome to vizmath
+
+## Setup
+pip install vizmath
+
+## Usage
+coming soon, check out https://medium.com/@nickgerend for tutorials (including Tableau Public tips!)
---
+++ packages/vizmath/vizmath-0.0.23.tar.gz/vizmath-0.0.23/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+import setuptools as st
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+setup(
+    name="vizmath",
+    version="0.0.23",
+    description="Visualization math toolkit.",
+    long_description="Welcome to vizmath! Please visit [GitHub](https://github.com/nickgerend/vizmath/blob/main/README.md)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/nickgerend/vizmath",
+    author="Nick Gerend",
+    author_email="nickgerend@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=st.find_namespace_packages(),
+    install_requires=["numpy", "scipy", "matplotlib", "pandas"],
+    include_package_data=True,
+    package_data={'': ['data/*.csv']},
+)
