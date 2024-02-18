4592f332ac2ebad3a3a485d0ba244bc8c09eb34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:03:20 +0000

    Add package FruceAPI/1.2.0/FruceAPI-1.2.0-py3-none-any.whl

---
+++ packages/FruceAPI/FruceAPI-1.2.0-py3-none-any.whl/FruceAPI/exceptions/__init__.py
@@ -0,0 +1,11 @@
+class UnauthorizedError(Exception):
+    pass
+
+class NoPermissionError(Exception):
+    pass
+
+class InvalidRequestError(Exception):
+    pass
+
+class MusicNotFoundError(Exception):
+    pass
