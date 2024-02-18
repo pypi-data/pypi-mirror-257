399c20102d8851338e26d402d51c0ac25cd3d77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:31 +0000

    Add package systems-manager/0.30.2/systems_manager-0.30.2-py2.py3-none-any.whl

---
+++ packages/systems-manager/systems_manager-0.30.2-py2.py3-none-any.whl/systems_manager/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+from systems_manager.version import __version__, __author__, __credits__
+from systems_manager.systems_manager import SystemsManager, main, systems_manager
+"""
+system-manager
+
+Install/Update/Clean/Manage your System!
+"""
+
+__version__ = __version__
+__author__ = __author__
+__credits__ = __credits__
