e5c097418b36be10c05853629de3f27989da0435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 03:04:36 +0000

    Add package indexify/0.0.9/indexify-0.0.9.tar.gz

---
+++ packages/indexify/indexify-0.0.9.tar.gz/indexify-0.0.9/indexify/__init__.py
@@ -0,0 +1,11 @@
+from .index import Index
+from .client import IndexifyClient
+from .extraction_policy import ExtractionPolicy
+from .settings import DEFAULT_SERVICE_URL
+
+__all__ = [
+    "Index",
+    "IndexifyClient",
+    "ExtractionPolicy",
+    "DEFAULT_SERVICE_URL",
+]
