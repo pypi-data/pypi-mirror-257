ca3339f12f49ef202fa8fecf839a8746f8765ac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:31 +0000

    Add package llm-templates/0.1.5/llm_templates-0.1.5-py3-none-any.whl

---
+++ packages/llm-templates/llm_templates-0.1.5-py3-none-any.whl/llm_templates/common.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from jinja2 import Environment
+
+
+class Content(BaseModel):
+    role: str
+    content: str
+
+
+# simplified version of openai.types.chat completion_create_params.py
+class Conversation(BaseModel):
+    model: Optional[str]
+    messages: List[Content]
+
+
+# Function to raise exception from Jinja2 templates
+def raise_exception(message):
+    raise Exception(message)
+
+
+def get_jinja_env(**kwargs):
+    jinja_env = Environment()
+    jinja_env.globals['raise_exception'] = raise_exception
+    jinja_env.globals.update(kwargs)
+    return jinja_env
+
