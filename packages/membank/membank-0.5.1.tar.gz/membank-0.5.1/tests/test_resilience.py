1f262f6125b3e90e851ae2c32ac133eb9782bd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:34 +0000

    Add package membank/0.5.1/membank-0.5.1.tar.gz

---
+++ packages/membank/membank-0.5.1.tar.gz/membank-0.5.1/tests/test_resilience.py
@@ -0,0 +1,24 @@
+"""Resilience tests."""
+from tests import base as b
+from tests.test_interface import Perforator
+
+import sqlalchemy as sa
+
+
+@b.add_memory()
+class CleanData(b.TestCase):
+    """Testcase that cleans __meta_dataclasses__ table while membank has been loaded."""
+
+    def test_delete_meta(self):
+        """Clean __meta_dataclasses__ table."""
+        p = Perforator("test")
+        self.memory.put(p)
+        self.assertTrue(self.memory.get.perforator(name="test"))
+        engine = self.memory._get_engine()
+        with engine.connect() as conn:
+            conn.execute(sa.text("DELETE FROM __meta_dataclasses__"))
+            conn.commit()
+        self.assertIsNone(self.memory.get.perforator(name="test"))
+        self.memory.put(Perforator("some other perforator"))
+        self.assertTrue(self.memory.get.perforator(name="some other perforator"))
+        self.assertEqual(p, self.memory.get.perforator(name="test"))
