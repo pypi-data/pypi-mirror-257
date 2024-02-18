45d23ad35f68df8b889e19eca7caffb85004e1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 00:04:42 +0000

    Add package karaml/0.1.4/karaml-0.1.4.tar.gz

---
+++ packages/karaml/karaml-0.1.4.tar.gz/karaml-0.1.4/karaml_tools/__init__.py
@@ -0,0 +1,4 @@
+
+#print("Running __init__.py in karaml_tools package")
+#print("Import everything from karaml_tools.py")
+from .karaml_tools import *
---
+++ packages/karaml/karaml-0.1.4.tar.gz/karaml-0.1.4/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+setup(
+    name='karaml',
+    version='0.1.4',
+    description='KARaML Tools wrapper package',
+    long_description='Provides karaml_tools, a small package with the bare bones to get KARaML Tools running',
+    url='https://bb-ai.net/KARaML/KARaML_Tools.html',
+    author='brandonb',
+    author_email='B.Bennett@leeds.ac.uk',
+    packages=['karaml'],
+    classifiers=['Development Status :: 1 - Planning'],
+)
