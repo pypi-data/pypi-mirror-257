73faaf4f171dc3daec03c4cbb8deea9a427d21d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-18 14:03:14 +0000

    Add package apsimNGpy/0.0.25/apsimNGpy-0.0.25.tar.gz

---
+++ packages/apsimNGpy/apsimNGpy-0.0.25.tar.gz/apsimNGpy-0.0.25/apsimNGpy/core/__init__.py
@@ -0,0 +1,5 @@
+from apsimNGpy.core.pythonet_config import LoadPythonnet, APSIM_PATH
+from apsimNGpy.core.apsim import ApsimModel
+import apsimNGpy
+from apsimNGpy.core.base_data import DetectApsimExamples, LoadExampleFiles
+__all__ = [LoadPythonnet, APSIM_PATH, ApsimModel, DetectApsimExamples, LoadExampleFiles, apsimNGpy]
