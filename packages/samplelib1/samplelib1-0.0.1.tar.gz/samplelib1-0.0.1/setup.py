40bd44460e550f081a2e5138aed000e48eb45718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:03:48 +0000

    Add package samplelib1/0.0.1/samplelib1-0.0.1.tar.gz

---
+++ packages/samplelib1/samplelib1-0.0.1.tar.gz/samplelib1-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='samplelib1',
+    version='0.0.1',
+    packages=['samplelib1'],
+    install_requires=[
+        # List your dependencies here if any
+    ],
+)
