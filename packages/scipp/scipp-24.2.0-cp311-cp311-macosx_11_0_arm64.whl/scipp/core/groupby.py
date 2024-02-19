b1564a702b3c933f59795574f572779a9c9d6fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:31 +0000

    Add package scipp/24.2.0/scipp-24.2.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/scipp/scipp-24.2.0-cp311-cp311-macosx_11_0_arm64.whl/scipp/core/groupby.py
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @author Matthew Andrew
+
+from typing import Optional, Union
+
+from .._scipp import core as _cpp
+
+
+def groupby(
+    data: Union[_cpp.DataArray, _cpp.Dataset],
+    /,
+    group: Union[_cpp.Variable, str],
+    *,
+    bins: Optional[_cpp.Variable] = None,
+) -> Union[_cpp.GroupByDataArray, _cpp.GroupByDataset]:
+    """Group dataset or data array based on values of specified labels.
+
+    Parameters
+    ----------
+    data:
+        Input data to reduce.
+    group:
+        Name of labels to use for grouping or Variable to use for grouping
+    bins:
+        Optional bins for grouping label values.
+
+    Returns
+    -------
+    :
+        GroupBy helper object.
+    """
+    if bins is None:
+        return _cpp.groupby(data, group)
+    else:
+        return _cpp.groupby(data, group, bins)
