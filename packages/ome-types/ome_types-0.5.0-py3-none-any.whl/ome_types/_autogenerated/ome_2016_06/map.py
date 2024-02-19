e671808c34a6da6ce1c7d8c279df45e25a669595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:04:00 +0000

    Add package ome-types/0.5.0/ome_types-0.5.0-py3-none-any.whl

---
+++ packages/ome-types/ome_types-0.5.0-py3-none-any.whl/ome_types/_autogenerated/ome_2016_06/map.py
@@ -0,0 +1,46 @@
+from typing import List, Optional
+
+from ome_types._mixins._base_type import OMEType
+from xsdata_pydantic_basemodel.pydantic_compat import Field
+
+__NAMESPACE__ = "http://www.openmicroscopy.org/Schemas/OME/2016-06"
+
+
+class Map(OMEType):
+    """
+    This is a Mapping of key/value pairs.
+
+    Attributes
+    ----------
+    m : list["Map.M"]
+        This is a key/value pair used to build up a Mapping. The Element and
+        Attribute name are kept to single letters to minimize the length at the
+        expense of readability as they are likely to occur many times.
+    """
+
+    ms: List["Map.M"] = Field(
+        default_factory=list,
+        metadata={
+            "name": "M",
+            "type": "Element",
+            "namespace": "http://www.openmicroscopy.org/Schemas/OME/2016-06",
+        },
+    )
+
+    class M(OMEType):
+        value: str = Field(
+            default="",
+            metadata={
+                "required": True,
+            },
+        )
+        k: Optional[str] = Field(
+            default=None,
+            metadata={
+                "name": "K",
+                "type": "Attribute",
+            },
+        )
+
+
+M = Map.M
