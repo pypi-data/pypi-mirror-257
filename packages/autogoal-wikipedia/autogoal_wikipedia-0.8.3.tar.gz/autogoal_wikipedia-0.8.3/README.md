0542422d0aa7dc22972a127b4c85f287ded96467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:41 +0000

    Add package autogoal-wikipedia/0.8.3/autogoal_wikipedia-0.8.3.tar.gz

---
+++ packages/autogoal-wikipedia/autogoal_wikipedia-0.8.3.tar.gz/autogoal_wikipedia-0.8.3/README.md
@@ -0,0 +1,1 @@
+# AutoGOAL WIKIPEDIA Algorithm Library
---
+++ packages/autogoal-wikipedia/autogoal_wikipedia-0.8.3.tar.gz/autogoal_wikipedia-0.8.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['autogoal_wikipedia']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['autogoal-contrib>=0.8.2,<0.9.0', 'autogoal==1.0.2', 'wikipedia>=1.4.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'autogoal-wikipedia',
+    'version': '0.8.3',
+    'description': 'wikipedia algorithm library wrapper for AutoGOAL',
+    'long_description': '# AutoGOAL WIKIPEDIA Algorithm Library',
+    'author': 'Suilan Estevez-Velarde',
+    'author_email': 'suilanestevez@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://autogoal.github.io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.13,<3.11',
+}
+
+
+setup(**setup_kwargs)
