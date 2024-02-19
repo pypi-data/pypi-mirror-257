999a71ad1a1249f827033ae28453417262877af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:56 +0000

    Add package dral/0.5.2/dral-0.5.2-py3-none-any.whl

---
+++ packages/dral/dral-0.5.2-py3-none-any.whl/dral/format/base.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from ..generator import DralOutputFile
+
+
+class BaseFormat(ABC):
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def _make_default(self, objects: List[DralOutputFile], model: Optional[DralOutputFile]) -> None:
+        pass
+
+    def make(self, objects: List[DralOutputFile], model: Optional[DralOutputFile] = None) -> None:
+        self._make_default(objects, model=model)
---
+++ packages/dral/dral-0.5.2-py3-none-any.whl/dral/utils.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import glob
+from importlib import resources
+from pathlib import Path
+from typing import Optional, Tuple, Union
+
+
+class Utils:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def get_template(template: Union[str, Path], name: str) -> Path:
+        if isinstance(template, Path):
+            return template / name
+        else:
+            with resources.path(f"dral.templates.{template}", name) as item:
+                return Path(item)
+
+    @staticmethod
+    def get_template_dir(language: str, name: str) -> Path:
+        templates_path = Path(__file__).parent / "templates" / language
+        return templates_path / name
+
+    @staticmethod
+    def get_svd_file(device: str) -> Optional[Path]:
+        devices_path = Path(__file__).parent / "devices"
+        svd = glob.glob(f"{devices_path}/**/{device}.svd", recursive=True)
+        if svd:
+            return devices_path / Path(svd[0])
+        return None
+
+    @staticmethod
+    def get_device_info(svd: Path) -> Tuple[str, str, str]:
+        svd = svd.resolve()
+        return svd.stem, "", ""
+
+    @staticmethod
+    def get_model_template_dir(language: str) -> Path:
+        model_path = Path(__file__).parent / "templates" / "model" / language
+        return model_path
+
+    @staticmethod
+    def get_forbidden_words(language: str) -> Optional[Path]:
+        forbidden_words_path = Path(__file__).parent / "templates" / language / "forbidden.yaml"
+        if forbidden_words_path.exists():
+            return forbidden_words_path
+        return None
