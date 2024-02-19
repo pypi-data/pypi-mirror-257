0714d3b07199c205ecca4829e559da1eb1cc7616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:06:44 +0000

    Add package cs-models/0.0.645/cs-models-0.0.645.tar.gz

---
+++ packages/cs-models/cs-models-0.0.645.tar.gz/cs-models-0.0.645/src/cs_models/resources/DocChunk/schemas.py
@@ -0,0 +1,19 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class DocChunkResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    source_type = fields.String(required=True)
+    source_id = fields.Integer(required=True)
+    chunk_cui = fields.String(required=True)
+    chunk_text = fields.String(required=True)
+    chunk_embedding = fields.String(required=True)
+    embedding_source = fields.String(required=True)
+    is_deleted = fields.Boolean(allow_none=True)
+    updated_at = fields.DateTime(dump_only=True)
