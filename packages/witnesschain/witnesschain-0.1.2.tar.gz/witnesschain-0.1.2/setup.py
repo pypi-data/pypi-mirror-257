b1e657acb093ad8b5851504799fb3b4e3594d9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:27 +0000

    Add package witnesschain/0.1.2/witnesschain-0.1.2.tar.gz

---
+++ packages/witnesschain/witnesschain-0.1.2.tar.gz/witnesschain-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='witnesschain',
+version='0.1.2',
+author='arun',
+author_email='x@example.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
