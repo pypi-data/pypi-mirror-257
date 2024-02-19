735d203367c85926f4a0ef02923f5ddc32b13234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:04:03 +0000

    Add package litexcnc/1.2.4/litexcnc-1.2.4-py3-none-any.whl

---
+++ packages/litexcnc/litexcnc-1.2.4-py3-none-any.whl/litexcnc/driver/watchdog.c
@@ -0,0 +1,141 @@
+/********************************************************************
+* Description:  watchdog.c
+*               A Litex-CNC component that generates will bite when
+*               it doesn't get petted on time.
+*
+* Author: Peter van Tol <petertgvantol AT gmail DOT com>
+* License: GPL Version 2
+*    
+* Copyright (c) 2022 All rights reserved.
+*
+********************************************************************/
+
+/** This program is free software; you can redistribute it and/or
+    modify it under the terms of version 2 of the GNU General
+    Public License as published by the Free Software Foundation.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+    THE AUTHORS OF THIS LIBRARY ACCEPT ABSOLUTELY NO LIABILITY FOR
+    ANY HARM OR LOSS RESULTING FROM ITS USE.  IT IS _EXTREMELY_ UNWISE
+    TO RELY ON SOFTWARE ALONE FOR SAFETY.  Any machinery capable of
+    harming persons must have provisions for completely removing power
+    from all motors, etc, before persons enter any danger area.  All
+    machinery must be designed to comply with local and national safety
+    codes, and the authors of this software can not, and do not, take
+    any responsibility for such compliance.
+
+    This code was written as part of the LiteX-CNC project.
+*/
+#include <inttypes.h>
+#include <stdio.h>
+
+#include "rtapi.h"
+#include "rtapi_app.h"
+#include "litexcnc.h"
+
+#include "watchdog.h"
+
+int litexcnc_watchdog_init(litexcnc_t *litexcnc) {
+    
+    // Declarations
+    int r = 0;
+    char name[HAL_NAME_LEN + 1];
+
+    // Allocate memory
+    litexcnc->watchdog = (litexcnc_watchdog_t *)hal_malloc(sizeof(litexcnc_watchdog_t));
+
+    // Create pins
+    // - has_bitten
+    rtapi_snprintf(name, sizeof(name), "%s.watchdog.has_bitten", litexcnc->fpga->name); 
+    r = hal_pin_bit_new(name, HAL_IO, &(litexcnc->watchdog->hal.pin.has_bitten), litexcnc->fpga->comp_id);
+    if (r < 0) { goto fail_pins; }
+
+    // Create params
+    // - time-out in nano-seconds (including setting the default value)
+    rtapi_snprintf(name, sizeof(name), "%s.watchdog.timeout_ns", litexcnc->fpga->name); 
+    r = hal_param_u32_new(name, HAL_RW, &(litexcnc->watchdog->hal.param.timeout_ns), litexcnc->fpga->comp_id);
+    if (r < 0) { goto fail_params; }
+    // - time-out in cycles (read-only)
+    rtapi_snprintf(name, sizeof(name), "%s.watchdog.timeout_cycles", litexcnc->fpga->name); 
+    r = hal_param_u32_new(name, HAL_RO, &(litexcnc->watchdog->hal.param.timeout_cycles), litexcnc->fpga->comp_id);
+    if (r < 0) { goto fail_params; }
+
+    // Success
+    return 0;
+
+fail_pins:
+    LITEXCNC_ERR_NO_DEVICE("Error adding pin '%s', aborting\n", name);
+    return r;
+
+fail_params:
+    LITEXCNC_ERR_NO_DEVICE("Error adding param '%s', aborting\n", name);
+    return r;
+}
+
+uint8_t litexcnc_watchdog_prepare_write(litexcnc_t *litexcnc, uint8_t **data, long period) {
+
+
+    if (!litexcnc->watchdog->hal.param.timeout_ns) {
+        LITEXCNC_PRINT(
+            "Watchdog timeout not set. Using default value %"PRIu32" ns (3 times period).",
+            litexcnc->fpga->name,
+            (uint32_t) litexcnc->watchdog->hal.param.timeout_ns
+        );
+        litexcnc->watchdog->hal.param.timeout_ns = 3 * period;    }
+
+    // Recalculate timeout_cycles only required when timeout_ns changed
+    if (litexcnc->watchdog->hal.param.timeout_ns != litexcnc->watchdog->memo.timeout_ns ) {
+        // Store value to detect future scale changes
+        litexcnc->watchdog->memo.timeout_ns = litexcnc->watchdog->hal.param.timeout_ns;
+        // Validate new value (give warning when it is to close to the period of the thread)
+        if (litexcnc->watchdog->hal.param.timeout_ns < (1.5 * period)) {
+            LITEXCNC_PRINT(
+                "Watchdog timeout (%"PRIu32" ns) is dangerously short compared to litexcnc_write() period (%ld ns)\n",
+                litexcnc->fpga->name,
+                (uint32_t) litexcnc->watchdog->hal.param.timeout_ns,
+                period
+            );
+        }
+        // Convert the time to cycles
+        litexcnc->watchdog->hal.param.timeout_cycles = litexcnc->watchdog->memo.timeout_ns * ((double) litexcnc->clock_frequency / (double) 1e9) - 1;
+        // Limit the value to 0x7FFFFFFF
+        if (litexcnc->watchdog->hal.param.timeout_cycles > 0x7FFFFFFF) {
+            litexcnc->watchdog->hal.param.timeout_cycles = 0x7FFFFFFF;
+            litexcnc->watchdog->memo.timeout_ns = (litexcnc->watchdog->hal.param.timeout_cycles + 1) / ((double) litexcnc->clock_frequency / (double) 1e9);
+            LITEXCNC_ERR_NO_DEVICE("Requested watchdog timeout is out of range, setting it to max: %"PRIu32" ns\n", (uint32_t) litexcnc->watchdog->memo.timeout_ns);
+        }
+    }
+
+    // Store the parameter on the FPGA (also set the enable bit)
+    litexcnc_watchdog_data_write_t output;
+    output.timeout_cycles = htobe32(litexcnc->watchdog->hal.param.timeout_cycles + (0x80000000 * (~*(litexcnc->watchdog->hal.pin.has_bitten)))); 
+        
+    // Copy the data to the output and advance the pointer  
+    memcpy(*data, &output, LITEXCNC_WATCHDOG_DATA_WRITE_SIZE);
+    *data += LITEXCNC_WATCHDOG_DATA_WRITE_SIZE;
+    
+    // Success
+    return 0;
+}
+
+uint8_t litexcnc_watchdog_process_read(litexcnc_t *litexcnc, uint8_t** data) {
+
+    // Check whether the watchdog did bite
+    if (*(*data+3)) {
+        LITEXCNC_ERR_NO_DEVICE("Watchdog has bitten.");
+        *(litexcnc->watchdog->hal.pin.has_bitten) = 1;
+    }
+
+    // Proceed the buffer to the next element (note: 4-byte Words!)
+    (*data)+=4; 
+
+    // Success
+    return 0;
+}
---
+++ packages/litexcnc/litexcnc-1.2.4-py3-none-any.whl/litexcnc/firmware/watchdog.py
@@ -0,0 +1,139 @@
+#
+# This file is part of LiteX-CNC.
+#
+# Copyright (c) 2022 Peter van Tol <petertgvantolATgmailDOTcom>
+# SPDX-License-Identifier: MIT
+
+from litex.soc.interconnect.csr import *
+
+# Imports for Migen
+from migen import *
+from migen.genlib.cdc import MultiReg
+from litex.build.generic_platform import Pins, IOStandard
+from litex.soc.interconnect.csr import AutoCSR, CSRStorage
+
+class WatchDogModule(Module, AutoCSR):
+    """
+    Pet this, otherwise it will bite.
+    """
+    def __init__(self, enable_out, timeout=Signal(31), clock_domain="sys", default_timeout = 0, with_csr=True):
+        # Parameters for the watchdog:
+        # - the bit enable sets whether the watchdog is enabled. This is written when the 
+        #   timeout is read for the first-time from the driver to FPGA
+        # - timeout: counter which is decreased at each clock-cycle. When it is at 0, the 
+        #   watchdog will get very angry and bite.
+        self.enable  = Signal()
+        self.timeout = timeout
+        self.has_bitten = Signal()
+        
+        if enable_out is None:
+            enable_out = Signal()
+
+        # Procedure of the watchdog
+        sync = getattr(self.sync, clock_domain)
+        sync += [
+            If(
+                self.enable,
+                If(self.timeout > 0,
+                    # Still some time left before freaking out
+                    self.timeout.eq(self.timeout-1),
+                    self.has_bitten.eq(0),
+                    enable_out.eq(1)
+                ).Else(
+                    # The dog got angry, and will bite
+                    self.has_bitten.eq(1),
+                    enable_out.eq(0)
+                )
+            ).Else(
+                # Sleeping dogs don't bite, but the board is also not active
+                self.has_bitten.eq(0),
+                enable_out.eq(0)
+            )
+        ]
+
+        if with_csr:
+            self.add_csr(clock_domain)
+
+    def add_csr(self, clock_domain):
+        self._enable = CSRStorage(
+            description="Watchdog Enable.\n Write ``1`` to enable Watchdog.",
+            reset = self.enable.reset
+        )
+        self._timeout  = CSRStorage(
+            31, 
+            reset_less=True,
+            description="Watchdog timeout.\nThe amount of clock cycles remaining after which the "
+            "dog will bite. When the watchdog bites, the has_bitten signal is set.",
+            reset = self.timeout.reset)
+        self._has_bitten = CSRStorage(
+            description="Watchdog has bitten.\nFlag which is set when timeout has occurred",
+            reset = self.has_bitten.reset)
+
+        n = 0 if clock_domain == "sys" else 2
+        self.specials += [
+            MultiReg(self._enable.storage, self.enable, n=n),
+            MultiReg(self._timeout.storage, self.timeout,  n=n),
+            MultiReg(self._has_bitten.storage, self.has_bitten, n=n),
+        ]
+
+    @classmethod
+    def add_mmio_write_registers(cls, mmio, config: 'WatchdogModuleConfig'):
+        """
+        Adds the storage registers to the MMIO.
+
+        NOTE: Storage registers are meant to be written by LinuxCNC and contain
+        the flags and configuration for the module.
+        """
+        mmio.watchdog_data = CSRStorage(
+            size=32, 
+            description="Watchdog data.\nByte containing the enabled flag (bit 31) and the time (bit 30 - 0)."
+            "out in cpu cycles.", 
+            name='watchdog_data',
+            write_from_dev=True
+        )
+
+    @classmethod
+    def add_mmio_read_registers(cls, mmio, config: 'WatchdogModuleConfig'):
+        """
+        Adds the status registers to the MMIO.
+
+        NOTE: Status registers are meant to be read by LinuxCNC and contain
+        the current status of the module.
+        """
+        mmio.watchdog_has_bitten = CSRStatus(
+            size=1, 
+            description="Watchdog has bitten.\nFlag which is set when timeout has occurred.", 
+            name='watchdog_has_bitten'
+        )
+
+    @classmethod
+    def create_from_config(cls, soc, config: 'WatchdogModuleConfig'):
+        
+        # Create a pad for the enabled watchdog
+        soc.enable_out = None
+        if config.pin is not None:
+            soc.platform.add_extension([
+                ("watchdog", 0, Pins(config.pin), IOStandard(config.io_standard))
+            ])
+            soc.enable_out = soc.platform.request_all("watchdog").l[0]
+
+        # Create the watchdog
+        watchdog = WatchDogModule(
+            soc.enable_out,
+            timeout=soc.MMIO_inst.watchdog_data.storage[:31],
+            with_csr=False)
+        soc.submodules += watchdog
+        soc.sync+=[
+            # Watchdog input (fixed values)
+            watchdog.enable.eq(soc.MMIO_inst.watchdog_data.storage[31]),
+            # Watchdog output (status whether the dog has bitten)
+            soc.MMIO_inst.watchdog_has_bitten.status.eq(watchdog.has_bitten),
+            soc.MMIO_inst.watchdog_has_bitten.we.eq(True),
+            If(
+                soc.MMIO_inst.reset.storage,
+                soc.MMIO_inst.watchdog_data.storage.eq(0)
+            )
+        ]
+
+        return watchdog
+        
