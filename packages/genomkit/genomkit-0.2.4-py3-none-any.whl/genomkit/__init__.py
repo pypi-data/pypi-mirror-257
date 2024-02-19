6a4ab6dd6ec2bd8dcaff70cc4162269aef761a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:04:04 +0000

    Add package genomkit/0.2.4/genomkit-0.2.4-py3-none-any.whl

---
+++ packages/genomkit/genomkit-0.2.4-py3-none-any.whl/genomkit/__init__.py
@@ -0,0 +1,9 @@
+from .regions.gregion import GRegion
+from .regions.gregions import GRegions
+from .sequences.gsequence import GSequence
+from .sequences.gsequences import GSequences
+from .annotation.gannotation import GAnnotation
+from .alignments.galignments import GAlignments
+from .coverages.gcoverages import GCoverage
+
+__version__ = "0.2.4"
