bf3b1a1fc114e5285601a3fcec789ba5886572b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:03:48 +0000

    Add package gethyper/0.0.1/gethyper-0.0.1.tar.gz

---
+++ packages/gethyper/gethyper-0.0.1.tar.gz/gethyper-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gethyper',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A minimal package for reserving the name on PyPI',
+    long_description='More detailed description here',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourgithub/gethyper',  # Optional
+    license='MIT',  # Choose the appropriate license
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='example package',  # Optional
+)
