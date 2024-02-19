fe2d5927324fe3551fc4ab5d5afabe83790eea07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:03:36 +0000

    Add package sfutilpsg/0.1.3/sfutilpsg-0.1.3.tar.gz

---
+++ packages/sfutilpsg/sfutilpsg-0.1.3.tar.gz/sfutilpsg-0.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sfutilpsg',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'graphviz'
+    ],
+    author='mohan chinnappan',
+    author_email='mohan.chinnappan.n5@gmail.com',
+    description='A utility for managing Salesforce permissions and relationships',
+    url='https://github.com/mohan-chinnappan-n/sfutilpsg',
+    license='MIT',
+)
