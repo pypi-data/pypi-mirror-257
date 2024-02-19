44e4459792f437ac68d1e0bd82ded4cf1a678449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:06:10 +0000

    Add package DataPrepKit5.6/0.1.0/DataPrepKit5.6-0.1.0.tar.gz

---
+++ packages/DataPrepKit5.6/DataPrepKit5.6-0.1.0.tar.gz/DataPrepKit5.6-0.1.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 18:27:39 2024
+
+@author: MOHAMMED
+"""
+
+from setuptools import setup, find_packages
+setup(
+name='DataPrepKit5.6',
+version='0.1.0',
+author='Aborezka',
+author_email='MohammedAborezka@gmail.com',
+description='this package help you anlayze your data file insted of its type',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
