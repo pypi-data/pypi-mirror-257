382a885a7dbcbae46060b961bd60ae6e0e4710fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:57 +0000

    Add package moler/3.2.0/moler-3.2.0.tar.gz

---
+++ packages/moler/moler-3.2.0.tar.gz/moler-3.2.0/moler/cmd/unix/genericunix.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Generic Unix/Linux module
+"""
+
+__author__ = 'Marcin Usielski'
+__copyright__ = 'Copyright (C) 2018-2024, Nokia'
+__email__ = 'marcin.usielski@nokia.com'
+
+import re
+import abc
+import six
+
+from moler.cmd.commandtextualgeneric import CommandTextualGeneric
+from moler.helpers import remove_all_known_special_chars
+
+cmd_failure_causes = ['not found',
+                      'No such file or directory',
+                      'running it may require superuser privileges',
+                      'Cannot find device',
+                      'Input/output error',
+                      ]
+r_cmd_failure_cause_alternatives = f'{"|".join(cmd_failure_causes)}'
+
+
+@six.add_metaclass(abc.ABCMeta)
+class GenericUnixCommand(CommandTextualGeneric):
+    # _re_fail = re.compile(r_cmd_failure_cause_alternatives, re.IGNORECASE)
+
+    def __init__(self, connection, prompt=None, newline_chars=None, runner=None):
+        """
+        :param connection: Moler connection to device, terminal when command is executed.
+        :param prompt: prompt (on system where command runs).
+        :param newline_chars: Characters to split lines - list.
+        :param runner: Runner to run command.
+        """
+        super(GenericUnixCommand, self).__init__(connection=connection, prompt=prompt, newline_chars=newline_chars,
+                                                 runner=runner)
+        self.remove_all_known_special_chars_from_terminal_output = True
+        self.re_fail = re.compile(r_cmd_failure_cause_alternatives, re.IGNORECASE)
+
+    def _decode_line(self, line):
+        """
+        Method to delete new line chars and other chars we don not need to parse in on_new_line (color escape character)
+
+        :param line: Line with special chars, raw string from device
+        :return: line without special chars.
+        """
+        if self.remove_all_known_special_chars_from_terminal_output:
+            line = remove_all_known_special_chars(line)
+        return line
