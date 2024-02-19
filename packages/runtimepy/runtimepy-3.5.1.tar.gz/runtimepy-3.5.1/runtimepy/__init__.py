348400f8cf41baa0f9e06c5fd4e7dbdfea2fcb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:04:00 +0000

    Add package runtimepy/3.5.1/runtimepy-3.5.1.tar.gz

---
+++ packages/runtimepy/runtimepy-3.5.1.tar.gz/runtimepy-3.5.1/runtimepy/__init__.py
@@ -0,0 +1,16 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=186dece058b8aed58af9d04779e0965a
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "3.5.1"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
