23140b2582be06c613c51702b88363f838843526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:38 +0000

    Add package nonebot-plugin-bf1marneserverlist/0.1.14/nonebot_plugin_bf1marneserverlist-0.1.14-py3-none-any.whl

---
+++ packages/nonebot-plugin-bf1marneserverlist/nonebot_plugin_bf1marneserverlist-0.1.14-py3-none-any.whl/nonebot_plugin_bf1marneserverlist/__init__.py
@@ -0,0 +1,151 @@
+import json
+from typing import Annotated
+
+import httpx
+import nonebot_plugin_localstore as store
+from nonebot import get_plugin_config
+from nonebot import on_command
+from nonebot import require
+from nonebot.adapters.onebot.v11 import Message, MessageSegment, GroupMessageEvent
+from nonebot.adapters.onebot.v11.permission import GROUP_ADMIN, GROUP_OWNER
+from nonebot.params import _command_arg, CommandArg
+from nonebot.permission import SUPERUSER
+from nonebot.plugin import PluginMetadata
+from nonebot.typing import T_State
+
+from .config import Config
+
+require("nonebot_plugin_localstore")
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-bf1marneserverlist",
+    description="Onebot plugin for Battlefield 1 Marne",
+    usage="type /marne bind [serverID] and send /marne",
+
+    type="application",
+    # 发布必填，当前有效类型有：`library`（为其他插件编写提供功能），`application`（向机器人用户提供功能）。
+
+    homepage="https://github.com/SAFEluren/nonebot-plugin-bf1marneserverlist",
+    # 发布必填。
+
+    config=Config,
+    # 插件配置项类，如无需配置可不填写。
+
+    supported_adapters={"~onebot.v11"},
+    # 支持的适配器集合，其中 `~` 在此处代表前缀 `nonebot.adapters.`，其余适配器亦按此格式填写。
+    # 若插件可以保证兼容所有适配器（即仅使用基本适配器功能）可不填写，否则应该列出插件支持的适配器。
+)
+
+plugin_config = get_plugin_config(Config)
+data_dir = store.get_data_dir("bf1marneserverlist")
+
+marne_url = plugin_config.marne_url
+
+
+async def is_enable() -> bool:
+    return plugin_config.marne_plugin_enabled
+
+
+MARNE_MAIN = on_command('marne', block=True, priority=1)
+MARNE_BIND = on_command('marne bind', block=True, priority=1, permission=GROUP_OWNER | GROUP_ADMIN | SUPERUSER)
+
+
+# MARNE_MODS = on_command(f'marne modlist', block=True, priority=1)
+
+async def request_marneapi(marne_serverid):
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{marne_url}api/srvlst/{marne_serverid}")
+
+            content = response.text
+            if content is not None:
+                return content
+            else:
+                print("Response content is None")
+
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error occurred: {e}")
+
+
+@MARNE_MAIN.handle()
+# async def marne_info(event: GroupMessageEvent):
+async def _marneinfo(event: GroupMessageEvent):
+    session = event.group_id
+
+    try:
+        with open(data_dir / f'{session}.json', 'r', encoding='utf-8') as f:
+            group = json.load(f)
+    except FileNotFoundError:
+        await MARNE_MAIN.send('请先绑定服务器ID.')
+        return
+    serverID = group['id']
+    results = await request_marneapi(serverID)
+    if results is not None:
+        result = json.loads(results)
+        server_ID = result['id']
+        server_name = result['name']
+        server_description = result['description']
+        server_region = result['region']
+        server_country = result['country']
+        server_map = result['map']
+        server_mode = result['mode']
+        server_currentPlayers = result['currentPlayers']
+        server_maxPlayers = result['maxPlayers']
+
+        with open(data_dir / f'{session}.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
+
+        msg = Message([MessageSegment.text(f"查询成功")])
+        msg.append(f"\n绑定的服务器ID: {server_ID}")
+        msg.append(f"\n服务器名字: {server_name}")
+        msg.append(f"\n服务器简介: {server_description}")
+        msg.append(f"\n服务器区域: {server_region} - {server_country}")
+        msg.append(f"\n当前地图: {server_map}")
+        msg.append(f"\n游戏模式: {server_mode}")
+        msg.append(f"\n当前人数: {server_currentPlayers} / {server_maxPlayers}")
+
+        await MARNE_BIND.finish(msg)
+        # await MARNE_BIND.send(f'已绑定服务器ID:{serverID}')
+    else:
+        await MARNE_MAIN.finish('无法获取到服务器数据，请检查马恩服务器id是否正确，或服务器当前未开启。')
+
+
+# @MARNE_MODS.handle()
+# async def marne_mods(event:GroupMessageEvent, state:T_State):
+#     message = _command_arg(state) or event.get_message()
+#     session = event.group_id
+
+@MARNE_BIND.handle()
+async def _bind(event: GroupMessageEvent,args: Annotated[Message, CommandArg()]):
+    session = event.group_id
+    if len(args) == 0:
+        await MARNE_BIND.finish('未输入服务器ID')
+    try:
+        cmdargs=list(args[0].data.values())
+        print(cmdargs)
+        serverID = int(cmdargs[0])
+    except (TypeError,ValueError):
+        await MARNE_BIND.finish('格式错误，仅允许纯数字')
+        return
+
+    print(serverID)
+    result = await request_marneapi(serverID)
+    if result is not None:
+        result = json.loads(result)
+        serverName = result['name']
+        try:
+            with open(data_dir / f'{session}.json', 'w', encoding='utf-8') as f:
+                json.dump(result, f, ensure_ascii=False, indent=4)
+        except FileNotFoundError:
+            data_dir.mkdir(parents=True, exist_ok=True)
+            return
+
+        msg = Message([MessageSegment.text(f"绑定成功！")])
+        msg.append(f"\n绑定服务器ID: {serverID}")
+        msg.append(f"\n服务器名字:{serverName}")
+
+        await MARNE_BIND.send(msg)
+        # await MARNE_BIND.send(f'已绑定服务器ID:{serverID}')
+    else:
+        print(result)
+        await MARNE_MAIN.send('无法获取到服务器数据，请检查输入马恩服务器ID是否正确，或服务器当前未开启。')
