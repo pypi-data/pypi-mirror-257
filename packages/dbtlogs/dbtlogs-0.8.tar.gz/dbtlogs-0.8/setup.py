593155a48c51288616592c1f07e414488d2f4841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:18 +0000

    Add package dbtlogs/0.8/dbtlogs-0.8.tar.gz

---
+++ packages/dbtlogs/dbtlogs-0.8.tar.gz/dbtlogs-0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbtlogs',
+    version='0.8',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'databricks-sql-connector',
+        'requests',
+        'pytz',
+        'typing',
+        'datetime',
+        'apache-airflow',
+        'snowflake-sqlalchemy',
+        'snowflake-connector-python'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dbtlog = dbtlogs.main:dbtlog',
+        ],
+    },
+)
