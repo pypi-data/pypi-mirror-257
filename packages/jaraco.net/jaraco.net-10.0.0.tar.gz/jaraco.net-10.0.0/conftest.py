eec298c10599726bcd679435c5da0dc2d1eb2ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:04:24 +0000

    Add package jaraco.net/10.0.0/jaraco.net-10.0.0.tar.gz

---
+++ packages/jaraco.net/jaraco.net-10.0.0.tar.gz/jaraco.net-10.0.0/conftest.py
@@ -0,0 +1,62 @@
+import os
+import platform
+import sys
+import importlib
+import socket
+import functools
+import time
+
+import pytest
+import jaraco.functools
+from jaraco.context import ExceptionTrap
+
+
+missing = ExceptionTrap(ImportError).raises
+
+
+@missing
+def pywin32_missing():
+    importlib.import_module('win32service')
+
+
+collect_ignore = (
+    [
+        'jaraco/net/devices/linux.py',
+        'jaraco/net/devices/win32.py',
+        'jaraco/net/devices/darwin.py',
+    ]
+    + [
+        # modules only import on Windows
+        'jaraco/net/dns.py',
+    ]
+    * pywin32_missing()
+    + [
+        # ping test has different exception on older Pythons
+        'jaraco/net/icmp.py',
+    ]
+    * (sys.version_info < (3, 10))
+    + [
+        # ping is not working on Linux nor Windows
+        'jaraco/net/icmp.py',
+    ]
+    * (platform.system() in {"Linux", "Windows"})
+)
+
+
+@pytest.fixture(autouse=True)
+def retry_ntp_query(request):
+    """
+    ntp.query is flaky (by design), so be resilient during tests.
+    """
+    if not request.node.name.endswith('net.ntp.query'):
+        return
+
+    retry = jaraco.functools.retry(
+        retries=4,
+        trap=socket.timeout,
+        cleanup=functools.partial(time.sleep, 4),
+    )
+    globs = request.node.dtest.globs
+    globs['query'] = retry(globs['query'])
+    if os.environ.get('GITHUB_ACTIONS') and platform.system != 'Linux':
+        pytest.skip("Test is flaky")
