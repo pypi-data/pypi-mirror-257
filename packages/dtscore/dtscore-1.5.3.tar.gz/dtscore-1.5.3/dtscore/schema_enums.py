339e4aaf1a2d690b4896a6b83f08c8359c72edaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:06:03 +0000

    Add package dtscore/1.5.3/dtscore-1.5.3.tar.gz

---
+++ packages/dtscore/dtscore-1.5.3.tar.gz/dtscore-1.5.3/dtscore/schema_enums.py
@@ -0,0 +1,21 @@
+"""
+    Schema enums
+"""
+from enum import Enum
+
+# ----------------------------------------------------------------------------------------------------
+#   Schema enums
+ReportType = Enum('ReportType', [
+    'StrategySummary', 'StrategyBollDetail', 'StrategyProfitLossDetail', 'StrategyAdxDetail', 'StrategyTradesDetail',
+    'StrategyTransactionDetail', 'PortfolioSummary', 'PortfolioDetail', 'PortfolioHoldingOrderSheet',
+    'PortfolioHoldingDetail', 'PortfolioHoldingSummary', 'PortfolioSingleFileDetail'
+    ])
+AnalysisType = Enum("AnalysisType",["Summary", "Detail", "OrderSheet", "HoldingDetail"])
+DbEnvironment = Enum('DbEnvironment',['Prod','Dev','Staging','Test','Static'])
+LogLevel = Enum('LogLevel',['Error', 'Info', 'Warn', 'Debug', 'Verbose'])
+RunType = Enum('RunType',['StrategyAnalysis', 'SinglePortfolioAnalysis', 'MultiPortfolioAnalysis'])
+StrategyName = Enum('StrategyName',['BOLL','RBOL','ADX','CBOL'])
+TradingSide = Enum('TradingSide',['Long','Short','Both'])
+SkidType = Enum('SkidType',['Percent','Absolute'])
+DatasetType = Enum('DatasetType',['FullDay','PartialDay'])
+MarketEntryMethod = Enum('MarketEntryMethod',['MarketOrder','ConditionalOrder'])
