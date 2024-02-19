604318c729e0a11eb0edd5f81cf009d2e6ac2b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:04:18 +0000

    Add package benimang/0.5.16/benimang-0.5.16.tar.gz

---
+++ packages/benimang/benimang-0.5.16.tar.gz/benimang-0.5.16/test/test_bfunc.py
@@ -0,0 +1,12 @@
+import pytest
+
+from beni.bfunc import deobfuscate, obfuscate
+
+_PASSWORD = 'test_password'
+_DATA = '(故意地)混淆，使困惑，使模糊不清to make sth less clear and more difficult to understand, usually deliberately'.encode()
+
+
+@pytest.mark.asyncio
+async def test_obfuscate():
+    magicContent = obfuscate(_DATA)
+    assert deobfuscate(magicContent) == _DATA
