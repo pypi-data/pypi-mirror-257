d7b20e53d7addc014454484571acc4fc2c7be7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:57 +0000

    Add package autogoal-keras/0.8.4/autogoal_keras-0.8.4.tar.gz

---
+++ packages/autogoal-keras/autogoal_keras-0.8.4.tar.gz/autogoal_keras-0.8.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['autogoal_keras']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['autogoal-contrib>=0.8.2,<0.9.0',
+ 'autogoal>=1.0.2,<2.0.0',
+ 'keras>=2.12.0,<3.0.0',
+ 'tensorflow>=2.12.0,<3.0.0',
+ 'tensorflow_addons>=0.22.0,<0.23.0']
+
+setup_kwargs = {
+    'name': 'autogoal-keras',
+    'version': '0.8.4',
+    'description': 'Keras algorithm library wrapper for AutoGOAL',
+    'long_description': '# AutoGOAL KERAS Algorithm Library',
+    'author': 'Suilan Estevez-Velarde',
+    'author_email': 'suilanestevez@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://autogoal.github.io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.13,<3.11',
+}
+
+
+setup(**setup_kwargs)
