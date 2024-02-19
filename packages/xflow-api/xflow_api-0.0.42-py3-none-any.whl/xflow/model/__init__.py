3bbf251a7c0c43e140e425cc8566e174d445cd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:04:20 +0000

    Add package xflow-api/0.0.42/xflow_api-0.0.42-py3-none-any.whl

---
+++ packages/xflow-api/xflow_api-0.0.42-py3-none-any.whl/xflow/model/__init__.py
@@ -0,0 +1,10 @@
+from xflow.model.rw_api import export_model
+from xflow.model.structures import ModelTypes
+from xflow.model.metric import Metric
+
+
+__all__ = [
+    "export_model",
+    "ModelTypes",
+    "Metric"
+]
