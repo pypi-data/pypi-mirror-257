8730b282c706864d3cac283493fbe219c1e8df27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:04:15 +0000

    Add package pymeu/0.0.8/pymeu-0.0.8.tar.gz

---
+++ packages/pymeu/pymeu-0.0.8.tar.gz/pymeu-0.0.8/pymeu/constants.py
@@ -0,0 +1,35 @@
+CHUNK_SIZE = 1984
+
+HELPER_VERSIONS = {
+    '11.00.00',
+    '12.00.00.414.73'
+}
+
+ME_VERSIONS = {
+    '11.00.00.230',
+    '11.00.25.230',
+    '12.00.78.414'
+}
+
+PRODUCT_CODES = {
+    '102',  #PanelView Plus 7
+    '51'    #PanelView Plus 6
+}
+
+PRODUCT_TYPES = {
+    '24'
+}
+
+CREATE_DIR_SUCCESS = 183 # 'b\xb7'
+
+GET_UNK1_VALUES = {
+    b'\x02\x00'
+}
+
+GET_UNK2_VALUES = {
+    b'\x01\x60'
+}
+
+GET_UNK3_VALUES = {
+    b'\x64\x00'
+}
