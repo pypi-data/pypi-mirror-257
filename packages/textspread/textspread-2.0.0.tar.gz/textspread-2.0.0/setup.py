28de648081158fd6dee6745e1909886e73a5dd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:06:43 +0000

    Add package textspread/2.0.0/textspread-2.0.0.tar.gz

---
+++ packages/textspread/textspread-2.0.0.tar.gz/textspread-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='textspread',
+    version='2.0.0',
+    packages=find_packages(),
+    install_requires=[
+        "openpyxl"
+    ],
+)
