d8b5569596a855ade5fe480c3a0b9fd1b411031e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:17 +0000

    Add package NetKet/3.11.1/netket-3.11.1.tar.gz

---
+++ packages/NetKet/netket-3.11.1.tar.gz/netket-3.11.1/netket/utils/array.py
@@ -0,0 +1,89 @@
+# Copyright 2021 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import jax
+
+from .types import Array, DType, Shape
+from .struct import dataclass
+
+
+@dataclass(cache_hash=True)
+class HashableArray:
+    """
+    This class wraps a numpy or jax array in order to make it hashable and
+    equality comparable (which is necessary since a well-defined hashable object
+    needs to satisfy :code:`obj1 == obj2` whenever :code:`hash(obj1) == hash(obj2)`.
+
+    The underlying array can also be accessed using :code:`numpy.asarray(self)`.
+    """
+
+    wrapped: Array
+    """The wrapped array. Note that this array is read-only."""
+
+    def __pre_init__(self, wrapped):
+        if isinstance(wrapped, HashableArray):
+            wrapped = wrapped.wrapped
+        else:
+            if isinstance(wrapped, jax.Array):
+                # __array__ only works if it's a numpy array.
+                wrapped = np.array(wrapped)
+            else:
+                wrapped = wrapped.copy()
+            if isinstance(wrapped, np.ndarray):
+                wrapped.flags.writeable = False
+
+        return (wrapped,), {}
+
+    def __hash__(self):
+        return hash(self.wrapped.tobytes())
+
+    def __eq__(self, other):
+        return type(other) is HashableArray and np.all(self.wrapped == other.wrapped)
+
+    def __array__(self, dtype: DType = None):
+        if dtype is None:
+            dtype = self.wrapped.dtype
+        return self.wrapped.__array__(dtype)
+
+    @property
+    def dtype(self) -> DType:
+        return self.wrapped.dtype
+
+    @property
+    def size(self) -> int:
+        return self.wrapped.size
+
+    @property
+    def ndim(self) -> int:
+        return self.wrapped.ndim
+
+    @property
+    def shape(self) -> Shape:
+        return self.wrapped.shape
+
+
+def array_in(x, ys):
+    """
+    Interpret ys as a list of arrays, and test if x is equal to any y in ys,
+    with exactly the same shape but not exactly the same dtype.
+
+    Note:
+        In numpy, :code:`x in ys` is equivalent to :code:`any(x == ys)`,
+        which is usually not what we intend when :code:`x.size > 1`.
+        JAX arrays will raise an error rather than silently compute it.
+    """
+    x = x.reshape(1, -1)
+    ys = ys.reshape(ys.shape[0], -1)
+    return (x == ys).all(axis=1).any()
