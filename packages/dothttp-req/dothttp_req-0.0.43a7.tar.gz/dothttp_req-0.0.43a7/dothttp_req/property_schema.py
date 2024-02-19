f33bd360374d12fdb46304d4b91c0df76184b014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 16:03:37 +0000

    Add package dothttp-req/0.0.43a7/dothttp_req-0.0.43a7.tar.gz

---
+++ packages/dothttp-req/dothttp_req-0.0.43a7.tar.gz/dothttp_req-0.0.43a7/dothttp_req/property_schema.py
@@ -0,0 +1,9 @@
+property_schema = {"type": "object",
+                   "patternProperties": {"\\w*$": {"type": "object",
+                                                   "patternProperties": {"\\w*$": {"type": ["number",
+                                                                                            "string",
+                                                                                            "object",
+                                                                                            "array",
+                                                                                            "null",
+                                                                                            "boolean",
+                                                                                            "integer"]}}}}}
