e27f4273483b1350bdd5d50380ad6bcb15b54afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:20 +0000

    Add package shamo/1.2.1/shamo-1.2.1.tar.gz

---
+++ packages/shamo/shamo-1.2.1.tar.gz/shamo-1.2.1/CHANGELOG.md
@@ -0,0 +1,70 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.2.1] - 24-02-19
+
+### Fixed
+
+- Fixed broadcast error when checking for empty affine matrix or shape vector in the grid sampler.
+
+## [1.2.0] - 21-10-13
+
+### Added
+
+- Added rectangular electrodes for tDCS.
+
+### Fixed
+
+- Fixed field interpolation. Before, the only case in which field interpolation worked was when the field was aligned with the structural image used to generate the mesh. Now, it works no matter the orientation of the field.
+- Fixed template embedding in the package. It looks like, before, template files were not included in the package/not accessible.
+- Fixed moving file between different drives. Shamo now uses `shutil` to perform this action.
+- Fixed `None` mask in grid conversion.
+
+### Changed
+
+- All `.nii` files are now `.nii.gz` files.
+- HD-tDCS solutions now contain the raw GetDP results so that it is possible to regenerate the `.pos` files. This allows the user to only keep the files of interest while still having the ability to get the others back if needed.
+
+## [1.1.1] - 21-06-01
+
+### Fixed
+
+- Fixed structure parsing in `mesh_from_surfaces` to allow single child tissues.
+
+## [1.1.0] - 21-04-22
+
+### Added
+
+- Added logging support for subprocesses and C libraries via `subprocess_to_logger` and `stream_to_logger` context managers.
+- Added documentation on how to use the logging library with `shamo`.
+- Added the `mesh_from_fem` method to provide a way to generate a mesh from a previously built one by merging tissues together.
+- Added the `mesh_from_surfaces` method to provide a way to generate a mesh from a set of surface meshes.
+- Added `SurrMaskedScalar` and `SurrMaskedScalarNii` to enable more sensitivity analysis.
+- Added `SurrMaskedScalarNiiJ`, `SurrMaskedScalarNiiMagJ` and `SurrMaskedScalarNiiV` for tDCS.
+
+### Changed
+
+- Moved GetDP logging to the new logging `subprocess_to_logger` context manager.
+
+## [1.0.0] - 20-12-16
+
+### Added
+
+- Added `ObjABC`, `ObjFile` and `ObjDir` classes to provide a base for any savable/loadable object.
+- Added `Group`, `Tissue`, `Field`, `Sensor`, `PointSensor` and `FEM` classes to provide a way to generate realistic finite element model based on labelled volumes.
+- Added `ProbABC`, `CompABC`, `CompFilePath`, `CompGridSampler`, `CompSensors`, `CompTissueProp`, `CompTissues` and `ProbGetDP` classes to define any problem.
+- Added `SolGetDP` class to store information about the solution of a `ProbGetDP` problem.
+- Added `DistABC`, `DistConstant`, `DistNormal`, `DistTruncNormal` and `DistUniform` to model random parameters for parametric problems.
+- Added `ProbParamABC`, `CompParamTissueProp`, `CompParamValue`, and `ProbParamGetDP` classes to define any parametric problem.
+- Added `SolParamABC` and `SolParamGetDP` classes to store information about the solution of a `ProbParamABC` problem.
+- Added `ProbEEGLeadfield` and `SolEEGLeadfield` classes to solve the EEG forward problem.
+- Added `ProbParamEEGLeadfield`, `SolParamEEGLeadfield` and `SurrEEGLeadfield` classes to solve the parametric EEG forward problem. Also added `SurrEEGLeadfieldToRef` class to evalaute the sensitivity of the EEG leadfield matrix.
+- Added `ProbHDTDCSSim` and `SolHDTDCSSim` classes to simulate a HD-tDCS.
+- Added `ProbParamHDTDCSSim` and `SolParamHDTDCSSim` classes to simulate a parametric HD-tDCS.
+- Added `SurrogateABC` class to define any surrogate model based on a `SolParamABC`.
+- Added `SurrScalar` class to define any surrogate model returning a scalar to perform sensitivity analysis.
+- Added `shamo-report` command to facilitate bug report.
+- Added automated full reference with sphinx autosummary.
---
+++ packages/shamo/shamo-1.2.1.tar.gz/shamo-1.2.1/src/shamo/hd_tdcs/simulation/single/problem.py
@@ -0,0 +1,139 @@
+"""Implement `ProbHDTDCSSim` class."""
+import shutil
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import nibabel as nib
+
+from shamo.core.problems.single import CompGridSampler, CompSensors, ProbGetDP
+from shamo.utils.onelab import pos_to_nii
+
+from .solution import SolHDTDCSSim
+
+
+class ProbHDTDCSSim(ProbGetDP):
+    """A problem definition to simulate HD-tDCS.
+
+    Attributes
+    ----------
+    sigmas : shamo.core.problems.single.CompTissueProp
+        The electrical conductivity of the tissues.
+    references : shamo.core.problems.single.CompSensors
+        The electrodes to use as the references.
+    source : shamo.core.problems.single.CompSensors
+        The electrode to use as the source.
+    current : float
+        The injected current.
+    grid : shamo.core.problems.single.CompGridSampler
+        The grid sampler if the source space must be based on a grid.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.grid = CompGridSampler()
+        self.references = CompSensors()
+        self.source = CompSensors()
+        self.current = 1.0
+
+    @property
+    def template(self):
+        """Return the name of the template PRO file.
+
+        Returns
+        -------
+        str
+            The name of the template PRO file.
+        """
+        return "hdtdcs_simulation.tmplt"
+
+    def solve(self, name, parent_path, model, **kwargs):
+        """Simulate HD-tDCS.
+
+        Parameters
+        ----------
+        name : str
+            The name of the solution.
+        parent_path : str, byte or os.PathLike
+            The path to the parent directory of the solution.
+        model : shamo.fem.FEM
+            The finite element model to solve the problem for.
+
+        Returns
+        -------
+        shamo.hd_tdcs.SolHDTDCSSim
+            The simulation results.
+        """
+        for n, t in model.tissues.items():
+            self._vol.set(n, t.vol.group)
+
+        with TemporaryDirectory(dir=os.environ.get("SHAMO_TMP_DIR", None)) as d:
+            self._check_components(**model)
+            problem_path = self._gen_pro_file(d, **kwargs, **model)
+            self._run_getdp(model, d)
+            sol = SolHDTDCSSim(
+                name,
+                parent_path,
+                source=self.source["sensors"],
+                references=self.references["sensors"],
+                sigmas=self.sigmas,
+                current=self.current,
+            )
+            sol["model_json_path"] = str(sol.get_relative_path(model.json_path))
+            shutil.move(str(problem_path.with_suffix(".pre")), str(sol.path / f"{name}.pre"))
+            shutil.move(str(problem_path.with_suffix(".res")), str(sol.path / f"{name}.res"))
+            shutil.move(str(problem_path), str(sol.path / f"{name}.pro"))
+            for p in Path(d).iterdir():
+                if p.suffix == ".pos":
+                    if self.grid.use_grid:
+                        self.grid.nii_from_pos(p, sol.path / f"{name}_{p.stem}.nii.gz")
+                    shutil.move(str(p), str(sol.path / f"{name}_{p.name}"))
+            sol.save()
+        return sol
+
+    def _check_components(self, **kwargs):
+        """Check if the components are properly set."""
+        super()._check_components(**kwargs)
+        self.grid.check("grid", **kwargs)
+        self.source.check("source", **kwargs)
+        self.references.check("references", **kwargs)
+
+    def _prepare_pro_file_params(self, **kwargs):
+        """Return the parameters required to generate the PRO file.
+
+        Returns
+        -------
+        dict [str, ...]
+            The parameters required to generate the PRO file.
+        """
+        params = super()._prepare_pro_file_params(**kwargs)
+        params.update(
+            {
+                "use_grid": self.grid.to_pro_param(**kwargs),
+                "sources": self.source.to_pro_param(**kwargs),
+                "sinks": self.references.to_pro_param(**kwargs),
+                "current": self.current,
+            }
+        )
+        return params
+
+    def _prepare_py_file_params(self, **kwargs):
+        """Return the parameters required to generate the PY file.
+
+        Returns
+        -------
+        dict [str, ...]
+            The parameters required to generate the PY file.
+        """
+        params = super()._prepare_py_file_params(**kwargs)
+        params.update(
+            {
+                "source": self.source.to_py_param(**kwargs),
+                "references": self.references.to_py_param(**kwargs),
+                "current": self.current,
+                "use_grid": self.grid.use_grid,
+            }
+        )
+        if self.grid.use_grid:
+            params["grid"] = self.grid.to_py_param(**kwargs)
+        return params
