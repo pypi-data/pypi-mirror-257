6b16f25c7c2988db4ab739fe34bd73c281a3c0c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 16:03:43 +0000

    Add package cat-aspect-extraction/1.4.2/cat_aspect_extraction-1.4.2-py3-none-any.whl

---
+++ packages/cat-aspect-extraction/cat_aspect_extraction-1.4.2-py3-none-any.whl/cat_aspect_extraction/__init__.py
@@ -0,0 +1,12 @@
+from .cat import CAt
+from .attention import Attention, RBFAttention, CosineAttention, EuclideanAttention, SoftmaxAttention, MeanAttention
+
+__all__ = [
+    'CAt',
+    'Attention',
+    'RBFAttention',
+    'CosineAttention',
+    'EuclideanAttention',
+    'SoftmaxAttention',
+    'MeanAttention'
+]
---
+++ packages/cat-aspect-extraction/cat_aspect_extraction-1.4.2-py3-none-any.whl/cat_aspect_extraction/attention.py
@@ -0,0 +1,67 @@
+import numpy as np
+from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
+
+from abc import ABC, abstractmethod
+
+class Attention(ABC):
+
+    def super_attention(self, z: np.array, n: int) -> np.ndarray:
+        s = z.sum()
+        if s == 0: return np.ones((1, n)) / n
+        return (z.sum(axis=1) / s).reshape(1, -1)
+
+    @abstractmethod
+    def attention(self, vectors: np.array, candidates: np.array) -> np.ndarray:
+        """
+        Compute attention vector for a given list of tokens as vector
+
+        Parameters:
+        -----------
+        - matrix (np.ndarray) : Matrix of tokens as vector (shape: (n, d))
+        - candidates (np.ndarray) : Matrix of candidate words as vector (shape: (m, d))
+
+        Returns:
+        --------
+        - np.ndarray : Attention vector (shape: (h, n))
+        """
+        pass
+
+class RBFAttention(Attention):
+
+    def __init__(self, gamma: float = .03) -> None:
+        """
+        Parameters:
+        -----------
+        - gamma (float) : Gamma parameter for RBF kernel (default 0.03)
+        """
+        self.gamma = gamma
+
+    def attention(self, vectors: np.array, candidates: np.array) -> np.ndarray:
+        z = rbf_kernel(vectors, candidates, gamma=self.gamma)
+        return self.super_attention(z, len(vectors))
+    
+
+class CosineAttention(Attention):
+
+    def attention(self, vectors: np.array, candidates: np.array) -> np.ndarray:
+        z = cosine_similarity(vectors, candidates)
+        return self.super_attention(z, len(vectors))
+    
+class EuclideanAttention(Attention):
+
+    def attention(self, vectors: np.array, candidates: np.array) -> np.ndarray:
+        z = euclidean_distances(vectors, candidates)
+        return self.super_attention(z, len(vectors))
+    
+class SoftmaxAttention(Attention):
+
+    def attention(self, vectors: np.array, candidates: np.array) -> np.ndarray:
+        z = candidates.dot(vectors.T)
+        z = np.exp(z - np.max(z, axis=1, keepdims=True))
+        return z / z.sum(axis=1, keepdims=True)
+    
+class MeanAttention(Attention):
+
+    def attention(self, vectors: np.array, candidates: np.array = None) -> np.ndarray:
+        return (np.ones((1, len(vectors))) / len(vectors)).reshape(1, -1)
