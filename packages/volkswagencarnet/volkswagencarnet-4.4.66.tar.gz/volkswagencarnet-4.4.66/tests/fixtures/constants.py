177bb43680ba00004aaf4bae9c4985e3b63ad142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:35 +0000

    Add package volkswagencarnet/4.4.66/volkswagencarnet-4.4.66.tar.gz

---
+++ packages/volkswagencarnet/volkswagencarnet-4.4.66.tar.gz/volkswagencarnet-4.4.66/tests/fixtures/constants.py
@@ -0,0 +1,12 @@
+"""Misc constants and other data for tests."""
+
+import os
+from pathlib import Path
+
+MOCK_VIN = "WVWZZZAUZFW000000"
+
+current_path = Path(os.path.dirname(os.path.realpath(__file__)))
+resource_path = os.path.join(current_path, "resources")
+status_report_json_file = os.path.join(resource_path, "responses", "status.json")
+timers_json_file = os.path.join(resource_path, "responses", "timer.json")
+timers_no_settings_json_file = os.path.join(resource_path, "responses", "timer_without_settings.json")
---
+++ packages/volkswagencarnet/volkswagencarnet-4.4.66.tar.gz/volkswagencarnet-4.4.66/tests/integration_test.py
@@ -0,0 +1,61 @@
+"""
+Integration tests.
+
+These tests use actual credentials, and should thus be used with care.
+Credentials have to be specified in credentials.py.
+"""
+
+import logging
+
+import pytest
+from aiohttp import ClientSession
+
+from volkswagencarnet import vw_connection
+from volkswagencarnet.vw_timer import TimerData
+
+try:
+    from credentials import username, password, spin, vin
+except ImportError:
+    username = password = spin = vin = None
+
+
+@pytest.mark.skipif(
+    username is None or password is None, reason="Username or password is not set. Check credentials.py.sample"
+)
+@pytest.mark.asyncio
+async def test_successful_login():
+    """Test that login succeeds."""
+    async with ClientSession() as session:
+        connection = vw_connection.Connection(session, username, password)
+        await connection.doLogin()
+        assert connection.logged_in is True
+
+
+@pytest.mark.skipif(
+    username is None or password is None, reason="Username or password is not set. Check credentials.py.sample"
+)
+@pytest.mark.asyncio
+@pytest.mark.skip("Not sure if this is even a good idea...")
+async def test_set_timer():
+    """Test that login succeeds."""
+    async with ClientSession() as session:
+        connection = vw_connection.Connection(session, username, password)
+        await connection.doLogin()
+        data = TimerData({}, {})
+        await connection.setTimersAndProfiles(vin=vin, data=data.timersAndProfiles)
+
+        assert 1 == 1
+
+
+@pytest.mark.skipif(
+    username is None or password is None, reason="Username or password is not set. Check credentials.py.sample"
+)
+@pytest.mark.asyncio
+@pytest.mark.skip("Not yet implemented")
+async def test_spin_action():
+    """
+    Test something that uses s-pin.
+
+    Not yet implemented...
+    """
+    logging.getLogger().debug(f"using vin: {vin} and s-pin: {spin}")
