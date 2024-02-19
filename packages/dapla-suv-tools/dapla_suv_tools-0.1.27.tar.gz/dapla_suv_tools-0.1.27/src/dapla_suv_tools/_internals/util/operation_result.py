2d006dc6fc5a508e5bcc033e89619e1df49315b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:49 +0000

    Add package dapla-suv-tools/0.1.27/dapla_suv_tools-0.1.27.tar.gz

---
+++ packages/dapla-suv-tools/dapla_suv_tools-0.1.27.tar.gz/dapla_suv_tools-0.1.27/src/dapla_suv_tools/_internals/util/operation_result.py
@@ -0,0 +1,14 @@
+from dapla_suv_tools._internals.util import constants
+
+
+class OperationResult:
+    result_json: dict
+    result: str
+    operation_log: dict
+
+    def __init__(self, value: dict | str, success: bool = True,  log: dict = None):
+        if success and isinstance(value, str):
+            value = {"result": value}
+        self.result = constants.OPERATION_OK if success else constants.OPERATION_ERROR
+        self.result_json = value
+        self.operation_log = {} if log is None else log
