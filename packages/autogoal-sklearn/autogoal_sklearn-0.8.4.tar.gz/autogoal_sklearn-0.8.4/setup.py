b01f5f61e34e09684546db7515fd929c981890bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:57 +0000

    Add package autogoal-sklearn/0.8.4/autogoal_sklearn-0.8.4.tar.gz

---
+++ packages/autogoal-sklearn/autogoal_sklearn-0.8.4.tar.gz/autogoal_sklearn-0.8.4/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['autogoal_sklearn', 'autogoal_sklearn.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['autogoal-contrib>=0.8.2,<0.9.0',
+ 'autogoal>=1.0.2,<2.0.0',
+ 'numpy>=1.21.6,<2.0.0',
+ 'scikit-learn==1.0',
+ 'sklearn-crfsuite>=0.3.6,<0.4.0']
+
+setup_kwargs = {
+    'name': 'autogoal-sklearn',
+    'version': '0.8.4',
+    'description': 'scikit-learn algorithm library for AutoGOAL',
+    'long_description': '# AutoGOAL Sklearn Algorithm Library',
+    'author': 'Suilan Estevez-Velarde',
+    'author_email': 'suilanestevez@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://autogoal.github.io',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9.13,<3.11',
+}
+
+
+setup(**setup_kwargs)
