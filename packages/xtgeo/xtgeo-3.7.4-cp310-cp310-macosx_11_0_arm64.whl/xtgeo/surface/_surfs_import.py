63b63b5af57abbeefe71199aac2f58369f4b8290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 16:03:40 +0000

    Add package xtgeo/3.7.4/xtgeo-3.7.4-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/xtgeo/xtgeo-3.7.4-cp310-cp310-macosx_11_0_arm64.whl/xtgeo/surface/_surfs_import.py
@@ -0,0 +1,44 @@
+"""Import multiple surfaces"""
+
+import xtgeo
+from xtgeo.common import null_logger
+
+logger = null_logger(__name__)
+
+
+def from_grid3d(grid, subgrids, rfactor):
+    """Get surfaces, subtype and order from 3D grid, including subgrids"""
+
+    logger.info("Extracting surface from 3D grid...")
+
+    # determine layers
+    layers = []
+    names = []
+    if subgrids and grid.subgrids is not None:
+        last = ""
+        for sgrd, srange in grid.subgrids.items():
+            layers.append(str(srange[0]) + "_top")
+            names.append(sgrd + "_top")
+            last = str(srange[-1])
+            lastname = sgrd
+        # base of last layer
+        layers.append(last + "_base")
+        names.append(lastname + "_base")
+    else:
+        layers.append("top")
+        names.append("top")
+        layers.append("base")
+        names.append("base")
+
+    # next extract these layers
+    layerstack = []
+    for inum, lay in enumerate(layers):
+        layer = xtgeo.surface_from_grid3d(
+            grid, template=None, where=lay, rfactor=rfactor
+        )
+        layer.name = names[inum]
+        layerstack.append(layer)
+
+    logger.info("Extracting surface from 3D grid... DONE")
+
+    return layerstack, "tops", "stratigraphic"
