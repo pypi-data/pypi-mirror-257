052b7ed89ed15d48c6253794d6d12a7f84f6bb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:35 +0000

    Add package pysenal/0.1.3/pysenal-0.1.3.tar.gz

---
+++ packages/pysenal/pysenal-0.1.3.tar.gz/pysenal-0.1.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+VERSION = "0.1.3"
+
+requirments = []
+
+setup(
+    install_requires=requirments,
+    version=VERSION,
+    packages=find_packages(exclude=('tests',)),
+)
