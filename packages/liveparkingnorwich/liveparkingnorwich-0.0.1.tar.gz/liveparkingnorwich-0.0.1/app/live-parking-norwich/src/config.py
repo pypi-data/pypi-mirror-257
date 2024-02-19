5023ef4b862415debb027cdec22b63c655efb5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:04:40 +0000

    Add package liveparkingnorwich/0.0.1/liveparkingnorwich-0.0.1.tar.gz

---
+++ packages/liveparkingnorwich/liveparkingnorwich-0.0.1.tar.gz/liveparkingnorwich-0.0.1/app/live-parking-norwich/src/config.py
@@ -0,0 +1,5 @@
+class Config():
+
+    XML_URL = "https://datex.norfolk.cdmf.info/carparks/content.xml"
+    XML_NAMESPACE = { "d2lm": "http://datex2.eu/schema/1_0/1_0" }
+    DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
