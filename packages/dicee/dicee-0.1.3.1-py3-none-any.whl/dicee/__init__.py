250ce9ae108b77d1d6a496584f402d4bf41932a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:03:54 +0000

    Add package dicee/0.1.3.1/dicee-0.1.3.1-py3-none-any.whl

---
+++ packages/dicee/dicee-0.1.3.1-py3-none-any.whl/dicee/__init__.py
@@ -0,0 +1,8 @@
+from .static_funcs import *  # noqa
+from .trainer import DICE_Trainer  # noqa
+from .knowledge_graph_embeddings import KGE  # noqa
+from .executer import Execute # noqa
+from .dataset_classes import * # noqa
+from .query_generator import QueryGenerator # noqa
+from .scripts import * # noqa
+__version__ = '0.1.3.1'
---
+++ packages/dicee/dicee-0.1.3.1-py3-none-any.whl/dicee/models/__init__.py
@@ -0,0 +1,8 @@
+from .base_model import * # noqa
+from .real import * # noqa
+from .complex import * # noqa
+from .quaternion import * # noqa
+from .octonion import * # noqa
+from .clifford import Keci, KeciBase, CMult # noqa
+from .pykeen_models import * # noqa
+from .function_space import * # noqa
