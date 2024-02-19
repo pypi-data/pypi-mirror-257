6b2b4dd46e115b5649fc2da4cf924ceeedc28f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:04:56 +0000

    Add package pyftg/2.0/pyftg-2.0-py3-none-any.whl

---
+++ packages/pyftg/pyftg-2.0-py3-none-any.whl/pyftg/aio/gateway.py
@@ -0,0 +1,60 @@
+import asyncio
+import logging
+
+import grpc
+
+from ..aiinterface import AIInterface
+from ..enum.status_code import StatusCode
+from ..protoc import service_pb2, service_pb2_grpc
+from .ai_controller import AIController
+
+
+def load_ai(ai_path: str) -> AIInterface:
+    path = ai_path.split('.')
+    module_name, class_name = path[0], path[int(len(path) == 2)]
+    return getattr(__import__(module_name), class_name)()
+
+class Gateway:
+    def __init__(self, host='127.0.0.1', port=50051):
+        self.host = host
+        self.port = port
+        self.channel = grpc.aio.insecure_channel(
+            target=f"{host}:{port}",
+            compression=grpc.Compression.Gzip,
+        )
+        self.stub = service_pb2_grpc.ServiceStub(self.channel)
+        self.registered_agents: 'dict[str, AIInterface]' = dict()
+        self.agents: 'list[AIInterface]' = [None, None]
+        self.ais: 'list[AIController]' = [None, None]
+    
+    def load_agent(self, ai_names: 'list[str]'):
+        for i, ai_name in enumerate(ai_names):
+            if ai_name:
+                self.agents[i] = load_ai(ai_name)
+    
+    def register_ai(self, name: str, agent: AIInterface):
+        self.registered_agents[name] = agent
+
+    async def run_game(self, characters: 'list[str]', agents: 'list[str]', game_number: int):
+        for i in range(2):
+            if agents[i] == 'Sandbox':
+                agents[i] = None
+            elif agents[i] in self.registered_agents:
+                self.agents[i] = self.registered_agents[agents[i]]
+        response = await self.stub.RunGame(service_pb2.RunGameRequest(character_1=characters[0], character_2=characters[1], player_1=agents[0], player_2=agents[1], game_number=game_number))
+        if StatusCode(response.status_code) == StatusCode.FAILED:
+            raise Exception(response.response_message)
+        await self.start_ai()
+    
+    async def start_ai(self):
+        tasks = list()
+        loop = asyncio.get_event_loop()
+        for i, agent in enumerate(self.agents):
+            if agent:
+                self.ais[i] = AIController(self.stub, agent, i == 0)
+                tasks.append(loop.create_task(self.ais[i].run()))
+                logging.info(f"AI controller for P{i+1} ({agent.name()}) is ready.")
+        await asyncio.gather(*tasks)
+    
+    async def close(self):
+        await self.channel.close()
