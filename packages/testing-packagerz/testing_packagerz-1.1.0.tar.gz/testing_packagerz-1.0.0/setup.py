12ec57f7973a951b8e3743d19e7ffe78e3f78c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:04:15 +0000

    Add package testing-packagerz/1.0.0/testing_packagerz-1.1.0.tar.gz

---
+++ packages/testing-packagerz/testing_packagerz-1.1.0.tar.gz/testing_packagerz-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testing_packagerz',
+    version='1.0.0',
+    description='A simple Python package',
+    packages=find_packages(),
+    install_requires=[
+        # List of dependencies
+    ],
+    author='Chibkay',
+    author_email='chibk.okoli@gmail.com',
+    url='https://github.com/Chibkay/',
+)
