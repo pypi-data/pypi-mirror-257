c86e31e0e0ebcef3aec55fe59e84bff998d3af24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:04 +0000

    Add package EduRPA/0.0.2/EduRPA-0.0.2.tar.gz

---
+++ packages/EduRPA/EduRPA-0.0.2.tar.gz/EduRPA-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+import setuptools
+import pkg_resources
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with pathlib.Path('./requirements.txt').open() as requirements_txt:
+    requirements = requirements_txt.readlines()
+    install_requires = [
+            str(requirement)
+            for requirement
+            in pkg_resources.parse_requirements(requirements)
+        ]
+
+setuptools.setup(
+    name="EduRPA",
+    version="0.0.2",
+    author="david",
+    author_email="davidhuynh0222@gmail.com",
+    description="education librabry for RPA",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edu-rpa/edu-rpa-library",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = install_requires,
+)
