bac176ac43877300fd10aebeb47e8b2f68b6f862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:04:16 +0000

    Add package f6-core/1.0.0/f6_core-1.0.0.tar.gz

---
+++ packages/f6-core/f6_core-1.0.0.tar.gz/f6_core-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
+
+setup(
+    name="f6_core",
+    version="1.0.0",
+    packages=find_packages(exclude="tests"),
+    description="""The "F6" program is a program designed to work with electronic
+     paper accounting of attendance and academic performance of students of secondary professional
+     organizations, which generates reports in the form of an extract – "Form 6".""",
+    long_description=README_MD,
+    long_description_content_type="text/markdown",
+    url="",
+    # author_name="Ivan Degtyarev",
+    # author_email="",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+    keywords="f6, f6_core",
+
+)
