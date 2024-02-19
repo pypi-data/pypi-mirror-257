459f78aac4485fb7541a6f6c04bd80c2b462e11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:03:59 +0000

    Add package zcmds-win32/1.2.19/zcmds_win32-1.2.19-py2.py3-none-any.whl

---
+++ packages/zcmds-win32/zcmds_win32-1.2.19-py2.py3-none-any.whl/zcmds_win32/cmds/git_bash.py
@@ -0,0 +1,17 @@
+import os
+
+from zcmds_win32.unix_tool_path import unix_tool_exec
+
+CMDNAME = "bash.exe"
+
+GIT_BIN = r"C:\Program Files\Git\usr\bin"
+
+ENV = os.environ.copy()
+ENV["PATH"] = f"{GIT_BIN};{ENV['PATH']}"
+os.environ.update(ENV)
+
+
+def main() -> int:
+    return unix_tool_exec(
+        CMDNAME,
+    )
