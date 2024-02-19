3a2633a3765440c4bb087d516940293ebedc9a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:28 +0000

    Add package witnesschain/0.1.3/witnesschain-0.1.3.tar.gz

---
+++ packages/witnesschain/witnesschain-0.1.3.tar.gz/witnesschain-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup (
+	name		= "witnesschain",
+	version		= "0.1.3",
+	author		= "arun",
+	author_email	= "x@example.com",
+	description	= "A short description of your package",
+	packages	= find_packages(),
+	classifiers	= [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	url		= "https://github.com/iudx/iudx-python-sdk",
+	python_requires	= ">=3.6",
+)
