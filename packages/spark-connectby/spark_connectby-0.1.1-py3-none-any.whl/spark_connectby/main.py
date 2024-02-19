c5f8cae3ca2ca88386faf57491591644042a1baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:51 +0000

    Add package spark-connectby/0.1.1/spark_connectby-0.1.1-py3-none-any.whl

---
+++ packages/spark-connectby/spark_connectby-0.1.1-py3-none-any.whl/spark_connectby/main.py
@@ -0,0 +1,23 @@
+from datetime import datetime, date
+
+from pyspark.sql import SparkSession
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    spark = SparkSession.builder.getOrCreate()
+
+    df = spark.createDataFrame([
+        (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
+        (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
+        (3, 4., 'string3', date(2000, 3, 1), datetime(2000, 1, 3, 12, 0))
+    ], schema='a long, b double, c string, d date, e timestamp')
+
+
+    df.show(truncate=False)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
