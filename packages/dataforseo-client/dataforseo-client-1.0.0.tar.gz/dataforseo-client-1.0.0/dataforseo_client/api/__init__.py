6f8f4277f2caae9222c357f0135cd28f4af0941f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:52 +0000

    Add package dataforseo-client/1.0.0/dataforseo-client-1.0.0.tar.gz

---
+++ packages/dataforseo-client/dataforseo-client-1.0.0.tar.gz/dataforseo-client-1.0.0/dataforseo_client/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.app_data_api import AppDataApi
+from openapi_client.api.appendix_api import AppendixApi
+from openapi_client.api.backlinks_api import BacklinksApi
+from openapi_client.api.business_data_api import BusinessDataApi
+from openapi_client.api.content_analysis_api import ContentAnalysisApi
+from openapi_client.api.content_generation_api import ContentGenerationApi
+from openapi_client.api.dataforseo_labs_api import DataforseoLabsApi
+from openapi_client.api.domain_analytics_api import DomainAnalyticsApi
+from openapi_client.api.keywords_data_api import KeywordsDataApi
+from openapi_client.api.merchant_api import MerchantApi
+from openapi_client.api.on_page_api import OnPageApi
+from openapi_client.api.serp_api import SerpApi
+
