c1a6fa1f52eabafaa9a328fcbc6b6c38def3fd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:44 +0000

    Add package oliver-codegen/0.1.3/oliver_codegen-0.1.3-py3-none-any.whl

---
+++ packages/oliver-codegen/oliver_codegen-0.1.3-py3-none-any.whl/src/util/error.py
@@ -0,0 +1,9 @@
+from src.core.log import logger
+
+
+def error_exit(msg: str, excp=None):
+    if excp is not None:
+        logger.error(msg, excp)
+        exit(-1)
+    logger.error(msg)
+    exit(-1)
