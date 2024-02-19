a7fdd47e2efaed8ee8dad0ae1e685b52f114368b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 16:03:36 +0000

    Add package py-stars-tools/0.0.3/py_stars_tools-0.0.3.tar.gz

---
+++ packages/py-stars-tools/py_stars_tools-0.0.3.tar.gz/py_stars_tools-0.0.3/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="py_stars_tools",
+    version="0.0.3",
+    packages=find_packages(),
+    author="Stars Bing",
+    author_email="bing@starsxu.com",
+    description="stars for tools",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
