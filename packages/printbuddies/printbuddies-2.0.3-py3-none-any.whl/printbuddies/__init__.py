80c4e32eca8b6fa512ce1d16f77f787f3a86e13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:03:57 +0000

    Add package printbuddies/2.0.3/printbuddies-2.0.3-py3-none-any.whl

---
+++ packages/printbuddies/printbuddies-2.0.3-py3-none-any.whl/printbuddies/__init__.py
@@ -0,0 +1,23 @@
+from .buds import ProgBar, Spinner, clear, print_in_place, ticker
+from .colormap import ColorMap, Tag
+from .gradient import RGB, Gradient
+from .progress import BarColumn, Progress, TaskProgressColumn, TimerColumn, track
+
+__version__ = "2.0.3"
+
+__all__ = [
+    "track",
+    "Gradient",
+    "ProgBar",
+    "Spinner",
+    "clear",
+    "print_in_place",
+    "ticker",
+    "ColorMap",
+    "Tag",
+    "RGB",
+    "Progress",
+    "TimerColumn",
+    "BarColumn",
+    "TaskProgressColumn",
+]
