78fc1989bb6a37515c3e2824a580e28d3ab6119f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:03:47 +0000

    Add package bcdata/0.9.2/bcdata-0.9.2-py3-none-any.whl

---
+++ packages/bcdata/bcdata-0.9.2-py3-none-any.whl/bcdata/__init__.py
@@ -0,0 +1,13 @@
+from .bc2pg import bc2pg
+from .bcdc import get_table_definition, get_table_name
+from .wcs import get_dem
+from .wfs import (
+    define_requests,
+    get_count,
+    get_data,
+    get_features,
+    list_tables,
+    validate_name,
+)
+
+__version__ = "0.9.2"
