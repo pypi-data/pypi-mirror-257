41ac1db20368fb80f64f766b143987ca38629ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 16:03:38 +0000

    Add package palmfrog/1.0.1/palmfrog-1.0.1.tar.gz

---
+++ packages/palmfrog/palmfrog-1.0.1.tar.gz/palmfrog-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='palmfrog',
+    version='1.0.1',
+    packages=['palmfrog'],  # Explicitly specify your package
+    entry_points={
+        'console_scripts': [
+            'palmfrog=palmfrog.main:main',
+        ],
+    },
+    install_requires=[
+        'requests',
+        'pycryptodome',
+    ],
+    author='Palmfrog',
+    author_email='hello@palmfrog.net',
+    description='ChatGPT for terminal applications with palmfrog.net website.',
+    license='MIT',
+    keywords='chatgpt terminal chatbot palmfrog camouflage',
+)
+
