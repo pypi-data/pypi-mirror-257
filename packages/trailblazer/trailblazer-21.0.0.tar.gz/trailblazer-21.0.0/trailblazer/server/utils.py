f81cc11e057944e9569f2a0aff944afa23878aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 15:03:56 +0000

    Add package trailblazer/21.0.0/trailblazer-21.0.0.tar.gz

---
+++ packages/trailblazer/trailblazer-21.0.0.tar.gz/trailblazer-21.0.0/trailblazer/server/utils.py
@@ -0,0 +1,41 @@
+import datetime
+
+from flask import Request
+
+from trailblazer.dto import AnalysesRequest, AnalysisUpdateRequest, FailedJobsRequest
+from trailblazer.dto.create_job_request import CreateJobRequest
+
+
+def parse_analyses_request(request: Request) -> AnalysesRequest:
+    """Parse a request for retrieving analyses."""
+    query_params = {}
+    for key in request.args.keys():
+        if key_has_list_of_values(key):
+            query_params[key[:-2]] = request.args.getlist(key)
+        else:
+            query_params[key] = request.args.get(key)
+    return AnalysesRequest.model_validate(query_params)
+
+
+def key_has_list_of_values(key: str) -> bool:
+    return key.endswith("[]")
+
+
+def parse_analysis_update_request(request: Request) -> AnalysisUpdateRequest:
+    return AnalysisUpdateRequest.model_validate(request.json)
+
+
+def parse_get_failed_jobs_request(request: Request) -> FailedJobsRequest:
+    return FailedJobsRequest.model_validate(request.args)
+
+
+def stringify_timestamps(data: dict) -> dict[str, str]:
+    """Convert datetime into string before dumping in order to avoid information loss"""
+    for key, val in data.items():
+        if isinstance(val, datetime.datetime):
+            data[key] = str(val)
+    return data
+
+
+def parse_job_create_request(request: Request) -> CreateJobRequest:
+    return CreateJobRequest.model_validate(request.json)
---
+++ packages/trailblazer/trailblazer-21.0.0.tar.gz/trailblazer-21.0.0/trailblazer/services/utils.py
@@ -0,0 +1,21 @@
+from trailblazer.dto import AnalysisResponse, FailedJobsResponse
+from trailblazer.dto.job_response import JobResponse
+from trailblazer.store.models import Analysis, Job
+
+
+def create_failed_jobs_response(failed_job_statistics: list[dict]) -> FailedJobsResponse:
+    return FailedJobsResponse(jobs=failed_job_statistics)
+
+
+def create_analysis_response(analysis: Analysis) -> AnalysisResponse:
+    analysis_data: dict = analysis.to_dict()
+    analysis_data["jobs"] = [job.to_dict() for job in analysis.analysis_jobs]
+    analysis_data["upload_jobs"] = [job.to_dict() for job in analysis.upload_jobs]
+    analysis_data["user"] = analysis.user.to_dict() if analysis.user else None
+    return AnalysisResponse.model_validate(analysis_data)
+
+
+def create_job_response(job: Job) -> JobResponse:
+    return JobResponse(
+        slurm_id=job.slurm_id, analysis_id=job.analysis_id, status=job.status, id=job.id
+    )
