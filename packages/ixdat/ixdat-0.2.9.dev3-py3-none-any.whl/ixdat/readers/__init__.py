42f0aa457a3e7b59f89ce6c5cfb78e9d10758fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:03:37 +0000

    Add package ixdat/0.2.9.dev3/ixdat-0.2.9.dev3-py3-none-any.whl

---
+++ packages/ixdat/ixdat-0.2.9.dev3-py3-none-any.whl/ixdat/readers/__init__.py
@@ -0,0 +1,69 @@
+"""Import readers and build the READER_CLASSES dictionary for direct import
+
+Constants:
+    READER_CLASSES (dict): Dictionary of {reader_name: ReaderClass} where
+        reader_name is the name of the backend (like "directory") and ReaderClass
+        is the reader class for parsing files.
+"""
+from ..techniques import TECHNIQUE_CLASSES
+
+# ixdat
+from .ixdat_csv import IxdatCSVReader, IxdatSpectrumReader
+
+# potentiostats
+from .biologic import BiologicReader
+from .autolab import NovaASCIIReader
+from .ivium import IviumDatasetReader
+from .chi import CHInstrumentsTXTReader
+from .nordic import NordicTDMSReader
+
+# mass spectrometers
+from .pfeiffer import PVMassSpecReader
+from .rgasoft import StanfordRGASoftReader
+from .cinfdata import CinfdataTXTReader
+from .cinfdata_db import CinfdataDBReader
+
+# ec-ms
+from .zilien import ZilienTSVReader, ZilienTMPReader, ZilienSpectrumReader
+from .ec_ms_pkl import EC_MS_CONVERTER
+
+# spectroelectrochemistry
+from .msrh_sec import MsrhSECReader, MsrhSECDecayReader
+
+# xrd
+from .xrdml import XRDMLReader
+
+# xps
+from .avantage import AvantageAVGReader
+
+# xas
+from .qexafs import QexafsDATReader
+
+# Measruement.read() looks for readers here:
+READER_CLASSES = {
+    "ixdat": IxdatCSVReader,
+    "biologic": BiologicReader,
+    "autolab": NovaASCIIReader,
+    "ivium": IviumDatasetReader,
+    "chi": CHInstrumentsTXTReader,
+    "pfeiffer": PVMassSpecReader,
+    "rgasoft": StanfordRGASoftReader,
+    "cinfdata": CinfdataTXTReader,
+    "cinfdata_db": CinfdataDBReader,
+    "zilien": ZilienTSVReader,
+    "zilien_tmp": ZilienTMPReader,
+    "EC_MS": EC_MS_CONVERTER,
+    "msrh_sec": MsrhSECReader,
+    "msrh_sec_decay": MsrhSECDecayReader,
+    "qexafs": QexafsDATReader,
+    "nordic": NordicTDMSReader,
+}
+
+
+# Spectrum.read() looks for readers here:
+SPECTRUM_READER_CLASSES = {
+    "ixdat": IxdatSpectrumReader,
+    "avantage": AvantageAVGReader,
+    "zilien": ZilienSpectrumReader,
+    "xrdml": XRDMLReader,
+}
