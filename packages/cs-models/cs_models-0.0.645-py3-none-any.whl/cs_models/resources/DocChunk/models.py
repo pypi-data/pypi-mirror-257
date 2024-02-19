80b527278ea5a49761ecc30a99c7224225a49f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:06:44 +0000

    Add package cs-models/0.0.645/cs_models-0.0.645-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.645-py3-none-any.whl/cs_models/resources/DocChunk/models.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    Text,
+    Boolean,
+)
+
+from ...database import Base
+
+
+class DocChunkModel(Base):
+    __tablename__ = "doc_chunks"
+
+    id = Column(Integer, primary_key=True)
+    source_type = Column(String(50), nullable=False)
+    source_id = Column(Integer, nullable=False)
+    chunk_cui = Column(String(50), nullable=False)
+    chunk_text = Column(Text, nullable=False)
+    chunk_embedding = Column(Text, nullable=False)
+    embedding_source = Column(String(50), nullable=False)
+    is_deleted = Column(Boolean, nullable=True)
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
