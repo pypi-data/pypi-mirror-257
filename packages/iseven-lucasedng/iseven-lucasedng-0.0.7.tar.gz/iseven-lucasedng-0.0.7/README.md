692723c48c25ec9c53d2feff6f7413802c225db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:04:02 +0000

    Add package iseven-lucasedng/0.0.7/iseven-lucasedng-0.0.7.tar.gz

---
+++ packages/iseven-lucasedng/iseven-lucasedng-0.0.7.tar.gz/iseven-lucasedng-0.0.7/README.md
@@ -0,0 +1,23 @@
+# is_even - lucasedng
+
+Under construction! Not ready for use yet! Currently experimenting and planning!
+
+Developed by Lucas Gonçalves (c) 2024
+
+## Examples of How To Use
+
+Using is_even method
+
+```python
+from is_even import is_even
+
+# Check if a number is even
+number = 4
+if is_even(number):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is not even.")
+```
+This simple library allows you to easily check if a number is even. The is_even method takes an integer as input and returns `True` if the number is even, and `False` otherwise.
+
+Check out more on my GitHub or follow me for updates.
---
+++ packages/iseven-lucasedng/iseven-lucasedng-0.0.7.tar.gz/iseven-lucasedng-0.0.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.7'
+DESCRIPTION = 'A smart way to find out if a number is even'
+LONG_DESCRIPTION = 'A package that allows you to use an intelligent way to find out if a number is even.'
+
+# Setting up
+setup(
+    name="iseven-lucasedng",
+    version=VERSION,
+    author="lucasedng (Lucas Gonçalves)",
+    author_email="lucasedng@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'number', 'even'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
