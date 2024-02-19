31f48a752ab4ce715e1dba9b9b5eaaafc44a7e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:03:35 +0000

    Add package mantis-cli/14.6.0/mantis-cli-14.6.0.tar.gz

---
+++ packages/mantis-cli/mantis-cli-14.6.0.tar.gz/mantis-cli-14.6.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from mantis import VERSION
+
+setup(
+    name='mantis-cli',
+    version=VERSION,
+    description='Management command to build and deploy webapps, especially based on Django',
+    long_description=open('README.md').read(),
+    author='Erik Telepovský',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/mantis-cli',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['cffi', 'cryptography', 'pycryptodome', 'PyYAML', 'prettytable'],
+    entry_points={
+        'console_scripts': ['mantis=mantis.command_line:run'],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Development Status :: 5 - Production/Stable'
+    ],
+    license='GNU General Public License (GPL)',
+    keywords="management deployment docker command",
+)
