57c9efcef464a03f3d4f937f31e0acc0d8584fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:04:04 +0000

    Add package dedlin/1.19.1/dedlin-1.19.1.tar.gz

---
+++ packages/dedlin/dedlin-1.19.1.tar.gz/dedlin-1.19.1/dedlin/tools/export.py
@@ -0,0 +1,18 @@
+"""
+Export to file formats, particularly markdown
+"""
+
+import mistune
+
+
+def export_markdown(lines: list[str], preferred_line_break: str) -> str:
+    """Write to file.
+
+    Args:
+        lines (list[str]): The lines
+        preferred_line_break (str): The preferred line break
+    Returns:
+        str: The markdown
+    """
+    markdown = mistune.create_markdown()
+    return markdown(preferred_line_break.join(lines))
