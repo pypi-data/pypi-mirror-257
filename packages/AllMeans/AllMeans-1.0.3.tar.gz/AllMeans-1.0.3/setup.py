439d240178adcee2d97edc862218b7ca2ba6e42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 04:03:44 +0000

    Add package AllMeans/1.0.3/AllMeans-1.0.3.tar.gz

---
+++ packages/AllMeans/AllMeans-1.0.3.tar.gz/AllMeans-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# README
+with open("README.md", "r") as f:
+    description = f.read()
+
+# LICENSE
+with open("LICENSE", "r") as f:
+    license = f.read()
+
+setup(
+    name = 'AllMeans',
+    version = '1.0.3',
+    author = 'Kai Maurin-Jones',
+    description = 'A package for fully automatic topic modelling',
+    packages = find_packages(),
+    python_requires = '>=3.11.4',
+    license = license,
+    install_requires = [
+        'nltk==3.8.1',
+        'numpy==1.24.3',
+        'scikit-learn==1.3.2',
+        'sentence-transformers==2.2.2',
+    ],
+    long_description = description,
+    long_description_content_type = "text/markdown",    
+)
