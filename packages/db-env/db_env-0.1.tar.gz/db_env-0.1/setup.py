5e035b9f6e4efdd00e22053ebeca7807d5e3cbe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:05:51 +0000

    Add package db-env/0.1/db_env-0.1.tar.gz

---
+++ packages/db-env/db_env-0.1.tar.gz/db_env-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name=           'db_env',
+    version=        '0.1',
+    description=    'library that creates a relational DB with the usage of SQLite. More information follows.',
+    author=         'Daniel Huber',
+    packages=find_packages(),
+)
