63f68a9a5b1a360c2ac6600cef7381066abe426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:04:36 +0000

    Add package m269-lib/0.0.1/m269_lib-0.0.1.tar.gz

---
+++ packages/m269-lib/m269_lib-0.0.1.tar.gz/m269_lib-0.0.1/m269_lib/__init__.py
@@ -0,0 +1,22 @@
+"""This is a pedagogical algorithms and data structures library.
+
+It currently implements the Stack abstract data type (ADT).
+
+To install the library in the currently active environment, enter in a terminal:
+```bash
+pip install m269-lib
+```
+To learn how to use the library, see the examples in the documentation of the classes.
+
+The code repository is at https://github.com/dsa-ou/m269-lib.
+
+## Licence
+
+`m269-lib` is Copyright © 2024 by The Open University, UK.
+The code is licensed under a [BSD 3-clause licence](https://github.com/dsa-ou/m269-lib/blob/main/LICENSE).
+The documentation is licensed under a
+[Creative Commons Attribution 4.0 International Licence](http://creativecommons.org/licenses/by/4.0).
+"""
+
+# Import all classes so that they can be directly imported from m269_lib.
+from .stack import *
