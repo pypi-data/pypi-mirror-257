76c3c1f1d0e3a5fa753f72e3ffaec26ecbba39af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 16:03:38 +0000

    Add package ReadPDFBYJayesh/1.0.0/ReadPDFBYJayesh-1.0.0.tar.gz

---
+++ packages/ReadPDFBYJayesh/ReadPDFBYJayesh-1.0.0.tar.gz/ReadPDFBYJayesh-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ReadPDFBYJayesh',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Jayesh Vani',
+    author_email='jayeshvani2@gmail.com',
+    description='Read Pdf File',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
