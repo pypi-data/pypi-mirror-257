10312892e996a832e796f957f2a181488682e625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 14:04:17 +0000

    Add package raknet/0.2.0/raknet-0.2.0-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/raknet/raknet-0.2.0-pp39-pypy39_pp73-win_amd64.whl/raknet/__init__.py
@@ -0,0 +1,24 @@
+__all__ = [
+    "RakPeer",
+    "Packet",
+    "PacketPriority",
+    "PacketReliability",
+    "MessageIdentifiers",
+    "ConnectionAttemptError",
+    "StartupError",
+]
+
+for name in __all__:
+    from importlib import import_module
+
+    module = import_module("raknet.raknet_python")
+    globals()[f"_{name}"] = module.__dict__[name]
+    del module
+
+RakPeer = globals()["_RakPeer"]
+Packet = globals()["_Packet"]
+PacketPriority = globals()["_PacketPriority"]
+PacketReliability = globals()["_PacketReliability"]
+MessageIdentifiers = globals()["_MessageIdentifiers"]
+ConnectionAttemptError = globals()["_ConnectionAttemptError"]
+StartupError = globals()["_StartupError"]
