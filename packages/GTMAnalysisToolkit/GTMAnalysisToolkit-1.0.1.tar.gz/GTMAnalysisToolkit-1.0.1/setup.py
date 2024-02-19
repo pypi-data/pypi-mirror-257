eb00f0ca0923a361fb060a2e168aa26edbc5d06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-19 03:04:24 +0000

    Add package GTMAnalysisToolkit/1.0.1/GTMAnalysisToolkit-1.0.1.tar.gz

---
+++ packages/GTMAnalysisToolkit/GTMAnalysisToolkit-1.0.1.tar.gz/GTMAnalysisToolkit-1.0.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Generative Topographic Mapping and Analysis Toolkit'
+
+# Read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="GTMAnalysisToolkit",
+    version=VERSION,
+    author="Eng. Alberto Biscalchin",
+    author_email="<biscalchin.mau.se@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'scikit-learn',
+    ],
+    keywords=[
+        "generative topographic mapping", "dimensionality reduction", "data visualization",
+        "machine learning", "high-dimensional data analysis"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
