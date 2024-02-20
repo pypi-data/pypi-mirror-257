136af07724451099f5034ea08fba9e40b3551b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 18:04:18 +0000

    Add package encryPython/1.0.1/encryPython-1.0.1.tar.gz

---
+++ packages/encryPython/encryPython-1.0.1.tar.gz/encryPython-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='encryPython',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[],  # Add any dependencies here
+    author='Harsh Jani',
+    author_email='jani99harsh@gmail.com',
+    description='A Python package for reading Excel files.',
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
