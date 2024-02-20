4394ca57a6c54d698695f3487b0887a0ba26d6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:05:31 +0000

    Add package qepy/7.2.0rc0/qepy-7.2.0rc0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/qepy/qepy-7.2.0rc0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/qepy/qepylibs/qepy_pp/pp_module.py
@@ -0,0 +1,27 @@
+"""
+Module pp_module
+
+
+Defined at postproc.fpp lines 14-253
+
+"""
+from __future__ import print_function, absolute_import, division
+import libqepy_pp
+import f90wrap.runtime
+import logging
+
+_arrays = {}
+_objs = {}
+
+
+_array_initialisers = []
+_dt_array_initialisers = []
+
+try:
+    for func in _array_initialisers:
+        func()
+except ValueError:
+    logging.debug('unallocated array(s) detected on import of module "pp_module".')
+
+for func in _dt_array_initialisers:
+    func()
