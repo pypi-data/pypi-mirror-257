b3b92b0b8b03992c500d21892af319957c40c88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:03:58 +0000

    Add package PDFFiller/0.2.1/PDFFiller-0.2.1.tar.gz

---
+++ packages/PDFFiller/PDFFiller-0.2.1.tar.gz/PDFFiller-0.2.1/PDFFiller/theme/theme.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from PDFFiller.components import TextFieldTheme, DebugBoxTheme, CheckMarkTheme, ImageBoxTheme
+
+
+@dataclass
+class Theme:
+    version: Optional = "1.0"
+    debug_box: Optional[DebugBoxTheme] = field(default_factory=lambda: DebugBoxTheme())
+    text_field: Optional[TextFieldTheme] = field(default_factory=lambda: TextFieldTheme())
+    check_mark: Optional[CheckMarkTheme] = field(default_factory=lambda: CheckMarkTheme())
+    image_box: Optional[ImageBoxTheme] = field(default_factory=lambda: ImageBoxTheme())
