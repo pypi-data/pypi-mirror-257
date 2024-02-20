db93865f261243a4f5617e2ce3301ff47859c5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 18:04:23 +0000

    Add package easy-whisper-local/1.0/easy-whisper-local-1.0.tar.gz

---
+++ packages/easy-whisper-local/easy-whisper-local-1.0.tar.gz/easy-whisper-local-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easy-whisper-local',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'openai-whisper'
+    ]
+)
