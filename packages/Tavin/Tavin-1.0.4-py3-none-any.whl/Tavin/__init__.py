c050ad4eb7102b05ffe94cdfe9b3a7a32ac27dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:03:58 +0000

    Add package Tavin/1.0.4/Tavin-1.0.4-py3-none-any.whl

---
+++ packages/Tavin/Tavin-1.0.4-py3-none-any.whl/Tavin/__init__.py
@@ -0,0 +1,24 @@
+import webview
+from flask import *
+
+class Tavin:
+    def __init__(self, env):
+        self.app = Flask(env)
+        self.api = webview_api()
+        self.window = webview.create_window(url = self.app)
+
+    def get_app(self):
+        return self.app
+
+    def _route(self, route):
+        path = self.path
+        self.app.add_url_rule(path, path, route)
+        self.path = ''
+
+    def route(self, path):
+        self.path = path
+        return self._route
+
+    def run(self):
+        self.api.set_window(self.window)
+        webview.start()
