4d931da7a8b9b3732671ca989e41c705b7e24557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:42 +0000

    Add package zedtoolmotion/1.0.0/zedtoolmotion-1.0.0.tar.gz

---
+++ packages/zedtoolmotion/zedtoolmotion-1.0.0.tar.gz/zedtoolmotion-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zedtoolmotion',
+    version='1.0.0',
+    packages=find_packages(),
+    description='A package for my animation',
+    author='Yousseif Muhammed',
+    license='MIT', 
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
