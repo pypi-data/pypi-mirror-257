8370cd033981197712a2523684f9634cc7f12f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:53 +0000

    Add package peroxymanova/0.2.0/peroxymanova-0.2.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.whl

---
+++ packages/peroxymanova/peroxymanova-0.2.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.whl/peroxymanova/_oxide.pyi
@@ -0,0 +1,9 @@
+import numpy as np
+from typing import Any
+
+def permanova(
+    sqdistances: np.ndarray[Any, np.dtype[np.float64]],
+    labels: np.ndarray[Any, np.dtype[np.uint]],
+) -> tuple[float, float]: ...
+
+ordinal_encoding_dtypes = np.str_ | np.int64 | np.int32 | np.int16
