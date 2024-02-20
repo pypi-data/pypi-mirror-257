4b4663f7ec4d69e7c95ca5afa84247278ceff267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:52 +0000

    Add package dgii-rnc/0.2/dgii_rnc-0.2.tar.gz

---
+++ packages/dgii-rnc/dgii_rnc-0.2.tar.gz/dgii_rnc-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dgii_rnc',
+    version='0.2',
+    author='Luis C Garcia',
+    packages=find_packages(where="src"),
+    install_requires=[
+        'polars'
+    ],
+    license="MIT",
+    python_requires = ">=3.6"
+)
