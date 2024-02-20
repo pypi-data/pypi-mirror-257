a77f158207cc58261f40d260f3c9f8fef7fd738f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 05:05:06 +0000

    Add package ydata-sdk/0.11.0rc1/ydata_sdk-0.11.0rc1-py310-none-any.whl

---
+++ packages/ydata-sdk/ydata_sdk-0.11.0rc1-py310-none-any.whl/ydata/sdk/datasources/_models/datasources/mysql.pyi
@@ -0,0 +1,6 @@
+from ydata.sdk.datasources._models.datasource import DataSource
+
+class MySQLDataSource(DataSource):
+    query: str
+    def to_payload(self) -> None: ...
+    def __init__(self, uid, author, name, datatype, metadata, status, query) -> None: ...
