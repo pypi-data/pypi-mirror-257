ccb54178bd489fe21180bd6b024b0aa39dc67949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:05:56 +0000

    Add package safir/5.2.1/safir-5.2.1.tar.gz

---
+++ packages/safir/safir-5.2.1.tar.gz/safir-5.2.1/tests/dependencies/gafaelfawr_test.py
@@ -0,0 +1,98 @@
+"""Test the Gafaelfawr auth FastAPI dependencies."""
+
+from __future__ import annotations
+
+import json
+from typing import Annotated
+from unittest.mock import ANY
+
+import pytest
+from _pytest.logging import LogCaptureFixture
+from fastapi import Depends, FastAPI
+from httpx import AsyncClient
+from structlog.stdlib import BoundLogger
+
+from safir.dependencies.gafaelfawr import (
+    auth_delegated_token_dependency,
+    auth_dependency,
+    auth_logger_dependency,
+)
+from safir.logging import configure_logging
+
+
+@pytest.mark.asyncio
+async def test_auth_dependency() -> None:
+    app = FastAPI()
+
+    @app.get("/")
+    async def handler(
+        user: Annotated[str, Depends(auth_dependency)]
+    ) -> dict[str, str]:
+        return {"user": user}
+
+    async with AsyncClient(app=app, base_url="https://example.com") as client:
+        r = await client.get("/")
+        assert r.status_code == 422
+
+        r = await client.get("/", headers={"X-Auth-Request-User": "someuser"})
+        assert r.status_code == 200
+        assert r.json() == {"user": "someuser"}
+
+
+@pytest.mark.asyncio
+async def test_auth_delegated_token_dependency() -> None:
+    app = FastAPI()
+
+    @app.get("/")
+    async def handler(
+        token: Annotated[str, Depends(auth_delegated_token_dependency)],
+    ) -> dict[str, str]:
+        return {"token": token}
+
+    async with AsyncClient(app=app, base_url="https://example.com") as client:
+        r = await client.get("/")
+        assert r.status_code == 422
+
+        r = await client.get(
+            "/", headers={"X-Auth-Request-Token": "sometoken"}
+        )
+        assert r.status_code == 200
+        assert r.json() == {"token": "sometoken"}
+
+
+@pytest.mark.asyncio
+async def test_auth_logger_dependency(caplog: LogCaptureFixture) -> None:
+    configure_logging(name="myapp", profile="production", log_level="info")
+
+    app = FastAPI()
+
+    @app.get("/")
+    async def handler(
+        logger: Annotated[BoundLogger, Depends(auth_logger_dependency)],
+    ) -> dict[str, str]:
+        logger.info("something")
+        return {}
+
+    caplog.clear()
+    async with AsyncClient(app=app, base_url="https://example.com") as client:
+        r = await client.get("/", headers={"User-Agent": ""})
+        assert r.status_code == 422
+
+        r = await client.get(
+            "/", headers={"User-Agent": "", "X-Auth-Request-User": "someuser"}
+        )
+        assert r.status_code == 200
+
+    assert len(caplog.record_tuples) == 1
+    assert json.loads(caplog.record_tuples[0][2]) == {
+        "event": "something",
+        "httpRequest": {
+            "requestMethod": "GET",
+            "requestUrl": "https://example.com/",
+            "remoteIp": "127.0.0.1",
+        },
+        "logger": "myapp",
+        "request_id": ANY,
+        "severity": "info",
+        "user": "someuser",
+    }
---
+++ packages/safir/safir-5.2.1.tar.gz/safir-5.2.1/tests/middleware/ivoa_test.py
@@ -0,0 +1,64 @@
+"""Test IVOA middleware."""
+
+from __future__ import annotations
+
+from typing import Annotated
+
+import pytest
+from fastapi import FastAPI, Query
+from httpx import AsyncClient
+
+from safir.middleware.ivoa import CaseInsensitiveQueryMiddleware
+
+
+def build_app() -> FastAPI:
+    """Construct a test FastAPI app with the middleware registered."""
+    app = FastAPI()
+    app.add_middleware(CaseInsensitiveQueryMiddleware)
+    return app
+
+
+@pytest.mark.asyncio
+async def test_case_insensitive() -> None:
+    app = build_app()
+
+    @app.get("/")
+    async def handler(param: str) -> dict[str, str]:
+        return {"param": param}
+
+    @app.get("/simple")
+    async def simple_handler() -> dict[str, str]:
+        return {"foo": "bar"}
+
+    @app.get("/list")
+    async def list_handler(
+        param: Annotated[list[str], Query()]
+    ) -> dict[str, list[str]]:
+        return {"param": param}
+
+    async with AsyncClient(app=app, base_url="https://example.com") as client:
+        r = await client.get("/", params={"param": "foo"})
+        assert r.status_code == 200
+        assert r.json() == {"param": "foo"}
+
+        r = await client.get("/", params={"PARAM": "foo"})
+        assert r.status_code == 200
+        assert r.json() == {"param": "foo"}
+
+        r = await client.get("/", params={"pARam": "foo"})
+        assert r.status_code == 200
+        assert r.json() == {"param": "foo"}
+
+        r = await client.get("/", params={"paramX": "foo"})
+        assert r.status_code == 422
+
+        r = await client.get("/simple")
+        assert r.status_code == 200
+        assert r.json() == {"foo": "bar"}
+
+        r = await client.get(
+            "/list",
+            params=[("param", "foo"), ("PARAM", "BAR"), ("parAM", "baZ")],
+        )
+        assert r.status_code == 200
+        assert r.json() == {"param": ["foo", "BAR", "baZ"]}
