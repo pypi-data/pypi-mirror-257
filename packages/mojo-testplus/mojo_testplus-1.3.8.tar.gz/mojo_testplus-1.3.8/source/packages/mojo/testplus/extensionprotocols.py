ac01da023a90c86dcfe0bf95c8f2325c1a500e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 15:04:05 +0000

    Add package mojo-testplus/1.3.8/mojo_testplus-1.3.8.tar.gz

---
+++ packages/mojo-testplus/mojo_testplus-1.3.8.tar.gz/mojo_testplus-1.3.8/source/packages/mojo/testplus/extensionprotocols.py
@@ -0,0 +1,17 @@
+
+from typing import Type
+
+from mojo.testplus.testjob import TestJob
+
+from mojo.extension.extensionprotocol import ExtProtocol
+
+class TestPlusExtensionProtocol(ExtProtocol):
+
+    ext_protocol_name = "mojo-testplus"
+
+    def get_testplus_default_job_type() -> Type[TestJob]:
+        """
+            Used to lookup and return the most relevant default job type as determined
+            by the super factory search path.
+        """
+        return 
