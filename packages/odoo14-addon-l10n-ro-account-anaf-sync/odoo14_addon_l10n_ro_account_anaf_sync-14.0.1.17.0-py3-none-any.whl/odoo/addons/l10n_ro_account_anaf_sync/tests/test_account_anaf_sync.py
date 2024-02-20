d3b4abf36e4eb57233783f3f8a857917e58ab55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:17 +0000

    Add package odoo14-addon-l10n-ro-account-anaf-sync/14.0.1.17.0/odoo14_addon_l10n_ro_account_anaf_sync-14.0.1.17.0-py3-none-any.whl

---
+++ packages/odoo14-addon-l10n-ro-account-anaf-sync/odoo14_addon_l10n_ro_account_anaf_sync-14.0.1.17.0-py3-none-any.whl/odoo/addons/l10n_ro_account_anaf_sync/tests/test_account_anaf_sync.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2023 Terrabit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+
+from odoo.tests.common import TransactionCase
+
+
+class TestAccountANAFSync(TransactionCase):
+    def setUp(self):
+        super(TestAccountANAFSync, self).setUp()
+        self.test_company = self.env["res.company"].create({"name": "Test Sync"})
+        self.sync = self.env["l10n.ro.account.anaf.sync"].create(
+            {
+                "company_id": self.test_company.id,
+                "client_id": "123",
+                "client_secret": "123",
+            }
+        )
+
+    def test_anaf_api(self):
+        self.sync.test_anaf_api()
+
+    def test_revoke_access_token(self):
+        self.sync.write({"access_token": "123"})
+        self.sync.revoke_access_token()
+
+    def test_get_access_token(self):
+        self.sync.get_token_from_anaf_website()
+
+    def test_einvoice_calll(self):
+        params = {
+            "zile": 1,
+            "cif": "1234567890",
+        }
+        self.sync._l10n_ro_einvoice_call("/listaMesajeFactura", params, method="GET")
