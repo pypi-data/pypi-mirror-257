b5187925acc507cd5277d68a2ef60cda73252f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:14 +0000

    Add package edupils/0.1.17/edupils-0.1.17-py3-none-any.whl

---
+++ packages/edupils/edupils-0.1.17-py3-none-any.whl/edupils/desafios/turtle.py
@@ -0,0 +1,10 @@
+# In each file
+class TurtleDrawing:
+    def __init__(self):
+        pass
+
+    def draw_square(self, size):
+        # Implementation
+        pass
+
+# And so on for other classes and functions
