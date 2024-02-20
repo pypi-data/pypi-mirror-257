0acefa3df20893acc7d8c2d4e3dc5c471c0e73c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:05:24 +0000

    Add package trailblazer/21.0.1/trailblazer-21.0.1-py3-none-any.whl

---
+++ packages/trailblazer/trailblazer-21.0.1-py3-none-any.whl/trailblazer/services/analysis_service.py
@@ -0,0 +1,54 @@
+from trailblazer.dto import (
+    AnalysesRequest,
+    AnalysesResponse,
+    AnalysisResponse,
+    AnalysisUpdateRequest,
+    CreateAnalysisRequest,
+)
+from trailblazer.exc import MissingAnalysis
+from trailblazer.services.utils import create_analysis_response
+from trailblazer.store.models import Analysis, Job
+from trailblazer.store.store import Store
+
+
+class AnalysisService:
+    def __init__(self, store: Store):
+        self.store = store
+
+    def get_analyses(self, query: AnalysesRequest) -> AnalysesResponse:
+        analyses, total_analysis_count = self.store.get_filtered_sorted_paginated_analyses(query)
+        return self.create_analyses_response(
+            analyses=analyses, total_analysis_count=total_analysis_count
+        )
+
+    def get_analysis(self, analysis_id: int) -> AnalysisResponse:
+        if not (analysis := self.store.get_analysis_with_id(analysis_id)):
+            raise MissingAnalysis(f"Analysis with id: {analysis_id} not found")
+        return create_analysis_response(analysis)
+
+    def update_analysis(self, analysis_id: int, update: AnalysisUpdateRequest) -> AnalysisResponse:
+        analysis: Analysis = self.store.update_analysis(
+            analysis_id=analysis_id,
+            comment=update.comment,
+            status=update.status,
+            is_visible=update.is_visible,
+        )
+        return create_analysis_response(analysis)
+
+    def add_pending_analysis(self, request_data: CreateAnalysisRequest) -> AnalysisResponse:
+        analysis: Analysis = self.store.add_pending_analysis(request_data)
+        return create_analysis_response(analysis)
+
+    def create_analyses_response(
+        self, analyses: list[Analysis], total_analysis_count: int
+    ) -> AnalysesResponse:
+        response_data: list[dict] = []
+        for analysis in analyses:
+            analysis_data = analysis.to_dict()
+            failed_job: Job = self.store.get_latest_failed_job_for_analysis(analysis.id)
+            analysis_data["failed_job"] = failed_job.to_dict() if failed_job else None
+            response_data.append(analysis_data)
+        return AnalysesResponse(analyses=response_data, total_count=total_analysis_count)
+
+    def update_ongoing_analyses(self) -> None:
+        self.store.update_ongoing_analyses()
