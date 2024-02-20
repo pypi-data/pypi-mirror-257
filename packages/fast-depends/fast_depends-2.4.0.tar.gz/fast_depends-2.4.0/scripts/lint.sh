dced58629f0a4de878b587fbf6d393f57bb59655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:05:27 +0000

    Add package fast-depends/2.4.0/fast_depends-2.4.0.tar.gz

---
+++ packages/fast-depends/fast_depends-2.4.0.tar.gz/fast_depends-2.4.0/scripts/lint.sh
@@ -0,0 +1,8 @@
+echo "Running mypy..."
+mypy fast_depends
+
+echo "Running ruff linter (isort, flake, pyupgrade, etc. replacement)..."
+ruff fast_depends tests --fix
+
+echo "Running ruff formatter (black replacement)..."
+ruff format tests
