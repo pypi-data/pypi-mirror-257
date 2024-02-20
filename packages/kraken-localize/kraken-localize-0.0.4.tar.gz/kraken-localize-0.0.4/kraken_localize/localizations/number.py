95c0bd4a21c8125753789dbfe4ea34e1bbfb5981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:33 +0000

    Add package kraken-localize/0.0.4/kraken-localize-0.0.4.tar.gz

---
+++ packages/kraken-localize/kraken-localize-0.0.4.tar.gz/kraken-localize-0.0.4/kraken_localize/localizations/number.py
@@ -0,0 +1,71 @@
+
+import copy
+from kraken_localize.helpers import json
+from kraken_localize.helpers import things
+import os
+from babel.numbers import format_decimal, format_currency, format_number, format_compact_decimal, format_percent
+
+import datetime
+
+from pint import UnitRegistry
+
+
+def _get_number(code, language='en_US', digits=None, unitCode=None):
+    """
+    """
+    if isinstance(code, dict):
+        value = code.get('value', None)
+        unitCode = code.get('unitCode', None)
+    else:
+        value=code
+
+
+    if not unitCode:
+        digits = '#' + str(digits ) if digits else None
+        result = format_decimal(value, locale=language, format= digits )
+        return result
+
+    ureg = UnitRegistry(auto_reduce_dimensions=True)
+    value = value * ureg.parse_units(unitCode)    
+    
+    result = value.format_babel(locale=language)
+    return result
+
+def _get_unitCode(code, language='en_US', digits=None, unitCode=None):
+    """Returns unit of measure
+    """
+    if isinstance(code, dict):
+        value = code.get('value', None)
+        unitCode = code.get('unitCode', None)
+    else:
+        value=code
+
+
+    if not unitCode:
+        digits = '#' + str(digits ) if digits else None
+        result = format_decimal(value, locale=language, format= digits )
+        return result
+
+    ureg = UnitRegistry(auto_reduce_dimensions=True)
+    value = value * ureg.parse_units(unitCode)    
+
+    result = value.units.format_babel(locale=language)
+    return result
+    
+
+
+def _get_number2(code, language='en_US', digits=None, unitCode=None):
+    """
+    """
+    if isinstance(code, dict):
+        value = code.get('value', None)
+        unitCode = code.get('unitCode', None)
+    else:
+        value=code
+
+    digits = '#' + str(digits ) if digits else None
+    result = format_decimal(value, locale=language, format= digits )
+
+
+    return result + ' ' + str(unitCode) if unitCode else result
+
