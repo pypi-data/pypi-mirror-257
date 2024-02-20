4170d3e9792110614cbd6c4ae69c1609174d1abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 05:03:51 +0000

    Add package ts-deepscan/2.1.2/ts_deepscan-2.1.2-py3-none-any.whl

---
+++ packages/ts-deepscan/ts_deepscan-2.1.2-py3-none-any.whl/ts_deepscan/analyser/CryptoAnalyser.py
@@ -0,0 +1,33 @@
+import pyminr
+
+from pathlib import Path
+
+from ..analyser import FileAnalyser
+from ..commentparser.language import Lang, classify
+
+
+class CryptoAnalyser(FileAnalyser):
+    category_name = 'crypto'
+
+    def _match(self, path: Path):
+        return classify(path) != Lang.Unknown
+
+    @property
+    def options(self) -> dict:
+        return {
+            'includeCrypto': True
+        }
+
+    def analyse(self, path: Path):
+        result = []
+
+        def _report_result(algorithm, coding):
+            result.append({
+                'algorithm': algorithm,
+                'coding': coding
+            })
+
+        with path.open('rb') as fp:
+            pyminr.find_crypto_algorithms(fp.read(), _report_result)
+
+        return result
