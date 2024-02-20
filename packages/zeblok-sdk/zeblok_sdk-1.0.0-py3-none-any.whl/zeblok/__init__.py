052ca8870a5c337197c24a7aa5a224164d0a6713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:21 +0000

    Add package zeblok-sdk/1.0.0/zeblok_sdk-1.0.0-py3-none-any.whl

---
+++ packages/zeblok-sdk/zeblok_sdk-1.0.0-py3-none-any.whl/zeblok/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.0.0'
+AI_API_DOCKER_FOLDER = 'dockerhub.zeblok.io/aimodel'
+AI_PIPELINE_DOCKER_FOLDER = 'zeblok'
+
+# AVAILABLE PLAN TYPES
+PLAN_TYPE_WORKSTATION = 'Ai-WorkStation'
+PLAN_TYPE_API = 'Ai-API'
+PLAN_TYPE_CAAS = 'CAAS'
+PLAN_TYPE_MICROSERVICE = 'Ai-MicroService'
+PLAN_TYPE_ORCHESTRATION = 'Orchestration-Addon'
