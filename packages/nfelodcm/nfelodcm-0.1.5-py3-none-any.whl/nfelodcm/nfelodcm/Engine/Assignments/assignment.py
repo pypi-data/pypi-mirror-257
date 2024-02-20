70cfcd21bd2f548ed153ee6292155fe8e3246937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:05:12 +0000

    Add package nfelodcm/0.1.5/nfelodcm-0.1.5-py3-none-any.whl

---
+++ packages/nfelodcm/nfelodcm-0.1.5-py3-none-any.whl/nfelodcm/nfelodcm/Engine/Assignments/assignment.py
@@ -0,0 +1,41 @@
+from .assignments import (
+    game_id_repl, penalty_formatting, desc_based_plays
+)
+
+## a dictionary that holds assignments and the columns they add ##
+assignments = {
+    'game_id_repl' : {
+        'func' : game_id_repl,
+        'new_columns' : []
+    },
+    'penalty_formatting' : {
+        'func' : penalty_formatting,
+        'new_columns' : []
+    },
+    'desc_based_plays' : {
+        'func' : desc_based_plays,
+        'new_columns' : [
+            'qb_dropback_all',  'rush_attempt_all',
+            'play_call'
+        ]
+    }
+}
+
+def assign(df, assignment):
+    """
+    An extension on the pandas assign() function which allows the dcm to add non-standard fields
+    to the CSVs it downloads.
+    """
+    ## get the function ##
+    func = assignments[assignment]['func']
+    ## return the assignment ##
+    return func(df)
+
+
+def assignment_columns_added(assignment):
+    """
+    When an assignment is used and adds columns to a dataframe not in the map,
+    assignment_columns_added() will returns a list of the columns added, so they can
+    be passed when reading from cache
+    """
+    return assignments[assignment]['new_columns']
