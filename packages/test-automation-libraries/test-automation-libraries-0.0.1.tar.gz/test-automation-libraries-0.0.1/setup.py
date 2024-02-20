4db382cafd970a3c953a94cf079e16df08ce02fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:04:52 +0000

    Add package test-automation-libraries/0.0.1/test-automation-libraries-0.0.1.tar.gz

---
+++ packages/test-automation-libraries/test-automation-libraries-0.0.1.tar.gz/test-automation-libraries-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test-automation-libraries',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'selenium',
+        'paramiko',
+        'markdown',
+    ],
+    author='Kuldip Mehta',
+    author_email='kulsmehta13@gmail.com',
+    description='Basic libraries for test autoamtion',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kulsmehta13/test-automation-libraries',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
