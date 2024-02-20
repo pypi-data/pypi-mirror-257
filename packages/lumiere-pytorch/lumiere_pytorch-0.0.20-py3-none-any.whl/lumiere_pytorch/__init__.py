6ff73fb3edca1af0d722b9ec9318ccfd54ed2c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:36 +0000

    Add package lumiere-pytorch/0.0.20/lumiere_pytorch-0.0.20-py3-none-any.whl

---
+++ packages/lumiere-pytorch/lumiere_pytorch-0.0.20-py3-none-any.whl/lumiere_pytorch/__init__.py
@@ -0,0 +1,17 @@
+from lumiere_pytorch.lumiere import (
+    ConvolutionInflationBlock,
+    AttentionInflationBlock,
+    TemporalDownsample,
+    TemporalUpsample,
+    set_time_dim_
+)
+
+from lumiere_pytorch.lumiere import Lumiere
+
+from lumiere_pytorch.mp_lumiere import (
+    MPLumiere,
+    MPConvolutionInflationBlock,
+    MPAttentionInflationBlock,
+    MPTemporalDownsample,
+    MPTemporalUpsample,
+)
