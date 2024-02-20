e4cca0c87ffd5fd5e52deee1846ec32cb8cd18b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:05:28 +0000

    Add package movoid-robotframework-selenium/1.0.0/movoid_robotframework_selenium-1.0.0.tar.gz

---
+++ packages/movoid-robotframework-selenium/movoid_robotframework_selenium-1.0.0.tar.gz/movoid_robotframework_selenium-1.0.0/movoid_robotframework_selenium/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/30 21:16
+# Description   : 
+"""
+from .main import RobotSeleniumBasic
