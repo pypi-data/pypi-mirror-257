bfe5d7b0abe33aed87807b9689009e6b1a41a6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:28 +0000

    Add package xdatasets/0.3.5/xdatasets-0.3.5-py3-none-any.whl

---
+++ packages/xdatasets/xdatasets-0.3.5-py3-none-any.whl/xdatasets/__init__.py
@@ -0,0 +1,8 @@
+"""Easy access to Earth observation datasets with xarray."""
+
+from .core import Query
+from .tutorial import list_available_datasets, load_dataset
+
+__author__ = """Sebastien Langlois"""
+__email__ = "sebastien.langlois62@gmail.com"
+__version__ = "0.3.5"
