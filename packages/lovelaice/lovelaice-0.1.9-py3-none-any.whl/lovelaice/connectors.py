dc7ff7b7560580c8d3af9ae3022a5571a540eeef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:07:07 +0000

    Add package lovelaice/0.1.9/lovelaice-0.1.9-py3-none-any.whl

---
+++ packages/lovelaice/lovelaice-0.1.9-py3-none-any.whl/lovelaice/connectors.py
@@ -0,0 +1,34 @@
+import os
+import abc
+from typing import Coroutine
+
+from .models import Message
+
+
+class LLM(abc.ABC):
+    @abc.abstractmethod
+    def query(self, messages: list[Message]):
+        pass
+
+    def query_sync(self, messages: list[Message]) -> str:
+        return "".join(self.query(messages))
+
+
+try:
+    from mistralai.client import MistralClient
+    from mistralai.models.chat_completion import ChatMessage
+
+
+    class MistralLLM(LLM):
+        def __init__(self, model:str, api_key:str=None) -> None:
+            self.client = MistralClient(api_key=api_key or os.getenv("MISTRAL_API_KEY"))
+            self.model = model
+
+        def query(self, messages: list[Message]):
+            for response in self.client.chat_stream(
+                self.model, [ChatMessage(role=m.role, content=m.content) for m in messages]
+            ):
+                yield response.choices[0].delta.content
+
+except ImportError:
+    pass
