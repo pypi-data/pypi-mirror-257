a1df7c88920110da802c2a876b67a009057fb5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:00 +0000

    Add package owega/5.0.4/owega-5.0.4.tar.gz

---
+++ packages/owega/owega-5.0.4.tar.gz/owega-5.0.4/owega/OweHandlers/handle_genconf.py
@@ -0,0 +1,17 @@
+"""Handle /genconf."""
+from ..utils import genconfig
+from ..OwegaSession import OwegaSession as ps
+
+
+# generates config file
+def handle_genconf(temp_file, messages, given="", temp_is_temp=False):
+    """Handle /genconf."""
+    genconfig()
+    return messages
+
+
+item_genconf = {
+    "fun": handle_genconf,
+    "help": "generates a sample config file",
+    "commands": ["genconf"],
+}
