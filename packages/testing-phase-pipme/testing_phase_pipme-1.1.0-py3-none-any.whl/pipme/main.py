1eae8dbb1d7da1a1196eed2804a12d7e98addbcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:05:29 +0000

    Add package testing-phase-pipme/1.1.0/testing_phase_pipme-1.1.0-py3-none-any.whl

---
+++ packages/testing-phase-pipme/testing_phase_pipme-1.1.0-py3-none-any.whl/pipme/main.py
@@ -0,0 +1,29 @@
+import os
+import check_inits
+import get_installed_packages
+
+def run_cli(shell_path:str=os.getcwd()):
+    """ 
+    this function runs when user runs "pipme" in the shell 
+    """
+    print("HELLO", shell_path) 
+
+    # ask confirmation if this is the root folder where they want to pip
+
+    # ask if they registered at pypi.org and have an access token ready or not
+
+    # create __init__.py files to each subfolder
+    # this creates __init_.py files inside all subfolders if it doesnt already exist
+    check_inits.check_all_folders_got_init(shell_path)
+
+
+    # get all packages imported into the folder
+    packages_installed = get_installed_packages.get_user_installed_packages(shell_path)
+    # this will go into the requirements array-------->>
+
+    
+
+
+if __name__=="__main__":
+    current_shell_path = os.getcwd()
+    run_cli(current_shell_path)
