dce1c7b08d546aac73bc4f7f43c6dd830a63a9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:07:08 +0000

    Add package polykin/0.4.1/polykin-0.4.1-py3-none-any.whl

---
+++ packages/polykin/polykin-0.4.1-py3-none-any.whl/polykin/copolymerization/__init__.py
@@ -0,0 +1,15 @@
+# PolyKin: A polymerization kinetics library for Python.
+#
+# Copyright Hugo Vale 2023
+
+"""
+This module implements classes to analyze and fit copolymerization data.
+
+"""
+
+from .binary import *
+from .copodataset import *
+from .fitting import *
+from .multicomponent import *
+from .penultimate import *
+from .terminal import *
