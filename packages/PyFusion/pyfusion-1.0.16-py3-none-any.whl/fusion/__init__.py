491e3b8bbdc89cfc43742352823e695503ec9e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:05:32 +0000

    Add package PyFusion/1.0.16/pyfusion-1.0.16-py3-none-any.whl

---
+++ packages/PyFusion/pyfusion-1.0.16-py3-none-any.whl/fusion/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for fusion."""
+
+__author__ = """Fusion Devs"""
+__email__ = "fusion_developers@jpmorgan.com"
+__version__ = "1.0.16"
+
+from fusion.fusion import Fusion, FusionCredentials
+from fusion.fs_sync import fsync
