87fdadce8d5257ad205f49d753e7800ac26054d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:04:55 +0000

    Add package volkswagencarnet/4.4.67/volkswagencarnet-4.4.67.tar.gz

---
+++ packages/volkswagencarnet/volkswagencarnet-4.4.67.tar.gz/volkswagencarnet-4.4.67/tests/fixtures/connection.py
@@ -0,0 +1,64 @@
+"""Session and connection related test fixtures."""
+
+import os
+
+import pytest
+import pytest_asyncio
+from aiohttp import CookieJar, ClientSession
+
+from volkswagencarnet.vw_connection import Connection
+from volkswagencarnet.vw_timer import TimerData
+from volkswagencarnet.vw_utilities import json_loads
+from .constants import timers_json_file, resource_path, timers_no_settings_json_file
+
+
+@pytest_asyncio.fixture
+async def session():
+    """Client session that can be used in tests."""
+    jar = CookieJar()
+    jar.load(os.path.join(resource_path, "dummy_cookies.pickle"))
+    sess = ClientSession(headers={"Connection": "keep-alive"}, cookie_jar=jar)
+    yield sess
+    await sess.close()
+
+
+@pytest.fixture
+def connection(session):
+    """Real connection for integration tests."""
+    return Connection(session=session, username="", password="", country="DE", interval=999, fulldebug=True)
+
+
+class TimersConnection:
+    """Connection that has timers defined."""
+
+    def __init__(self, sess, **kwargs):
+        """Init."""
+        self._session = sess
+
+    async def doLogin(self):
+        """No-op login."""
+        return True
+
+    async def update(self):
+        """No-op update."""
+        return True
+
+    async def getTimers(self, vin):
+        """Get timers data from backend."""
+        # test with a "real" response
+        with open(timers_json_file) as f:
+            json = json_loads(f.read()).get("timer", {})
+            data = TimerData(**json)
+            return data
+
+
+class TimersConnectionNoSettings(TimersConnection):
+    """Connection that returns timers without basic settings."""
+
+    async def getTimers(self, vin):
+        """Get timers data from backend."""
+        # test with a "real" response
+        with open(timers_no_settings_json_file) as f:
+            json = json_loads(f.read()).get("timer", {})
+            data = TimerData(**json)
+            return data
