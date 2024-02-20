2e58cab735e32349634905f496fd1281c9fd79af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 18:04:24 +0000

    Add package py-checksum/0.3.2/py_checksum-0.3.2-py3-none-any.whl

---
+++ packages/py-checksum/py_checksum-0.3.2-py3-none-any.whl/py_checksum/cli.py
@@ -0,0 +1,13 @@
+from .checksum import generate_checksums_for_directory
+
+# Directory to scan, adjust to your requirement
+directory_path = '.'
+
+# Folders to exclude from the scan
+excluded_folders = ['node_modules', 'env']
+
+def main():
+    generate_checksums_for_directory(directory_path, excluded_folders)
+
+if __name__ == "__main__":
+    main()
