82683dcfa5845025b724874757afd40f3250ad25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:55 +0000

    Add package PdfLibrary/1.0.0/PdfLibrary-1.0.0.tar.gz

---
+++ packages/PdfLibrary/PdfLibrary-1.0.0.tar.gz/PdfLibrary-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='PdfLibrary',
+    version='1.0.0',
+    packages=find_packages(),
+    author='jayesh vani',
+    author_email='jayeshvani2@gmail.com',
+    description='extract text from pdf file ',
+    # url='https://github.com/yourusername/my_library',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
