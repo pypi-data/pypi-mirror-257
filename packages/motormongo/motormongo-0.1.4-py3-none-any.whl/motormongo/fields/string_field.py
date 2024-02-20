c92ad3c893570f81805019f182dd2b35e8c618e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 05:04:59 +0000

    Add package motormongo/0.1.4/motormongo-0.1.4-py3-none-any.whl

---
+++ packages/motormongo/motormongo-0.1.4-py3-none-any.whl/motormongo/fields/string_field.py
@@ -0,0 +1,29 @@
+from motormongo.fields.field import Field
+
+
+class StringField(Field):
+    def __init__(self, min_length=None, max_length=None, regex=None, **kwargs):
+        super().__init__(type=str, **kwargs)
+        self.min_length = min_length
+        self.max_length = max_length
+        self.regex = regex
+
+    def __set__(self, obj, value):
+        if value is not None:
+            if not isinstance(value, str):
+                raise ValueError(
+                    f"Value for {self.name} must be a string: {value} is of type {type(value)}"
+                )
+            if self.min_length is not None and len(value) < self.min_length:
+                raise ValueError(
+                    f"Value for {self.name} must be at least {self.min_length} characters: '{value}'"
+                )
+            if self.max_length is not None and len(value) > self.max_length:
+                raise ValueError(
+                    f"Value for {self.name} must be no more than {self.max_length} characters: '{value}'"
+                )
+            if self.regex and not self.regex.match(value):
+                raise ValueError(
+                    f"Value for {self.name} does not match required regex pattern ({self.regex}): '{value}'"
+                )
+        super().__set__(obj, value)
