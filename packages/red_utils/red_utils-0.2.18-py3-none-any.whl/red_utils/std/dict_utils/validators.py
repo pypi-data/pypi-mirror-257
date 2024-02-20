c00b3b03626fd9eb741b6a8d8975df131d5fcb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:04:50 +0000

    Add package red_utils/0.2.18/red_utils-0.2.18-py3-none-any.whl

---
+++ packages/red_utils/red_utils-0.2.18-py3-none-any.whl/red_utils/std/dict_utils/validators.py
@@ -0,0 +1,31 @@
+"""Functions to validate inputs for other `red_utils.std.dict_utils` methods."""
+
+from __future__ import annotations
+
+from typing import Any
+
+def validate_dict(_dict: dict[str, str] = None) -> dict[Any, Any]:
+    """Validates an input dict.
+
+    Params:
+        _dict (dict): The Python `dict` to validate
+
+    Returns
+    -------
+        (dict): A validated `dict`
+
+    Raises
+    ------
+        ValueError: If _dict is `None` or an invalid type
+        TypeError: When `_dict`'s type is not `dict`
+
+    """
+    if _dict is None:
+        raise ValueError("Missing dict to evaluate")
+
+    if not isinstance(_dict, dict):
+        raise TypeError(
+            f"Invalid type for input _dict: ({type(_dict)}). Must be dict[str, str]"
+        )
+
+    return _dict
---
+++ packages/red_utils/red_utils-0.2.18-py3-none-any.whl/red_utils/std/path_utils/__init__.py
@@ -0,0 +1,14 @@
+"""Utilities for interacting with the Python `pathlib` module."""
+
+from __future__ import annotations
+
+from .constants import VALID_RETURN_TYPES
+from .operations import (
+    crawl_dir,
+    delete_path,
+    ensure_dirs_exist,
+    export_json,
+    file_ts,
+    list_files,
+    scan_dir,
+)
