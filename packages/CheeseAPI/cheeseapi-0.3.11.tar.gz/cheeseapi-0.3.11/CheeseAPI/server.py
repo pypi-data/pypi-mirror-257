5d0fb00e1db8a1e6f39c915dabd9c24f6a3bbfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:04:47 +0000

    Add package CheeseAPI/0.3.11/cheeseapi-0.3.11.tar.gz

---
+++ packages/CheeseAPI/cheeseapi-0.3.11.tar.gz/cheeseapi-0.3.11/CheeseAPI/server.py
@@ -0,0 +1,23 @@
+import os
+
+from CheeseType import IPv4, Port, NonNegativeInt
+
+class Server:
+    def __init__(self) -> None:
+        self.host: IPv4 = '0.0.0.0'
+        self.port: Port = 5214
+        self._workers: NonNegativeInt = 1
+        self.static: str | None = None
+        self.backlog: int = 128
+
+    @property
+    def workers(self) -> NonNegativeInt:
+        return self._workers
+
+    @workers.setter
+    def workers(self, value):
+        value = NonNegativeInt(value)
+        if value == 0:
+            self._workers = os.cpu_count() * 2 + 1
+        else:
+            self._workers = value
