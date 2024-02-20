2b661ec1d2c8d5b41b037cbd63e2666ca70eafa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:04:42 +0000

    Add package gai-common/0.73/gai-common-0.73.tar.gz

---
+++ packages/gai-common/gai-common-0.73.tar.gz/gai-common-0.73/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+version_file = os.path.join(base_dir, 'VERSION')
+with open(version_file, 'r') as f:
+    VERSION = f.read().strip()
+
+setup(
+    name='gai-common',
+    version=VERSION,
+    author="kakkoii1337",
+    author_email="kakkoii1337@gmail.com",
+    packages=find_packages(),
+    description = """""",
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        "Development Status :: 3 - Alpha",        
+        'License :: OSI Approved :: MIT License',
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",        
+        'Operating System :: OS Independent',
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",        
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",        
+    ],
+    python_requires='>=3.10',        
+    install_requires=[
+    ],
+
+)
