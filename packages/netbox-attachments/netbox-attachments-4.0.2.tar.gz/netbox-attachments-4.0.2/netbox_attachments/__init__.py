9fe4d0eda4a747ade620eb4e3e8a527e70d414d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:18 +0000

    Add package netbox-attachments/4.0.2/netbox-attachments-4.0.2.tar.gz

---
+++ packages/netbox-attachments/netbox-attachments-4.0.2.tar.gz/netbox-attachments-4.0.2/netbox_attachments/__init__.py
@@ -0,0 +1,23 @@
+from extras.plugins import PluginConfig
+
+from .version import __version__
+
+
+class NetBoxAttachmentsConfig(PluginConfig):
+    name = 'netbox_attachments'
+    verbose_name = 'Netbox Attachments'
+    description = 'Netbox plugin to manage attachments for any model'
+    version = __version__
+    author = 'Jan Krupa'
+    base_url = 'netbox-attachments'
+    default_settings = {
+        'apps': ['dcim', 'ipam', 'circuits', 'tenancy', 'virtualization', 'wireless'],
+        'display_default': "additional_tab",
+        'display_setting': {}
+    }
+    required_settings = []
+    min_version = '3.5.0'
+    max_version = '3.7.99'
+
+
+config = NetBoxAttachmentsConfig
