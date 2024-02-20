0bb04c896f3648dfa31c2994fcf306db886ada8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:20 +0000

    Add package i3-auto-floating/1.0/i3_auto_floating-1.0.tar.gz

---
+++ packages/i3-auto-floating/i3_auto_floating-1.0.tar.gz/i3_auto_floating-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='i3_auto_floating',
+    version='1.0',
+    description='Remember floating state of i3wm windows and automatically set it when a new window of the same app is created.',
+    author='Artem Avetisyan',
+    packages=find_packages(),
+    license='MIT',
+    include_package_data=True,
+    install_requires=[
+        'i3ipc>=2.2.1',  # Example dependency with minimum version specified
+    ],
+    entry_points={
+        'console_scripts': [
+            'i3_toggle_floating = i3_auto_floating.toggle_floating:__main__',
+            'i3_auto_floating = i3_auto_floating.auto_floating:__main__',
+        ],
+    },
+)
