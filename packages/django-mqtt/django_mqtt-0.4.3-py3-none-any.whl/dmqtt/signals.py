04e9beeb4b0d38c59a40abfb5383eda27ab01586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:03 +0000

    Add package django-mqtt/0.4.3/django_mqtt-0.4.3-py3-none-any.whl

---
+++ packages/django-mqtt/django_mqtt-0.4.3-py3-none-any.whl/dmqtt/signals.py
@@ -0,0 +1,47 @@
+import fnmatch
+import functools
+import json
+import logging
+import re
+
+import django.dispatch
+
+logger = logging.getLogger(__name__)
+
+connect = django.dispatch.Signal(providing_args=["userdata", "flags", "rc"])
+message = django.dispatch.Signal(providing_args=["userdata", "msg"])
+
+
+def topic(matcher, as_json=True, **extras):
+    def wrap(func):
+        @functools.wraps(func)
+        def inner(msg, **kwargs):
+            if fnmatch.fnmatch(msg.topic, matcher):
+                logger.debug("Matched %s for %s", matcher, func)
+                if as_json:
+                    kwargs["data"] = json.loads(msg.payload.decode("utf8"))
+                func(topic=msg.topic, msg=msg, **kwargs)
+
+        message.connect(inner, **extras)
+        return inner
+
+    return wrap
+
+
+def regex(pattern, *, as_json=True, **extras):
+    matcher = re.compile(pattern)
+
+    def wrap(func):
+        @functools.wraps(func)
+        def inner(msg, **kwargs):
+            match = matcher.match(msg.topic)
+            if match:
+                logger.debug("Matched %s for %s", match, func)
+                if as_json:
+                    kwargs["data"] = json.loads(msg.payload.decode("utf8"))
+                func(topic=msg.topic, match=match, msg=msg, **kwargs)
+
+        message.connect(inner, **extras)
+        return inner
+
+    return wrap
