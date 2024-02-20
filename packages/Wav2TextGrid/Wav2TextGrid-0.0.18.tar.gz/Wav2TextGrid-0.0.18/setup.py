19a94bc554b2d196b17cbbb45553e5262f51f9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:04:57 +0000

    Add package Wav2TextGrid/0.0.18/Wav2TextGrid-0.0.18.tar.gz

---
+++ packages/Wav2TextGrid/Wav2TextGrid-0.0.18.tar.gz/Wav2TextGrid-0.0.18/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name='Wav2TextGrid',
+    version='0.0.18',
+    author="Prad Kadambi",
+    # long_description="Please find documentation at https://github.com/pkadambi/Wav2TextGrid",
+    description="A python forced alignment package",
+    author_email="pkadambi@asu.edu",
+    packages=find_packages(),
+    install_requires=required_packages,
+    classifiers=['Programming Language :: Python :: 3', 'License :: OSI Approved :: MIT License', 'Operating System :: OS Independent'],
+    python_requires='>=3.10',
+    entry_points={
+        'console_scripts': [
+            'wav2textgrid = Wav2TextGrid.wav2textgrid:main'
+        ]
+    }
+)
