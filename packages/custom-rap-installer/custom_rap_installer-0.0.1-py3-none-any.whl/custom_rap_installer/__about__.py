1447521318582e0ed4354b92238874b5cd863dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:05:14 +0000

    Add package custom-rap-installer/0.0.1/custom_rap_installer-0.0.1-py3-none-any.whl

---
+++ packages/custom-rap-installer/custom_rap_installer-0.0.1-py3-none-any.whl/custom_rap_installer/__about__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 KUNBUS GmbH
+# SPDX-License-Identifier: GPL-2.0-or-later
+# """Metadata of package."""
+__author__ = "Sven Sager"
+__copyright__ = "Copyright (C) 2024 KUNBUS GmbH"
+__license__ = " GPL-2.0-or-later"
+__version__ = "0.0.1"
