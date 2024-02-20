db16a1bbf797b8d9ec04ebb3855263793d432a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:05:12 +0000

    Add package polars/0.20.10/polars-0.20.10.tar.gz

---
+++ packages/polars/polars-0.20.10.tar.gz/polars-0.20.10/crates/polars-ops/src/series/ops/unique.rs
@@ -0,0 +1,52 @@
+use std::hash::Hash;
+
+use polars_core::hashing::_HASHMAP_INIT_SIZE;
+use polars_core::prelude::*;
+use polars_core::utils::NoNull;
+
+fn unique_counts_helper<I, J>(items: I) -> IdxCa
+where
+    I: Iterator<Item = J>,
+    J: Hash + Eq,
+{
+    let mut map = PlIndexMap::with_capacity_and_hasher(_HASHMAP_INIT_SIZE, Default::default());
+    for item in items {
+        map.entry(item)
+            .and_modify(|cnt| {
+                *cnt += 1;
+            })
+            .or_insert(1 as IdxSize);
+    }
+    let out: NoNull<IdxCa> = map.into_values().collect();
+    out.into_inner()
+}
+
+/// Returns a count of the unique values in the order of appearance.
+pub fn unique_counts(s: &Series) -> PolarsResult<Series> {
+    if s.dtype().to_physical().is_numeric() {
+        if s.bit_repr_is_large() {
+            let ca = s.bit_repr_large();
+            Ok(unique_counts_helper(ca.iter()).into_series())
+        } else {
+            let ca = s.bit_repr_small();
+            Ok(unique_counts_helper(ca.iter()).into_series())
+        }
+    } else {
+        match s.dtype() {
+            DataType::String => {
+                Ok(unique_counts_helper(s.str().unwrap().into_iter()).into_series())
+            },
+            DataType::Null => {
+                let ca = if s.is_empty() {
+                    IdxCa::new(s.name(), [] as [IdxSize; 0])
+                } else {
+                    IdxCa::new(s.name(), [s.len() as IdxSize])
+                };
+                Ok(ca.into_series())
+            },
+            dt => {
+                polars_bail!(opq = unique_counts, dt)
+            },
+        }
+    }
+}
