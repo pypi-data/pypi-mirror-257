8f049d93fde337b5dc5ad3c848fc99cacede9f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 18:04:20 +0000

    Add package mupy-hiro/0.0.5/mupy-hiro-0.0.5.tar.gz

---
+++ packages/mupy-hiro/mupy-hiro-0.0.5.tar.gz/mupy-hiro-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mupy-hiro',
+    version='0.0.5',
+    packages=['mupy', 'mupy.utils', 'mupy.server', 'mupy.mutation', 'mupy.test_runners'],
+    url='https://mupy.hirodiscount.com',
+    license='APACHE 2.0',
+    author='Jordan Nguejip',
+    author_email='mupy@hirodiscount.com',
+    description='A easy to use mutation analysis tool for beginners',
+    keywords=['mutation', 'testing', 'ast', 'mutation testing', 'mutation analysis', 'faults', 'fault', 'fault base '
+                                                                                                        'testing'],
+    install_requires=['flask', 'flask_cors','graphviz'],
+)
