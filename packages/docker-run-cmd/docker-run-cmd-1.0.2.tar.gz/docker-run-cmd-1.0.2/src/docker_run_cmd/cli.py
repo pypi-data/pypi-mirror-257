2ecf8cc0b58f9615a658132dd21440b8cd1cd618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:04:55 +0000

    Add package docker-run-cmd/1.0.2/docker-run-cmd-1.0.2.tar.gz

---
+++ packages/docker-run-cmd/docker-run-cmd-1.0.2.tar.gz/docker-run-cmd-1.0.2/src/docker_run_cmd/cli.py
@@ -0,0 +1,18 @@
+"""
+Main entry point.
+"""
+
+import sys
+
+from docker_run_cmd.run import unit_test
+
+def main() -> int:
+    """Main entry point."""
+    unit_test()
+    return 0
+
+if __name__ == "__main__":
+    sys.argv.append("Dockerfile")
+    sys.argv.append("--")
+    sys.argv.append("--version")
+    sys.exit(unit_test())
