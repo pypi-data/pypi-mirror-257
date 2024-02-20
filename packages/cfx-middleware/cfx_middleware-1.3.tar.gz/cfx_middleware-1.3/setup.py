e7ee09353ec2af02e43e7b78498424ba959b37f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 16:04:39 +0000

    Add package cfx-middleware/1.3/cfx_middleware-1.3.tar.gz

---
+++ packages/cfx-middleware/cfx_middleware-1.3.tar.gz/cfx_middleware-1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cfx_middleware",
+    version="1.3",
+    packages=find_packages(),
+    install_requires=["dotenv"],
+    python_requires=">=3",
+    author="Victor Dominguez",
+    author_email="e-victor_dominguez@usiglobal.com",
+    description="Provide a Middleware between the system and CFX",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://dev.azure.com/USI-Applications/USI%20Global%20Development/_git/PCD.Middleware.CFX.Python",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
