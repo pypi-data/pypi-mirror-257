268947fe7b504b2084b7b31b9ac5e0e0d59d2530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:26 +0000

    Add package richbibtext/0.1.0/richbibtext-0.1.0-py3-none-any.whl

---
+++ packages/richbibtext/richbibtext-0.1.0-py3-none-any.whl/richbibtext/__init__.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import bibtexparser
+
+
+def RichBibtextReader(source_path):
+    source_path = Path(source_path)
+    library = bibtexparser.parse_file(str(source_path))
+    json_items = []
+    for entry in library.entries:
+        json_bl = {k: v for k, v in entry.items()}
+        for _kv in json_bl["note"].split("; "):
+            kv = _kv.split(":", maxsplit=1)
+            if len(kv) == 2:
+                json_bl[kv[0].strip()] = kv[1].strip()
+        json_items.append(json_bl)
