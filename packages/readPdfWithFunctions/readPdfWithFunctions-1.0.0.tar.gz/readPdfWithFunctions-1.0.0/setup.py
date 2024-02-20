a19ccedf05d301e9f61f08b574624d717e8d6d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:04:14 +0000

    Add package readPdfWithFunctions/1.0.0/readPdfWithFunctions-1.0.0.tar.gz

---
+++ packages/readPdfWithFunctions/readPdfWithFunctions-1.0.0.tar.gz/readPdfWithFunctions-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='readPdfWithFunctions',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Harsh Jani',
+    author_email='jani99harsh@gmail.com',
+    description='read pdf file with functions',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
