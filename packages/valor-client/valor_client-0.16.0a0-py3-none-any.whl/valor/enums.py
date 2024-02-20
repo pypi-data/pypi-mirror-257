fbcdcafc6bc5da773c0197c7d31fc256a4bfffe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 14:04:52 +0000

    Add package valor-client/0.16.0a0/valor_client-0.16.0a0-py3-none-any.whl

---
+++ packages/valor-client/valor_client-0.16.0a0-py3-none-any.whl/valor/enums.py
@@ -0,0 +1,31 @@
+from enum import Enum
+
+
+class AnnotationType(str, Enum):
+    NONE = "none"
+    BOX = "box"
+    POLYGON = "polygon"
+    MULTIPOLYGON = "multipolygon"
+    RASTER = "raster"
+
+
+class TaskType(str, Enum):
+    SKIP = "skip"
+    EMPTY = "empty"
+    CLASSIFICATION = "classification"
+    OBJECT_DETECTION = "object-detection"
+    SEMANTIC_SEGMENTATION = "semantic-segmentation"
+
+
+class TableStatus(str, Enum):
+    CREATING = "creating"
+    FINALIZED = "finalized"
+    DELETING = "deleting"
+
+
+class EvaluationStatus(str, Enum):
+    PENDING = "pending"
+    RUNNING = "running"
+    DONE = "done"
+    FAILED = "failed"
+    DELETING = "deleting"
