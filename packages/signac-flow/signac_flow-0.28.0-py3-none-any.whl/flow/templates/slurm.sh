e6ec021844c931f48a0cf7c1a2447485be9958d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:05:13 +0000

    Add package signac-flow/0.28.0/signac_flow-0.28.0-py3-none-any.whl

---
+++ packages/signac-flow/signac_flow-0.28.0-py3-none-any.whl/flow/templates/slurm.sh
@@ -0,0 +1,25 @@
+{% extends "base_script.sh" %}
+{% block header %}
+    {% block preamble %}
+#!/bin/bash
+#SBATCH --job-name="{{ id }}"
+        {% set memory_requested = operations | calc_memory(parallel)  %}
+        {% if memory_requested %}
+#SBATCH --mem={{ memory_requested|format_memory }}
+        {% endif %}
+        {% if partition %}
+#SBATCH --partition={{ partition }}
+        {% endif %}
+        {% set walltime = operations | calc_walltime(parallel) %}
+        {% if walltime %}
+#SBATCH -t {{ walltime|format_timedelta }}
+        {% endif %}
+        {% if job_output %}
+#SBATCH --output={{ job_output }}
+#SBATCH --error={{ job_output }}
+        {% endif %}
+    {% endblock preamble %}
+    {% block tasks %}
+#SBATCH --ntasks={{ resources.ncpu_tasks }}
+    {% endblock tasks %}
+{% endblock header %}
