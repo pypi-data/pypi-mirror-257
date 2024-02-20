6aabeae92f24d8e90a23ad8e42fb03107242e173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:27 +0000

    Add package motormongo/0.1.5/motormongo-0.1.5.tar.gz

---
+++ packages/motormongo/motormongo-0.1.5.tar.gz/motormongo-0.1.5/motormongo/fields/binary_field.py
@@ -0,0 +1,53 @@
+from typing import Callable, Optional, Union
+
+from bson import Binary
+
+from motormongo.fields.field import Field
+
+
+class BinaryField(Field):
+    def __init__(
+        self,
+        hash_function: Optional[Callable[[str], bytes]] = None,
+        return_decoded: bool = False,
+        encode: Optional[Callable[[str], bytes]] = None,
+        decode: Optional[Callable[[bytes], str]] = None,
+        **kwargs,
+    ):
+        super().__init__(type=Binary, **kwargs)
+        self.hash_function = hash_function
+        self.return_decoded = return_decoded
+        self.encode = encode if encode is not None else (lambda x: x.encode("utf-8"))
+        self.decode = decode if decode is not None else (lambda x: x.decode("utf-8"))
+
+    def __set__(self, obj, value: Union[str, bytes, Binary]):
+        # Initial type check
+        if value is not None and not isinstance(value, (str, bytes, Binary)):
+            raise ValueError(
+                f"Value must be a string, bytes object, or bson.Binary. Got {type(value).__name__}"
+            )
+
+        # Apply encoding if value is a string (and potentially hash function afterwards)
+        if isinstance(value, str):
+            value = self.encode(value)
+            if self.hash_function is not None:
+                value = self.hash_function(value)
+
+        # Convert bytes to bson.Binary for MongoDB storage
+        if isinstance(value, bytes):
+            value = Binary(value)
+
+        super().__set__(obj, value)
+
+    def __get__(self, obj, objtype=None):
+        value = super().__get__(
+            obj, objtype
+        )  # Leverage Field's __get__ for default handling
+        # Decode the value if it's stored as Binary and return_decoded is True
+        if self.return_decoded:
+            try:
+                print(f"returning decoded value {self.decode(value)}")
+                return self.decode(value)
+            except Exception as e:
+                raise ValueError(f"Error decoding Binary field: {e}")
+        return value
---
+++ packages/motormongo/motormongo-0.1.5.tar.gz/motormongo-0.1.5/motormongo/fields/datetime_field.py
@@ -0,0 +1,54 @@
+from datetime import date, datetime
+
+from motormongo.fields.field import Field
+
+
+class DateTimeField(Field):
+    def __init__(
+        self, auto_now=False, auto_now_add=False, datetime_formats=None, **kwargs
+    ):
+        super().__init__(type=(datetime, date, str, None), **kwargs)
+        self.auto_now = auto_now
+        self.auto_now_add = auto_now_add
+        # Set default formats if none provided
+        if datetime_formats is None:
+            datetime_formats = ["%Y-%m-%dT%H:%M:%S", "%Y-%m-%d"]
+        self.datetime_formats = datetime_formats
+
+    def __set_name__(self, owner, name):
+        super().__set_name__(owner, name)
+
+    def __get__(self, obj, objtype=None):
+        if self.auto_now:
+            return datetime.utcnow()
+        return super().__get__(obj, objtype)
+
+    def __set__(self, obj, value):
+        if value is not None:
+            if isinstance(value, str):
+                parsed = False
+                for fmt in self.datetime_formats:
+                    try:
+                        parsed_value = datetime.strptime(value, fmt)
+                        value = parsed_value
+                        parsed = True
+                        break  # Parsing succeeded
+                    except ValueError:
+                        continue  # Try the next format
+                if not parsed:
+                    formats_str = ", ".join(self.datetime_formats)
+                    raise ValueError(
+                        f"Value for {self.name} must be a datetime object, a date object, or a string in one of the formats: {formats_str}"
+                    )
+            elif isinstance(value, date) and not isinstance(value, datetime):
+                # Convert date to datetime for consistency
+                value = datetime(value.year, value.month, value.day)
+            elif not isinstance(value, datetime):
+                raise ValueError(
+                    f"Value for {self.name} must be a datetime object, a date object, or a string representation of datetime"
+                )
+
+        if self.auto_now or (self.auto_now_add and not obj.__dict__.get(self.name)):
+            value = datetime.utcnow()
+
+        super().__set__(obj, value)
