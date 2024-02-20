d34667f0bf3591a3c764e1908668a7a8537b8bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 17:05:24 +0000

    Add package synchros1s2/0.0.2/synchros1s2-0.0.2.tar.gz

---
+++ packages/synchros1s2/synchros1s2-0.0.2.tar.gz/synchros1s2-0.0.2/README.md
@@ -0,0 +1,17 @@
+# sync2jira
+
+## Importer le package
+
+
+- [ ] [Importer](https://gitlab.com/ex-novo-team/sync2jira/) projet
+
+```
+cd existing_repo
+git remote add origin https://gitlab.com/ex-novo-team/sync2jira.git
+git branch -M package
+git push -uf origin package
+```
+
+## Remplir le fichier de config 
+Ce script Python configure plusieurs variables et paramètres nécessaires pour Jira. Il définit des chemins de fichiers, des formats de journalisation, des informations d'authentification et des URL Jira.
+
---
+++ packages/synchros1s2/synchros1s2-0.0.2.tar.gz/synchros1s2-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='synchros1s2',
+    version='0.0.2',
+    packages=find_packages(),  # Recherche tous les packages dans le répertoire courant
+    install_requires=[
+        'requests',
+        'SQLAlchemy',
+        'pytest',
+        'setuptools'
+    ],
+    author='wefine',
+    author_email='wefine2529@ebuthor.com',
+    description='Mon package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[  # Liste de classifications pour votre package
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
