29fed36b6c23a82718dc66244d06e245f5d008fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 05:04:12 +0000

    Add package power-grid-model-io/1.2.68/power_grid_model_io-1.2.68-py3-none-any.whl

---
+++ packages/power-grid-model-io/power_grid_model_io-1.2.68-py3-none-any.whl/power_grid_model_io/mappings/value_mapping.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: Contributors to the Power Grid Model project <powergridmodel@lfenergy.org>
+#
+# SPDX-License-Identifier: MPL-2.0
+"""
+Value substitution helper class
+"""
+
+from typing import Dict, Optional, Union
+
+from power_grid_model_io.mappings.field_mapping import FieldMapping
+
+Value = Union[int, float, str, bool]
+
+#             attr      key    value
+Values = Dict[str, Dict[Value, Value]]
+
+
+# pylint: disable=too-few-public-methods
+class ValueMapping(FieldMapping[Dict[Value, Value]]):
+    """
+    Value substitution helper class
+    """
+
+    def __init__(self, mapping: Optional[Values] = None):
+        super().__init__(mapping=mapping)
+        if mapping is not None:
+            self._log.debug(
+                "Set value mapping", n_attributes=len(mapping), n_mappings=sum(len(m) for m in mapping.values())
+            )
+
+    def get_substitutions(self, attr: str, table: Optional[str] = None) -> Dict[Value, Value]:
+        """
+        Find the substitutions for a given attribute.
+        """
+        return self._get_mapping(attr=attr, table=table)
