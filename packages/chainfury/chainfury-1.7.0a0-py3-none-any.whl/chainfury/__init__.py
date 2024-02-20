7c70efcdd0e8739d993d2b4a3e112a1ce0988639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 05:03:55 +0000

    Add package chainfury/1.7.0a0/chainfury-1.7.0a0-py3-none-any.whl

---
+++ packages/chainfury/chainfury-1.7.0a0-py3-none-any.whl/chainfury/__init__.py
@@ -0,0 +1,25 @@
+import os
+import dotenv
+
+_dotenv_fp = os.getenv("CF_DOTENV", ".env")
+if os.path.exists(_dotenv_fp):
+    dotenv.load_dotenv(_dotenv_fp)
+
+from chainfury.utils import (
+    exponential_backoff,
+    UnAuthException,
+    DoNotRetryException,
+    logger,
+    CFEnv,
+)
+from chainfury.base import Var, Node, Secret, Chain, Model, Edge
+from chainfury.core import (
+    model_registry,
+    programatic_actions_registry,
+    ai_actions_registry,
+    memory_registry,
+    AIAction,
+    Memory,
+)
+from chainfury.client import get_client
+from chainfury import components
---
+++ packages/chainfury/chainfury-1.7.0a0-py3-none-any.whl/chainfury/version.py
@@ -0,0 +1,6 @@
+__version__ = "1.7.0a0"
+_major, _minor, _patch = __version__.split(".")
+_major = int(_major)
+_minor = int(_minor)
+
+version = __version__
