cdea71d7fdf72717b7b029ae23795aab1684d3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:07:08 +0000

    Add package polykin/0.4.1/polykin-0.4.1.tar.gz

---
+++ packages/polykin/polykin-0.4.1.tar.gz/polykin-0.4.1/src/polykin/properties/__init__.py
@@ -0,0 +1,12 @@
+# PolyKin: A polymerization kinetics library for Python.
+#
+# Copyright Hugo Vale 2023
+
+"""
+This module implements methods to evaluate physical property equations and
+estimate physical properties often used in polymer reactor models.
+"""
+
+from .equations import *
+from .mixing_rules import *
+from .pvt_polymer import *
