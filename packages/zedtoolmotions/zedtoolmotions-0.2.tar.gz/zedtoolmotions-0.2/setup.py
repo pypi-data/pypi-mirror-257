03bd1e3c533c2b00363b2c8fb7468a3b7f9056c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:29 +0000

    Add package zedtoolmotions/0.2/zedtoolmotions-0.2.tar.gz

---
+++ packages/zedtoolmotions/zedtoolmotions-0.2.tar.gz/zedtoolmotions-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zedtoolmotions',
+    version='0.2',
+    packages=find_packages(),
+    description='A package for animations',
+    author='Yousseif Muhammed',
+    author_email='me@usif.tech',
+    url='https://github.com/yourusername/zedtoolmotions',
+    license='MIT',
+    install_requires=[
+        'colorama',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
---
+++ packages/zedtoolmotions/zedtoolmotions-0.2.tar.gz/zedtoolmotions-0.2/zedtoolmotions/animation.py
@@ -0,0 +1,42 @@
+import time
+import colorama
+
+class Animation:
+    def __init__(self):
+        pass
+
+    def animate(self):
+        text = f"""
+{colorama.Fore.RESET}-----------------------------------------------------------------------
+{colorama.Fore.LIGHTGREEN_EX}                                       ████████████████████████████████               
+                                       ████████████████████████████████               
+  ███                                  ████████████████████████████████               
+  █████                                ████████████████████████████████               
+   ██████                              ████             █             █               
+     █████                             ███             █              █               
+       █████                           ██████████     ███████    ██████               
+         █████                         █████████    █████████    ██████               
+          █████                        ███████     █████████     ██████               
+        █████                          ██████     ██████████    ███████               
+      ██████                           █████    ████████████    ███████               
+     █████                             ███     █████████████    ███████               
+   █████                               ██             █████     ███████               
+ █████                                 █              █████     ███████               
+████             █████████████████     ████████████████████████████████               
+                 █████████████████     ████████████████████████████████               
+                 █████████████████     ████████████████████████████████               
+                                       ████████████████████████████████               
+{colorama.Fore.RESET}-----------------------------------------------------------------------
+{colorama.Fore.RED}╦ ╦┌─┐┬ ┬┌─┐┌─┐┌─┐┬┌─┐  ╔╦╗┬ ┬┬ ┬┌─┐┌┬┐┌┬┐┌─┐┌┬┐
+╚╦╝│ ││ │└─┐└─┐├┤ │├┤   ║║║│ │├─┤├─┤││││││├┤  ││
+ ╩ └─┘└─┘└─┘└─┘└─┘┴└    ╩ ╩└─┘┴ ┴┴ ┴┴ ┴┴ ┴└─┘─┴┘
+{colorama.Fore.RESET}-----------------------------------------------------------------------
+{colorama.Fore.LIGHTRED_EX}[$] Developed By {colorama.Fore.RESET}" {colorama.Fore.GREEN}Yousseif Muhammed {colorama.Fore.RESET}"
+{colorama.Fore.LIGHTYELLOW_EX}[$] Telegram Account & Channel: {colorama.Fore.RESET}({colorama.Fore.GREEN} @YousseifMuhammed {colorama.Fore.RESET}| {colorama.Fore.GREEN}@ZEDTOOL {colorama.Fore.RESET})
+{colorama.Fore.CYAN}[$] Don't Change Rights, you donkey ;<)
+{colorama.Fore.RESET}-----------------------------------------------------------------------
+"""
+
+        for char in text:
+            print(char, end='', flush=True)
+            time.sleep(0.1)  # Adjust the delay as needed
