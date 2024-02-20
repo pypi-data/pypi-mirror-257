23ba21a6659c450dbf8c4a2161a1240d3c615ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 17:05:08 +0000

    Add package python-zlib-fork-safe/66.0.3/python-zlib-fork-safe-66.0.3.tar.gz

---
+++ packages/python-zlib-fork-safe/python-zlib-fork-safe-66.0.3.tar.gz/python-zlib-fork-safe-66.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class InstallCommand(install):
+    def run(self):
+        raise RuntimeError("You are trying to install a stub package python-zlib-fork-safe. Maybe you are using the wrong pypi?")
+
+
+setup(
+    name='python-zlib-fork-safe',
+    version='66.0.3',
+    author='Yandex',
+    author_email='security@yandex-team.ru',
+    url='https://ya.ru',
+    readme="README.md",
+    long_description="""This is a security placeholder package.
+If you want to claim this name for legitimate purposes,
+please contact us at security@yandex-team.ru or pypi-security@yandex-team.ru""",
+    long_description_content_type='text/markdown',
+    description='A package to prevent Dependency Confusion attacks against Yandex.',
+    cmdclass={
+        'install': InstallCommand,
+    },
+)
