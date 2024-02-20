c1b6f0f17af316cd53896695270cda0fb4363807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:03:59 +0000

    Add package acellera-acemodel/0.0.0/acellera-acemodel-0.0.0.tar.gz

---
+++ packages/acellera-acemodel/acellera-acemodel-0.0.0.tar.gz/acellera-acemodel-0.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="acellera-acemodel",
+        version="0.0.0",
+        author="Acellera",
+        author_email="info@acellera.com",
+        description="",
+        long_description="",
+        long_description_content_type="text/markdown",
+        url="",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: POSIX :: Linux",
+        ],
+        packages=setuptools.find_packages(),
+        zip_safe=False,
+        install_requires=["python"],
+    )
