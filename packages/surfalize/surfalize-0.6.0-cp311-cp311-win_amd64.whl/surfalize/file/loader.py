1c662cca8d034642dad2aa26d083d507e5fe75cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:15 +0000

    Add package surfalize/0.6.0/surfalize-0.6.0-cp311-cp311-win_amd64.whl

---
+++ packages/surfalize/surfalize-0.6.0-cp311-cp311-win_amd64.whl/surfalize/file/loader.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from ..exceptions import UnsupportedFileFormatError
+
+from .vk import read_vk4, read_vk6_vk7
+from .plu import read_plu
+from .plux import read_plux
+from .sur import read_sur
+from .zmg import read_zmg
+from .opd import read_opd
+from .xyz import read_xyz
+
+dispatcher = {
+    '.sur': read_sur,
+    '.vk4': read_vk4,
+    '.vk6': read_vk6_vk7,
+    '.vk7': read_vk6_vk7,
+    '.plu': read_plu,
+    '.plux': read_plux,
+    '.zmg': read_zmg,
+    '.opd': read_opd,
+    '.xyz': read_xyz
+}
+
+supported_formats = list(dispatcher.keys())
+
+def load_file(filepath):
+    filepath = Path(filepath)
+    try:
+        loader = dispatcher[filepath.suffix]
+    except KeyError:
+        raise UnsupportedFileFormatError(f"File format {filepath.suffix} is currently not supported.")
+    return loader(filepath)
