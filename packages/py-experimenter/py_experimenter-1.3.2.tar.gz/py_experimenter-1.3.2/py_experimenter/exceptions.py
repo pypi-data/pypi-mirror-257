9576df296aa2289155e01e9d008bf0ea014e69c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:05:20 +0000

    Add package py-experimenter/1.3.2/py_experimenter-1.3.2.tar.gz

---
+++ packages/py-experimenter/py_experimenter-1.3.2.tar.gz/py_experimenter-1.3.2/py_experimenter/exceptions.py
@@ -0,0 +1,66 @@
+class PyExperimenterError(Exception):
+    pass
+
+
+class ParameterCombinationError(PyExperimenterError):
+    pass
+
+
+class DatabaseError(PyExperimenterError):
+    pass
+
+
+class DatabaseCreationError(DatabaseError):
+    pass
+
+
+class DatabaseConnectionError(DatabaseError):
+    pass
+
+
+class DatabaseQueryError(DatabaseError):
+    pass
+
+
+class EmptyFillDatabaseCallError(DatabaseError):
+    pass
+
+
+class CreatingTableError(DatabaseError):
+    pass
+
+
+class InvalidResultFieldError(DatabaseError):
+    pass
+
+
+class InvalidLogFieldError(DatabaseError):
+    pass
+
+
+class TableHasWrongStructureError(CreatingTableError):
+    pass
+
+
+class NoExperimentsLeftException(PyExperimenterError):
+    pass
+
+
+class ConfigError(PyExperimenterError):
+    pass
+
+
+class NoConfigFileError(ConfigError, FileNotFoundError):
+    pass
+
+
+class InvalidConfigError(ConfigError):
+    pass
+
+
+class InvalidValuesInConfiguration(ConfigError):
+    pass
+
+
+class MissingLogTableError(ConfigError):
+    pass
