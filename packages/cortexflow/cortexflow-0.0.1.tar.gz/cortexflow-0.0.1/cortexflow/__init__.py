0bf1ade31a94e382bae0006aec8fc066f36a8a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 04:04:22 +0000

    Add package cortexflow/0.0.1/cortexflow-0.0.1.tar.gz

---
+++ packages/cortexflow/cortexflow-0.0.1.tar.gz/cortexflow-0.0.1/cortexflow/__init__.py
@@ -0,0 +1,8 @@
+from .cortexflow import cross_validator
+from .cortexflow import regressor
+from .cortexflow import classifier
+from .cortexflow import neural_classification_evaluation
+from .cortexflow import neural_regression_evaluation
+
+
+
