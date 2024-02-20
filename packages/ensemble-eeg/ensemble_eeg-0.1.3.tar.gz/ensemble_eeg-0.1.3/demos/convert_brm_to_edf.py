6f24c93823cd46599ae21f2a6524440aa0143fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 03:03:58 +0000

    Add package ensemble-eeg/0.1.3/ensemble_eeg-0.1.3.tar.gz

---
+++ packages/ensemble-eeg/ensemble_eeg-0.1.3.tar.gz/ensemble_eeg-0.1.3/demos/convert_brm_to_edf.py
@@ -0,0 +1,8 @@
+import glob
+import brm_to_edf
+
+# !!! change this to the location of the brm files you want to convert
+files = glob.glob("path/2/brm/files")
+
+for file in files:
+    brm_to_edf.convert_brm_to_edf(file, is_fs_64hz=True)
