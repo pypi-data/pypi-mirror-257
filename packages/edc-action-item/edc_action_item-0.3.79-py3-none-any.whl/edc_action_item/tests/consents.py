c0dd6fac7490f9b6794c01351fa5c44b529f14e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-20 06:06:06 +0000

    Add package edc-action-item/0.3.79/edc_action_item-0.3.79-py3-none-any.whl

---
+++ packages/edc-action-item/edc_action_item-0.3.79-py3-none-any.whl/edc_action_item/tests/consents.py
@@ -0,0 +1,14 @@
+from edc_consent.consent_definition import ConsentDefinition
+from edc_protocol.research_protocol_config import ResearchProtocolConfig
+
+consent_v1 = ConsentDefinition(
+    model="edc_action_item.subjectconsent",
+    start=ResearchProtocolConfig().study_open_datetime,
+    end=ResearchProtocolConfig().study_close_datetime,
+    gender=["M", "F"],
+    updates_versions=[],
+    version="1",
+    age_min=16,
+    age_max=64,
+    age_is_adult=18,
+)
