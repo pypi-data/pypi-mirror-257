e6584e315afdc8a223fae62ab0bf56608eb56721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:10 +0000

    Add package ca-api-wrapper/1.0.0/ca-api-wrapper-1.0.0.tar.gz

---
+++ packages/ca-api-wrapper/ca-api-wrapper-1.0.0.tar.gz/ca-api-wrapper-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ca-api-wrapper',
+    version='1.0.0',
+    author='Vick Mu',
+    author_email='arbi3400@gmail.com',
+    packages=find_packages(),
+    license='LICENSE',
+    description='A NON-OFFICIAL Python package for accessing the ChannelAdvisor RESTful API',
+    long_description=open('README.md').read(),
+    install_requires=[
+        # Your package dependencies
+    ],
+)
