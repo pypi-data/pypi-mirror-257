db394a8eb662e5713945b336615b01994a0336aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:38 +0000

    Add package express-py/2024.2.21.post0/express_py-2024.2.21.post0-py3-none-any.whl

---
+++ packages/express-py/express_py-2024.2.21.post0-py3-none-any.whl/express/parsers/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+
+class BaseParser(object):
+    """
+    Base Parser class.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.version = kwargs.get("version", None)
+
+    def _get_file_content(self, file_path):
+        """
+        Returns the content of a given file.
+
+        Args:
+            file_path (str): file path.
+
+        Returns:
+             str
+        """
+        content = ""
+        if file_path and os.path.exists(file_path):
+            with open(file_path) as f:
+                content = f.read()
+        return content
