3cb49eeb204be9ac48fdc4f24fd41694009aa254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:04:35 +0000

    Add package aiosfstream-ng/0.7.5/aiosfstream-ng-0.7.5.tar.gz

---
+++ packages/aiosfstream-ng/aiosfstream-ng-0.7.5.tar.gz/aiosfstream-ng-0.7.5/aiosfstream_ng/_metadata.py
@@ -0,0 +1,13 @@
+"""Package metadata"""
+TITLE = "aiosfstream-ng"
+DESCRIPTION = "Salesforce Streaming API client for asyncio"
+KEYWORDS = "salesforce asyncio aiohttp comet cometd bayeux push streaming"
+URL = "https://github.com/bensnyde/aiosfstream-ng"
+PROJECT_URLS = {
+    "CI": "https://travis-ci.org/robertmrk/aiosfstream",
+    "Coverage": "https://coveralls.io/github/robertmrk/aiosfstream",
+    "Docs": "http://aiosfstream.readthedocs.io/"
+}
+VERSION = "0.7.5"
+AUTHOR = "Benton Snyder"
+AUTHOR_EMAIL = "benton@bensnyde.me"
