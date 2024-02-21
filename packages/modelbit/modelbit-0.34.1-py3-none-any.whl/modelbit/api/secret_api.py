5277eca89d45a06d5bbe2d763a89390583fd64e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 07:05:29 +0000

    Add package modelbit/0.34.1/modelbit-0.34.1-py3-none-any.whl

---
+++ packages/modelbit/modelbit-0.34.1-py3-none-any.whl/modelbit/api/secret_api.py
@@ -0,0 +1,30 @@
+import base64
+import logging
+from typing import Any, Dict, Optional
+
+from .api import MbApi
+
+logger = logging.getLogger(__name__)
+
+
+class SecretDesc:
+  secretValue: Optional[bytes] = None
+
+  def __init__(self, data: Dict[str, Any]):
+    if "secretValue64" in data:
+      self.secretValue = base64.b64decode(data["secretValue64"])
+
+  def __repr__(self):
+    return str(self.__dict__)
+
+
+class SecretApi:
+  api: MbApi
+
+  def __init__(self, api: MbApi):
+    self.api = api
+
+  def getSecret(self, branch: str, secretName: str, runtimeName: str) -> Optional[SecretDesc]:
+    resp = self.api.getJsonOrThrow("api/cli/v1/secrets/get",
+                                   dict(secretName=secretName, runtimeName=runtimeName, branch=branch))
+    return SecretDesc(resp['secretInfo']) if resp is not None else None
