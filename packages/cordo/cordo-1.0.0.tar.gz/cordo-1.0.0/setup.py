02690dc247c54e932afa933e0dc8d8491ed9b7bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:28 +0000

    Add package cordo/1.0.0/cordo-1.0.0.tar.gz

---
+++ packages/cordo/cordo-1.0.0.tar.gz/cordo-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('cordo/README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cordo',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+    description='Python Library to Validate and Reterive Co-ordinates data.',
+    long_description= long_description,
+    long_description_content_type='text/markdown',
+    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
