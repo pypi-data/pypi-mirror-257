74d224801da4ce606b940d124e7c3705f6d75dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:05 +0000

    Add package gradio-source-buttons/0.0.1/gradio_source_buttons-0.0.1.tar.gz

---
+++ packages/gradio-source-buttons/gradio_source_buttons-0.0.1.tar.gz/gradio_source_buttons-0.0.1/README.md
@@ -0,0 +1,10 @@
+
+# gradio_source_buttons
+A Custom Gradio component.
+
+## Example usage
+
+```python
+import gradio as gr
+from gradio_source_buttons import source_buttons
+```
---
+++ packages/gradio-source-buttons/gradio_source_buttons-0.0.1.tar.gz/gradio_source_buttons-0.0.1/demo/app.py
@@ -0,0 +1,18 @@
+
+import gradio as gr
+from gradio_source_buttons import source_buttons
+
+def add_source():
+    return [{"link": "https://www.github.com", "hostname": "Github.com"}, {"link": "https://www.hkk.de", "hostname": "HKK.de"}]
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Change the value (keep it JSON) and the front-end will update automatically.")
+    source_buttons(value=[])
+    buttons = source_buttons()
+    click = gr.Button("Click me")
+    source_buttons(value=[{"link": "https://www.google.com", "hostname": "Google.com"}, {"link": "https://www.hkk.de", "hostname": "HKK.de"}])
+
+    click.click(add_source, inputs=[], outputs=[buttons])
+
+
+demo.launch()
