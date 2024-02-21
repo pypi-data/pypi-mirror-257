68e3ba528410bfafa8ac40973cb4b7e1e2b21c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:05:39 +0000

    Add package akello/0.0.19/akello-0.0.19-py3-none-any.whl

---
+++ packages/akello/akello-0.0.19-py3-none-any.whl/akello/plugins/metriport/__init__.py
@@ -0,0 +1,59 @@
+import os
+from akello.plugins.mixin_base import PatientMixinBase
+from akello.plugins.metriport.api import Organization, Facility, Patient, Document
+
+
+"""
+TODO:
+- Get Patient and register an event log
+- attach the event ID to metriport's meta data
+- lookup the metriport event ID in the patients event logs and attach the data
+"""
+
+
+class MetriportMixinStartFHIRConsolidatedQuery(PatientMixinBase):
+
+    # Trigger a consolidated data query for the given patient
+
+    def run(self, *args, **kwargs):
+        print("running MetriportMixinStartFHIRConsolidatedQuery")        
+
+        api_key = os.getenv('METRIPORT_API_KEY', None)
+        api_url = os.getenv('METRIPORT_API_URL', None)
+        assert api_key and api_url
+
+        patient_registry = kwargs['patient_registry']
+        patient = Patient(api_key=api_key, api_url=api_url)
+    
+        resp = patient.start_fhir_consolidated_data_query(patient_registry.patient_mrn, patient_registry.id)
+        assert resp.status_code == 200 or resp.status_code == 404        
+        if resp.status_code == 404:
+            print("Patient not found in Metriport")
+            return
+        return resp
+
+
+class MetriportMixinStartDocumentQuery(PatientMixinBase):
+
+    # Triggers a document query for the specified patient across HIEs.
+
+    def run(self, *args, **kwargs):
+        print("running MetriportMixinStartDocumentQuery")        
+
+        api_key = os.getenv('METRIPORT_API_KEY', None)
+        api_url = os.getenv('METRIPORT_API_URL', None)
+        assert api_key and api_url
+
+        patient_registry = kwargs['patient_registry']
+        document = Document(api_key=api_key, api_url=api_url)
+
+        resp = document.start_document_query(patient_registry.patient_mrn, patient_registry.id) 
+        assert resp.status_code == 200 or resp.status_code == 404
+        if resp.status_code == 404:
+            print("Patient not found in Metriport")
+            return
+        return resp
+
+        
+        
+        
