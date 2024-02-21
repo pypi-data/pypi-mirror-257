56ee18da38d9fa9b0f8dc18172a63df168c48d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:04:18 +0000

    Add package design.plone.contenttypes/6.1.14/design.plone.contenttypes-6.1.14-py3-none-any.whl

---
+++ packages/design.plone.contenttypes/design.plone.contenttypes-6.1.14-py3-none-any.whl/design/plone/contenttypes/events/common.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def onModify(context, event):
+    for description in event.descriptions:
+        if "IBasic.title" in getattr(
+            description, "attributes", []
+        ) or "IDublinCore.title" in getattr(description, "attributes", []):
+            for child in context.listFolderContents():
+                child.reindexObject(idxs=["parent"])
