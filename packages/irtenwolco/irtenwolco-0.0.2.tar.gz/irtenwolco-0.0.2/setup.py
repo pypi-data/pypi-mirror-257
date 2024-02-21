67411b285418cb7e9aa98506e4f989022d4ee20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:35 +0000

    Add package irtenwolco/0.0.2/irtenwolco-0.0.2.tar.gz

---
+++ packages/irtenwolco/irtenwolco-0.0.2.tar.gz/irtenwolco-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'foobar power'
+
+setup(
+    version=VERSION,
+    author="Binyamin Haymann",
+    author_email="binyamin@ilrd.co.il",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'good_practice'],
+)
