7931eebb34068f4f9f83699a56eca91b9ce43bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:06:12 +0000

    Add package django-oscar-iyzipay/0.1.11/django_oscar_iyzipay-0.1.11-py3-none-any.whl

---
+++ packages/django-oscar-iyzipay/django_oscar_iyzipay-0.1.11-py3-none-any.whl/oscar_iyzipay/iyzipay_address/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IyzipayAddressConfig(AppConfig):
+    name = "iyzipay_address"
---
+++ packages/django-oscar-iyzipay/django_oscar_iyzipay-0.1.11-py3-none-any.whl/oscar_iyzipay/iyzipay_order/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IyzipayOrderConfig(AppConfig):
+    name = "iyzipay_order"
