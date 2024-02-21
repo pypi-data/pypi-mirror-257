c3c40099ba53b22710f6a19d83b80d6251ef3450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:04:51 +0000

    Add package foxes/0.6.1/foxes-0.6.1-py3-none-any.whl

---
+++ packages/foxes/foxes-0.6.1-py3-none-any.whl/foxes/models/partial_wakes/__init__.py
@@ -0,0 +1,10 @@
+"""
+Partial wake models.
+"""
+
+from .rotor_points import RotorPoints
+from .top_hat import PartialTopHat
+from .axiwake import PartialAxiwake
+from .distsliced import PartialDistSlicedWake
+from .grid import PartialGrid
+from .mapped import Mapped
