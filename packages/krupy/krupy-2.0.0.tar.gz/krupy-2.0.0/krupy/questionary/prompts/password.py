a7a96d8f55e916c504031f5d7cbc8fff16c9dc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:06:55 +0000

    Add package krupy/2.0.0/krupy-2.0.0.tar.gz

---
+++ packages/krupy/krupy-2.0.0.tar.gz/krupy-2.0.0/krupy/questionary/prompts/password.py
@@ -0,0 +1,70 @@
+from typing import Any
+from typing import Optional
+
+from .. import Style
+from ..constants import DEFAULT_QUESTION_PREFIX
+from ..prompts import text
+from ..question import Question
+from ..prompts.common import AnyFormattedText
+
+
+def password(
+    message: AnyFormattedText,
+    default: str = "",
+    validate: Any = None,
+    qmark: AnyFormattedText = DEFAULT_QUESTION_PREFIX,
+    style: Optional[Style] = None,
+    qcount: Optional[str] = None,
+    **kwargs: Any,
+) -> Question:
+    """A text input where a user can enter a secret which won't be displayed on the CLI.
+
+    This question type can be used to prompt the user for information
+    that should not be shown in the command line. The typed text will be
+    replaced with ``*``.
+
+    Example:
+        >>> import questionary
+        >>> questionary.password("What's your secret?").ask()
+        ? What's your secret? ********
+        'secret42'
+
+    <img alt="../../../../images/password.gif" src="../../../../images/password.gif">
+
+    This is just a really basic example, the prompt can be customised using the
+    parameters.
+
+    Args:
+        message: Question text.
+
+        default: Default value will be returned if the user just hits
+                 enter.
+
+        validate: Require the entered value to pass a validation. The
+                  value can not be submitted until the validator accepts
+                  it (e.g. to check minimum password length).
+
+                  This can either be a function accepting the input and
+                  returning a boolean, or an class reference to a
+                  subclass of the prompt toolkit Validator class.
+
+        qmark: Question prefix displayed in front of the question.
+               By default this is a ``?``.
+
+        style: A custom color and style for the question parts. You can
+               configure colors as well as font types for different elements.
+
+    Returns:
+        [Question](): Question instance, ready to be prompted (using ``.ask()``).
+    """
+
+    return text.text(
+        message,
+        default,
+        validate,
+        qmark,
+        style,
+        qcount,
+        is_password=True,
+        **kwargs,
+    )
---
+++ packages/krupy/krupy-2.0.0.tar.gz/krupy-2.0.0/krupy/questionary/prompts/press_any_key_to_continue.py
@@ -0,0 +1,78 @@
+from typing import Any
+from typing import Optional
+
+from prompt_toolkit import PromptSession
+from prompt_toolkit.formatted_text import to_formatted_text
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.keys import Keys
+from prompt_toolkit.styles import Style
+
+from ..question import Question
+from ..styles import merge_styles_default
+from ..constants import DEFAULT_QUESTION_PREFIX
+from ..prompts.common import AnyFormattedText
+
+
+def press_any_key_to_continue(
+    message: AnyFormattedText,
+    style: Optional[Style] = None,
+    qmark: AnyFormattedText = DEFAULT_QUESTION_PREFIX,
+    qcount: Optional[str] = None,
+    **kwargs: Any,
+):
+    """Wait until user presses any key to continue.
+
+    Example:
+        >>> import questionary
+        >>> questionary.press_any_key_to_continue().ask()
+         Press any key to continue...
+        ''
+
+    Args:
+        message: Question text. Defaults to ``"Press any key to continue..."``
+
+        style: A custom color and style for the question parts. You can
+               configure colors as well as font types for different elements.
+
+    Returns:
+        [Question](): Question instance, ready to be prompted (using ``.ask()``).
+    """
+    merged_style = merge_styles_default([style])
+
+    if message is None and help is None:
+        message = "Press any key to continue..."
+
+    def get_prompt_tokens():
+        tokens = []
+
+        if qcount is not None:
+            tokens.append(("class:qcount", "{} ".format(qcount)))
+        if isinstance(qmark, list):
+            for x in qmark:
+                tokens.append((x[0], "{}".format(x[1])))
+            tokens.append(("class:qmark", " {}".format("")))
+        elif isinstance(qmark, str) and len(qmark.strip()) > 0:
+            tokens.append(("class:qmark", "{} ".format(qmark)))
+        if isinstance(message, list):
+            for x in message:
+                tokens.append((x[0], "{}".format(x[1])))
+        elif isinstance(message, str) and len(message.strip()) > 0:
+            tokens.append(("class:question", "{}".format(message)))
+        tokens.append(("class:question", " {}".format("\n")))
+
+        return to_formatted_text(tokens)
+
+    def exit_with_result(event):
+        event.app.exit(result=None)
+
+    bindings = KeyBindings()
+
+    @bindings.add(Keys.Any)
+    def any_key(event):
+        exit_with_result(event)
+
+    return Question(
+        PromptSession(
+            get_prompt_tokens, key_bindings=bindings, style=merged_style, **kwargs
+        ).app
+    )
