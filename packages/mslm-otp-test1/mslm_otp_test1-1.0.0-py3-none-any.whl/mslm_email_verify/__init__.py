8be0088d5df2eac47b0e946d0e0c610228cb3aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:06:02 +0000

    Add package mslm-otp-test1/1.0.0/mslm_otp_test1-1.0.0-py3-none-any.whl

---
+++ packages/mslm-otp-test1/mslm_otp_test1-1.0.0-py3-none-any.whl/mslm_email_verify/__init__.py
@@ -0,0 +1,5 @@
+from mslm_lib.mslm_errors import RequestQuotaExceededError, MslmError
+from mslm_lib.req_opts import ReqOpts as SingleVerifyReqOpts
+from .email_verify import EmailVerify
+from .single_verify_resp import SingleVerifyResp
+from .single_verify_resp_mx import SingleVerifyRespMx
