962ed19a28f2a48a4137f5080a3faa501089ec5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:05:41 +0000

    Add package mentat/1.0.10/mentat-1.0.10.tar.gz

---
+++ packages/mentat/mentat-1.0.10.tar.gz/mentat-1.0.10/mentat/conversation.py
@@ -0,0 +1,388 @@
+from __future__ import annotations
+
+import asyncio
+import json
+import logging
+import subprocess
+from typing import List, Optional
+
+from openai import RateLimitError
+from openai.types.chat import (
+    ChatCompletionAssistantMessageParam,
+    ChatCompletionContentPartParam,
+    ChatCompletionMessageParam,
+    ChatCompletionSystemMessageParam,
+    ChatCompletionUserMessageParam,
+)
+
+from mentat.llm_api_handler import (
+    TOKEN_COUNT_WARNING,
+    count_tokens,
+    get_max_tokens,
+    prompt_tokens,
+)
+from mentat.parsers.file_edit import FileEdit
+from mentat.parsers.parser import ParsedLLMResponse
+from mentat.session_context import SESSION_CONTEXT
+from mentat.transcripts import ModelMessage, TranscriptMessage, UserMessage
+from mentat.utils import add_newline
+
+
+class MentatAssistantMessageParam(ChatCompletionAssistantMessageParam):
+    parsed_llm_response: ParsedLLMResponse
+
+
+class Conversation:
+    def __init__(self):
+        self._messages = list[ChatCompletionMessageParam]()
+
+        # This contains a list of messages used for transcripts
+        self.literal_messages = list[TranscriptMessage]()
+
+    async def display_token_count(self):
+        session_context = SESSION_CONTEXT.get()
+        stream = session_context.stream
+        config = session_context.config
+        code_context = session_context.code_context
+
+        if "gpt-4" not in config.model:
+            stream.send(
+                "Warning: Mentat has only been tested on GPT-4. You may experience"
+                " issues with quality. This model may not be able to respond in"
+                " mentat's edit format.",
+                style="warning",
+            )
+            if "gpt-3.5" not in config.model:
+                stream.send(
+                    "Warning: Mentat does not know how to calculate costs or context"
+                    " size for this model.",
+                    style="warning",
+                )
+
+        messages = self.get_messages()
+        code_message = await code_context.get_code_message(
+            prompt_tokens(
+                messages,
+                config.model,
+            ),
+            suppress_context_check=True,
+        )
+        messages.append(
+            ChatCompletionSystemMessageParam(
+                role="system",
+                content=code_message,
+            )
+        )
+        tokens = prompt_tokens(messages, config.model)
+
+        context_size = get_max_tokens()
+        if tokens + config.token_buffer > context_size:
+            _plural = len(code_context.include_files) > 1
+            _exceed = tokens > context_size
+            message: dict[tuple[bool, bool], str] = {
+                (False, False): " is close to",
+                (False, True): " exceeds",
+                (True, False): "s are close to",
+                (True, True): "s exceed",
+            }
+            stream.send(
+                f"Included file{message[(_plural, _exceed)]} token limit"
+                f" ({tokens} / {context_size}).",
+                style="warning",
+            )
+        else:
+            stream.send(
+                f"Prompt and included files token count: {tokens} / {context_size}",
+                style="info",
+            )
+
+    # The transcript logger logs tuples containing the actual message sent by the user or LLM
+    # and (for LLM messages) the LLM conversation that led to that LLM response
+    def add_transcript_message(self, transcript_message: TranscriptMessage):
+        transcript_logger = logging.getLogger("transcript")
+        transcript_logger.info(json.dumps(transcript_message))
+        self.literal_messages.append(transcript_message)
+
+    def add_user_message(self, message: str, image: Optional[str] = None):
+        """Used for actual user input messages"""
+        content: List[ChatCompletionContentPartParam] | str = message
+        if image:
+            content = [
+                {
+                    "type": "text",
+                    "text": message,
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": image,
+                    },
+                },
+            ]
+        self.add_transcript_message(UserMessage(message=content, prior_messages=None))
+        self.add_message(ChatCompletionUserMessageParam(role="user", content=content))
+
+    def add_model_message(
+        self,
+        message: str,
+        messages_snapshot: list[ChatCompletionMessageParam],
+        parsed_llm_response: ParsedLLMResponse,
+    ):
+        """Used for actual model output messages"""
+        self.add_transcript_message(
+            ModelMessage(message=message, prior_messages=messages_snapshot)
+        )
+        self.add_message(
+            MentatAssistantMessageParam(
+                parsed_llm_response=parsed_llm_response,
+                role="assistant",
+                content=message,
+            )
+        )
+
+    def add_message(self, message: ChatCompletionMessageParam):
+        """Used for adding messages to the models conversation. Does not add a left-side message to the transcript!"""
+        self._messages.append(message)
+
+    def get_messages(
+        self,
+        include_system_prompt: bool = True,
+        include_parsed_llm_responses: bool = False,
+    ) -> list[ChatCompletionMessageParam]:
+        """Returns the messages in the conversation. The system message may change throughout
+        the conversation and messages may contain additional metadata not supported by the API,
+        so it is important to access the messages through this method.
+        """
+        session_context = SESSION_CONTEXT.get()
+        config = session_context.config
+
+        _messages = [
+            (  # Remove metadata from messages by default
+                ChatCompletionAssistantMessageParam(
+                    role=msg["role"], content=msg.get("content")
+                )
+                if msg["role"] == "assistant" and include_parsed_llm_responses is False
+                else msg
+            )
+            for msg in self._messages.copy()
+        ]
+
+        if config.no_parser_prompt or not include_system_prompt:
+            return _messages
+        else:
+            parser = config.parser
+            prompt = parser.get_system_prompt()
+            prompt_message: ChatCompletionMessageParam = (
+                ChatCompletionSystemMessageParam(
+                    role="system",
+                    content=prompt,
+                )
+            )
+            return [prompt_message] + _messages
+
+    def clear_messages(self) -> None:
+        """Clears the messages in the conversation"""
+        self._messages = list[ChatCompletionMessageParam]()
+
+    async def _stream_model_response(
+        self,
+        messages: list[ChatCompletionMessageParam],
+    ) -> ParsedLLMResponse:
+        session_context = SESSION_CONTEXT.get()
+        stream = session_context.stream
+        code_file_manager = session_context.code_file_manager
+        config = session_context.config
+        parser = config.parser
+        llm_api_handler = session_context.llm_api_handler
+        cost_tracker = session_context.cost_tracker
+
+        stream.send(
+            None,
+            channel="loading",
+        )
+        response = await llm_api_handler.call_llm_api(
+            messages,
+            config.model,
+            stream=True,
+            response_format=parser.response_format(),
+        )
+        stream.send(
+            None,
+            channel="loading",
+            terminate=True,
+        )
+
+        num_prompt_tokens = prompt_tokens(messages, config.model)
+        stream.send(f"Total token count: {num_prompt_tokens}", style="info")
+        if num_prompt_tokens > TOKEN_COUNT_WARNING:
+            stream.send(
+                "Warning: LLM performance drops off rapidly at large context sizes. Use"
+                " /clear to clear context or use /exclude to exclude any uneccessary"
+                " files.",
+                style="warning",
+            )
+
+        stream.send("Streaming... use control-c to interrupt the model at any point\n")
+        async with parser.interrupt_catcher():
+            parsed_llm_response = await parser.stream_and_parse_llm_response(
+                add_newline(response)
+            )
+        # Sampler and History require previous_file_lines
+        for file_edit in parsed_llm_response.file_edits:
+            file_edit.previous_file_lines = code_file_manager.file_lines.get(
+                file_edit.file_path, []
+            )
+        if not parsed_llm_response.interrupted:
+            cost_tracker.display_last_api_call()
+        else:
+            # Generator doesn't log the api call if we interrupt it
+            cost_tracker.log_api_call_stats(
+                num_prompt_tokens,
+                count_tokens(
+                    parsed_llm_response.full_response, config.model, full_message=False
+                ),
+                config.model,
+                display=True,
+            )
+
+        messages.append(
+            ChatCompletionAssistantMessageParam(
+                role="assistant", content=parsed_llm_response.full_response
+            )
+        )
+        self.add_model_message(
+            parsed_llm_response.full_response, messages, parsed_llm_response
+        )
+
+        return parsed_llm_response
+
+    async def get_model_response(self) -> ParsedLLMResponse:
+        session_context = SESSION_CONTEXT.get()
+        stream = session_context.stream
+        config = session_context.config
+        code_context = session_context.code_context
+
+        messages_snapshot = self.get_messages()
+
+        # Get current code message
+        prompt = messages_snapshot[-1].get("content")
+        if isinstance(prompt, list):
+            text_prompts = [
+                p.get("text", "") for p in prompt if p.get("type") == "text"
+            ]
+            prompt = " ".join(text_prompts)
+        code_message = await code_context.get_code_message(
+            prompt_tokens(messages_snapshot, config.model),
+            prompt=(
+                prompt  # Prompt can be image as well as text
+                if isinstance(prompt, str)
+                else ""
+            ),
+        )
+        messages_snapshot.insert(
+            0 if config.no_parser_prompt else 1,
+            ChatCompletionSystemMessageParam(role="system", content=code_message),
+        )
+
+        try:
+            response = await self._stream_model_response(messages_snapshot)
+        except RateLimitError:
+            stream.send(
+                "Rate limit error received from OpenAI's servers using model"
+                f' {config.model}.\nUse "/config model <model_name>" to switch to a'
+                " different model.",
+                style="error",
+            )
+            return ParsedLLMResponse("", "", list[FileEdit]())
+        return response
+
+    def remaining_context(self) -> int | None:
+        ctx = SESSION_CONTEXT.get()
+        return get_max_tokens() - prompt_tokens(self.get_messages(), ctx.config.model)
+
+    def can_add_to_context(self, message: str) -> bool:
+        """
+        Whether or not the model has enough context remaining to add this message.
+        Will take token buffer into account and uses full_message=True.
+        """
+        ctx = SESSION_CONTEXT.get()
+
+        remaining_context = self.remaining_context()
+        return (
+            remaining_context is not None
+            and remaining_context
+            - count_tokens(message, ctx.config.model, full_message=True)
+            - ctx.config.token_buffer
+            > 0
+        )
+
+    async def run_command(self, command: list[str]) -> bool:
+        """
+        Runs a command and, if there is room, adds the output to the conversation under the 'system' role.
+        """
+        ctx = SESSION_CONTEXT.get()
+
+        ctx.stream.send("Running command: ", end="", style="info")
+        ctx.stream.send(" ".join(command), style="warning")
+        ctx.stream.send("Command output:", style="info")
+
+        try:
+            process = subprocess.Popen(
+                command,
+                cwd=ctx.cwd,
+                text=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+            output = list[str]()
+            while True:
+                if process.stdout is None:
+                    break
+                line = process.stdout.readline()
+                if not line:
+                    break
+                output.append(line)
+                ctx.stream.send(line, end="")
+                # This gives control back to the asyncio event loop so we can actually print what we sent
+                # Unfortunately asyncio.sleep(0) won't work https://stackoverflow.com/a/74505785
+                # Note: if subprocess doesn't flush, output can't and won't be streamed.
+                await asyncio.sleep(0.01)
+        except FileNotFoundError:
+            output = [f"Invalid command: {' '.join(command)}"]
+            ctx.stream.send(output[0])
+        output = "".join(output)
+        message = f"Command ran:\n{' '.join(command)}\nCommand output:\n{output}"
+
+        if self.can_add_to_context(message):
+            self.add_message(
+                ChatCompletionSystemMessageParam(role="system", content=message)
+            )
+            ctx.stream.send(
+                "Successfully added command output to model context.", style="success"
+            )
+            return True
+        else:
+            ctx.stream.send(
+                "Not enough tokens remaining in model's context to add command output"
+                " to model context.",
+                style="error",
+            )
+            return False
+
+    def _get_user_message(self, message: ChatCompletionUserMessageParam) -> str:
+        if not message["content"]:
+            return ""
+        elif isinstance(message["content"], str):
+            return message["content"]
+        else:
+            full = ""
+            for part in message["content"]:
+                if part["type"] == "text":
+                    full += part["text"]
+            return full
+
+    def amend(self) -> Optional[str]:
+        for i, message in reversed(list(enumerate(self._messages))):
+            if message["role"] == "user" and self._get_user_message(message):
+                self._messages = self._messages[:i]
+                return self._get_user_message(message)
---
+++ packages/mentat/mentat-1.0.10.tar.gz/mentat-1.0.10/mentat/session_stream.py
@@ -0,0 +1,140 @@
+from __future__ import annotations
+
+import asyncio
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import Any, AsyncGenerator, Dict, List, cast
+from uuid import UUID, uuid4
+
+from mentat.broadcast import Broadcast
+
+
+class StreamMessageSource(Enum):
+    SERVER = "server"
+    CLIENT = "client"
+
+
+@dataclass(slots=True)
+class StreamMessage:
+    id: UUID
+    channel: str
+    source: StreamMessageSource
+    data: Any
+    extra: Dict[str, Any]
+    created_at: datetime
+
+
+class SessionStream:
+    """
+    Used to send and receive messages from the client.
+
+    Channels and their expected behavior (starred channels are sent by the client):
+
+    default: Any data sent to the client over this channel should be displayed. Valid kwargs: color, style
+
+    *session_exit: Sent by the client, suggesting that the session should exit whenever possible.
+    client_exit: Sent by the server, client should shut down when recieved.
+    session_stopped: Sent by the server directly before server shuts down. Server can't be contacted after recieved.
+
+    loading: Used to tell the client to display a loading bar. Valid kwargs: terminate
+
+    input_request: Used to request input from the client (data unused). Valid kwargs: command_autocomplete
+    *input_request:<message_id>: Sent by the client. The channel the response to an input_request is sent over.
+
+    edits_complete: A boolean sent when edits have been completed. True if any edits were accepted.
+
+    *completion_request: Sent by the client, retrieves completions for given data. Valid kwargs: command_autocomplete
+    completion_request:<message_id>: The response to the given completion request.
+
+    default_prompt: The prefilled prompt to show on next user input request. Should be additive and reset
+    after every input request. See TerminalClient for exact implementation.
+
+    *interrupt: Sent by the client. Sent whenever client interrupts current work. Equivalent to ctrl-C
+
+    context_update: A JSON object describing the context sent whenever the context changes. JSON Schema:
+    {
+        "cwd": "Mentat's cwd",
+        "diff_context_display": "The display for the diff context",
+        "auto_context_tokens": The number of auto context tokens,
+        "features": ["List of user included features"],
+        "auto_features": ["List of auto included features"],
+        "git_diff_paths": ["List of all paths with git diffs; used to color the changed included features"],
+        "total_tokens": Total tokens in context,
+        "total_cost": Total cost so far
+    }
+    """
+
+    def __init__(self):
+        self.messages: List[StreamMessage] = []
+        self.interrupt_lock = asyncio.Lock()
+        self._broadcast = Broadcast()
+
+    def start(self):
+        self._broadcast.connect()
+
+    def stop(self):
+        self._broadcast.disconnect()
+
+    # Since there is no maximum queue size, use the synchronous version of this function instead
+    async def send_async(
+        self,
+        data: Any,
+        source: StreamMessageSource = StreamMessageSource.SERVER,
+        channel: str = "default",
+        **kwargs: Any,
+    ):
+        message = StreamMessage(
+            id=uuid4(),
+            source=source,
+            channel=channel,
+            data=data,
+            created_at=datetime.utcnow(),
+            extra=kwargs,
+        )
+
+        self.messages.append(message)
+        await self._broadcast.publish_async(channel=channel, message=message)
+
+        return message
+
+    def send(
+        self,
+        data: Any,
+        source: StreamMessageSource = StreamMessageSource.SERVER,
+        channel: str = "default",
+        **kwargs: Any,
+    ):
+        message = StreamMessage(
+            id=uuid4(),
+            source=source,
+            channel=channel,
+            data=data,
+            created_at=datetime.utcnow(),
+            extra=kwargs,
+        )
+
+        self.messages.append(message)
+        self._broadcast.publish(channel=channel, message=message)
+
+        return message
+
+    async def recv(self, channel: str = "default") -> StreamMessage:
+        """Listen for a single event on a channel"""
+        with self._broadcast.subscribe(channel) as subscriber:
+            async for event in subscriber:
+                stream_message = cast(StreamMessage, event.message)
+                return stream_message
+            raise Exception("recv should not complete without receiving an Event")
+
+    async def listen(
+        self, channel: str = "default"
+    ) -> AsyncGenerator[StreamMessage, None]:
+        """Listen to all messages on a channel indefinitely"""
+        with self._broadcast.subscribe(channel) as subscriber:
+            async for event in subscriber:
+                yield event.message
+
+    async def join(self) -> None:
+        """Blocks until all sent events have been processed"""
+        await self._broadcast.join()
