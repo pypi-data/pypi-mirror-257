b9c4ecea12cd6858f34c6e7e378d584584edbaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:04:10 +0000

    Add package most_sdk/0.1.0/most_sdk-0.1.0-py3-none-any.whl

---
+++ packages/most_sdk/most_sdk-0.1.0-py3-none-any.whl/most_sdk/routes.py
@@ -0,0 +1,35 @@
+class Enrichment:
+    def __init__(self, client):
+        """MOST Enrichment Wrapper
+        :param most_sdk.client.Client client: MOST SDK client
+        :rtype: most_sdk.routes.Enrichment
+        """
+
+        self.client = client
+        self.domain = 'big-data/enrichment'
+
+    def base(self, path: str, query: str, cpf: str, birthdate: str) -> dict:
+        data = {
+            'query': query,
+            'parameters': {'cpf': cpf, 'data_nascimento': birthdate},
+        }
+
+        return self.client.post(path=f'{self.domain}/{path}', original_data=data)
+
+    def query(self, query: str, cpf: str, birthdate: str) -> dict:
+        return self.base(path='', query=query, cpf=cpf, birthdate=birthdate)
+
+    def send(self, query: str, cpf: str, birthdate: str) -> dict:
+        return self.base(path='async', query=query, cpf=cpf, birthdate=birthdate)
+
+    def get(
+        self,
+        protocol: str = '',
+    ) -> dict:
+        path = 'async/status'
+
+        data = {
+            'processId': protocol,
+        }
+
+        return self.client.post(path=f'{self.domain}/{path}', original_data=data)
