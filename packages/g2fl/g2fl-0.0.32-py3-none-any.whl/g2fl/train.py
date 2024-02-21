c1591d21c9eebefb720bcbba52c0eaed7bd8cd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:06:56 +0000

    Add package g2fl/0.0.32/g2fl-0.0.32-py3-none-any.whl

---
+++ packages/g2fl/g2fl-0.0.32-py3-none-any.whl/g2fl/train.py
@@ -0,0 +1,264 @@
+import math
+import time
+
+import numpy as np
+import torch
+from torch import nn
+
+from g2fl.plotting import Animator
+
+
+class Accumulator:
+    """在n个变量上累加"""
+
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+class Timer:
+    """Record multiple running times."""
+
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+
+def sgd(params, lr, batch_size):
+    """小批量随机梯度下降。"""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+
+def evaluate_loss(net, data_iter, loss):
+    """Evaluate the loss of a model on the given dataset."""
+
+    metric = Accumulator(2)  # Sum of losses, no. of examples
+    for X, y in data_iter:
+        out = net(X)
+        y = y.reshape(out.shape)
+        ls = loss(out, y)
+        metric.add(ls.sum(), ls.numel())
+    return metric[0] / metric[1]
+
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    y_type_y_hat = y_hat.type(y.dtype)
+    cmp = y_type_y_hat == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    if isinstance(net, nn.Module):
+        net.eval()
+        if not device:
+            device = next(iter(net.parameters())).device
+    metric = Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_epoch_reg(net, train_iter, loss, updater):
+    """训练模型一个迭代周期"""
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        ls = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            ls.mean().backward()
+            updater.step()
+        else:
+            ls.sum().backward()
+            updater(X.shape[0])
+        metric.add(float(ls.sum()), accuracy(y_hat, y), y.numel())
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train_reg(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(
+        xlabel="epoch",
+        xlim=[1, num_epochs],
+        ylim=[0.3, 0.9],
+        legend=["train loss", "train acc", "test acc"],
+    )
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_reg(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert 1 >= train_acc > 0.7, train_acc
+
+
+def train_conv(net, train_iter, test_iter, num_epochs, lr, device):
+    def init_weights(m):
+        if type(m) is nn.Linear or type(m) is nn.Conv2d:
+            nn.init.xavier_uniform_(m.weight)
+
+    net.apply(init_weights)
+    print("training on ", device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(
+        xlabel="epoch",
+        xlim=[1, num_epochs],
+        legend=["train loss", "train acc", "test acc"],
+    )
+    timer, num_batches = Timer(), len(train_iter)
+    for epoch in range(num_epochs):
+        metric = Accumulator(3)
+        net.train()
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            ls = loss(y_hat, y)
+            ls.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(ls * X.shape[0], accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_l = metric[0] / metric[2]
+            train_acc = metric[1] / metric[2]
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches, (train_l, train_acc, None))
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+        animator.add(epoch + 1, (None, None, test_acc))
+    print(f"loss {train_l:.3f}, train acc {train_acc:.3f}, " f"test acc {test_acc:.3f}")
+    print(
+        f"{metric[2] * num_epochs / timer.sum():.1f} examples/sec " f"on {str(device)}"
+    )
+
+
+def grad_clipping(net, theta):
+    """裁剪梯度，避免梯度爆炸"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad**2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+
+def predict_rnn(prefix, num_preds, net, vocab, device):
+    """在prefix后面生成新字符"""
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.tensor([outputs[-1]], device=device).reshape((1, 1))
+    for y in prefix[1:]:
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return "".join([vocab.idx_to_token[i] for i in outputs])
+
+
+def train_epoch_rnn(net, train_iter, loss, updater, device, use_random_iter):
+    """训练网络一个迭代周期（定义见第8章）"""
+    state, timer = None, Timer()
+    metric = Accumulator(2)
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                state.detach_()
+            else:
+                for s in state:
+                    s.detach_()
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+        ls = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            ls.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            ls.backward()
+            grad_clipping(net, 1)
+            updater(batch_size=1)
+        metric.add(ls * y.numel(), y.numel())
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+
+def train_rnn(net, train_iter, vocab, lr, num_epochs, device, use_random_iter=False):
+    """训练模型（定义见第8章）"""
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(
+        xlabel="epoch", ylabel="perplexity", legend=["train"], xlim=[10, num_epochs]
+    )
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr)
+    else:
+        updater = lambda batch_size: sgd(net.params, lr, batch_size)
+    predict = lambda prefix: predict_rnn(prefix, 50, net, vocab, device)
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch_rnn(
+            net, train_iter, loss, updater, device, use_random_iter
+        )
+        if (epoch + 1) % 10 == 0:
+            # print(predict('time traveller'))
+            animator.add(epoch + 1, [ppl])
+    print(f"困惑度 {ppl:.1f}, {speed:.1f} 词元/秒 {str(device)}")
+    print(predict("time traveller"))
+    print(predict("traveller"))
