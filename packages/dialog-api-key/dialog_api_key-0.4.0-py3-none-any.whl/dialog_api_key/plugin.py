e7c2d52764675e57c3a172a6df4c6023731b2414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:05:25 +0000

    Add package dialog-api-key/0.4.0/dialog_api_key-0.4.0-py3-none-any.whl

---
+++ packages/dialog-api-key/dialog_api_key-0.4.0-py3-none-any.whl/dialog_api_key/plugin.py
@@ -0,0 +1,30 @@
+from http import HTTPStatus
+
+from decouple import Csv, config
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+
+API_KEY_HEADER = config("API_KEY_HEADER", default="X-Api-Key")
+API_KEYS = config("API_KEYS", cast=Csv(post_process=set))
+API_KEY_IGNORED_PATHS = config(
+    "API_KEY_IGNORED_PATHS", cast=Csv(post_process=set), default="/docs,/openapi.json"
+)
+
+
+def api_key_middleware(request: Request, call_next):
+    if request.url.path in API_KEY_IGNORED_PATHS:
+        return call_next(request)
+
+    api_key = request.headers.get(API_KEY_HEADER)
+    if not api_key or api_key not in API_KEYS:
+        return JSONResponse(
+            {"detail": "Invalid API Key."},
+            status_code=HTTPStatus.UNAUTHORIZED,
+        )
+
+    return call_next(request)
+
+
+def register_plugin(app: FastAPI):
+    app.middleware("http")(api_key_middleware)
