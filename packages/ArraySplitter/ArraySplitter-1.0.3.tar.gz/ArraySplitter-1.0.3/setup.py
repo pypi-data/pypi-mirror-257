3fcd10ccc5bad9ce2c9ca0015c9c24fd06a806c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 14:05:03 +0000

    Add package ArraySplitter/1.0.3/ArraySplitter-1.0.3.tar.gz

---
+++ packages/ArraySplitter/ArraySplitter-1.0.3.tar.gz/ArraySplitter-1.0.3/setup.py
@@ -0,0 +1,46 @@
+import re
+
+from setuptools import find_packages, setup
+
+version = None
+for line in open("./ArraySplitter/__init__.py"):
+    m = re.search("__version__\s*=\s*(.*)", line)
+    if m:
+        version = m.group(1).strip()[1:-1]  # quotes
+        break
+assert version
+
+setup(
+    name="ArraySplitter",
+    version=version,
+    packages=["ArraySplitter"],
+    package_data={"": ["README.md"]},
+    python_requires=">=3.6",
+    include_package_data=True,
+    scripts=[],
+    license="BSD",
+    url="https://github.com/aglabx/ArraySplitter",
+    author="Aleksey Komissarov",
+    author_email="ad3002@gmail.com",
+    description="De Novo Decomposition of Satellite DNA Arrays into Monomers within Telomere-to-Telomere Assemblies",
+    install_requires=[
+        "PyExp",
+        "intervaltree",
+        "editdistance",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    entry_points={
+        'console_scripts': [
+            'ArraySplitter=ArraySplitter:main',
+            'arraysplitter=ArraySplitter:main',
+        ],
+    },
+)
