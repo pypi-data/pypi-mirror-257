386811b09e127425d321c8d63deb61f26103e36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 05:04:37 +0000

    Add package BIOViewer/0.1.2/BIOViewer-0.1.2.tar.gz

---
+++ packages/BIOViewer/BIOViewer-0.1.2.tar.gz/BIOViewer-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='BIOViewer',
+   version='0.1.2',
+   description='A useful module to display time series biosignals',
+   long_description='this module contains a lightweight viewer that is used to display biosignals',
+   author='Moritz Alkofer', 
+   packages=['BIOViewer'],
+   install_requires=[], #list dependencies
+)
