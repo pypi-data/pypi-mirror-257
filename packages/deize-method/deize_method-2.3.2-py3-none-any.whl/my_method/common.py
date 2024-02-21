b6adf4d428859d43853c7b0c9e685abbbf25105e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:11 +0000

    Add package deize-method/2.3.2/deize_method-2.3.2-py3-none-any.whl

---
+++ packages/deize-method/deize_method-2.3.2-py3-none-any.whl/my_method/common.py
@@ -0,0 +1,29 @@
+import os
+import queue
+import re
+import subprocess
+import threading
+import time
+import wave
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Final, Optional, Union
+
+import numpy as np
+
+__all__ = [
+    "os",
+    "re",
+    "datetime",
+    "Final",
+    "Union",
+    "subprocess",
+    "time",
+    "Any",
+    "Optional",
+    "Path",
+    "np",
+    "wave",
+    "threading",
+    "queue",
+]
