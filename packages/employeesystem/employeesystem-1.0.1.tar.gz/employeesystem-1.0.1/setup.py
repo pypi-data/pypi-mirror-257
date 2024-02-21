9d14704da29175d68eeffbe8de9d758ea173995d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:05:34 +0000

    Add package employeesystem/1.0.1/employeesystem-1.0.1.tar.gz

---
+++ packages/employeesystem/employeesystem-1.0.1.tar.gz/employeesystem-1.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='employeesystem',
+version='1.0.1',
+author='Grayson Huey',
+author_email='graysonhuey02@gmail.com',
+description='Manage your workplace employees',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: Free For Educational Use',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.11',
+)
