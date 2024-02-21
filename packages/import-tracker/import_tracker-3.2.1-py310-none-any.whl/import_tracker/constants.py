d8ff5c432fd80de2e0a212cd7c087929a2173655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 05:04:16 +0000

    Add package import-tracker/3.2.1/import_tracker-3.2.1-py310-none-any.whl

---
+++ packages/import-tracker/import_tracker-3.2.1-py310-none-any.whl/import_tracker/constants.py
@@ -0,0 +1,18 @@
+"""
+Shared constants across the various parts of the library
+"""
+
+# Standard
+import sys
+
+# The name of this package (import_tracker)
+THIS_PACKAGE = sys.modules[__name__].__package__.partition(".")[0]
+
+# Labels for direct vs transitive dependencies
+TYPE_DIRECT = "direct"
+TYPE_TRANSITIVE = "transitive"
+
+# Info section headers
+INFO_TYPE = "type"
+INFO_STACK = "stack"
+INFO_OPTIONAL = "optional"
