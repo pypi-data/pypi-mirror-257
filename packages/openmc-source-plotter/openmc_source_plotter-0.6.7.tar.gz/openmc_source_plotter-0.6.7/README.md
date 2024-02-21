7da86ae7d38a979f0de054498bbfb63d0a7273eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:43 +0000

    Add package openmc-source-plotter/0.6.7/openmc_source_plotter-0.6.7.tar.gz

---
+++ packages/openmc-source-plotter/openmc_source_plotter-0.6.7.tar.gz/openmc_source_plotter-0.6.7/README.md
@@ -0,0 +1,191 @@
+[![CI with install](https://github.com/fusion-energy/openmc_source_plotter/actions/workflows/ci_with_install.yml/badge.svg?branch=main)](https://github.com/fusion-energy/openmc_source_plotter/actions/workflows/ci_with_install.yml)
+
+[![Upload Python Package](https://github.com/fusion-energy/openmc_source_plotter/actions/workflows/python-publish.yml/badge.svg)](https://github.com/fusion-energy/openmc_source_plotter/actions/workflows/python-publish.yml)
+
+A Python package for plotting the positions, directions or energy distributions of OpenMC sources.
+
+# Installation
+
+You will need to have OpenMC version 0.14.0 or newer installed first.
+
+```bash
+pip install openmc_source_plotter
+```
+
+# Features
+
+The package simply extends the default ```openmc.IndependentSourceBase``` and ```openmc.Model``` to provides additional functions that:
+
+- extract the positions, directions and energy of particles
+- visualise a source with respect to:
+  - direction
+  - energy
+  - position
+
+Or just provide the initial particles with ```sample_initial_particles```
+
+# Example plots
+
+Below are some basic examples, for more examples see the [examples folder](https://github.com/fusion-energy/openmc_source_plotter/tree/main/examples) for example usage scripts.
+
+
+## Plot of energy distribution of the source
+
+```python
+import openmc
+import openmc_source_plotter  # extends openmc.IndependentSource with plotting functions
+
+# initialises a new source object
+my_source = openmc.IndependentSource()
+
+# sets the energy distribution to a muir distribution neutrons for DT fusion neutrons
+my_source.energy = openmc.stats.muir(e0=14080000.0, m_rat=5.0, kt=20000.0)
+
+# plots the particle energy distribution
+plot = my_source.plot_source_energy(n_samples=2000)
+
+plot.show()
+```
+
+![openmc particle source energy plot](https://user-images.githubusercontent.com/8583900/143615694-a3578115-f8a2-4971-bf26-458177b4f113.png)
+
+## Plot of energy distribution of two sources
+
+```python
+import openmc
+import openmc_source_plotter  # extends openmc.IndependentSource with plotting functions
+
+# initialises a new source object
+my_dt_source = openmc.IndependentSource()
+
+# sets the energy distribution to a muir distribution DT neutrons
+my_dt_source.energy = openmc.stats.muir(e0=14080000.0, m_rat=5.0, kt=20000.0)
+
+# initialises a new source object
+my_dd_source = openmc.IndependentSource()
+# sets the energy distribution to a muir distribution DD neutrons
+my_dd_source.energy = openmc.stats.muir(e0=2080000.0, m_rat=2.0, kt=20000.0)
+
+# plots the particle energy distribution by building on the first plot
+figure1 = my_dd_source.plot_source_energy(n_samples=10000)
+figure2 = my_dt_source.plot_source_energy(figure=figure1, n_samples=10000)
+
+figure2.show()
+```
+
+![openmc particle source energy plot](https://user-images.githubusercontent.com/8583900/151376414-fb1555eb-61d1-4c82-bc4d-a05f62819c5d.png)
+
+## Plot direction of particles
+
+```python
+import openmc
+import openmc_source_plotter  # extends openmc.IndependentSource with plotting functions
+
+# initializes a new source object
+my_source = openmc.IndependentSource()
+
+# sets the direction to isotropic
+my_source.angle = openmc.stats.Isotropic()
+
+# plots the particle energy distribution
+plot = my_source.plot_source_direction(n_samples=200)
+
+plot.show()
+```
+
+![openmc particle source direction plot](https://user-images.githubusercontent.com/8583900/143615706-3b3a8467-0233-42d6-a66c-d536c80a01d8.png)
+
+
+<!-- ## Plot gamma spectrum of particles
+
+```python
+import openmc
+import openmc_source_plotter  # adds plot_gamma_emission plot to materials
+
+
+openmc.config["chain_file"] = "chain-endf.xml"
+
+my_material = openmc.Material()
+my_material.add_nuclide("Xe135", 1e-12)
+my_material.add_nuclide("U235", 1)
+my_material.add_nuclide("U238", 1)
+my_material.add_nuclide("Co60", 1e-9)
+my_material.volume = 1  # must be set so number of atoms can be found
+
+# adds labels to the most active 3 gamma energies
+plt = my_material.plot_gamma_emission(label_top=3)
+plt.xscale("log")  # modify axis from default settings
+plt.savefig("gamma_spec.png")
+```
+
+![openmc gamma spectrum](https://user-images.githubusercontent.com/8583900/228280129-b8160e18-9ca9-4b20-a4e1-d2948908daf6.png) -->
+
+## Plot position of particles
+
+```python
+import openmc
+import openmc_source_plotter  # extends openmc.IndependentSource with plotting functions
+
+# initialises a new source object
+my_source = openmc.IndependentSource()
+
+# the distribution of radius is just a single value
+radius = openmc.stats.Discrete([10], [1])
+
+# the distribution of source z values is just a single value
+z_values = openmc.stats.Discrete([0], [1])
+
+# the distribution of source azimuthal angles
+# values is a uniform distribution between 0 and 2 Pi
+angle = openmc.stats.Uniform(a=0.0, b=2 * 3.14159265359)
+
+# this makes the ring source using the three distributions and a radius
+my_source.space = openmc.stats.CylindricalIndependent(
+    r=radius, phi=angle, z=z_values, origin=(0.0, 0.0, 0.0)
+)
+
+# plots the particle energy distribution
+plot = my_source.plot_source_position()
+
+plot.show()
+```
+
+
+![openmc particle source position plot](https://user-images.githubusercontent.com/8583900/179424915-bee56a87-6214-46ef-8625-92b8f4cbd1b3.png)
+
+
+## Extract particle objects
+
+A list of ```openmc.Particle``` objects can be obtained using ```model.sample_initial_particles()``` or ```openmc.SourceBase.sample_initial_particles()```
+
+```python
+import openmc
+import openmc_source_plotter  # extents openmc.Model with sample_initial_particles method
+
+settings = openmc.Settings()
+settings.particles = 1
+settings.batches = 1
+my_source = openmc.IndependentSource()
+my_source.energy = openmc.stats.muir(e0=14080000.0, m_rat=5.0, kt=20000.0)
+settings.source = my_source
+materials = openmc.Materials()
+sph = openmc.Sphere(r=100, boundary_type="vacuum")
+cell = openmc.Cell(region=-sph)
+geometry = openmc.Geometry([cell])
+
+model = openmc.Model(geometry, materials, settings)
+
+particles = model.sample_initial_particles(n_samples=10)
+
+print(particles)
+>>>[<SourceParticle: neutron at E=1.440285e+07 eV>, <SourceParticle: neutron at E=1.397691e+07 eV>, <SourceParticle: neutron at E=1.393681e+07 eV>, <SourceParticle: neutron at E=1.470896e+07 eV>, <SourceParticle: neutron at E=1.460563e+07 eV>, <SourceParticle: neutron at E=1.420684e+07 eV>, <SourceParticle: neutron at E=1.413932e+07 eV>, <SourceParticle: neutron at E=1.412428e+07 eV>, <SourceParticle: neutron at E=1.464779e+07 eV>, <SourceParticle: neutron at E=1.391648e+07 eV>]
+
+print(particles[0].E)
+>>>1.440285e+07
+```
+
+## Related packages
+
+Tokamak sources can also be plotted using the [openmc-plasma-source](https://github.com/fusion-energy/openmc-plasma-source) package
+
+![openmc_source_plotter_openmc-plasma-source_tokamak](https://user-images.githubusercontent.com/8583900/187487894-ba0bd025-46f2-4c7d-8b15-3d260aed47a0.png)
---
+++ packages/openmc-source-plotter/openmc_source_plotter-0.6.7.tar.gz/openmc_source_plotter-0.6.7/src/openmc_source_plotter/core.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+
+"""Provides functions for plotting source information"""
+
+import typing
+
+import numpy as np
+import openmc
+import openmc.lib
+import plotly.graph_objects
+
+import pkg_resources
+
+system_openmc_version = pkg_resources.parse_version(openmc.__version__)
+min_openmc_version = pkg_resources.parse_version("0.14.0")
+if system_openmc_version < min_openmc_version:
+    msg = (
+        "openmc_source_plotter requires openmc version 0.14.0 or above. "
+        f"You have openmc version {system_openmc_version} installed. "
+        "Please update the version of openmc installed"
+    )
+    raise ImportError(msg)
+
+
+def sample_initial_particles(self, n_samples: int = 1000, prn_seed: int = None):
+
+    if isinstance(self, openmc.Model):
+
+        self.settings.export_to_xml()
+        self.materials.export_to_xml()
+        self.geometry.export_to_xml()
+
+    else:  # source object
+
+        settings = openmc.Settings()
+        settings.particles = 1
+        settings.batches = 1
+        settings.source = self
+
+        materials = openmc.Materials()
+
+        sph = openmc.Sphere(r=9999999999, boundary_type="vacuum")
+        cell = openmc.Cell(region=-sph)
+        geometry = openmc.Geometry([cell])
+
+        settings.export_to_xml()
+        materials.export_to_xml()
+        geometry.export_to_xml()
+
+    openmc.lib.init(output=False)
+    particles = openmc.lib.sample_external_source(
+        n_samples=n_samples, prn_seed=prn_seed
+    )
+    openmc.lib.finalize()
+
+    return particles
+
+
+def plot_source_energy(
+    self,
+    figure: plotly.graph_objects.Figure = None,
+    n_samples: int = 2000,
+    prn_seed: int = 1,
+    energy_bins: typing.Union[str, np.array] = "auto",
+    name: typing.Optional[str] = None,
+):
+    """makes a plot of the initial creation positions of an OpenMC source
+
+    Args:
+        figure: Optional base plotly figure to use for the plot. Passing in
+            a pre made figure allows one to build up plots with from
+            multiple sources. Defaults to None which makes a new figure for
+            the plot.
+        source: The openmc.Source object or list of openmc.Source objects
+            to plot.
+        n_samples: The number of source samples to obtain.
+        prn_seed: The pseudorandom number seed
+        energy_bins: Defaults to 'auto' which uses inbuilt auto binning in
+            Numpy bins can also be manually set by passing in a numpy array
+            of bin edges.
+        name: the legend name to use
+    """
+
+    if figure is None:
+        figure = plotly.graph_objects.Figure()
+        figure.update_layout(
+            title="Particle energy",
+            xaxis={"title": "Energy (eV)"},
+            yaxis={"title": "Probability"},
+            showlegend=True,
+        )
+
+    data = self.sample_initial_particles(n_samples, prn_seed)
+
+    e_values = [particle.E for particle in data]
+
+    # Calculate pdf for source energies
+    probability, bin_edges = np.histogram(e_values, bins=energy_bins, density=True)
+
+    # scaling by strength
+    if isinstance(self, openmc.SourceBase):
+        probability = probability * self.strength
+
+    # Plot source energy histogram
+    figure.add_trace(
+        plotly.graph_objects.Scatter(
+            x=bin_edges[:-1],
+            y=probability * np.diff(bin_edges),
+            line={"shape": "hv"},
+            hoverinfo="text",
+            name=name,
+        )
+    )
+
+    return figure
+
+
+def plot_source_position(
+    self,
+    figure=None,
+    n_samples: int = 2000,
+    prn_seed: int = 1,
+):
+    """makes a plot of the initial creation positions of an OpenMC source(s)
+
+    Args:
+        figure: Optional base plotly figure to use for the plot. Passing in
+            a pre made figure allows one to build up plots with from
+            multiple sources. Defaults to None which makes a new figure for
+            the plot.
+        source: The openmc.Source object or list of openmc.Source objects
+            to plot.
+        n_samples: The number of source samples to obtain.
+        prn_seed: The pseudorandom number seed
+    """
+
+    if figure is None:
+        figure = plotly.graph_objects.Figure()
+        figure.update_layout(
+            title="Particle creation position",
+            showlegend=True,
+        )
+
+    data = self.sample_initial_particles(n_samples, prn_seed)
+
+    text = ["Energy = " + str(particle.E) + " eV" for particle in data]
+
+    figure.add_trace(
+        plotly.graph_objects.Scatter3d(
+            x=[particle.r[0] for particle in data],
+            y=[particle.r[1] for particle in data],
+            z=[particle.r[2] for particle in data],
+            hovertext=text,
+            text=text,
+            mode="markers",
+            marker={
+                "size": 2,
+                "color": [particle.E for particle in data],
+            },
+        )
+    )
+    title = "Particle production coordinates coloured by energy"
+    figure.update_layout(title=title)
+
+    return figure
+
+
+def plot_source_direction(
+    self,
+    figure=None,
+    n_samples: int = 2000,
+    prn_seed: int = 1,
+):
+    """makes a plot of the initial creation positions of an OpenMC source(s)
+
+    Args:
+        figure: Optional base plotly figure to use for the plot. Passing in
+            a pre made figure allows one to build up plots with from
+            multiple sources. Defaults to None which makes a new figure for
+            the plot.
+        source: The openmc.Source object or list of openmc.Source objects
+            to plot.
+        n_samples: The number of source samples to obtain.
+        prn_seed: The pseudorandom number seed
+    """
+
+    figure = plotly.graph_objects.Figure()
+    figure.update_layout(title="Particle initial directions")
+
+    data = self.sample_initial_particles(n_samples, prn_seed)
+
+    biggest_coord = max(
+        max([particle.r[0] for particle in data]),
+        max([particle.r[1] for particle in data]),
+        max([particle.r[2] for particle in data]),
+    )
+    smallest_coord = min(
+        min([particle.r[0] for particle in data]),
+        min([particle.r[1] for particle in data]),
+        min([particle.r[2] for particle in data]),
+    )
+
+    figure.add_trace(
+        {
+            "type": "scatter3d",
+            "marker": {"color": "rgba(255,255,255,0)"},
+            "x": [biggest_coord, smallest_coord],
+            "y": [biggest_coord, smallest_coord],
+            "z": [biggest_coord, smallest_coord],
+        }
+    )
+    figure.add_trace(
+        {
+            "type": "cone",
+            "cauto": False,
+            "x": [particle.r[0] for particle in data],
+            "y": [particle.r[1] for particle in data],
+            "z": [particle.r[2] for particle in data],
+            "u": [particle.u[0] for particle in data],
+            "v": [particle.u[1] for particle in data],
+            "w": [particle.u[2] for particle in data],
+            "cmin": 0,
+            "cmax": 1,
+            "anchor": "tail",
+            "colorscale": "Viridis",
+            "hoverinfo": "u+v+w+norm",
+            "sizemode": "absolute",
+            "sizeref": 30,
+            "showscale": False,
+        }
+    )
+
+    return figure
+
+
+"""
+Extents the openmc.Source class to add source plotting
+methods for energy, direction and position. Source sampling methods are
+also provided for convenience. Additional methods are plot_source_energy(),
+plot_source_position(), plot_source_direction(), sample_initial_particles()
+"""
+openmc.SourceBase.sample_initial_particles = sample_initial_particles
+openmc.model.Model.sample_initial_particles = sample_initial_particles
+openmc.Model.sample_initial_particles = sample_initial_particles
+
+openmc.SourceBase.plot_source_energy = plot_source_energy
+openmc.model.Model.plot_source_energy = plot_source_energy
+openmc.Model.plot_source_energy = plot_source_energy
+
+openmc.SourceBase.plot_source_position = plot_source_position
+openmc.model.Model.plot_source_position = plot_source_position
+openmc.Model.plot_source_position = plot_source_position
+
+openmc.SourceBase.plot_source_direction = plot_source_direction
+openmc.model.Model.plot_source_direction = plot_source_direction
+openmc.Model.plot_source_direction = plot_source_direction
