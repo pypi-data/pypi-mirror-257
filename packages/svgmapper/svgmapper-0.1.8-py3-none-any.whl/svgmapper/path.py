ba19db2cfb093a9dbb103cbfd6127131cfa094ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:13 +0000

    Add package svgmapper/0.1.8/svgmapper-0.1.8-py3-none-any.whl

---
+++ packages/svgmapper/svgmapper-0.1.8-py3-none-any.whl/svgmapper/path.py
@@ -0,0 +1,45 @@
+import svgwrite
+from shapely import Polygon
+from svgmapper.translate import Translator
+
+
+class Path:
+    def __init__(self, geometry: Polygon, name: str = '', translator: Translator = None):
+        self.geometry = geometry
+        self.dwg = svgwrite.Drawing()
+        self.name = name
+        self.path_string = ''
+        self.translator = translator
+        self.path = None
+        self.write()
+
+    def simplify(self):
+        max_coords = 20000
+        coord_num = len(self.geometry.exterior.coords)
+        if coord_num < max_coords: return None
+        tolerance = 0
+        while coord_num > max_coords:
+            tolerance += 100
+            self.geometry = self.geometry.simplify(tolerance)
+            coord_num = len(self.geometry.exterior.coords)
+
+    def translate(self, x, y):
+        lat, long = self.translator.translate(x, y) if self.translator else (x, y)
+        return f'{lat}, {long}'
+
+    def write(self):
+        self.simplify()
+        coordinates = self.geometry.exterior.coords
+        data = [self.translate(x, y) for x, y in coordinates]
+        path_data = "M " + " ".join(data) + " Z"
+
+        # Handle interior coordinates (holes)
+        for interior in self.geometry.interiors:
+            interior_coordinates = interior.coords
+            interior_data = [self.translate(x, y) for x, y in interior_coordinates]
+            path_data += " M " + " ".join(interior_data) + " Z"
+
+        self.path = self.dwg.path(
+            d=path_data,
+            class_= self.name if self.name else 'path'
+        )
