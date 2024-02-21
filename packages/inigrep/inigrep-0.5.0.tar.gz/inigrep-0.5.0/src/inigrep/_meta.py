deb668470e2183a2b2f95e99adde2b522fd9c9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 07:05:36 +0000

    Add package inigrep/0.5.0/inigrep-0.5.0.tar.gz

---
+++ packages/inigrep/inigrep-0.5.0.tar.gz/inigrep-0.5.0/src/inigrep/_meta.py
@@ -0,0 +1,39 @@
+CODENAME: str = """
+
+""".strip()
+
+GIT_LASTHASH: str = """
+28621c1c7c2e07801fa8f9b7f97fc025e82e4cea
+""".strip()
+
+GIT_LASTSUMMARY: str = """
+Bump version to 0.5.0
+""".strip()
+
+LICENSE: str = """
+LGPLv2
+""".strip()
+
+MAINTAINER: str = """
+Alois Mahdal <netvor+inigrep@vornet.cz>
+""".strip()
+
+NAME: str = """
+inigrep
+""".strip()
+
+PKGNAME: str = """
+python-inigrep
+""".strip()
+
+TAGLINE: str = """
+grep for (some) INIs
+""".strip()
+
+VCS_BROWSER: str = """
+https://gitlab.com/vornet/python/python-inigrep
+""".strip()
+
+VERSION: str = """
+0.5.0
+""".strip()
