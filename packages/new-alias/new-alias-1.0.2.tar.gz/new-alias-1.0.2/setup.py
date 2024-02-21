d46c82df61a66bbb3f1b48f3ee8fd3a1fd6c22db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:44 +0000

    Add package new-alias/1.0.2/new-alias-1.0.2.tar.gz

---
+++ packages/new-alias/new-alias-1.0.2.tar.gz/new-alias-1.0.2/setup.py
@@ -0,0 +1,37 @@
+'''
+setup.py
+'''
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='new-alias',
+    version='1.0.2',
+    packages=find_packages(),
+    install_requires=requirements,
+    package_data={
+        'new_alias': ['attributes/*.txt'],
+    },  
+    entry_points={
+        'console_scripts': [
+            'new_alias=new_alias.main:main',  
+        ],
+    },
+    include_package_data=True,
+    
+    author='Aiden R. McCormack',
+    author_email='aidenm2244@proton.me',
+    description='An alias generator for online privacy protection',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Aiden2244/identity-generator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
