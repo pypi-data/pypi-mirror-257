dca19513d5a9e97d492b3a977e66cf88017f00b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:06:02 +0000

    Add package vec2text/0.0.12/vec2text-0.0.12-py3-none-any.whl

---
+++ packages/vec2text/vec2text-0.0.12-py3-none-any.whl/vec2text/__init__.py
@@ -0,0 +1,16 @@
+from . import (  # noqa: F401; analyze_utils,
+    aliases,
+    collator,
+    metrics,
+    models,
+    prompts,
+    trainers,
+    trainers_baseline,
+)
+from .api import (  # noqa: F401
+    invert_embeddings,
+    invert_strings,
+    load_corrector,
+    load_pretrained_corrector,
+)
+from .trainers import Corrector  # noqa: F401
