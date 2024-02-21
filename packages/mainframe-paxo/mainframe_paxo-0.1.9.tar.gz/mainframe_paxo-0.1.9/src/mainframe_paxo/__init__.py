9ec9a5d73d7c2f50c9c5b77c5c26443a627013de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:23 +0000

    Add package mainframe-paxo/0.1.9/mainframe_paxo-0.1.9.tar.gz

---
+++ packages/mainframe-paxo/mainframe_paxo-0.1.9.tar.gz/mainframe_paxo-0.1.9/src/mainframe_paxo/__init__.py
@@ -0,0 +1,7 @@
+# different work location
+locations = {
+    "rvk-office": {"desc": "Reykjavík office"},
+    "hel-office": {"desc": "Helsinki office"},
+    "tailscale": {"desc": "working over tailscale network, e.g. from home"},
+    "external": {"desc": "working from the internet without VPN"},
+}
