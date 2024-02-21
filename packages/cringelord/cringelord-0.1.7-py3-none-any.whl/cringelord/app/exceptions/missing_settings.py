8f06c38e233fd76dffda5b3b7ea57201fdbfab57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:04:56 +0000

    Add package cringelord/0.1.7/cringelord-0.1.7-py3-none-any.whl

---
+++ packages/cringelord/cringelord-0.1.7-py3-none-any.whl/cringelord/app/exceptions/missing_settings.py
@@ -0,0 +1,11 @@
+from cringelord.app.exceptions.single_argument_formatted_message import (
+    SingleArgumentFormattedMessageException
+)
+
+
+class MissingSettingError(SingleArgumentFormattedMessageException):
+    """
+    Raised when a setting the application requires is not present in the
+        config file.
+    """
+    message = "Your config is missing the '{}' settings."
---
+++ packages/cringelord/cringelord-0.1.7-py3-none-any.whl/cringelord/app/src_directory/ast_visitor/exceptions/arg_not_found.py
@@ -0,0 +1,8 @@
+from cringelord.app.exceptions.single_argument_formatted_message import (
+    SingleArgumentFormattedMessageException
+)
+
+
+class ArgNotFoundError(SingleArgumentFormattedMessageException):
+    """Raised when the arg is not found in a call."""
+    message = "No arg found at position {}."
