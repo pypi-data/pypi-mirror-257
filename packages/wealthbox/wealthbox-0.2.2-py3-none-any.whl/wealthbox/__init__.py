3e157f12b747a1a4c1010dd3f803272a3abcdddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:37 +0000

    Add package wealthbox/0.2.2/wealthbox-0.2.2-py3-none-any.whl

---
+++ packages/wealthbox/wealthbox-0.2.2-py3-none-any.whl/wealthbox/__init__.py
@@ -0,0 +1,38 @@
+from json import JSONDecodeError
+import requests
+
+__version__ = '0.1.0'
+
+class WealthBox(object):
+    def __init__(self, token=None):
+        self.token = token
+        self.base_url = f"https://api.crmworkspace.com/v1/"
+    
+   def api_request(self, endpoint, params=None):
+        url = self.base_url + endpoint
+        page = 1
+        total_pages = 9999999999
+        if params is None:
+            params = {}
+        
+        params.setdefault('per_page', '5000')
+        results = []
+
+        while page <= total_pages:
+            params['page'] = page
+            res = requests.get(url,
+                            params=params,
+                            headers={'ACCESS_TOKEN': self.token})
+            try:
+                res_json = res.json()
+                total_pages = res_json['meta']['total_pages']
+                results.extend(res_json[endpoint])
+                page += 1
+            except:# JSONDecodeError:
+                print(res.text)
+                return results
+
+        return results
+       
+    def get_contacts(self, filters=None):
+        return self.api_request('contacts',params=filters)
