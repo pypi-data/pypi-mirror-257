fbdf28508c521f777174d47ce329ded54f5eb983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 05:04:44 +0000

    Add package NEPython/1.0.2/NEPython-1.0.2.tar.gz

---
+++ packages/NEPython/NEPython-1.0.2.tar.gz/NEPython-1.0.2/README.md
@@ -0,0 +1,19 @@
+# NEPython
+
+NEPython (or NEPy, long: Not Enough Python) is a Python library that offers a collection of small, neat features to enhance your development experience.
+
+## Features
+
+- **Array and Dictionary Manipulation** A neat little sub-library to merge, sort and more.
+- **More Features Soon!:** Make sure to regulary check for an Update.
+```bash
+pip install --upgrade NEPython
+```
+- ...
+
+## Installation
+
+You can install NEPython using pip:
+
+```bash
+pip install NEPython
---
+++ packages/NEPython/NEPython-1.0.2.tar.gz/NEPython-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='NEPython',
+    version='1.0.2',
+    author='TheescapedShadow',
+    description='A comprehensive Python library providing essential tools and utilities for streamlined development.',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'tqdm',
+        'packaging',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    setup_requires=['wheel'],
+    include_package_data=True,
+)
