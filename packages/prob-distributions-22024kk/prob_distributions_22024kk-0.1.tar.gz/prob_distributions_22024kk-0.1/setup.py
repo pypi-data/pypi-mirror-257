26ae1a9d6de0693d70da5e204698f43d260baf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:41 +0000

    Add package prob-distributions-22024kk/0.1/prob_distributions_22024kk-0.1.tar.gz

---
+++ packages/prob-distributions-22024kk/prob_distributions_22024kk-0.1.tar.gz/prob_distributions_22024kk-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_distributions_22024kk',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_distributions_22024kk'],
+      author= 'Kavitha Katiki',
+      author_email='kkatiki@outlook.com',
+      zip_safe=False)
