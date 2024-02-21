f525a506aae88acd002eb411ca1c40c1a4c98e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:06:35 +0000

    Add package ooo-dev-tools/0.29.0/ooo_dev_tools-0.29.0.tar.gz

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.29.0.tar.gz/ooo_dev_tools-0.29.0/ooodev/write/write_text_table.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+from typing import cast, TYPE_CHECKING, TypeVar, Generic
+import uno
+
+
+if TYPE_CHECKING:
+    from com.sun.star.text import XTextContent
+    from com.sun.star.container import XEnumerationAccess
+
+from ooodev.adapter.text.text_table_comp import TextTableComp
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.proto.component_proto import ComponentT
+from ooodev.loader import lo as mLo
+from ooodev.loader.inst.lo_inst import LoInst
+from ooodev.utils.partial.lo_inst_props_partial import LoInstPropsPartial
+from ooodev.utils.partial.qi_partial import QiPartial
+from . import write_text_portions as mWriteTextPortions
+
+T = TypeVar("T", bound="ComponentT")
+
+
+class WriteTextTable(Generic[T], LoInstPropsPartial, TextTableComp, QiPartial, StylePartial):
+    """Represents writer text content."""
+
+    def __init__(self, owner: T, component: XTextContent, lo_inst: LoInst | None = None) -> None:
+        """
+        Constructor
+
+        Args:
+            owner (T): Owner of this component.
+            component (XTextContent): UNO object that supports ``com.sun.star.text.TextContent`` service.
+            lo_inst (LoInst, optional): Lo instance. Defaults to ``None``.
+        """
+        if lo_inst is None:
+            lo_inst = mLo.Lo.current_lo
+        self._owner = owner
+        LoInstPropsPartial.__init__(self, lo_inst=lo_inst)
+        TextTableComp.__init__(self, component)  # type: ignore
+        QiPartial.__init__(self, component=component, lo_inst=self.lo_inst)  # type: ignore
+        StylePartial.__init__(self, component=component)
+
+    def get_text_portions(self) -> mWriteTextPortions.WriteTextPortions[T]:
+        """Returns the text portions of this paragraph."""
+        return mWriteTextPortions.WriteTextPortions(
+            owner=self.owner, component=cast("XEnumerationAccess", self.component), lo_inst=self.lo_inst
+        )
+
+    # region Properties
+    @property
+    def owner(self) -> T:
+        """Owner of this component."""
+        return self._owner
+
+    # endregion Properties
