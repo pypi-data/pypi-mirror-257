6d4ddc37be1f8db8a6b3dd8f59df8f6617d272e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:06:09 +0000

    Add package smt/2.4.0/smt-2.4.0-cp311-cp311-musllinux_1_1_x86_64.whl

---
+++ packages/smt/smt-2.4.0-cp311-cp311-musllinux_1_1_x86_64.whl/smt/surrogate_models/__init__.py
@@ -0,0 +1,27 @@
+from .ls import LS
+from .qp import QP
+from .krg import KRG
+from .kpls import KPLS
+from .gekpls import GEKPLS
+from .kplsk import KPLSK
+from .genn import GENN
+from .mgp import MGP
+from .sgp import SGP
+
+from .krg_based import MixIntKernelType
+from smt.utils.design_space import (
+    DesignSpace,
+    FloatVariable,
+    IntegerVariable,
+    OrdinalVariable,
+    CategoricalVariable,
+)
+from smt.utils.kriging import MixHrcKernelType
+
+try:
+    from .idw import IDW
+    from .rbf import RBF
+    from .rmtc import RMTC
+    from .rmtb import RMTB
+except:
+    pass
