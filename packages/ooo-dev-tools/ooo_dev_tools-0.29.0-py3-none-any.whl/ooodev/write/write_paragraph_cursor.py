07bb90525c0b0c566f29f9e671c06c0d1708c422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:06:34 +0000

    Add package ooo-dev-tools/0.29.0/ooo_dev_tools-0.29.0-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.29.0-py3-none-any.whl/ooodev/write/write_paragraph_cursor.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import Any
+import uno
+
+from com.sun.star.text import XParagraphCursor
+
+from ooodev.adapter.text.paragraph_cursor_partial import ParagraphCursorPartial
+from ooodev.format.inner.style_partial import StylePartial
+from ooodev.loader.inst.lo_inst import LoInst
+from .write_text_cursor import WriteTextCursor
+
+
+class WriteParagraphCursor(WriteTextCursor, ParagraphCursorPartial, StylePartial):
+    """Represents a writer Paragraph cursor."""
+
+    def __init__(self, owner: Any, component: XParagraphCursor, lo_inst: LoInst | None = None) -> None:
+        """
+        Constructor
+
+        Args:
+            owner (WriteDoc): Doc that owns this component.
+            component (XParagraphCursor): A UNO object that supports ``com.sun.star.text.XParagraphCursor`` interface.
+            lo_inst (LoInst, optional): Lo instance. Defaults to ``None``.
+        """
+        WriteTextCursor.__init__(self, owner=owner, component=component, lo_inst=lo_inst)
+        ParagraphCursorPartial.__init__(self, component, None)  # type: ignore
+        StylePartial.__init__(self, component=component)
