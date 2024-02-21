8f49562fe0e2a2e5bfa89a4c354e2748502a7dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:05:23 +0000

    Add package calling-rate/0.4.0/calling_rate-0.4.0-py3-none-any.whl

---
+++ packages/calling-rate/calling_rate-0.4.0-py3-none-any.whl/calling_rate/__init__.py
@@ -0,0 +1,6 @@
+"""Calculate population size from calling rate of seabirds"""
+
+__version__ = "0.4.0"
+from .number_of_burrows_from_call_rates import *  # noqa
+from .raw_data_to_tdp import *  # noqa
+from .curator import *  # noqa
