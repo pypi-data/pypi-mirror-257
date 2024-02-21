f65b6b9d0f1465827c6af819f19cd0c2a370a051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:06:36 +0000

    Add package ForFloatRange/1.0.0/ForFloatRange-1.0.0.tar.gz

---
+++ packages/ForFloatRange/ForFloatRange-1.0.0.tar.gz/ForFloatRange-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ForFloatRange',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Range with float',
+    author='Nova D Andrew',
+    author_email='andrew.d.nova@icloud.com',
+    python_requires='>=3.6',
+)
