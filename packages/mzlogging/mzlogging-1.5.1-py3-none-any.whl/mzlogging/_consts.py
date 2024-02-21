396d170da571454eeb29100f277e874ce4e013fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:03 +0000

    Add package mzlogging/1.5.1/mzlogging-1.5.1-py3-none-any.whl

---
+++ packages/mzlogging/mzlogging-1.5.1-py3-none-any.whl/mzlogging/_consts.py
@@ -0,0 +1,20 @@
+REQUIRED_DB_KEYS = ["host", "user", "password", "database"]
+SUPPORTED_DATABASES = ["mysql"]
+
+LOG_LEVELS = {
+    0: "DEBUG",
+    1: "INFO",
+    2: "WARNING",
+    3: "ERROR",
+    4: "EXCEPTION",
+    5: "CRITICAL"
+}
+
+SEVERITY_LEVELS = {
+    0: "COSMETIC",
+    1: "LOW",
+    2: "NORMAL",
+    3: "HIGH",
+    4: "SEVERE",
+    5: "CRITICAL"
+}
