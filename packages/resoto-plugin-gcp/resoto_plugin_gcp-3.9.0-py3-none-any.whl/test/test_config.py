f4d4995bb8f4b4da065c4ec1ef2ea0744c5efef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 05:04:26 +0000

    Add package resoto-plugin-gcp/3.9.0/resoto_plugin_gcp-3.9.0-py3-none-any.whl

---
+++ packages/resoto-plugin-gcp/resoto_plugin_gcp-3.9.0-py3-none-any.whl/test/test_config.py
@@ -0,0 +1,15 @@
+from resotolib.proc import num_default_threads
+from resotolib.config import Config
+from resoto_plugin_gcp import GCPCollectorPlugin
+
+
+def test_args() -> None:
+    config = Config("dummy", "dummy")
+    GCPCollectorPlugin.add_config(config)
+    Config.init_default_config()
+    assert len(Config.gcp.service_account) == 0
+    assert len(Config.gcp.project) == 0
+    assert len(Config.gcp.collect) == 0
+    assert len(Config.gcp.no_collect) == 0
+    assert Config.gcp.project_pool_size == num_default_threads()
+    assert Config.gcp.fork_process is True
