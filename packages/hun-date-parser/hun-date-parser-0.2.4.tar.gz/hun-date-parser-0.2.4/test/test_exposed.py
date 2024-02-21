28dee73eab07c063759ac5f97da9cb7338b525ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:06:21 +0000

    Add package hun-date-parser/0.2.4/hun-date-parser-0.2.4.tar.gz

---
+++ packages/hun-date-parser/hun-date-parser-0.2.4.tar.gz/hun-date-parser-0.2.4/test/test_exposed.py
@@ -0,0 +1,48 @@
+import pytest
+from datetime import datetime, date, time
+
+from hun_date_parser import datetime2text, text2datetime, text2date, text2time
+
+
+def test_datetime2text():
+    candidates = datetime2text(datetime(2020, 12, 21))
+
+    assert set(candidates) == {'dates', 'times'}
+    assert len([c for c in candidates['dates'] if c]) == 2
+    assert len([c for c in candidates['times'] if c]) == 4
+
+
+tf_t2d = [
+    ('ma', [{'start_date': datetime(2020, 12, 27), 'end_date': datetime(2020, 12, 27, 23, 59, 59)}]),
+    ('ma reggel', [{'start_date': datetime(2020, 12, 27, 6), 'end_date': datetime(2020, 12, 27, 10, 59, 59)}]),
+    ('ma reggeltől tegnap estig', []),
+    ('Egerben leszek december 28-ától 2 napig',
+     [{'start_date': datetime(2020, 12, 28), 'end_date': datetime(2020, 12, 30, 23, 59, 59)}]),
+]
+
+
+@pytest.mark.parametrize("inp, out", tf_t2d)
+def test_text2datetime(inp, out):
+    now = datetime(2020, 12, 27)
+    assert text2datetime(inp, now=now) == out
+
+
+def test_text2date():
+    now = datetime(2020, 12, 27)
+    tf = [('ma', [{'start_date': date(2020, 12, 27), 'end_date': date(2020, 12, 27)}]),
+          ('ma reggel', [{'start_date': date(2020, 12, 27), 'end_date': date(2020, 12, 27)}]),
+          ('reggel nyolc óra', [])]
+
+    for inp, out in tf:
+        assert text2date(inp, now=now) == out
+
+
+def test_text2time():
+    now = datetime(2020, 12, 27)
+    tf = [('ma', []),
+          ('ma reggel', [{'start_date': time(6), 'end_date': time(10, 59, 59)}]),
+          ('ma délelőtt', [{'start_date': time(8), 'end_date': time(11, 59, 59)}]),
+          ('reggel nyolc óra', [{'start_date': time(8), 'end_date': time(8, 59, 59)}])]
+
+    for inp, out in tf:
+        assert text2time(inp, now=now) == out
