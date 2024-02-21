65fe7be4469de63d4d368edf60aca3380a6993a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:05:52 +0000

    Add package temp-testtt/0.0.4/temp_testtt-0.0.4.tar.gz

---
+++ packages/temp-testtt/temp_testtt-0.0.4.tar.gz/temp_testtt-0.0.4/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='temp_testtt',
+    author="",
+    author_email="",
+    python_requires="==3.7.*",
+    packages=find_packages(),
+    version='0.0.4',
+    install_requires=[],
+    url="",
+)
+
