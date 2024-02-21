2db7e088f9c5d892e1ad400f85a875163dea2732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:24 +0000

    Add package digiflow/3.5.0/digiflow-3.5.0-py3-none-any.whl

---
+++ packages/digiflow/digiflow-3.5.0-py3-none-any.whl/digiflow/__init__.py
@@ -0,0 +1,26 @@
+from .digflow_identifier import *
+from .digiflow_io import *
+from .digiflow_metadata import *
+from .digiflow_generate import *
+from .digiflow_export import *
+from .digiflow_validate import (
+    DDB_IGNORE_RULES_BASIC,
+    DDB_IGNORE_RULES_MVW,
+    DDB_IGNORE_RULES_NEWSPAPERS,
+    DDB_MEDIA_SCH,
+    DDB_NEWSP_SCH,
+	DDB_ERROR,
+	DDB_OTHER,
+    CLI_DIR,
+    PATH_CLI,
+    SCH_DIR,
+    PATH_MEDIA_SCH,
+    PATH_NEWSP_SCH,
+    DIGIS_MULTIVOLUME,
+    DIGIS_NEWSPAPER,
+    REPORT_FILE_SCHEMATRON,
+	REPORT_FILE_XSLT,
+	DigiflowDDBException,
+    ddb_validation,
+    ddb_validation_sch,
+)
