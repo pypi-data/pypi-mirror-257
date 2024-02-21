5b7ec318da5ff02269bce07d66ac64d44caef1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:06:37 +0000

    Add package alt-text/0.2.2/alt_text-0.2.2.tar.gz

---
+++ packages/alt-text/alt_text-0.2.2.tar.gz/alt_text-0.2.2/tests/test.py
@@ -0,0 +1,45 @@
+import sys
+
+sys.path.append("../")
+import src.alttext.alttext as alttext
+from src.alttext.descengine.bliplocal import BlipLocal
+from src.alttext.ocrengine.tesseract import Tesseract
+from src.alttext.langengine.privategpt import PrivateGPT
+import keys
+
+# HTML BOOK FILEPATHS
+HTML_BIRD = "../books/pg30221-h/pg30221-images.html"
+HTML_HUNTING = "../books/pg37122-h/pg37122-images.html"
+HTML_MECHANIC = "../books/pg71856-h/pg71856-images.html"
+HTML_INFINITY = "../books/pg71859-h/pg71859-images.html"
+
+# EPUB BOOK FILEPATHS
+EPUB1 = "../books/pg71856-images-3.epub"
+EPUB2 = "../books/pg71908-images-3.epub"
+EPUB3 = "../books/seuss.epub"
+
+HOST1 = "http://127.0.0.1:8001"
+
+
+def testHTML():
+    print("TESTING HTML")
+    alt: alttext.AltTextHTML = alttext.AltTextHTML(
+        BlipLocal("C:/Users/dacru/Desktop/ALT/image-captioning"),
+        Tesseract(),
+        PrivateGPT(HOST1),
+    )
+
+    alt.parseFile(HTML_HUNTING)
+    imgs = alt.getAllImgs()
+    src = imgs[4].attrs["src"]
+    print(src)
+    print(alt.genAltText(src))
+
+    # desc = alt.genDesc(alt.getImgData(src), src)
+    # print(desc)
+    # associations = alt.genAltAssociations(imgs)
+    # print(associations)
+
+
+if __name__ == "__main__":
+    testHTML()
