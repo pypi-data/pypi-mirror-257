8ad360d2244984871865a5b808984a4ff037843d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:15 +0000

    Add package m269-lib/0.0.2/m269_lib-0.0.2.tar.gz

---
+++ packages/m269-lib/m269_lib-0.0.2.tar.gz/m269_lib-0.0.2/m269_lib/__init__.py
@@ -0,0 +1,28 @@
+"""This is a pedagogical algorithms and data structures library.
+
+It currently implements the Bag (multiset), Stack and Queue abstract data types (ADTs).
+
+To install the library in the currently active environment, enter in a terminal:
+```bash
+pip install m269-lib
+```
+To learn how to use the library, see the examples in the documentation of the classes.
+
+The code repository is at https://github.com/dsa-ou/m269-lib.
+
+## Licence
+
+`m269-lib` is Copyright © 2024 by The Open University, UK.
+The code is licensed under a [BSD 3-clause licence](https://github.com/dsa-ou/m269-lib/blob/main/LICENSE).
+The documentation is licensed under a
+[Creative Commons Attribution 4.0 International Licence](http://creativecommons.org/licenses/by/4.0).
+"""
+
+# Import all classes so that they can be directly imported from m269_lib.
+# Provide short names for the recommended implementations.
+from .bag import *
+from .bag import PythonDictBag as Bag
+from .queue import *
+from .queue import LinkedListQueue as Queue
+from .stack import *
+from .stack import PythonListStack as Stack
