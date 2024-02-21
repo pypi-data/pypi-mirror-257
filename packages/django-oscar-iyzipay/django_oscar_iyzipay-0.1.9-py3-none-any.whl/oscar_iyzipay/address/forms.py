b323bbcf2eba7629b23bb08041c20762b6a4a952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:04:10 +0000

    Add package django-oscar-iyzipay/0.1.9/django_oscar_iyzipay-0.1.9-py3-none-any.whl

---
+++ packages/django-oscar-iyzipay/django_oscar_iyzipay-0.1.9-py3-none-any.whl/oscar_iyzipay/address/forms.py
@@ -0,0 +1,26 @@
+from oscar.apps.address import forms as base_forms
+from oscar.core.loading import get_model
+
+UserAddress = get_model("address", "useraddress")
+
+
+class UserAddressForm(base_forms.UserAddressForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta(base_forms.UserAddressForm.Meta):
+
+        fields = [
+            "first_name",
+            "last_name",
+            "TCKN",
+            "line1",
+            "line2",
+            "line3",
+            "line4",
+            "state",
+            "postcode",
+            "country",
+            "phone_number",
+            "notes",
+        ]
