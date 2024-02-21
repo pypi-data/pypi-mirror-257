085604236482177dbb8cfcc9a3a7815cee5df932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:06:44 +0000

    Add package fakeoai/0.1.1/fakeoai-0.1.1.tar.gz

---
+++ packages/fakeoai/fakeoai-0.1.1.tar.gz/fakeoai-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fakeoai",
+    version="0.1.1",
+    packages=find_packages(),
+    author="baobao",
+    author_email="1727283040@qq.com",
+    description="A brief description of what your package does",
+    license="MIT",
+    keywords="openai chatgpt",
+)
