2a53506c6eb1232b6ca921069a4e7b75074f9d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:06:10 +0000

    Add package yuanfen/2024.2.21.1/yuanfen-2024.2.21.1.tar.gz

---
+++ packages/yuanfen/yuanfen-2024.2.21.1.tar.gz/yuanfen-2024.2.21.1/src/yuanfen/time.py
@@ -0,0 +1,29 @@
+import time
+from datetime import datetime
+
+
+def now():
+    return datetime.now()
+
+
+def format(dt: datetime = None, format: str = "%Y-%m-%dT%H:%M:%S.%f") -> str:
+    if dt is None:
+        dt = now()
+    return dt.strftime(format)
+
+
+def parse(dt_str: str, format: str = "%Y-%m-%dT%H:%M:%S.%f") -> datetime:
+    return datetime.strptime(dt_str, format)
+
+
+def format_duration(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    if hours > 0:
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    else:
+        return f"{minutes:02d}:{seconds:02d}"
+
+
+def sleep(secs: float):
+    time.sleep(secs)
