c8c65bae151c4dfb721836e098366b23064e8d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:57 +0000

    Add package mslm-otp-test2/1.0.0/mslm_otp_test2-1.0.0.tar.gz

---
+++ packages/mslm-otp-test2/mslm_otp_test2-1.0.0.tar.gz/mslm_otp_test2-1.0.0/setup_mslm_otp.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+LONG_DESCRIPTION = """
+    # Mslm Otp Python Library
+    Where others see norm, we see opportunity. We build world-class solutions to the toughest problems. 
+    Excellence is a core value that defines our approach from top to bottom.
+    Documentation can be found at [https://github.com/mslmio/sdk-python](https://github.com/mslmio/sdk-python).
+    """
+
+
+setup(
+    name="mslm_otp_test2",
+    version="1.0.0",
+    description="Mslm Otp Python Library",
+    long_description=LONG_DESCRIPTION,
+    url="https://mslm.io",
+    author="Mslm",
+    author_email="support@mslm.io",
+    license="MIT",
+    packages=["mslm_otp", "mslm_lib"],
+    install_requires=["requests", "dataclasses"],
+)
