5a20061c896ef3efbe1dd334521279ec3bbcfe1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:29 +0000

    Add package ReadPdfFiles/1.0.4/ReadPdfFiles-1.0.4.tar.gz

---
+++ packages/ReadPdfFiles/ReadPdfFiles-1.0.4.tar.gz/ReadPdfFiles-1.0.4/readPdfFiles/readPdfWithTabula.py
@@ -0,0 +1,11 @@
+from tabula.io import read_pdf
+
+
+# Read PDF into DataFrame
+
+def readPdfWithTabula(pdf_file_path):
+    text = read_pdf(pdf_file_path, pages='all')
+    if text is not None:
+        return text
+    else:
+        return None
