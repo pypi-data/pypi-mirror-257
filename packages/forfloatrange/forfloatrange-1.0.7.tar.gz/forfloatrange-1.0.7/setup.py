395fd8118c5a4386cccba16276c1f3537b369477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 05:04:56 +0000

    Add package forfloatrange/1.0.7/forfloatrange-1.0.7.tar.gz

---
+++ packages/forfloatrange/forfloatrange-1.0.7.tar.gz/forfloatrange-1.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forfloatrange',
+    version='1.0.7',
+    packages=find_packages(),
+    description='Range with float',
+    author='Nova D Andrew',
+    author_email='andrew.d.nova@icloud.com',
+    python_requires='>=3.6',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
