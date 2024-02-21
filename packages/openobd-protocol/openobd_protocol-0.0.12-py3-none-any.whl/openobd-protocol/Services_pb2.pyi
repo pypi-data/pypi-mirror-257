a697dcd6e4d951aa0aca7568a75696197972d97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:04:56 +0000

    Add package openobd-protocol/0.0.12/openobd_protocol-0.0.12-py3-none-any.whl

---
+++ packages/openobd-protocol/openobd_protocol-0.0.12-py3-none-any.whl/openobd-protocol/Services_pb2.pyi
@@ -0,0 +1,7 @@
+import Authentication_pb2 as _Authentication_pb2
+import BasicResponse_pb2 as _BasicResponse_pb2
+import ModuleConfiguration_pb2 as _ModuleConfiguration_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
