9bd3c878c8563ef400b0d6bddf3aad14dc3b66f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:30 +0000

    Add package fromchaos/0.0.9/fromchaos-0.0.9.tar.gz

---
+++ packages/fromchaos/fromchaos-0.0.9.tar.gz/fromchaos-0.0.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fromchaos',
+    version='0.0.9',
+    packages=find_packages(),
+    install_requires=[
+        'openai==1.4.0',
+        'tenacity==8.2.2',
+        'google-generativeai==0.3.1'
+    ],
+    author='Ryder Damen',
+    author_email='dev@ryderdamen.com',
+    description='A python library for extracting organized data out of chaos using large-language-models (LLMs) like ChatGPT.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ryderdamen/fromchaos',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.8',
+)
