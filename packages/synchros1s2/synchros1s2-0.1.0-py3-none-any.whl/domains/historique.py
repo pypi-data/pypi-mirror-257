1c13fac54a07f9820eaea43e0104594bf3f4e82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:14 +0000

    Add package synchros1s2/0.1.0/synchros1s2-0.1.0-py3-none-any.whl

---
+++ packages/synchros1s2/synchros1s2-0.1.0-py3-none-any.whl/domains/historique.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+import config
+
+
+@dataclass
+class HistoriqueSynchronisation:
+    date: str
+    synchro_message: str
+
+    def save_synchronisation(self):
+        pass
