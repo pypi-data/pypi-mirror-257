29a01a3472cccebc72e51fd691c94eca25e7ef1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:06:11 +0000

    Add package pybell/0.1.1/pybell-0.1.1.tar.gz

---
+++ packages/pybell/pybell-0.1.1.tar.gz/pybell-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybell',
+    version='0.1.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Oliver Vea',
+    author_email='oliver.vea@gmail.com',
+    description='Simple module to play a ding with Python. Useful for notifying you that a long-running shell command is done.',
+    include_package_data=True,  # This tells setuptools to include files specified in MANIFEST.in
+)
