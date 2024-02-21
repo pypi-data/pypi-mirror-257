f6c11b803a76ddb1eb3349dc0b674cbe38d37f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:06:56 +0000

    Add package pyfa-converter/1.0.5.0/pyfa_converter-1.0.5.0-py3-none-any.whl

---
+++ packages/pyfa-converter/pyfa_converter-1.0.5.0-py3-none-any.whl/pyfa_converter/__init__.py
@@ -0,0 +1,15 @@
+from .main import PydanticConverter
+from .depends import (
+    FormDepends,
+    QueryDepends,
+    BodyDepends,
+    PyFaDepends,
+)
+
+__all__ = (
+    "PydanticConverter",
+    "FormDepends",
+    "QueryDepends",
+    "PyFaDepends",
+    "BodyDepends",
+)
