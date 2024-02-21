099d57b1e31ef439e9c49e46bbf1d2d80f479277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 07:05:28 +0000

    Add package example-package-HA-TUONG-NGUYEN-NEW/0.0.0.2/example_package_HA_TUONG_NGUYEN_NEW-0.0.0.2-py3-none-any.whl

---
+++ packages/example-package-HA-TUONG-NGUYEN-NEW/example_package_HA_TUONG_NGUYEN_NEW-0.0.0.2-py3-none-any.whl/NOT_SAME_NAME/__init__.py
@@ -0,0 +1,10 @@
+"""
+Created by hatuongnguyen on 04:09:30 - 2/21/24
+
+Project: SetupPythonPackageIndex
+Path: SetupPythonPackageIndex/src/NOT_SAME_NAME
+"""
+
+from .exam import g
+
+__all__ = ['g']
---
+++ packages/example-package-HA-TUONG-NGUYEN-NEW/example_package_HA_TUONG_NGUYEN_NEW-0.0.0.2-py3-none-any.whl/example_package_HA_TUONG_NGUYEN/__init__.py
@@ -0,0 +1,5 @@
+from .example import add_one
+
+__all__ = [
+    'example',
+]
