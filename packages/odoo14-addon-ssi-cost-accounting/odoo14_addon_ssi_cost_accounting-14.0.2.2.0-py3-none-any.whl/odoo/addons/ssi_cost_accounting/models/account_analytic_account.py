efa1fa6266ff3cd1f297a0e51d726e90b459c84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 14:05:03 +0000

    Add package odoo14-addon-ssi-cost-accounting/14.0.2.2.0/odoo14_addon_ssi_cost_accounting-14.0.2.2.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-cost-accounting/odoo14_addon_ssi_cost_accounting-14.0.2.2.0-py3-none-any.whl/odoo/addons/ssi_cost_accounting/models/account_analytic_account.py
@@ -0,0 +1,20 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class AccountAnalyticAccount(models.Model):
+    _name = "account.analytic.account"
+    _inherit = [
+        "account.analytic.account",
+        "mixin.date_duration",
+    ]
+    _order = "group_id, id"
+
+    # Mixin duration attribute
+    _date_start_readonly = False
+    _date_end_readonly = False
+    _date_start_required = False
+    _date_end_required = False
