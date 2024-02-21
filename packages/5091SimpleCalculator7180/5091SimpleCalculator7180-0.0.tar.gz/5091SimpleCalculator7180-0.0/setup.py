ac2b1b03e6aa505fd4850b99f33cb12bed00745a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:06:02 +0000

    Add package 5091SimpleCalculator7180/0.0/5091SimpleCalculator7180-0.0.tar.gz

---
+++ packages/5091SimpleCalculator7180/5091SimpleCalculator7180-0.0.tar.gz/5091SimpleCalculator7180-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='5091SimpleCalculator7180',
+      version='0.0',
+      description='For Learning only , created Calculator',
+      packages=['5091SimpleCalculator7180'],
+      author_email='kumar.san5091@gmail.com',
+      zip_safe=False)
