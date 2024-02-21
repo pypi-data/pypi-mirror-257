94931f7863c9ee152318e0ca5f06cd1df860d69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:04:10 +0000

    Add package textkit-learn/0.1.36/textkit_learn-0.1.36-py3-none-any.whl

---
+++ packages/textkit-learn/textkit_learn-0.1.36-py3-none-any.whl/tklearn/__init__.py
@@ -0,0 +1,7 @@
+from tklearn import config
+
+__version__ = "0.1.36"
+
+__all__ = []
+
+config.init_config()
---
+++ packages/textkit-learn/textkit_learn-0.1.36-py3-none-any.whl/tklearn/datasets/__init__.py
@@ -0,0 +1,4 @@
+from tklearn.datasets.dataset import Dataset
+from tklearn.datasets.loader import load_dataset, register
+
+__all__ = ["Dataset", "load_dataset", "register"]
