d4317444ff689e701c478085ebfc63f6fd9c4538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:12 +0000

    Add package kite/1.5.8/kite-1.5.8-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/kite/kite-1.5.8-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/kite/sources/compound_sources.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pyrocko.orthodrome as od
+from pyrocko.guts import Float
+
+from . import compound_engine as ce
+from .base import SandboxSource, SourceProcessor
+
+d2r = np.pi / 180.0
+r2d = 180.0 / np.pi
+km = 1e3
+
+
+class EllipsoidSource(SandboxSource):
+    __implements__ = "CompoundModel"
+
+    length_x = Float.T(
+        help="Length of x semi-axis of ellisoid before rotation in [m]", default=1.0
+    )
+    length_y = Float.T(
+        help="Length of y semi-axis of ellisoid before rotation in [m]", default=1.0
+    )
+    length_z = Float.T(
+        help="Length of z semi-axis of ellisoid before rotation in [m]", default=1.0
+    )
+    rotation_x = Float.T(
+        help="Clockwise rotation of ellipsoid around x-axis in [deg]", default=0.0
+    )
+    rotation_y = Float.T(
+        help="Clockwise rotation of ellipsoid around y-axis in [deg]", default=0.0
+    )
+    rotation_z = Float.T(
+        help="Clockwise rotation of ellipsoid around z-axis in [deg]", default=0.0
+    )
+
+    mu = Float.T(
+        help="Shear modulus, 2. Lame constant in [GPa]",
+        default=8.0,
+    )
+    lamda = Float.T(help="Lame constant in [GPa]", default=8.0)
+    cavity_pressure = Float.T(help="Pressure on the cavity walls in [GPa]", default=0.5)
+
+    @property
+    def volume(self):
+        K = self.lamda + 2 * self.mu / 3  # Bulk Modulus
+        V = 4.0 / 3 * np.pi * self.length_x * self.length_y * self.length_z
+        V = (self.cavity_pressure * V) / K
+        return V
+
+    def ECMParameters(self):
+        if self._sandbox:
+            east_shift, north_shift = od.latlon_to_ne_numpy(
+                self._sandbox.frame.llLat, self._sandbox.frame.llLon, self.lat, self.lon
+            )
+        else:
+            east_shift = 0.0
+            north_shift = 0.0
+
+        params = {
+            "x0": self.easting + east_shift,
+            "y0": self.northing + north_shift,
+            "z0": self.depth,
+            "rotx": self.rotation_x,
+            "roty": self.rotation_y,
+            "rotz": self.rotation_z,
+            "ax": self.length_x,
+            "ay": self.length_y,
+            "az": self.length_z,
+            "P": self.cavity_pressure * 1e9,
+            "mu": self.mu * 1e9,
+            "lamda": self.lamda * 1e9,
+        }
+        return params
+
+
+class PointCompoundSource(SandboxSource):
+    __implements__ = "CompoundModel"
+
+    rotation_x = Float.T(
+        help="Clockwise rotation of ellipsoid around x-axis in [deg]", default=0.0
+    )
+    rotation_y = Float.T(
+        help="Clockwise rotation of ellipsoid around y-axis in [deg]", default=0.0
+    )
+    rotation_z = Float.T(
+        help="Clockwise rotation of ellipsoid around z-axis in [deg]", default=0.0
+    )
+    dVx = Float.T(help="Volume change in x-plane in [m3]", default=1.0)
+    dVy = Float.T(help="Volume change in y-plane in [m3]", default=1.0)
+    dVz = Float.T(help="Volume change in z-plane in [m3]", default=1.0)
+    nu = Float.T(help="Poisson's ratio, typically 0.25", default=0.25)
+
+    @property
+    def volume(self):
+        # After Nikkhoo, M et al. 2017
+        return (self.dVx + self.dVy + self.dVz) / 1.8
+
+    def pointCDMParameters(self):
+        if self._sandbox:
+            east_shift, north_shift = od.latlon_to_ne_numpy(
+                self._sandbox.frame.llLat, self._sandbox.frame.llLon, self.lat, self.lon
+            )
+        else:
+            east_shift = 0.0
+            north_shift = 0.0
+
+        params = {
+            "x0": self.easting + east_shift,
+            "y0": self.northing + north_shift,
+            "z0": self.depth,
+            "rotx": self.rotation_x,
+            "roty": self.rotation_y,
+            "rotz": self.rotation_z,
+            "dVx": self.dVx**3,
+            "dVy": self.dVy**3,
+            "dVz": self.dVz**3,
+            "nu": self.nu,
+        }
+        return params
+
+
+class CompoundModelProcessor(SourceProcessor):
+    __implements__ = "CompoundModel"
+
+    def process(self, sources, sandbox, nthreads=0):
+        result = {
+            "processor_profile": dict(),
+            "displacement.e": np.zeros(sandbox.frame.npixel),
+            "displacement.n": np.zeros(sandbox.frame.npixel),
+            "displacement.d": np.zeros(sandbox.frame.npixel),
+        }
+
+        coords = sandbox.frame.coordinatesMeter
+
+        for src in sources:
+            if isinstance(src, EllipsoidSource):
+                res = ce.ECM(coords, **src.ECMParameters())
+            elif isinstance(src, PointCompoundSource):
+                res = ce.pointCDM(coords, **src.pointCDMParameters())
+            else:
+                raise AttributeError("Source of wrong type!")
+            result["displacement.e"] += res[0]
+            result["displacement.n"] += res[1]
+            result["displacement.d"] += res[2]
+
+        return result
---
+++ packages/kite/kite-1.5.8-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/kite/talpa/sources/compound_models.py
@@ -0,0 +1,154 @@
+import numpy as np
+
+from kite.sources import EllipsoidSource, PointCompoundSource
+
+from .base import PointSourceROI, SourceDelegate, SourceEditDialog
+
+d2r = np.pi / 180.0
+r2d = 180.0 / np.pi
+
+
+class EllipsoidSourceDelegate(SourceDelegate):
+    __represents__ = "EllipsoidSource"
+
+    display_backend = "Compound Model"
+    display_name = "EllipsoidSource"
+
+    parameters = [
+        "easting",
+        "northing",
+        "depth",
+        "length_x",
+        "length_y",
+        "length_z",
+        "rotation_x",
+        "rotation_y",
+        "rotation_z",
+        "mu",
+        "lamda",
+        "cavity_pressure",
+    ]
+    ro_parameters = ["volume"]
+
+    class EllipsoidalSourceDialog(SourceEditDialog):
+        def __init__(self, delegate, *args, **kwargs):
+            SourceEditDialog.__init__(
+                self, delegate, ui_file="ellipsoid_source.ui", *args, **kwargs
+            )
+
+            def setLabel(method, fmt, value, suffix=""):
+                method(fmt.format(value) + suffix)
+
+            self.volume.setValue = lambda v: setLabel(
+                self.volume.setText, "{:.2e}", v, " m<sup>3</sup>"
+            )
+
+            self.getSourceParameters()
+
+    ROIWidget = PointSourceROI
+    EditDialog = EllipsoidalSourceDialog
+
+    @staticmethod
+    def getRepresentedSource(sandbox):
+        return EllipsoidSource()
+
+    def formatListItem(self):
+        item = """
+<span style="font-weight: bold; font-style: oblique">
+    {idx}. {delegate.display_name}
+    <span style="color: #616161;">
+        ({delegate.display_backend})
+    </span>
+</span>
+<table style="color: #616161; font-size: small;">
+<tr>
+    <td>Depth:</td><td>{source.depth:.2f} m</td>
+</tr><tr>
+    <td>Dimensions</td><td>
+        x: {source.length_x:.0f} m;
+        y: {source.length_y:.0f} m;
+        z: {source.length_z:.0f} m
+    </td>
+</tr><tr>
+    <td>Rotations</td><td>
+        x: {source.rotation_x:.1f}&deg;;
+        y: {source.rotation_y:.1f}&deg;;
+        z: {source.rotation_z:.1f}&deg;
+    </td>
+</tr><tr style="font-weight: bold;">
+    <td>Volume:</td><td>{source.volume:.2e} m<sup>3</sup></td>
+</tr></table>
+"""
+        return item.format(idx=self.index.row() + 1, delegate=self, source=self.source)
+
+
+class PointCompoundSourceDelegate(SourceDelegate):
+    __represents__ = "PointCompoundSource"
+
+    display_backend = "Compound Model"
+    display_name = "PointCompoundSource"
+
+    parameters = [
+        "easting",
+        "northing",
+        "depth",
+        "dVx",
+        "dVy",
+        "dVz",
+        "rotation_x",
+        "rotation_y",
+        "rotation_z",
+        "nu",
+    ]
+    ro_parameters = ["volume"]
+
+    class CompoundSourceDialog(SourceEditDialog):
+        def __init__(self, delegate, *args, **kwargs):
+            SourceEditDialog.__init__(
+                self, delegate, ui_file="pCDM_source.ui", *args, **kwargs
+            )
+
+            def setLabel(method, fmt, value, suffix=""):
+                method(fmt.format(value) + suffix)
+
+            self.volume.setValue = lambda v: setLabel(
+                self.volume.setText, "{:.2e}", v, " m<sup>3</sup>"
+            )
+
+            self.getSourceParameters()
+
+    ROIWidget = PointSourceROI
+    EditDialog = CompoundSourceDialog
+
+    @staticmethod
+    def getRepresentedSource(sandbox):
+        return PointCompoundSource()
+
+    def formatListItem(self):
+        item = """
+<span style="font-weight: bold; font-style: oblique">
+    {idx}. {delegate.display_name}
+    <span style="color: #616161;">
+        ({delegate.display_backend})
+    </span>
+</span>
+<table style="color: #616161; font-size: small;">
+<tr>
+    <td>Depth:</td><td>{source.depth:.2f} m</td>
+</tr><tr>
+    <td>Volume Change</td><td>
+        x: {source.dVx:.0f} m<sup>3</sup>;
+        y: {source.dVy:.0f} m<sup>3</sup>;
+        z: {source.dVz:.0f} m<sup>3</sup>
+    </td>
+</tr><tr>
+    <td>Rotations</td><td>
+        x: {source.rotation_x:.1f}&deg;;
+        y: {source.rotation_y:.1f}&deg;;
+        z: {source.rotation_z:.1f}&deg;
+    </td>
+</tr><tr style="font-weight: bold;">
+    <td>Volume:</td><td>{source.volume:.2e} m<sup>3</sup></td>
+</tr></table>
+"""
+        return item.format(idx=self.index.row() + 1, delegate=self, source=self.source)
