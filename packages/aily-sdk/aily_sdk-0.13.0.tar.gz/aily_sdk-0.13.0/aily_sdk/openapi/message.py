c761d46694a1557e266898be7137150ffcf002c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 16:05:32 +0000

    Add package aily-sdk/0.13.0/aily_sdk-0.13.0.tar.gz

---
+++ packages/aily-sdk/aily_sdk-0.13.0.tar.gz/aily_sdk-0.13.0/aily_sdk/openapi/message.py
@@ -0,0 +1,20 @@
+import json
+
+from aily_sdk.openapi import OpenAPIClient
+
+
+class MessageResult:
+    def __init__(self, result):
+        self.code = result['code']
+        self.msg = result['msg']
+
+        self.session_id = None
+        if 'data' in result:
+            self.session_id = result['data']['session_id']
+
+
+class MessageClient:
+    def __init__(self, client, session):
+        self.client = client
+        self.session = session
+
