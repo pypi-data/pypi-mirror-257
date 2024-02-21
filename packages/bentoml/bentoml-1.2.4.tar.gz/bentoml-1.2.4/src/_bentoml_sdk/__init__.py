0bb9ea82e7a72deb07f018971942ec44dc7eccd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 04:04:17 +0000

    Add package bentoml/1.2.4/bentoml-1.2.4.tar.gz

---
+++ packages/bentoml/bentoml-1.2.4.tar.gz/bentoml-1.2.4/src/_bentoml_sdk/__init__.py
@@ -0,0 +1,30 @@
+from bentoml._internal.utils.pkg import pkg_version_info
+
+if (ver := pkg_version_info("pydantic")) < (2,):
+    raise ImportError(
+        f"The new SDK runs on pydantic>=2.0.0, but the you have {'.'.join(map(str, ver))}. "
+        "Please upgrade it."
+    )
+
+from ._pydantic import add_custom_preparers
+
+add_custom_preparers()
+del add_custom_preparers
+# ruff: noqa
+
+from .api import api
+from .service import depends
+from .service import Service
+from .service import service
+from .service import runner_service
+from .io_models import IODescriptor
+
+__all__ = [
+    "api",
+    "depends",
+    "Service",
+    "service",
+    "runner_service",
+    # io descriptors
+    "IODescriptor",
+]
