c219f6584493db6accc35fa8a16114ffecc1e099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 14:04:56 +0000

    Add package ats-case/1.2.13/ats_case-1.2.13.tar.gz

---
+++ packages/ats-case/ats_case-1.2.13.tar.gz/ats_case-1.2.13/ats_case/common/error.py
@@ -0,0 +1,50 @@
+class APIError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "APIError - API服务接口[{}], 连接失败.".format(repr(self.value))
+
+
+class MeterNeedSecurityAuthentication(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "MeterNeedSecurityAuthentication - 电表需要安全认证, 表位:[{}].".format(repr(self.value))
+
+
+class MeterSecurityAuthenticationError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "MeterSecurityAuthenticationError - 电表安全认证失败, {}.".format(repr(self.value))
+
+
+class MeterOperationError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "MeterOperationError - 电表操作失败, {}.".format(repr(self.value))
+
+
+class BenchReadNoneError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "BenchReadNoneError - 表台读取误差错误, {}.".format(repr(self.value))
+
+
+class ClientError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "ClientReturnError - 客户端返回结果为空. {}".format(repr(self.value))
+
+# MeterTimeoutError 电表连接超时
+# 服务接口错误  电表错误  客户端错误 加密机错误
+# 连接错误 代码错误 操作错误
