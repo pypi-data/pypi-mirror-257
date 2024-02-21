d3c59ff373259bdfa0439f6b645b7e710085e515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 06:05:30 +0000

    Add package rsa-crypto-python/2.0.4/rsa-crypto-python-2.0.4.tar.gz

---
+++ packages/rsa-crypto-python/rsa-crypto-python-2.0.4.tar.gz/rsa-crypto-python-2.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rsa-crypto-python',
+    version='2.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'python-dotenv'
+    ],
+    test_suite='tests',
+)
