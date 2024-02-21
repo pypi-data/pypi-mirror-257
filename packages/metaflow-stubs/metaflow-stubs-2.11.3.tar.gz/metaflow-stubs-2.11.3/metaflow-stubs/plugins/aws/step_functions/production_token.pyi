030c2a438b1580351abe00d075d1abb93a66d74c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 15:05:50 +0000

    Add package metaflow-stubs/2.11.3/metaflow-stubs-2.11.3.tar.gz

---
+++ packages/metaflow-stubs/metaflow-stubs-2.11.3.tar.gz/metaflow-stubs-2.11.3/metaflow-stubs/plugins/aws/step_functions/production_token.pyi
@@ -0,0 +1,18 @@
+##################################################################################
+#                       Auto-generated Metaflow stub file                        #
+# MF version: 2.11.3                                                             #
+# Generated on 2024-02-21T06:18:26.310136                                        #
+##################################################################################
+
+from __future__ import annotations
+
+
+def new_token(token_prefix, prev_token = None):
+    ...
+
+def load_token(token_prefix):
+    ...
+
+def store_token(token_prefix, token):
+    ...
+
