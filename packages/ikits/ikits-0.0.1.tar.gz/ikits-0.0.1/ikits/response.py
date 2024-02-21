a2df8c35a14a20818d2e83de67cf5b188904a25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-21 03:05:32 +0000

    Add package ikits/0.0.1/ikits-0.0.1.tar.gz

---
+++ packages/ikits/ikits-0.0.1.tar.gz/ikits-0.0.1/ikits/response.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+class Code:
+    SUCCESS = 0
+
---
+++ packages/ikits/ikits-0.0.1.tar.gz/ikits-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+name = "ikits"
+version = "0.0.1"
+packages = find_packages()
+
+setup(
+    name=name,
+    version=version,
+    packages=packages,
+    author='',
+    author_email='',
+    maintainer='',
+    maintainer_email='',
+    url='',
+    license='',
+    description='',
+)
+
