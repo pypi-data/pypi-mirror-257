b3f679fd0d5032fbaad1f5b6bd7521bdd160b55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:04:51 +0000

    Add package pymldd/0.0.2/pymldd-0.0.2.tar.gz

---
+++ packages/pymldd/pymldd-0.0.2.tar.gz/pymldd-0.0.2/setup.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2024 - Intelligentica (Morocco)
+Email: a.benmhamed@intelligentica.net
+Website: https://intelligentica.net
+"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pymldd",
+    version="0.0.2",
+    author="Abdelouahed Ben Mhamed",
+    author_email="a.benmhamed@intelligentica.net",
+    description="A Python library for solving combinatorial optimization using Multivalued  Decision Diagrams and Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AIM-BENMHAMED/pymldd",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        # Add any dependencies your library may have
+    ],
+    entry_points={
+        "console_scripts": [
+            "pymldd-hello = pymldd:hello"
+        ],
+    },
+)
