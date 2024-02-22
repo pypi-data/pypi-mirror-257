6665098901c92064e0763ec2bf5ff34eb39a2d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:39 +0000

    Add package ds20kdb-avt/0.2.45/ds20kdb-avt-0.2.45.tar.gz

---
+++ packages/ds20kdb-avt/ds20kdb-avt-0.2.45.tar.gz/ds20kdb-avt-0.2.45/src/ds20kdb/deprecated_create_credentials_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print(
+	"--------------------------------------------------------------------------------"
+	"The create_credentials_file command has been renamed. Please use\n"
+	"ds20k_create_credentials_file instead.\n"
+	"\n"
+    "Some command names may be used sporadically and are easily forgotten.\n"
+    "The ds20k_ prefix is now employed to make it easier to discover\n"
+    "DarkSide-20k database-related commands through tab-completion\n"
+    "on the command line.\n"
+	"--------------------------------------------------------------------------------"
+)
