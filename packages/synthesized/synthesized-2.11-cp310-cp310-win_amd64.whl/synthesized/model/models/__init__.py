4ce64fb119cf96926061addaea61447fe9c8ad61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:05:05 +0000

    Add package synthesized/2.11/synthesized-2.11-cp310-cp310-win_amd64.whl

---
+++ packages/synthesized/synthesized-2.11-cp310-cp310-win_amd64.whl/synthesized/model/models/__init__.py
@@ -0,0 +1,22 @@
+from .address import AddressModel, PostcodeModel
+from .bank import BankModel
+from .company import CompanyModel
+from .enumeration import Enumeration
+from .histogram import Histogram
+from .kde import KernelDensityEstimate
+from .person import GenderModel, PersonModel
+from .string import FormattedStringModel, SequentialFormattedString
+
+__all__ = [
+    "AddressModel",
+    "BankModel",
+    "CompanyModel",
+    "Histogram",
+    "KernelDensityEstimate",
+    "PersonModel",
+    "GenderModel",
+    "PostcodeModel",
+    "FormattedStringModel",
+    "SequentialFormattedString",
+    "Enumeration",
+]
