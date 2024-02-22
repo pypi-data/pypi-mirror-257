5230ecda6e2f7ef5c33cca24b61d0f024d7c8819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:19 +0000

    Add package endstone/1.20.62.0/endstone-1.20.62.0-cp311-cp311-manylinux_2_31_x86_64.whl

---
+++ packages/endstone/endstone-1.20.62.0-cp311-cp311-manylinux_2_31_x86_64.whl/endstone/logger.py
@@ -0,0 +1,10 @@
+__all__ = ["Logger"]
+
+for name in __all__:
+    from importlib import import_module
+
+    module = import_module("endstone._internal.endstone_python")
+    globals()[f"_{name}"] = module.__dict__[name]
+    del module
+
+Logger = globals()["_Logger"]
