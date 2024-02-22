44a08eaf1f385eb1a3d00f3a423ed2e2c6844002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:04:51 +0000

    Add package pymldd/0.0.1/pymldd-0.0.1-py3-none-any.whl

---
+++ packages/pymldd/pymldd-0.0.1-py3-none-any.whl/pymldd/main.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2024 - Intelligentica (Morocco)
+Email: a.benmhamed@intelligentica.net
+Website: https://intelligentica.net
+"""
+
+
+def hello():
+    print("Hello Multivalued Decision Diagram with Machine Learning")
