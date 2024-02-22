1207dc1f3ae30654ce5aebb3dd1fe7923c73362a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:05:00 +0000

    Add package lca-algebraic-dev/1.0.5.1651233.dev0/lca_algebraic_dev-1.0.5.1651233.dev0-py2.py3-none-any.whl

---
+++ packages/lca-algebraic-dev/lca_algebraic_dev-1.0.5.1651233.dev0-py2.py3-none-any.whl/lca_algebraic/__init__.py
@@ -0,0 +1,59 @@
+#
+# This file defines several utility functions above brightway2 to be used by notebooks
+#
+
+
+import lca_algebraic.helpers
+
+from .base_utils import *
+from .helpers import *
+from .io import *
+from .lca import *
+from .params import *
+from .stats import *
+
+
+def deleteDb(db_name):
+    del bw.databases[db_name]
+
+
+def resetDb(db_name, foreground=True):
+    """Create or cleanup a user DB"""
+    if db_name in bw.databases:
+        error("Db %s was here. Reseting it" % db_name)
+        del bw.databases[db_name]
+    db = bw.Database(db_name)
+    db.write(dict())
+    if foreground:
+        setForeground(db_name)
+    else:
+        setBackground(db_name)
+
+
+def initProject(project_name):
+    """Setup the project if not already done."""
+    bw.projects.set_current(project_name)
+    bw.bw2setup()
+
+
+def initDb(project_name):
+    """Deprecated : use initProject(...)"""
+    error("Deprecated : use initProject")
+    initProject(project_name)
+
+
+def importDb(dbname, path, parallel=False):
+    """Import eco invent DB"""
+
+    if dbname in bw.databases:
+        error("Database '%s' has already been imported " % dbname)
+    else:
+        ei34 = bw.SingleOutputEcospold2Importer(path, dbname, use_mp=parallel)
+        ei34.apply_strategies()
+        ei34.statistics()
+        ei34.write_database()
+
+
+# Global print options
+np.set_printoptions(threshold=30)
+pd.options.display.float_format = "{:,g}".format
