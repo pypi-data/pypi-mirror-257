619200a998163f34e8ea831f2a3bf211bb2e247e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:07:56 +0000

    Add package flask-edk-threads/1.0.1/flask_edk_threads-1.0.1.tar.gz

---
+++ packages/flask-edk-threads/flask_edk_threads-1.0.1.tar.gz/flask_edk_threads-1.0.1/src/flask_edk_threads/abstract_event.py
@@ -0,0 +1,36 @@
+import abc
+import uuid
+
+
+class AbstractEvent(abc.ABC):
+    def __init__(self):
+        self.event_id = str(uuid.uuid4()).replace("-", "")
+        self.task_thread = None
+
+    @abc.abstractmethod
+    def action(self): ...
+
+    def call_back(self):
+        """
+        定义回调函数
+        :return:
+        :rtype:
+        """
+        ...
+
+    def criteria_desc(self):
+        """
+        默认的为 空的字典
+        :return:
+        :rtype:
+        """
+        return dict()
+
+    def fall_back(self, e):
+        """
+        如果有异常发生的时候的错误处理
+        :param: e 系统捕获的异常
+        :return:
+        :rtype:
+        """
+        ...
