11714cdd6765c8f29da7c0623cffe21b7c415ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:08:13 +0000

    Add package email-validaor/1.0.1/email_validaor-1.0.1.tar.gz

---
+++ packages/email-validaor/email_validaor-1.0.1.tar.gz/email_validaor-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name = 'email_validaor',
+    version = '1.0.1',
+    author = 'Abdiel Wilson',
+    author_email = 'wilsonabdiel86@gmail.com',
+    license = 'MIT License',
+    description = 'This Python function, email_validator, checks if an input string is a valid email address',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/Wilsonabdiel/regex-gen',
+    py_modules = ['email_validator', 'app'],
+    packages = find_packages(),
+    install_requires = [requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        cooltool=my_tool:cli
+    '''
+)
