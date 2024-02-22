b7f2a950cc69fe836a11abad6401c21c1ee06590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:17 +0000

    Add package tradingcornerstone/0.1/tradingcornerstone-0.1.tar.gz

---
+++ packages/tradingcornerstone/tradingcornerstone-0.1.tar.gz/tradingcornerstone-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='tradingcornerstone',  
+     version='0.1',
+     scripts=['tradingcornerstone'] ,
+     author="f1",
+     author_email="ll@gmail.com",
+     description="Place holder!",
+     long_description="Place Holder!",
+     url="https://placeholder",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 1 - Planning",
+     ],
+ )
