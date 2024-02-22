4405ce7c42cc96212b06ba156ff14a570bbbe229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:05:11 +0000

    Add package lute3/3.2.2/lute3-3.2.2.tar.gz

---
+++ packages/lute3/lute3-3.2.2.tar.gz/lute3-3.2.2/tests/unit/models/test_Book.py
@@ -0,0 +1,44 @@
+"""
+Book tests.
+"""
+
+from lute.models.book import Book
+
+
+def test_create_book(english):
+    """
+    When a book is created with given content, the content
+    is split into separate Text objects.
+    """
+    fulltext = "Here is a dog. And a cat."
+    scenario(english, fulltext, 5, ["Here is a dog.[4]", "And a cat.[3]"])
+    scenario(english, fulltext, 500, ["Here is a dog. And a cat.[7]"])
+    scenario(
+        english,
+        fulltext + " And a thing.",
+        8,
+        ["Here is a dog. And a cat.[7]", "And a thing.[3]"],
+    )
+    scenario(
+        english,
+        "Here is a dog.\nAnd a cat.",
+        500,
+        ["Here is a dog.\nAnd a cat.[7]"],
+    )
+    scenario(
+        english,
+        "Here is a dog.\n\n\nAnd a cat.",
+        500,
+        ["Here is a dog.\n\n\nAnd a cat.[7]"],
+    )
+
+
+def scenario(english, fulltext, maxwords, expected):
+    """
+    Check scenarios.
+    """
+    b = Book.create_book("hi", english, fulltext, maxwords)
+
+    actuals = [f"{t.text}[{t.word_count}]" for t in b.texts]
+    print(actuals)
+    assert "/".join(actuals) == "/".join(expected), f"scen {maxwords}"
