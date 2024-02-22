9b3583a3c26ca20ce7873a427afcb05a12d0b433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:52 +0000

    Add package splight-lib/5.1.2/splight_lib-5.1.2-py3-none-any.whl

---
+++ packages/splight-lib/splight_lib-5.1.2-py3-none-any.whl/splight_lib/client/communication/classmap.py
@@ -0,0 +1,7 @@
+from splight_lib.models.communication import CommunicationContext
+from splight_lib.models.event import CommunicationEvent
+
+CLASSMAP = {
+    CommunicationContext: "v2/engine/communication/context/",
+    CommunicationEvent: "v2/engine/communication/",
+}
