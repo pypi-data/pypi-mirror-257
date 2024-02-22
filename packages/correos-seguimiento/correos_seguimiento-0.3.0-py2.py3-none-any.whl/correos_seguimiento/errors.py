24bb2aba5b6d256f9f68e7f8aeaf7be45f49d4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:05:08 +0000

    Add package correos-seguimiento/0.3.0/correos_seguimiento-0.3.0-py2.py3-none-any.whl

---
+++ packages/correos-seguimiento/correos_seguimiento-0.3.0-py2.py3-none-any.whl/correos_seguimiento/errors.py
@@ -0,0 +1,22 @@
+class UndefinedCredentials(Exception):
+    pass
+
+
+class InvalidCredentials(Exception):
+    pass
+
+
+class InvalidEndpoint(Exception):
+    pass
+
+
+class UnknownApiResponse(Exception):
+    pass
+
+
+class InvalidApiResponse(Exception):
+    pass
+
+
+class TimeOut(Exception):
+    pass
