667c3b8e06848341b195a501051737c33a071de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:04:53 +0000

    Add package sosstat/0.1.0/sosstat-0.1.0-py3-none-any.whl

---
+++ packages/sosstat/sosstat-0.1.0-py3-none-any.whl/sosstat/collect/ip.py
@@ -0,0 +1,67 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2024 Robin Jarry
+
+import pathlib
+import re
+
+from . import D
+
+
+IFACE_RE = re.compile(
+    r"""
+    ^(?P<name>[^@:]+?)(?:@(?P<link>[^:]+))?:\s+
+        <(?P<flags>[\w,-]+)>\s+
+        mtu\s+(?P<mtu>\d+)
+        (?:.*?\smaster\s+(?P<master>\S+)\s)?.*\n
+    ^\s+link/ether\s+(?P<mac>[a-f\d:]+)\s.*\n
+    (?:^\s+
+        (?P<kind>
+            vlan
+            |
+            bridge
+            |
+            bond
+            |
+            veth
+            |
+            vxlan
+            |
+            tun
+            |
+            bond_slave
+            |
+            bridge_slave
+        )\s+
+        (?:
+            protocol\s+\S+\s+id\s+(?P<vlan>\d+)
+            |
+            type\s+(?P<tun_type>\S+)
+            |
+            mode\s+(?P<bond_mode>\S+)
+            |
+            state\s+(?P<slave_state>\S+)
+        )?.*
+    \n)?
+    """,
+    re.VERBOSE | re.MULTILINE,
+)
+ADDR_RE = re.compile(
+    r"inet6? (?P<addr>(?:\d+(?:\.\d+){3}|[a-f\d:]+)/\d+) .*scope global",
+)
+
+
+def parse_report(path: pathlib.Path, data: D):
+    data.interfaces = ip = D()
+    f = path / "sos_commands/networking/ip_-d_address"
+    if not f.is_file():
+        return
+    for block in re.split(r"^\d+: ", f.read_text(), flags=re.MULTILINE):
+        match = IFACE_RE.search(block)
+        if not match:
+            continue
+        d = D({k: v for (k, v) in match.groupdict().items() if v is not None})
+        for dev in path.glob(f"sys/class/net/{d.name}/device"):
+            d.device = dev.resolve().name
+        ip[d.name] = d
+        for m in ADDR_RE.finditer(block):
+            d.setdefault("ip", []).append(m.group("addr"))
