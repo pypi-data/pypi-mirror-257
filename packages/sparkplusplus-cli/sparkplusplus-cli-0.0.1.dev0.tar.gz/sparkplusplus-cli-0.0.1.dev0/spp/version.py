0e65c77b4f8eede15c9524b1b2a0ed5bc3ff9122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:04 +0000

    Add package sparkplusplus-cli/0.0.1.dev0/sparkplusplus-cli-0.0.1.dev0.tar.gz

---
+++ packages/sparkplusplus-cli/sparkplusplus-cli-0.0.1.dev0.tar.gz/sparkplusplus-cli-0.0.1.dev0/spp/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: spp.version
+.. moduleauthor:: Ashish Patel <ashishpatel0720@gmail.com>
+"""
+
+__version__ = "0.0.1-dev0"  #: the working version
+__release__ = "0.0.1-dev0"  #: the release version
