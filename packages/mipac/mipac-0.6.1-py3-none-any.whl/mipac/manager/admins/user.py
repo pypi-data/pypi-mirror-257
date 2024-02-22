94bf3d1daac1cbee69cf54fec8f19fda90ec76ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:08:07 +0000

    Add package mipac/0.6.1/mipac-0.6.1-py3-none-any.whl

---
+++ packages/mipac/mipac-0.6.1-py3-none-any.whl/mipac/manager/admins/user.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from mipac.actions.admins.user import AdminUserActions
+from mipac.http import HTTPClient
+
+if TYPE_CHECKING:
+    from mipac.client import ClientManager
+
+
+class AdminUserManager:
+    def __init__(self, user_id: str | None = None, *, session: HTTPClient, client: ClientManager):
+        self.__user_id = user_id
+        self.__session: HTTPClient = session
+        self.__client: ClientManager = client
+
+    @property
+    def action(self) -> AdminUserActions:
+        return AdminUserActions(
+            user_id=self.__user_id, session=self.__session, client=self.__client
+        )
