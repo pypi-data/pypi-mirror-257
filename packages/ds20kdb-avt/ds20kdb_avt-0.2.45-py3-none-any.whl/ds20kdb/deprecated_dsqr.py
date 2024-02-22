e7e5bd85cedf434a55f5f79f2297282fdadc94bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:39 +0000

    Add package ds20kdb-avt/0.2.45/ds20kdb_avt-0.2.45-py3-none-any.whl

---
+++ packages/ds20kdb-avt/ds20kdb_avt-0.2.45-py3-none-any.whl/ds20kdb/deprecated_dsqr.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print(
+	"--------------------------------------------------------------------------------"
+	"The dsqr command is deprecated and will be removed from a future\n"
+	"version of this software. Please use ds20k_read_qrcode instead.\n"
+	"\n"
+    "Some command names may be used sporadically and are easily forgotten.\n"
+    "The ds20k_ prefix is now employed to make it easier to discover\n"
+    "DarkSide-20k database-related commands through tab-completion\n"
+    "on the command line.\n"
+	"--------------------------------------------------------------------------------"
+)
---
+++ packages/ds20kdb-avt/ds20kdb_avt-0.2.45-py3-none-any.whl/ds20kdb/deprecated_submit_vtile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print(
+	"--------------------------------------------------------------------------------"
+	"The submit_vtile command is deprecated and will be removed from a future\n"
+	"version of this software. Please use ds20k_submit_vtile instead.\n"
+	"\n"
+    "Some command names may be used sporadically and are easily forgotten.\n"
+    "The ds20k_ prefix is now employed to make it easier to discover\n"
+    "DarkSide-20k database-related commands through tab-completion\n"
+    "on the command line.\n"
+	"--------------------------------------------------------------------------------"
+)
