0bb93c60e2d4273adc5e5120e67da1177103d02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:22 +0000

    Add package pysica/0.4.0/pysica-0.4.0-py3-none-any.whl

---
+++ packages/pysica/pysica-0.4.0-py3-none-any.whl/pysica/functions/__init__.py
@@ -0,0 +1,33 @@
+# COPYRIGHT (c) 2020-2024 Pietro Mandracci
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+""" Utility functions
+
+    This subpackage contains some modules which provide several utility functions.
+
+    Modules
+    -------
+
+    *physics*
+        functions used to calculate or convert some physical quantities
+    *optical*
+        functions for analysis of the optical properties of thin films (absorption, reflection)
+    *mathematics*
+        functions used to calculate averages, derivatives and other calculations
+    *pdf*
+        collection of probability distribution functions used in the package
+    *statistics*
+        some statistical tools   
+"""
