3f5b31c56a880f8a21f0b199a10a809a2765cd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:07:11 +0000

    Add package oarepo-runtime/1.5.11/oarepo-runtime-1.5.11.tar.gz

---
+++ packages/oarepo-runtime/oarepo-runtime-1.5.11.tar.gz/oarepo-runtime-1.5.11/oarepo_runtime/records/systemfields/__init__.py
@@ -0,0 +1,10 @@
+from .icu import ICUField, ICUSortField, ICUSuggestField
+from .mapping import MappingSystemFieldMixin, SystemFieldDumperExt
+
+__all__ = (
+    "ICUField",
+    "ICUSuggestField",
+    "ICUSortField",
+    "MappingSystemFieldMixin",
+    "SystemFieldDumperExt",
+)
