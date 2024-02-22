90147f7702dde29308a199734637fd41fad86750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:17 +0000

    Add package Leytonium/12/Leytonium-12-py3-none-any.whl

---
+++ packages/Leytonium/Leytonium-12-py3-none-any.whl/leytonium/agi.py
@@ -0,0 +1,27 @@
+# Copyright 2020 Andrzej Cichocki
+
+# This file is part of Leytonium.
+#
+# Leytonium is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Leytonium is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Leytonium.  If not, see <http://www.gnu.org/licenses/>.
+
+'Search for identifier in project.'
+from .common import findproject
+from lagoon import ag
+import sys
+
+def main():
+    ag._ws[exec](*sys.argv[1:], findproject())
+
+if '__main__' == __name__:
+    main()
---
+++ packages/Leytonium/Leytonium-12-py3-none-any.whl/leytonium/t.bash
@@ -0,0 +1,1 @@
+diff -U 100 <(tree -C) <(tree -C -a -L 1) | tail -n +4 | head -n -1 | cut -c 2-
