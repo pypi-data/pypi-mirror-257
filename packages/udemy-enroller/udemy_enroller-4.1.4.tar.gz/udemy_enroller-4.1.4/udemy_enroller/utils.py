1f90e0ede2b494d0018fd599b65338f17773974e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:04:43 +0000

    Add package udemy-enroller/4.1.4/udemy_enroller-4.1.4.tar.gz

---
+++ packages/udemy-enroller/udemy_enroller-4.1.4.tar.gz/udemy_enroller-4.1.4/udemy_enroller/utils.py
@@ -0,0 +1,16 @@
+"""Utility functions."""
+import os
+
+
+def get_app_dir() -> str:
+    """
+    Get the app directory where all data related to the script is stored.
+
+    :return:
+    """
+    app_dir = os.path.join(os.path.expanduser("~"), ".udemy_enroller")
+
+    if not os.path.isdir(app_dir):
+        # If the app data dir does not exist create it
+        os.mkdir(app_dir)
+    return app_dir
