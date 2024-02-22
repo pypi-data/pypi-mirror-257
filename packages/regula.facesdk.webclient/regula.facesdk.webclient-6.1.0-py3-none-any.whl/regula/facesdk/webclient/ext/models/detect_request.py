5ea183c852b82a6b98a1595db5770531d9ae4a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:05:04 +0000

    Add package regula.facesdk.webclient/6.1.0/regula.facesdk.webclient-6.1.0-py3-none-any.whl

---
+++ packages/regula.facesdk.webclient/regula.facesdk.webclient-6.1.0-py3-none-any.whl/regula/facesdk/webclient/ext/models/detect_request.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from regula.facesdk.webclient.ext.common import Base64String, bytes_to_base64
+from regula.facesdk.webclient.gen import ApiValueError
+from regula.facesdk.webclient.gen.models import DetectRequest as GetDetectRequest
+
+
+class DetectRequest(GetDetectRequest):
+    def __init__(
+            self, image: Union[Base64String, bytes],
+            only_central_face=False, thumbnails=False,
+            local_vars_configuration=None, tag=None
+    ):
+        if not image:
+            raise ApiValueError(f"image: expected <not empty> - obtained <{image}>", "image")
+
+        if isinstance(image, bytes):
+            image = bytes_to_base64(image)
+
+        super().__init__(
+            image=image, thumbnails=thumbnails,
+            local_vars_configuration=local_vars_configuration, tag=tag
+        )
