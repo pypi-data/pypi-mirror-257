17164a37339de75512db5b669f1cff205c3e02ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:04 +0000

    Add package sparkplusplus/0.0.1.dev0/sparkplusplus-0.0.1.dev0.tar.gz

---
+++ packages/sparkplusplus/sparkplusplus-0.0.1.dev0.tar.gz/sparkplusplus-0.0.1.dev0/spp/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Awesome Cli for Developers
+
+.. currentmodule:: spp
+.. moduleauthor:: Ashish Patel <ashishpatel0720@gmail.com>
+"""
+
+from .version import __version__, __release__  # noqa
