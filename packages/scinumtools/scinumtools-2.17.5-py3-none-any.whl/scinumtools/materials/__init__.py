d3e84d7575bfd43df9d7409e6e6723b640e635e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:04:53 +0000

    Add package scinumtools/2.17.5/scinumtools-2.17.5-py3-none-any.whl

---
+++ packages/scinumtools/scinumtools-2.17.5-py3-none-any.whl/scinumtools/materials/__init__.py
@@ -0,0 +1,7 @@
+MAGNITUDE_PRECISION = 1e-7
+
+from .element import Element
+from .molecule import Molecule
+from .mixture import Mixture
+from .molecule_solver import MoleculeSolver
+from .mixture_solver import MixtureSolver
