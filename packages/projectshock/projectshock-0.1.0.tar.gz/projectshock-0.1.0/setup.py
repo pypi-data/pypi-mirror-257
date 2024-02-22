eaf134634bd72a24ea94351d232e0f17d13c7ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:05:00 +0000

    Add package projectshock/0.1.0/projectshock-0.1.0.tar.gz

---
+++ packages/projectshock/projectshock-0.1.0.tar.gz/projectshock-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='projectshock',
+version='0.1.0',
+author='Adam Jensen',
+author_email='jensendev@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
