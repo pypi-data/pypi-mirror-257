aed8b11dab59ba5d8cc762b386270d499097373e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:06:02 +0000

    Add package jaxspec/0.0.2/jaxspec-0.0.2-py3-none-any.whl

---
+++ packages/jaxspec/jaxspec-0.0.2-py3-none-any.whl/jaxspec/data/__init__.py
@@ -0,0 +1,7 @@
+# precommit is suppressing these imports
+from .obsconf import ObsConfiguration  # noqa: F401
+from .instrument import Instrument  # noqa: F401
+from .observation import Observation  # noqa: F401
+import astropy.units as u
+
+u.add_enabled_aliases({"counts": u.count})
