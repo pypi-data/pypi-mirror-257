d5d82673bcf4f26f7225efa28bf334c35a98d021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 05:04:55 +0000

    Add package l0n0lrdp/1.0.0/l0n0lrdp-1.0.0.tar.gz

---
+++ packages/l0n0lrdp/l0n0lrdp-1.0.0.tar.gz/l0n0lrdp-1.0.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="l0n0lrdp",
+    version="1.0.0",
+    author="l0n0l",
+    author_email="1038352856@qq.com",
+    description="屏幕共享",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/l00n00l/l0n0lrdp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={
+        "": ["*.html"]
+    },
+    install_requires=[
+        "aiohttp"
+    ],
+    entry_points={
+        "console_scripts": [
+            "l0n0lrdp = l0n0lrdp.server:start_server",
+        ]
+    }
+)
