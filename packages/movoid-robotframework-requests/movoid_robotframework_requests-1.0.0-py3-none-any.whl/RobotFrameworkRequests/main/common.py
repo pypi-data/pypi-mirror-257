90b8d0dd6a38ca30e166d33610f6748ba49c03bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:16 +0000

    Add package movoid-robotframework-requests/1.0.0/movoid_robotframework_requests-1.0.0-py3-none-any.whl

---
+++ packages/movoid-robotframework-requests/movoid_robotframework_requests-1.0.0-py3-none-any.whl/RobotFrameworkRequests/main/common.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : common
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/2/21 19:22
+# Description   : 
+"""
+
+from RobotFrameworkBasic import RobotBasic
+
+
+class BasicCommon(RobotBasic):
+    pass
