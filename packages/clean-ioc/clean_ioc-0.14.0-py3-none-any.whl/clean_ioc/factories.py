76d189d679f59e6a54ac3ec4722b7d569cd6428d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:07:44 +0000

    Add package clean-ioc/0.14.0/clean_ioc-0.14.0-py3-none-any.whl

---
+++ packages/clean-ioc/clean_ioc-0.14.0-py3-none-any.whl/clean_ioc/factories.py
@@ -0,0 +1,27 @@
+from typing import Any, Callable, TypeVar
+
+from .core import RegistrationFilter, Resolver, _default_registration_filter
+
+
+def use_registered(
+    cls: type, filter: RegistrationFilter = _default_registration_filter
+):
+    def factory(resolver: Resolver):
+        return resolver.resolve(cls, filter=filter)
+
+    return factory
+
+
+T = TypeVar("T")
+
+
+def create_type_mapping(
+    service_type: type[T],
+    key_getter: Callable[[T], Any],
+    filter: RegistrationFilter = _default_registration_filter,
+):
+    def factory(resolver: Resolver):
+        items = resolver.resolve(list[service_type], filter=filter)
+        return {key_getter(item): item for item in items}
+
+    return factory
