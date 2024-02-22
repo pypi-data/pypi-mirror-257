9ea28af64d831174bbe9e200957b5a29ce843a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 03:08:13 +0000

    Add package pytest-mh/1.0.11/pytest_mh-1.0.11.tar.gz

---
+++ packages/pytest-mh/pytest_mh-1.0.11.tar.gz/pytest_mh-1.0.11/pytest_mh/__init__.py
@@ -0,0 +1,41 @@
+"""
+.. avoid already-imported warning: PYTEST_DONT_REWRITE (hidden from sphinx)
+"""
+
+from __future__ import annotations
+
+from ._private.data import MultihostItemData
+from ._private.fixtures import MultihostFixture, mh
+from ._private.marks import KnownTopologyBase, KnownTopologyGroupBase, TopologyMark
+from ._private.multihost import (
+    MultihostConfig,
+    MultihostDomain,
+    MultihostHost,
+    MultihostHostOSFamily,
+    MultihostRole,
+    MultihostUtility,
+)
+from ._private.plugin import MultihostPlugin, pytest_addoption, pytest_configure
+from ._private.topology import Topology, TopologyDomain
+from ._private.topology_controller import TopologyController
+
+__all__ = [
+    "mh",
+    "MultihostConfig",
+    "MultihostDomain",
+    "MultihostFixture",
+    "MultihostHost",
+    "MultihostHostOSFamily",
+    "MultihostItemData",
+    "MultihostPlugin",
+    "MultihostRole",
+    "MultihostUtility",
+    "pytest_addoption",
+    "pytest_configure",
+    "Topology",
+    "TopologyController",
+    "TopologyDomain",
+    "TopologyMark",
+    "KnownTopologyBase",
+    "KnownTopologyGroupBase",
+]
