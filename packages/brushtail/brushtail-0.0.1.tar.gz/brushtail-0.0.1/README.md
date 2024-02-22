256498c9a0edad97d0b698b3ceb862ae1f2f441c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-22 04:05:06 +0000

    Add package brushtail/0.0.1/brushtail-0.0.1.tar.gz

---
+++ packages/brushtail/brushtail-0.0.1.tar.gz/brushtail-0.0.1/README.md
@@ -0,0 +1,18 @@
+# Brushtail 
+
+A simple way to structure web scraping; using chained functions to express dense workflow simply.
+
+In development, with a short roadmap to version 1. 
+
+
+## Background
+I find some of the structure of webscraping a little tedious; it can require many repetetive steps, and scripts often need to change as websites change. 
+
+Thus, a solution which can chain steps together, with a goal of creating more easily disposible (or fixable) scraping scripts. 
+
+The drawback of chaining things in this fashion is that by connecting many failure points, the overall script may fail easily; but that isn't an issue for my use cases.
+
+
+## Roadmap 
+- Clearer comments and typing
+- Code examples 
---
+++ packages/brushtail/brushtail-0.0.1.tar.gz/brushtail-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brushtail',
+    version='0.0.1',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        'requests',
+        'selenium',
+        'BeautifulSoup'
+    ],
+    author='Ethan Vellacott',
+    description='A small python framework for making simple, maintainable web scraping scripts.',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/EthanVellacott/brushtail',
+    python_requires='>=3.11',
+)
